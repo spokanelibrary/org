5e8cce41d3735da9323f0baea950537f57195002
Sean Girard <seangirard@yahoo.com>
2013-09-06 12:48:00 -0700

    common navbar

--- BUILDME.md
+++ BUILDME.md
@@ -4,18 +4,15 @@ building org
 * start with current roots theme
 * npm install (grunt watch during dev)
 * remove node_modules on update?
-
 * update style.css, add a screenshot
-
 
 mod org
 ===
 
 lib/org - includable mods
----
 
-* init.php
--  add nav menus
+* init.php (nav menus)
 
 templates/org
+
 * header-top-navbar
--- templates/org/header-top-navbar.php
+++ templates/org/header-top-navbar.php
@@ -1,3 +1,46 @@
+<header class="banner navbar navbar-static-top" role="navigation">
+  <ul class="nav navbar-nav nav-justified">
+    <li>
+      <a href="<?php echo home_url(); ?>/">
+        <span class="visible-xs"><i class="glyphicon glyphicon-home"></i></span>
+        <span class="hidden-xs"><i class="text-success glyphicon glyphicon-home"></i> Home</span>
+      </a>
+    </li>
+    <li>
+      <a href="/account/">
+        <span class="visible-xs"><i class="glyphicon glyphicon-user"></i></span>
+        <span class="hidden-xs"><i class="text-success glyphicon glyphicon-user"></i> My Account</span>
+      </a>
+    </li>
+    <li>
+      <a href="/search/">
+        <span class="visible-xs"><i class="glyphicon glyphicon-search"></i></span>
+        <span class="hidden-xs"><i class="text-success glyphicon glyphicon-search"></i> Search</span>
+      </a>
+    </li>
+    <li>
+      <a href="/browse/">
+        <span class="visible-xs"><i class="glyphicon glyphicon-book"></i></span>
+        <span class="hidden-xs"><i class="text-success glyphicon glyphicon-book"></i> Browse</span>
+      </a>
+    </li>
+    <li>
+      <a href="/download/">
+        <span class="visible-xs"><i class="glyphicon glyphicon-download-alt"></i></span>
+        <span class="hidden-xs"><i class="text-success glyphicon glyphicon-download-alt"></i> Download</span>
+      </a>
+    </li>
+    <li>
+      <a href="/connect/">
+        <span class="visible-xs"><i class="glyphicon glyphicon-envelope"></i></span>
+        <span class="hidden-xs"><i class="text-success glyphicon glyphicon-envelope"></i> Contact Us</span>
+      </a>
+    </li>
+  </ul>
+</header><!-- /.navbar -->
+
+
+
 <header class="banner navbar navbar-default navbar-static-top" role="banner">
   <div class="container">
     <div class="navbar-header">
