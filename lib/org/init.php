2233ed9e5246e1843ce0046df8400751704c44c8
Sean Girard <seangirard@yahoo.com>
2013-09-06 12:36:53 -0700

    lib/org/init

--- lib/org/init.php
+++ lib/org/init.php
@@ -3,6 +3,7 @@
   // Register wp_nav_menu() menus (http://codex.wordpress.org/Function_Reference/register_nav_menus)
   register_nav_menus(array(
     'common_navigation' => __('Common Navigation', 'org'),
+    'connect_navigation' => __('Connect Navigation', 'org'),
   ));
 
 ?>
