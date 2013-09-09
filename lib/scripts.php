e5ceaeb963606750be06c73bc0e367d8ef2e56e2
Sean Girard <seangirard@yahoo.com>
2013-09-09 13:45:48 -0700

    custom navbar

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8d8707b095e49a3fab60f5be9987d313');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '04290be25784ef658405fa3bf370ad76');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
