74c947ada7b4e2bd628bef49edd37c3881df2ead
Sean Girard <seangirard@yahoo.com>
2013-09-10 14:02:50 -0700

    add some basic positioning

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'fc56da6fb222ca65aae8a8274859d219');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9ba76eaa2530d905dc1c449573b01275');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
