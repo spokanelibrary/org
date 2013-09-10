2aad7d6b86bff771c1a590fccb57552286a4cab2
Sean Girard <seangirard@yahoo.com>
2013-09-10 11:58:19 -0700

    move handlebars/helper to plugins

--- lib/scripts.php
+++ lib/scripts.php
@@ -27,7 +27,7 @@ function roots_scripts() {
   }
 
   wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.6.2.min.js', false, null, false);
-  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', false, '74b3798f27bd377c77213e18b7a828fb', true);
+  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', false, 'fb0b6398af35a48eed0a7c98a5c20452', true);
   wp_enqueue_script('modernizr');
   wp_enqueue_script('jquery');
   wp_enqueue_script('roots_scripts');
