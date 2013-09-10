05288f214afc043df818bade61b104a6fc1fdbd7
Sean Girard <seangirard@yahoo.com>
2013-09-10 10:22:09 -0700

    footer

--- templates/footer.php
+++ templates/footer.php
@@ -2,7 +2,7 @@
   <div class="row">
     <div class="col-lg-12">
       <?php dynamic_sidebar('sidebar-footer'); ?>
-      <--
+      <!--
       <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
     	-->
     </div>
--- templates/org/footer.php
+++ templates/org/footer.php
@@ -1,1 +1,177 @@
-This is a footer.
+<div id="spl-more" style="/*background:rgb(100,150,75);*/">
+
+  <!-- <a name="spl-more">&nbsp;</a> -->
+
+  <footer class="content-info container" role="contentinfo">
+
+    <div class="panel panel-primary">
+      <div class="panel-body">
+
+        <!--
+        <h3 class="normal" style="margin-top:0;"><a name="" class="text-success">Read. Learn. Discover.</a></h3>
+        -->
+
+        <?php //echo do_shortcode('[spl_widget search-catalog]'); ?>
+
+        <div class="row">
+
+          <div class="col-md-3 col-sm-6">
+            <h4>
+              <a href="#">Get Connected</a> <small>&rarr;</small>
+            </h4>
+            <ul class="nav nav-pills nav-stacked">
+              <!--
+              <li class="active">
+                <a href="#">Get Connected</a>
+              </li>
+              -->
+              <li>
+                <a href="#">Send Us an Email</a>
+              </li>
+              <li>
+                <a href="#">Suggest a New Title</a>
+              </li>
+              <li>
+                <a href="#">Ask Us a Question</a>
+              </li>
+              <li>
+                <a href="#">Get a Library Card</a>
+              </li>
+            </ul>
+          </div><!-- /.col -->
+
+          <div class="col-md-3 col-sm-6">
+            <h4 class="text-muted">
+              <a href="#">Stay Current</a>
+              <small>&rarr;</small>
+            </h4>
+            <ul class="nav nav-pills nav-stacked">
+              <!--
+              <li class="active">
+                <a href="#">Stay Current</a>
+              </li>
+              -->
+              <li>
+                <a href="#">Upcoming Events</a>
+              </li>
+              <li>
+                <a href="#">Read Our Blog</a>
+              </li>
+              <li>
+                <a href="#">Your Local Branch</a>
+              </li>
+              <li>
+                <a href="#">Library Tips</a>
+              </li>
+            </ul>
+          </div><!-- /.col -->
+
+          <div class="col-md-3 col-sm-6">
+            <h4 class="text-muted">
+              <a href="#">In the Library</a>
+              <small>&rarr;</small>
+            </h4>
+            <ul class="nav nav-pills nav-stacked">
+              <!--
+              <li class="active">
+                <a href="#">In the Library</a>
+              </li>
+              -->
+              <li>
+                <a href="#">Free WiFi</a>
+              </li>
+              <li>
+                <a href="#">Reserve a Computer</a>
+              </li>
+              <li>
+                <a href="#">Printing <small>(10&#162; per page)</small></a>
+              </li>
+              <li>
+                <a href="#">Public Meeting Rooms</a>
+              </li>
+            </ul>
+          </div><!-- /.col -->
+
+          <div class="col-md-3 col-sm-6">
+            <h4 class="text-muted">
+              <a href="#">Digital Branch</a>
+              <small>&rarr;</small>
+            </h4>
+            <ul class="nav nav-pills nav-stacked">
+              <!--
+              <li class="active">
+                <a href="#">Download Something</a>
+              </li>
+              -->
+              <li>
+                <a href="#">Music <small>(.mp3)</small></a>
+              </li>
+              <li>
+                <a href="#">Magazines</a>
+              </li>
+              <li>
+                <a href="#">eBooks</a>
+              </li>
+              <li>
+                <a href="#">Audio Books</a>
+              </li>
+            </ul>
+          </div><!-- /.col -->
+
+        </div><!-- /.row -->
+      </div><!-- /.panel-body -->
+
+      <div class="panel-footer">
+        <ul class="nav nav-pills nav-justified">
+          <li>
+            <a href="#"><small><i class="text-muted glyphicon glyphicon-paperclip"></i> Administration</small> </a>
+          </li>
+          <li>
+            <a href="#"><small><i class="text-muted glyphicon glyphicon-info-sign"></i> Library Policies</small></a>
+          </li>
+          <li>
+            <a href="#"><small><i class="text-muted glyphicon glyphicon-briefcase"></i> Employment</small></a>
+          </li>
+          <li>
+            <a href="#"><small><i class="text-muted glyphicon glyphicon-road"></i> Strategic Plan</small></a>
+          </li>
+          <li>
+            <a href="#"><small><i class="text-muted glyphicon glyphicon-gift"></i> Support Your Library</small></a>
+          </li>
+        </ul>
+      </div><!-- /.panel-footer -->
+
+    </div><!-- /.panel -->
+
+    <div class="row">
+      <div class="col-md-12">
+        <span class="pull-right">
+          <img alt="City of Spokane logo" src="http://www.spokanelibrary.org/new/img/city-spokane-logo.png">
+        </span>
+
+        <p>
+          <strong class="text-muted" style="">Spokane Public Library</strong>
+        </p>
+        <address class="text-muted" style="">
+          906 West Main Avenue
+          <br>
+          Spokane, WA 99201
+          <br>
+          509.444.5300
+        </address>
+      </div><!-- /.col -->
+    </div><!-- /.row -->
+
+    <div class="row">
+      <div class="col-md-12">
+        <!--
+        <p class="text-muted">
+          &copy; <?php echo date('Y'); ?> <?php //bloginfo('name'); ?>
+        </p>
+        -->
+      </div><!-- /.col -->
+    </div><!-- /.row -->
+
+  </footer>
+
+</div>
