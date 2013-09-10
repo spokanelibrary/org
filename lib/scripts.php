fde7f632d85802ea519654606aa300dcf651ed3a
Sean Girard <seangirard@yahoo.com>
2013-09-10 13:45:22 -0700

    kill carousel gradients

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ceaaacb38a2a4f19b68aa24e2f14758f');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '811293c4c39228e969e53be607c9515e');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
