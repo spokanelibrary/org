a732fd90419d570f313aefe341346dc47906a9a5
Sean Girard <seangirard@yahoo.com>
2013-09-11 13:29:15 -0700

    darken(btn)

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd38efd1ab6b6c0e50645783e167bb27d');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '660322533a70dc71e4c68ce2b0a780e0');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
