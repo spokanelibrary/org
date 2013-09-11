64f2fc43c0729d1bd9e0d9c48ffd3cbbf974ae18
Sean Girard <seangirard@yahoo.com>
2013-09-11 13:36:33 -0700

    try out some btn gradients

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5589c80f65af12d65696cf9efee3849d');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '99ab48f611d6ce5ac6a09921bf199d76');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
