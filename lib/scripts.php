226ac3a0b4bbc64b85164b57fc2b7d7d2f7cc579
Sean Girard <seangirard@yahoo.com>
2013-09-11 11:10:44 -0700

    responsive navbar

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '7ee8997b867799b230bcb8e5daf3d902');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c67a544c115a25a3d4a34dd3ea4dc5ff');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
