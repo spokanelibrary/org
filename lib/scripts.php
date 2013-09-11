41b09d35de7343fd0cda9884fe0466346b1dbc66
Sean Girard <seangirard@yahoo.com>
2013-09-11 13:54:01 -0700

    gradien(btn)

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '4135c8b38f7b6dc01b93f81a81947cec');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '82870790a61f590cbbba078400397425');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
