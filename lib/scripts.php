f3e106c4a5273c58a50cbe9178a3adb0181b81b7
Sean Girard <seangirard@yahoo.com>
2013-09-11 10:03:59 -0700

    shoelace.less

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '3829855f6e7d527afe900cac08499356');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '90055a78ced5e066a4f3853d654dc8ad');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
