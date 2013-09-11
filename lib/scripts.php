000ed7220f6706a428b2a118aa4d82ee11f4362c
Sean Girard <seangirard@yahoo.com>
2013-09-11 11:00:32 -0700

    still need to zero out padding on navbar-collapse?

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'bee04b6a61097829bb3a7e1caa58e678');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9e2ea5140d6745de1190ab8bf6d988bf');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
