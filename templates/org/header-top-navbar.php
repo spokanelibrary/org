318278e3208512252f9dc143ce26108c7c61dc8d
Sean Girard <seangirard@yahoo.com>
2013-09-11 11:24:08 -0700

    responsive navbar

--- templates/org/header-top-navbar.php
+++ templates/org/header-top-navbar.php
@@ -49,7 +49,7 @@
   </div>
 </header><!-- /.navbar -->
 
-<header id="spl-primary-navbar" class="navbar-primary banner navbar navbar-inverse navbar-static-top" role="banner">
+<header id="spl-primary-navbar" class="banner navbar navbar-inverse navbar-static-top" role="banner">
   <div class="container">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
