13d296c03a6aea3d1548a00696677ec5ff4f9b83
Sean Girard <seangirard@yahoo.com>
2013-09-11 14:42:44 -0700

    navbar-common opacity

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'dfb8169a8548c00ec51021a6420a084f');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '678163903fc437e882c9d8f2f52a2a93');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
