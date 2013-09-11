fe244f60707d22b5665760c1f3e93ec84ed1a65c
Sean Girard <seangirard@yahoo.com>
2013-09-11 13:02:18 -0700

    buttons

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '37e6f24a2c3c72045d2a899681a0102f');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '3a5b9c38beb8d6e9e7a65ff725479b35');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
