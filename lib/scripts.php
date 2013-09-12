ad563afb0868d9dff8091ace706875f75f035682
Sean Girard <seangirard@yahoo.com>
2013-09-11 21:58:56 -0700

    common nav

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '27f6b3377a9bc8c44edb918ff77e66b0');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '2086945a6412dbf79dcb270e422a77d6');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
