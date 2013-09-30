b7dff11510c00c851bcc77da2fccfecc10bcb007
Sean Girard <seangirard@yahoo.com>
2013-09-30 11:35:02 -0700

    datepicker.less

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a450fa82186a8777c477ac710108b0c3');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '3278051af9bdb7895d22b5cde8e7bd00');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
