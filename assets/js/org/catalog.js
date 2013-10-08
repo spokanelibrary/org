174e7561c214969407bfb67e3e00f20f6c0a23a3
Sean Girard <seangirard@yahoo.com>
2013-10-08 14:38:17 -0700

    catalog.js

--- assets/js/org/catalog.js
+++ assets/js/org/catalog.js
@@ -96,6 +96,7 @@ var org = {
 				console.log( _self.user.sessionToken );
 				console.log(bib);
 				//$(this).button('complete');
+				$(this).button('reset');
 				$(this).button('error');
 				//.addClass('btn-danger').addClass('disabled');
 				$(this).addClass('disabled');
