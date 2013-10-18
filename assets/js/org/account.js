b9617620503e0187ce5d11f44acf3db005d43b96
Sean Girard <seangirard@yahoo.com>
2013-10-18 12:06:10 -0700

    my list

--- assets/js/org/account.js
+++ assets/js/org/account.js
@@ -362,9 +362,9 @@ var org = {
 		  .done(function(obj) {  
 		  	// pass results through
 				//$hidden.val(JSON.stringify(obj));
-				//console.log(obj);
+				console.log(obj);
 				//$submit.button('reset');
-				$form.data('process', 'http').submit();
+				//$form.data('process', 'http').submit();
 		  })
 		  .fail(function() {
 		  })
