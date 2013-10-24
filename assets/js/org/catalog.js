3f7bb3a6794114d7266799d19293cb3cf3f335ff
Sean Girard <seangirard@yahoo.com>
2013-10-24 10:57:47 -0700

    popover

--- assets/js/org/catalog.js
+++ assets/js/org/catalog.js
@@ -50,7 +50,7 @@ var org = {
 										html: true
 									, title: 'About Saved Lists'
 									, placement: 'top' 
-									, content: 'Saved Lists are a great way to remember titles you have finished, or would like to checkout in the future. <br /> Some more and <a href="./account">and a link</a>.';
+									, content: 'Saved Lists are a great way to remember titles you have finished, or would like to checkout in the future. <br /> Some more and <a href="./account">and a link</a>.'
 									}
 		$('.spl-list-about').popover(popover);
 		$('body').on('click', '.spl-list-about', function(e) {
