e0cc25a52578f6d49c8ebefb777545e3d6395ce6
Sean Girard <seangirard@yahoo.com>
2013-10-24 11:18:50 -0700

    popover

--- assets/js/org/catalog.js
+++ assets/js/org/catalog.js
@@ -61,7 +61,7 @@ var org = {
       e.preventDefault();
     });
     $(':not(#anything)').on('click', function (e) {
-		    $('.popover-link').each(function () {
+		    $('.spl-list-about').each(function () {
 		        //the 'is' for buttons that trigger popups
 		        //the 'has' for icons and other elements within a button that triggers a popup
 		        if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
