3e6645ecbd5f00f70254b14aa7914dfc71399e37
Sean Girard <seangirard@yahoo.com>
2013-10-25 09:57:24 -0700

    account profile

--- lib/scripts.php
+++ lib/scripts.php
@@ -27,7 +27,7 @@ function roots_scripts() {
   }
 
   wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.6.2.min.js', false, null, false);
-  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', false, 'd7c0c0fb682d6cbd9570695dd60831db', true);
+  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', false, 'ad36099fc14f564ce91b3f6066716160', true);
   wp_enqueue_script('modernizr');
   wp_enqueue_script('jquery');
   wp_enqueue_script('roots_scripts');
