9017b228441235d49678353d0619cec4700dbb8d
Sean Girard <seangirard@yahoo.com>
2013-10-25 13:05:08 -0700

    profile

--- templates/org/header-top-navbar.php
+++ templates/org/header-top-navbar.php
@@ -52,7 +52,7 @@
     <script id="spl-account-profile-tmpl" type="text/x-handlebars-template">
       {{#if user.sessionToken}}
       <div class="pull-right" id="spl-account-profile">
-        <div class="well well-sm" style="margin: 6px 0 0 0; background-color: rgb(125,175,100); opacity:.9;">
+        <div class="well well-sm" style="margin: 6px 0 0 0; background-color: #f0ad4e; opacity:.9;">
             <i class="glyphicon glyphicon-user text-success"></i>
             <a href="/account/"><strong>{{user.firstName}}</strong> <small>My Account</small></a>
             <span class="text-muted">&nbsp;&nbsp;</span>
