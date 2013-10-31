dcb0d88d232b20ff64f3f4b45adb1fdd57f5074d
Sean Girard <seangirard@yahoo.com>
2013-10-31 14:06:35 -0700

    move fb sdk

--- templates/org/header-top-navbar.php
+++ templates/org/header-top-navbar.php
@@ -1,3 +1,12 @@
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=289675684463099";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>  
 <header class="navbar-common banner navbar navbar-fixed-top" role="navigation">
   <ul class="nav nav-justified">
     <li>
