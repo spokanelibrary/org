2cf5820024f12acf5a8b0e79bb82d22d0c5345ed
Sean Girard <seangirard@yahoo.com>
2013-11-13 09:06:35 -0800

    responsive images

--- lib/org/init.php
+++ lib/org/init.php
@@ -16,7 +16,30 @@
   */
 
 add_post_type_support( 'page', 'excerpt' );
+
 update_option('image_default_link_type', 'none' );
+
+//  https://gist.github.com/mkdizajn/7352469
+//----------------------------------------------------------/
+//  responsive images [ 1) add img-responsive class 2) remove dimensions ]
+//----------------------------------------------------------/
+ 
+function bootstrap_responsive_images( $html ){
+  $classes = 'img-responsive'; // separated by spaces, e.g. 'img image-link'
+ 
+  // check if there are already classes assigned to the anchor
+  if ( preg_match('/<img.*? class="/', $html) ) {
+    $html = preg_replace('/(<img.*? class=".*?)(".*?\/>)/', '$1 ' . $classes . ' $2', $html);
+  } else {
+    $html = preg_replace('/(<img.*?)(\/>)/', '$1 class="' . $classes . '" $2', $html);
+  }
+  // remove dimensions from images,, does not need it!
+  $html = preg_replace( '/(width|height)=\"\d*\"\s/', "", $html );
+  return $html;
+}
+add_filter( 'the_content','bootstrap_responsive_images',10 );
+add_filter( 'post_thumbnail_html', 'bootstrap_responsive_images', 10 );
+
 
 function spl_tutorial($params) {  
     global $post;  
