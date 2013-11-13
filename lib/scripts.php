629287c624f7d44790c7ea7b465aae12aec00950
Sean Girard <seangirard@yahoo.com>
2013-11-13 09:41:41 -0800

    img margins

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0667f7d95fb833e9f156c31e0cc49f1e');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b65f539c9c971d62996f0c41ecb9909d');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
