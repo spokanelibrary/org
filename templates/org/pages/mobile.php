82e1803ba4a648175610c62000ecf2a169ac9c84
Sean Girard <seangirard@yahoo.com>
2014-03-06 13:38:06 -0800

    templates

---
+++ templates/org/pages/mobile.php
@@ -0,0 +1,52 @@
+<div class="list-group visible-sm">
+  <a href="/open/" class="list-group-item">
+    <span class="text-primary">
+      <i class="glyphicon glyphicon-time"></i>
+      Is my Library open right now?
+    </span>
+  </a>
+</div>
+
+<div class="list-group visible-xs">
+  <a href="/open/" class="list-group-item">
+    <span class="text-primary">
+      <i class="glyphicon glyphicon-time"></i>
+      Is my Library open right now?
+    </span>
+  </a>
+  <a href="#" class="list-group-item">
+    <span class="text-primary">
+      <i class="glyphicon glyphicon-user"></i>
+      My Account
+    </span>
+  </a>
+  <a href="#" class="list-group-item">
+    <span class="text-primary">
+      <i class="glyphicon glyphicon-search"></i>
+      Search the Catalog
+  </a>
+  <a href="#" class="list-group-item">
+  	<span class="text-primary">
+      <i class="glyphicon glyphicon-book"></i>
+      Browse Materials
+    </span>
+  </a>
+  <a href="#" class="list-group-item">
+  	<span class="text-primary">
+      <i class="glyphicon glyphicon-download-alt"></i>
+    	Mobile Apps &amp; Downloads
+    </span>
+  </a>
+  <a href="#" class="list-group-item">
+  	<span class="text-primary">
+      <i class="glyphicon glyphicon-envelope"></i>
+    	Contact Us
+    </span>
+  </a>
+  <a href="#spl-more" class="list-group-item">
+  	<span class="text-primary">
+      <i class="glyphicon glyphicon-link"></i>
+    	More Links
+    </span>
+  </a>
+</div>
