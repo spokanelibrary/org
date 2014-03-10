d79522cd0463b73bc04035389c301c4fa8cd82c0
Sean Girard <seangirard@yahoo.com>
2014-03-10 12:57:16 -0700

    add full-width app template

---
+++ template-application.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Template Name: Application (Full Width)
+*/
+?>
+
+
+<div class="row">
+
+	<div class="col-sm-12">
+		<?php get_template_part('templates/page', 'header'); ?>
+		<?php get_template_part('templates/content', 'page'); ?>
+	</div>
+
+</div>
