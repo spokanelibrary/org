e4405e70351dcd91651df5a7245a719b62d61cd4
Sean Girard <seangirard@yahoo.com>
2014-03-12 11:25:03 -0700

    add no-headline template

--- lib/config.php
+++ lib/config.php
@@ -20,7 +20,7 @@ define('POST_EXCERPT_LENGTH', 40); // Length in words for excerpt_length filter 
 function roots_main_class() {
   if (roots_display_sidebar()) {
     // Classes on pages with the sidebar
-    $class = 'col-sm-8';
+    $class = 'col-sm-8 col-md-9';
   } else {
     // Classes on full width pages
     $class = 'col-sm-12';
@@ -33,7 +33,7 @@ function roots_main_class() {
  * .sidebar classes
  */
 function roots_sidebar_class() {
-  return 'col-sm-4';
+  return 'col-sm-4 col-md-3';
 }
 
 /**
---
+++ template-no-headline.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Template Name: No Headline
+*/
+?>
+
+
+<div class="row">
+
+	<div class="col-sm-12">
+	<!-- <div class="col-md-10 col-lg-9"> -->
+		<?php //get_template_part('templates/page', 'header'); ?>
+		<?php get_template_part('templates/content', 'page'); ?>
+	</div>
+
+</div>
