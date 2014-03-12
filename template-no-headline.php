044773e8ce7a635cd21062179a990c6c2f2149a4
Sean Girard <seangirard@yahoo.com>
2014-03-12 11:26:51 -0700

    no headline

--- template-no-headline.php
+++ template-no-headline.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: No Headline
+Template Name: Application (No Headline)
 */
 ?>
 
@@ -8,7 +8,6 @@ Template Name: No Headline
 <div class="row">
 
 	<div class="col-sm-12">
-	<!-- <div class="col-md-10 col-lg-9"> -->
 		<?php //get_template_part('templates/page', 'header'); ?>
 		<?php get_template_part('templates/content', 'page'); ?>
 	</div>
