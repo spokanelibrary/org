be90fa2c5b5c3c970e03878824f8e5cb1dff9a44
Sean Girard <seangirard@yahoo.com>
2014-03-13 13:34:43 -0700

    custom

--- templates/org/pages/download.php
+++ templates/org/pages/download.php
@@ -1,18 +1,35 @@
-<div class="media">
-  <a class="pull-left" href="#">
-    <img class="media-object" src="/assets/img/png/spl-app-icons-music.png" alt="Download Music">
-  </a>
-  <div class="media-body">
-    <h4 class="media-heading">
-    	<a href="#">Music</a> <small>(.mp3)</small>
-    </h4>
-    <p>
-    	this is some info
-    </p>
-  </div>
-</div>
-
-
+<div class="row">
+	<div class="col-sm-6">
+		<div class="media">
+		  <a class="pull-left" href="#">
+		    <img class="media-object" src="/assets/img/png/spl-app-icons-music.png" alt="Download Music">
+		  </a>
+		  <div class="media-body">
+		    <h4 class="media-heading">
+		    	<a href="#">Freegal: Music Downloads</a> <small>(.mp3)</small>
+		    </h4>
+		    <p>
+		    	this is some info
+		    </p>
+		  </div>
+		</div><!-- /.media -->
+	</div><!-- /.col -->
+	<div class="col-sm-6">
+			<div class="media">
+		  <a class="pull-left" href="#">
+		    <img class="media-object" src="/assets/img/png/spl-app-icons-magazine.png" alt="Download Magazines">
+		  </a>
+		  <div class="media-body">
+		    <h4 class="media-heading">
+		    	<a href="#">Zinio: Magazine Downloads</a>
+		    </h4>
+		    <p>
+		    	this is some info
+		    </p>
+		  </div>
+		</div><!-- /.media -->
+	</div><!-- /.col -->
+</div><!-- /.row -->
 
 
 <h3>
