b3db86a24d1f28b6f37779ad031d8e911d31a7af
Sean Girard <seangirard@yahoo.com>
2014-03-13 13:50:22 -0700

    dl

--- templates/org/pages/download.php
+++ templates/org/pages/download.php
@@ -10,6 +10,7 @@
 			<div class="panel-body">
 				<div class="media">
 				  <a class="pull-left" href="#">
+				  	<h1><span class="label label-success"><i class="glyphicon glyphicon-music"></i></span></h1>
 				    <img class="media-object" src="/assets/img/png/spl-app-icons-music.png" alt="Download Music">
 				  </a>
 				  <div class="media-body">
