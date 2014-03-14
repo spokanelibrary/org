1fb52b235cd80490f3389e15be1648b4978d8ab6
Sean Girard <seangirard@yahoo.com>
2014-03-14 11:06:43 -0700

    contact

--- templates/org/forms/field/contact-us.php
+++ templates/org/forms/field/contact-us.php
@@ -17,46 +17,50 @@
     </button>
     </p>
     -->
-
-    <form class="form-horizontal" id="spl-form-contact" method="post" role="form">
+    <div class="panel panel-default">
+      <div class="panel-body">
+        <form class="form-horizontal" id="spl-form-contact" method="post" role="form">
+          
+          <?php include 'common/crass-response.php'; ?>
       
-      <?php include 'common/crass-response.php'; ?>
-  
-      <input type="hidden" id="spl-form-id" name="spl-form[id]" value="contact" />
-      
-      <input type="hidden" 
-            id="spl-form-ip" 
-            name="spl-form[ip]" 
-            value="<?php echo $_SERVER['REMOTE_ADDR']; ?>"
-            />
+          <input type="hidden" id="spl-form-id" name="spl-form[id]" value="contact" />
+          
+          <input type="hidden" 
+                id="spl-form-ip" 
+                name="spl-form[ip]" 
+                value="<?php echo $_SERVER['REMOTE_ADDR']; ?>"
+                />
 
-      <fieldset>
-        <legend class="text-muted">
-          <i class="glyphicon glyphicon-envelope"></i>
-          Send an Email
-        </legend>
+          <fieldset>
+            <legend class="text-muted">
+              <i class="glyphicon glyphicon-envelope"></i>
+              Send an Email
+            </legend>
 
-        <div class="form-group">
-          <label for="inputEmail3" class="col-sm-3 control-label">Your Name</label>
-          <div class="col-sm-9">
-            <input type="text" class="form-control" id="spl-contact-name" placeholder="">
-          </div>
-        </div>
-        <div class="form-group">
-          <div class="col-sm-offset-3 col-sm-6">
-            <button type="submit" class="btn btn-block btn-success">Send Now &rarr;</button>
-          </div>
-        </div>
-      </fieldset>
-    </form>
-
+            <div class="form-group">
+              <label for="inputEmail3" class="col-sm-3 control-label">Your Name</label>
+              <div class="col-sm-9">
+                <input type="text" class="form-control" id="spl-contact-name" placeholder="">
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="col-sm-offset-3 col-sm-6">
+                <button type="submit" class="btn btn-block btn-success">Send Now &rarr;</button>
+              </div>
+            </div>
+          </fieldset>
+        </form>
+      </div><!-- /.panel-body -->
+    </div><!-- /.panel -->
 
   </div><!-- /.row -->
 
   <div class="col-md-4">
-    
+    <div class="panel panel-warning">
+      <div class="panel-heading">
         <h4>Spokane Public Library</h4>
-      
+      </div><!-- /.panel-heading -->
+      <div class="panel-body">
         <p>      
           <blockquote>
           906 West Main Street 
@@ -70,7 +74,8 @@
         <a class="btn btn-block btn-default" href="/branch">
           <small class="glyphicon glyphicon-flag"></small>
           Branch Locations &amp; Hours &rarr;</a>
-      
+      </div><!-- /.panel-body -->
+    </div><!-- /.panel -->
     
     
   </div><!-- /.row -->
