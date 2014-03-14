10fcb7bed1c134c4ecfdcc0cd5537a0c30ead69a
Sean Girard <seangirard@yahoo.com>
2014-03-14 14:27:45 -0700

    contact

--- templates/org/forms/field/common/form-contact.php
+++ templates/org/forms/field/common/form-contact.php
@@ -4,3 +4,18 @@
     <input type="text" class="form-control required" id="spl-form-name" name="spl-form[name]" placeholder="">
   </div>
 </div>
+<div class="form-group">
+  <label class="col-sm-4 control-label">Contact Information</label>
+  <div class="col-sm-8">
+    <!-- Nav tabs -->
+		<ul class="nav nav-tabs">
+		  <li class="active"><a href="#coms" data-toggle="tab">Email &amp; Phone</a></li>
+		  <li><a href="#mail" data-toggle="tab">Mailing Address</a></li>
+		</ul>
+		<!-- Tab panes -->
+		<div class="tab-content">
+		  <div class="tab-pane active" id="coms">...</div>
+		  <div class="tab-pane" id="mail">...</div>
+		</div>
+  </div>
+</div>
