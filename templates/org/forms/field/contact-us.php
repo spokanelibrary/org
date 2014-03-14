a8279742f3f79174eaedc726c471882464b201f4
Sean Girard <seangirard@yahoo.com>
2014-03-14 10:53:49 -0700

    contact

--- templates/org/forms/field/contact-us.php
+++ templates/org/forms/field/contact-us.php
@@ -20,29 +20,14 @@
 
     <form class="form-horizontal" role="form">
       <div class="form-group">
-        <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
-        <div class="col-sm-10">
-          <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+        <label for="inputEmail3" class="col-sm-3 control-label">Your Message</label>
+        <div class="col-sm-9">
+          <input type="email" class="form-control" id="spl-contact-message" placeholder="">
         </div>
       </div>
       <div class="form-group">
-        <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
-        <div class="col-sm-10">
-          <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
-        </div>
-      </div>
-      <div class="form-group">
-        <div class="col-sm-offset-2 col-sm-10">
-          <div class="checkbox">
-            <label>
-              <input type="checkbox"> Remember me
-            </label>
-          </div>
-        </div>
-      </div>
-      <div class="form-group">
-        <div class="col-sm-offset-2 col-sm-10">
-          <button type="submit" class="btn btn-default">Sign in</button>
+        <div class="col-sm-offset-3 col-sm-9">
+          <button type="submit" class="btn btn-default">Send Now &rarr;</button>
         </div>
       </div>
     </form>
