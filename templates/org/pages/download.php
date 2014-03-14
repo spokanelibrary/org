e37df40e4baad807ca8be8a044ddba97b142d3d2
Sean Girard <seangirard@yahoo.com>
2014-03-14 07:42:21 -0700

    dl

--- templates/org/pages/download.php
+++ templates/org/pages/download.php
@@ -11,6 +11,46 @@
 				We’ll do our best to help.
 			</strong>
 		</p>
+
+
+		<div class="panel panel-primary" style="border-width-left:5px;">
+			<div class="panel-heading">
+				<h4>
+					<i class="glyphicon glyphicon-cloud-download"></i> 
+					Music
+				</h4>
+			</div><!-- /.panel-heading -->
+			<div class="panel-body">
+				<div class="media">
+				  <a class="pull-left btn btn-success" href="#">
+				  	<span class="media-object">
+				  		<img alt="" style="width: 50px; height: 50px;" src="/assets/img/png/spl-app-icons-music.png">
+				  	</span>
+					</a>
+				  <div class="media-body">
+				    <h4 class="media-heading">
+				    	<a href="#"><em>Freegal:</em> Music Downloads</a> <small>(.mp3)</small>
+				    </h4>
+				    <p>
+				    	<strong>Download 5 free songs every week</strong> – yours to keep! 
+				  	</p>
+				  	<p>
+							With over 3 million tracks from 11,000 record labels spanning more than 100 genres of music, we think everyone can find a reason (or 5) to like this service. 
+ 						</p>
+				    <p>
+				    	<h4>
+				    		<a href="#">Learn more</a> 
+				    		<small>&rarr;</small>
+				    	</h4>
+				    </p>
+				  </div>
+				</div><!-- /.media -->
+			</div><!-- /.panel-body -->
+		</div><!-- /.panel -->
+
+
+
+		
 	</div><!-- /.col -->
 	<div class="col-md-3">
 		<div class="panel panel-warning" style="border-width-left:5px;">
