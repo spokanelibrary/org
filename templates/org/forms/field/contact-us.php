01715f86b05d93b62a651e7da348362cedab196b
Sean Girard <seangirard@yahoo.com>
2014-03-14 10:18:24 -0700

    white-space

--- templates/org/forms/field/contact-us.php
+++ templates/org/forms/field/contact-us.php
@@ -2,7 +2,7 @@
 
   <div class="col-md-8">
     <p>
-      <a class="btn btn-lg btn-block btn-primary" href="/ask">
+      <a class="btn btn-lg btn-block btn-primary" style="white-space: normal;" href="/ask">
         <small class="glyphicon glyphicon-exclamation-sign"></small>
         For research questions <strong>please ask a librarian</strong> &rarr;
       </a>
