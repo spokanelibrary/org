2644ea8e9abb5acfc3ec49697c50107993a44e23
Sean Girard <seangirard@yahoo.com>
2014-03-14 14:04:49 -0700

    contact

--- templates/org/forms/field/contact-us.php
+++ templates/org/forms/field/contact-us.php
@@ -36,7 +36,9 @@
             </legend>
             <div class="form-group">
               <label for="spl-form-query-type" class="col-sm-4 control-label">
-                
+                <button type="button" class="btn btn-xs btn-info" data-toggle="collapse" data-target="#spl-explain-query-type">
+                  <i class="glyphicon glyphicon-info-sign"></i>
+                </button>
                 A question about
               </label>
               <div class="col-sm-8">
