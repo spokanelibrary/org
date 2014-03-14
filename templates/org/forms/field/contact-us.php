7b957a8183e5406f1e800c5039627aed87c56a94
Sean Girard <seangirard@yahoo.com>
2014-03-14 10:51:17 -0700

    contact

--- templates/org/forms/field/contact-us.php
+++ templates/org/forms/field/contact-us.php
@@ -8,7 +8,7 @@
       </a>
     </p>
     <p class="lead">
-      Answers to many <strong>Frequently Asked Questions</strong> can be found on our <a title="How do I?" href="/how">How do I...?</a> page.
+      Find answers to many <strong>Frequently Asked Questions</strong> on our <a title="How do I?" href="/how">How do I...?</a> page.
     </p>
     <!-- 
     <p>
@@ -17,6 +17,36 @@
     </button>
     </p>
     -->
+
+    <form class="form-horizontal" role="form">
+      <div class="form-group">
+        <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+        <div class="col-sm-10">
+          <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+        <div class="col-sm-10">
+          <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+          <div class="checkbox">
+            <label>
+              <input type="checkbox"> Remember me
+            </label>
+          </div>
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+          <button type="submit" class="btn btn-default">Sign in</button>
+        </div>
+      </div>
+    </form>
+
 
   </div><!-- /.row -->
 
