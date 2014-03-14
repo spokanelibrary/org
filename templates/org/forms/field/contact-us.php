93285a8675adaa22f00bf326d965791d80025d09
Sean Girard <seangirard@yahoo.com>
2014-03-14 09:47:22 -0700

    form

--- templates/org/forms/field/contact-us.php
+++ templates/org/forms/field/contact-us.php
@@ -1,9 +1,9 @@
 <div class="row">
 
-  <div class="col-md-9">
+  <div class="col-md-8">
     nine
   </div><!-- /.row -->
-  <div class="col-md-3">
+  <div class="col-md-4">
     three
   </div><!-- /.row -->
 
