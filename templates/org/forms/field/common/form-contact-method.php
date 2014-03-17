ad3f219dc8d6cb1577924234d7a4fe0abc143628
Sean Girard <seangirard@yahoo.com>
2014-03-17 11:35:52 -0700

    contact

--- templates/org/forms/field/common/form-contact-method.php
+++ templates/org/forms/field/common/form-contact-method.php
@@ -1,9 +1,6 @@
 <div class="form-group">
   <label for="spl-form-contact-method" class="col-sm-4 control-label">
-    <button type="button" class="btn btn-xs btn-info hidden-xs" data-toggle="collapse" data-target="#spl-explain-contact-method">
-      <i class="glyphicon glyphicon-info-sign"></i>
-    </button>
-    Contact me via
+    Please contact me via
   </label>
   <div class="col-sm-8">
     <select class="form-control" id="spl-form-contact-method" name="spl-form[contact-method]">
@@ -11,7 +8,7 @@
       <option value="phone">Telephone</option>
       <option value="postal">Postal Mail</option>
     </select>
-    <span class="help-block collapse" id="spl-explain-contact-method">
+    <span class="help-block">
       How should we reply? We prefer to use email. 
     </span>
   </div>
