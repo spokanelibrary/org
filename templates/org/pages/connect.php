56aeb09cae73e5c30302a3984d962e4d5f683ccc
Sean Girard <seangirard@yahoo.com>
2014-03-17 07:29:08 -0700

    connect

--- templates/org/pages/connect.php
+++ templates/org/pages/connect.php
@@ -1,1 +1,15 @@
-This is the connect page
+<div class="row">
+
+  <div class="col-sm-3">
+    Send an email
+  </div><!-- /.col -->
+
+  <div class="col-sm-3">
+    Ask a research questin
+  </div><!-- /.col -->
+
+  <div class="col-sm-3">
+    Get a library card
+  </div><!-- /.col -->
+
+</div><!-- /.row -->
