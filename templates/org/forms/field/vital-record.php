b3beaec8402fd57dfaa57aecbdfbb98a0499483b
Sean Girard <seangirard@yahoo.com>
2014-03-17 14:18:53 -0700

    vitals

--- templates/org/forms/field/vital-record.php
+++ templates/org/forms/field/vital-record.php
@@ -26,6 +26,13 @@
               <label class="col-sm-4 control-label text-muted">Request Type</label>
               <div class="col-sm-8">
 
+                <div class="radio">
+                  <label>
+                    <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+                    Option one is this and that&mdash;be sure to include why it's great
+                  </label>
+                </div>
+
                 <input type="radio" class="" id="spl-form-announcement-obit" name="spl-form[announcement]">
 
                 <input type="radio" class="" id="spl-form-announcement-birth" name="spl-form[announcement]">
