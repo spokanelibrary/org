6a2a1f5e1ca340b7a05eadc61383aebbeb1370df
Sean Girard <seangirard@yahoo.com>
2014-03-17 07:40:56 -0700

    connect

--- templates/org/pages/connect.php
+++ templates/org/pages/connect.php
@@ -1,7 +1,7 @@
 <div class="row">
 
   <div class="col-sm-4">
-    <div class="panel panel-default">
+    <div class="panel panel-success">
       <div class="panel-heading">
         <h4>
           <i class="glyphicon glyphicon-envelope text-muted"></i>
