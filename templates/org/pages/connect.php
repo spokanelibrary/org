07ced8f6c4760817fde310dee0b4a231e3e67f1a
Sean Girard <seangirard@yahoo.com>
2014-03-17 07:32:42 -0700

    connect

--- templates/org/pages/connect.php
+++ templates/org/pages/connect.php
@@ -1,15 +1,27 @@
 <div class="row">
 
-  <div class="col-sm-3">
+  <div class="col-sm-4">
     Send an email
   </div><!-- /.col -->
 
-  <div class="col-sm-3">
-    Ask a research questin
+  <div class="col-sm-4">
+    Ask a research question
   </div><!-- /.col -->
 
-  <div class="col-sm-3">
+  <div class="col-sm-4">
     Get a library card
+  </div><!-- /.col -->
+
+  <div class="col-sm-4">
+    Research obituaties &amp; birth announcements
+  </div><!-- /.col -->
+
+  <div class="col-sm-4">
+    Request a title
+  </div><!-- /.col -->
+
+  <div class="col-sm-4">
+    GFollow us on Facebook &amp; Twitter
   </div><!-- /.col -->
 
 </div><!-- /.row -->
