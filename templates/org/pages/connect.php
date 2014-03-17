5742c19d54648ebb567f95bc3ee90c2f7756c747
Sean Girard <seangirard@yahoo.com>
2014-03-17 09:15:07 -0700

    connect

--- templates/org/pages/connect.php
+++ templates/org/pages/connect.php
@@ -95,7 +95,7 @@
               <i class="glyphicon glyphicon-info-sign text-muted"></i> 
               <a class="" 
                 href="#" 
-                title="">Frequently asked question</a>
+                title="">Frequently asked questions</a>
               <span class="text-muted">&rarr;</span>
             </h4>
             <p>
