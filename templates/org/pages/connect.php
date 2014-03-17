6ebf08714b77395febecd9b33fff0c6b6673d226
Sean Girard <seangirard@yahoo.com>
2014-03-17 09:40:43 -0700

    contact

--- templates/org/pages/connect.php
+++ templates/org/pages/connect.php
@@ -1,5 +1,5 @@
 <div class="row">
-  <div class="col-md-9">
+  <div class="col-md-8">
     
     <div class="row">
       <div class="col-sm-6">
@@ -108,7 +108,7 @@
 
   </div><!-- /.col -->
   
-  <div class="col-md-3">
+  <div class="col-md-4">
     <h4 class="text-muted">Let's Socialize</h4>
     <p>
       Facebook
