c108a792030ebfd922c75af484e8d17abef4dc1c
Sean Girard <seangirard@yahoo.com>
2014-03-18 14:24:30 -0700

    intro

--- templates/org/forms/field/card-renew.php
+++ templates/org/forms/field/card-renew.php
@@ -2,7 +2,7 @@
 
   <div class="col-md-8">
 
-    <?php echo do_shortcode('[spl_widget login-form label post=renew intro="Please login to get started."]'); ?>
+    <?php echo do_shortcode('[spl_widget login-form label post=renew intro=Please login to get started.]'); ?>
 
     <?php echo do_shortcode('[spl_widget card-renewal]'); ?>
 
