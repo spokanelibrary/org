44721e4c761094ddbd574d2987363beda00251c9
Sean Girard <seangirard@yahoo.com>
2014-03-18 14:35:38 -0700

    renew

--- templates/org/forms/field/card-renew.php
+++ templates/org/forms/field/card-renew.php
@@ -33,7 +33,7 @@
 
             <?php //include 'common/field-contact-method.php'; ?>
             <?php //include 'common/field-login.php'; ?>
-            <?php include 'common/field-contact.php'; ?>
+            <?php //include 'common/field-contact.php'; ?>
             <?php //include 'common/field-submit.php'; ?>
             
           </fieldset>
