cff5821b5c033e639e5f9e08973f7647dba93539
Sean Girard <seangirard@yahoo.com>
2014-03-19 07:12:12 -0700

    trace

--- templates/org/forms/field/card-renew.php
+++ templates/org/forms/field/card-renew.php
@@ -5,16 +5,17 @@
     <?php //echo do_shortcode('[spl_widget login-form label post=renew intro="Please login to get started."]'); ?>
 
     <?php $renew =  json_decode(do_shortcode('[spl_widget card-renew]')); ?>
-    <pre>
+    
     <?php
+      echo '<pre>';
       echo 'request:';
       echo print_r($_REQUEST['spl-login'], true);
       echo 'session:';
       echo print_r($_SESSION['spl']['auth'], true); 
+      echo '</pre>';
       echo 'object:';
       echo print_r($renew, true);
     ?>
-    </pre>
     <?php //if ( !empty($renew->html) ) : ?>
     <?php //echo $renew->html; ?>
     <?php //elseif (isset($renew->status) && empty($renew->html) ) : ?>
