5284a412c788d2ed1e973368157287af302ec48e
Sean Girard <seangirard@yahoo.com>
2014-03-19 12:23:48 -0700

    renew

--- templates/org/forms/field/card-renew.php
+++ templates/org/forms/field/card-renew.php
@@ -74,6 +74,19 @@
               </div>
             </div>
 
+            <div class="form-group">
+              <label for="spl-form-city-st" class="col-sm-4 control-label">City, ST</label>
+              <div class="col-sm-8">
+                <input type="text" class="form-control required" id="spl-form-city-st" name="spl-form[contact][city-st]" value="<?php echo $renew->borrower->city_st; ?>" placeholder="">
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label for="spl-form-zip" class="col-sm-4 control-label">ZIP Code</label>
+              <div class="col-sm-8">
+                <input type="text" class="form-control required" id="spl-form-zip" name="spl-form[contact][zip]" value="<?php echo $renew->borrower->zip; ?>" placeholder="">
+              </div>
+            </div>
 
 
             <pre>
