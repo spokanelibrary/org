bacf97739d5139292b0dc929913b00f9e7a00188
Sean Girard <seangirard@yahoo.com>
2014-03-19 13:21:29 -0700

    renew

--- templates/org/forms/field/card-renew-response.php
+++ templates/org/forms/field/card-renew-response.php
@@ -1,84 +1,46 @@
-<div class="well" style="background:transparent;">
+<?php if ( isset( $GLOBALS['crass_response']->result['date'] ) ) : ?>
 
-  <form class="form-horizontal spl-form">
+<div class="alert alert-success">
+  <h2>Success!</h2>
 
-    <div id="spl-form-contact-response">
-      <h3>Processing <small>your message&hellip;</small></h3>
-    </div>
-
-    <!-- this should be a script or move out of js  -->
-    <input type="hidden" 
-            id="spl-form-id" 
-            name="spl-form[id]" 
-            value="contact-response" 
-            data-response-id="<?php echo $GLOBALS['crass_response']->result['id']; ?>" 
-            data-response-contact='<?php echo json_encode($GLOBALS['crass_response']->request); ?>' 
-            />
-  </form>
+  <blockquote>
+    <p>
+      Your card has been renewed until <strong><?php echo $GLOBALS['crass_response']->result['date']; ?></strong>.
+    </p>
+    <p>
+      If you have updated your contact information, your changes will not be reflected on your account until they are reviewed by library circulation staff.
+    </p>
+</blockquote>
 
 </div>
 
-<script id="spl-form-contact-response-tmpl" type="text/x-handlebars-template">
+<?php else: ?>
 
-<h3>Thank You! <small>We received your message.</small></h3>
+<div class="alert alert-error">
+  <h2>Oops!</h2>
 
-{{#with request}}
+  <blockquote>
+    <p>
+      It looks like we ran into a problem renewing your card.
+    </p>
+    <p>
+      Please give us a call at <strong>444-5333</strong> so we can take a look at this for you.
+    </p>
+    <p>
+      Thanks, and apologies for the inconvenience.
+    </p>
+  </blockquote>
 
-  <div class="row-fluid">
-    <dl class="dl-horizontal">
+</div>
 
-    {{#if name}}
-      <dt>Your Name</dt>
-      <dd>
-        <p>
-          {{name}}
-        </p>
-      </dd>
-    {{/if}}
-
-    {{#if barcode}}
-      <dt>Your Barcode</dt>
-      <dd>
-        <p>
-          {{barcode}}
-        </p>
-      </dd>
-    {{/if}}
-
-    {{#if email}}
-      <dt>Your Email</dt>
-      <dd>
-        <p>
-          {{email}}
-        </p>
-      </dd>
-    {{/if}}
-
-    {{#if message}}
-      <dt>Your Message</dt>
-      <dd>
-        <p>
-          {{message}}
-        </p>
-      </dd>
-    {{/if}}
-
-    </dl>
-
-    <div class="alert alert-info">
-      We will respond to your inquiry as soon as possible.
-    </div>
-
-  </div><!-- /.row-fluid -->
-
-{{/with}}
-</script>
+<?php endif; ?>
 
 <?php
 
+/*
 echo '<pre>';
 print_r($GLOBALS['crass_response']->result);
 //print_r($GLOBALS['crass_response']->request); 
 echo '</pre>';
-
+*/
 ?>
--- templates/org/forms/form-card-renew.php
+++ templates/org/forms/form-card-renew.php
@@ -1,9 +1,5 @@
 <?php
 
-echo '<pre>';
-print_r($GLOBALS['crass_response']);
-echo '</pre>';
-
 if ( !empty($GLOBALS['crass_response']->request) ) {
   include 'field/card-renew-response.php';
   } else {
