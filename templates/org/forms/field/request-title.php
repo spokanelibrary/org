d2aa9f68ac89d028026f51a43db7fe0607340318
Sean Girard <seangirard@yahoo.com>
2014-03-20 09:22:07 -0700

    request

---
+++ templates/org/forms/field/request-title.php
@@ -0,0 +1,51 @@
+<div class="row">
+
+  <div class="col-md-8">
+    
+    <?php echo do_shortcode('[spl_widget login-form label]'); ?>
+    
+    <div class="panel panel-default">
+      <div class="panel-body">
+        <form class="form-horizontal spl-form" id="spl-form-request" method="post" role="form">
+          
+          <?php include 'common/crass-response.php'; ?>
+      
+          <input type="hidden" id="spl-form-id" name="spl-form[id]" value="request" />
+          
+          <input type="hidden" 
+                id="spl-form-ip" 
+                name="spl-form[ip]" 
+                value="<?php echo $_SERVER['REMOTE_ADDR']; ?>"
+                />
+
+          <fieldset>
+            <legend class="text-muted">
+              <small class="glyphicon glyphicon-envelope"></small>
+              Suggest a new title or make an ILL request
+            </legend>
+
+            
+            <div class="form-group">
+              <label for="spl-form-message" class="col-sm-4 control-label">My message</label>
+              <div class="col-sm-8">
+                <textarea rows="6" class="form-control required" id="spl-form-message" name="spl-form[message]"></textarea>
+              </div>
+            </div>
+            
+            <?php //include 'common/field-contact-method.php'; ?>
+            <?php //include 'common/field-login.php'; ?>
+            <?php //include 'common/field-contact.php'; ?>
+            <?php //include 'common/field-submit.php'; ?>
+            
+          </fieldset>
+        </form>
+      </div><!-- /.panel-body -->
+    </div><!-- /.panel -->
+
+  </div><!-- /.col -->
+
+  <div class="col-md-4">
+    <?php echo do_shortcode('[spl_widget custom-page page=contact-info]'); ?>
+  </div><!-- /.col -->
+
+</div><!-- /.row -->
---
+++ templates/org/forms/form-request-title.php
@@ -0,0 +1,7 @@
+<?php
+if ( !empty($GLOBALS['crass_response']->request) ) {
+  include 'field/request-title-response.php';
+  } else {
+  include 'field/request-title.php';
+}
+?>
