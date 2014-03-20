cb61b24bf5551edc50fff9c513c795557eb60ac1
Sean Girard <seangirard@yahoo.com>
2014-03-20 13:32:11 -0700

    request

--- templates/org/forms/field/request/title.php
+++ templates/org/forms/field/request/title.php
@@ -1,88 +1,65 @@
 
-<nav class="navbar navbar-default" role="navigation">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <p class="clearfix">
-      <a class="navbar-brand" href="#">I'm looking for a &rarr;</a>
-      </p>
-    </div>
+<div class="row">
+  <div class="col-sm-4">
+    <button type="button" 
+            class="btn btn-block btn-default navbar-btn" 
+            data-toggle="button">Book</button>
+  </div><!-- /.col -->
+</div><!-- /.row -->
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      
-      <div class="row">
-        <div class="col-sm-4">
-          <button type="button" 
-                  class="btn btn-block btn-default navbar-btn" 
-                  data-toggle="button">Book</button>
-        </div><!-- /.col -->
-      </div><!-- /.row -->
+<div class="row">
+  <div class="col-sm-4">
+    <button type="button" 
+            class="btn btn-block btn-default navbar-btn" 
+            data-toggle="button">Book on CD</button>
+  </div><!-- /.col -->
+  <div class="col-sm-4">
+    <button type="button" 
+            class="btn btn-block btn-default navbar-btn" 
+            data-toggle="button">eBook</button>
+  </div><!-- /.col -->
+  <div class="col-sm-4">
+    <button type="button" 
+            class="btn btn-block btn-default navbar-btn" 
+            data-toggle="button">Audio Book <small>(download)</small></button>
+  </div><!-- /.col -->
+</div><!-- /.row -->
 
-      <div class="row">
-        <div class="col-sm-4">
-          <button type="button" 
-                  class="btn btn-block btn-default navbar-btn" 
-                  data-toggle="button">Book on CD</button>
-        </div><!-- /.col -->
-        <div class="col-sm-4">
-          <button type="button" 
-                  class="btn btn-block btn-default navbar-btn" 
-                  data-toggle="button">eBook</button>
-        </div><!-- /.col -->
-        <div class="col-sm-4">
-          <button type="button" 
-                  class="btn btn-block btn-default navbar-btn" 
-                  data-toggle="button">Audio Book <small>(download)</small></button>
-        </div><!-- /.col -->
-      </div><!-- /.row -->
+<div class="row">
+  <div class="col-sm-4">
+    <button type="button" 
+            class="btn btn-block btn-default navbar-btn" 
+            data-toggle="button">Music CD</button>
+  </div><!-- /.col -->
+  <div class="col-sm-4">
+    <button type="button" 
+            class="btn btn-block btn-default navbar-btn" 
+            data-toggle="button">DVD</button>
+  </div><!-- /.col -->
+  <div class="col-sm-4">
+    <button type="button" 
+            class="btn btn-block btn-default navbar-btn" 
+            data-toggle="button">Other <small>(tell us)</small></button>
+  </div><!-- /.col -->
+</div><!-- /.row -->
 
-      <div class="row">
-        <div class="col-sm-4">
-          <button type="button" 
-                  class="btn btn-block btn-default navbar-btn" 
-                  data-toggle="button">Music CD</button>
-        </div><!-- /.col -->
-        <div class="col-sm-4">
-          <button type="button" 
-                  class="btn btn-block btn-default navbar-btn" 
-                  data-toggle="button">DVD</button>
-        </div><!-- /.col -->
-        <div class="col-sm-4">
-          <button type="button" 
-                  class="btn btn-block btn-default navbar-btn" 
-                  data-toggle="button">Other <small>(tell us)</small></button>
-        </div><!-- /.col -->
-      </div><!-- /.row -->
-
-      <div class="row">
-        <div class="col-sm-4">
-          <button type="button" 
-                  class="btn btn-block btn-default navbar-btn" 
-                  data-toggle="button">Genealogy <small>(research copies)</small></button>
-        </div><!-- /.col -->
-        <div class="col-sm-4">
-          <button type="button" 
-                  class="btn btn-block btn-default navbar-btn" 
-                  data-toggle="button">Newspaper <small>(microfilm)</small></button>
-        </div><!-- /.col -->
-        <div class="col-sm-4">
-          <button type="button" 
-                  class="btn btn-block btn-default navbar-btn" 
-                  data-toggle="button">Magazine <small>or</small> Journal</button>
-        </div><!-- /.col -->
-      </div><!-- /.row -->
-
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
-
+<div class="row">
+  <div class="col-sm-4">
+    <button type="button" 
+            class="btn btn-block btn-default navbar-btn" 
+            data-toggle="button">Genealogy <small>(research copies)</small></button>
+  </div><!-- /.col -->
+  <div class="col-sm-4">
+    <button type="button" 
+            class="btn btn-block btn-default navbar-btn" 
+            data-toggle="button">Newspaper <small>(microfilm)</small></button>
+  </div><!-- /.col -->
+  <div class="col-sm-4">
+    <button type="button" 
+            class="btn btn-block btn-default navbar-btn" 
+            data-toggle="button">Magazine <small>or</small> Journal</button>
+  </div><!-- /.col -->
+</div><!-- /.row -->
 
 
 
