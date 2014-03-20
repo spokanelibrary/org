e473ae7c3ea7712f0d12e3754cd5dff7a324db62
Sean Girard <seangirard@yahoo.com>
2014-03-20 13:01:08 -0700

    request

--- templates/org/forms/field/request/title.php
+++ templates/org/forms/field/request/title.php
@@ -14,6 +14,8 @@
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      
+      <div class="row">
       <ul class="nav navbar-nav">
         <li class="active"><a href="#">Link</a></li>
         <li><a href="#">Link</a></li>
@@ -22,8 +24,8 @@
         <li><a href="#">Link</a></li>
       </ul>
 
-      <hr>
-
+      </div>
+      <div class="row">
       <ul class="nav navbar-nav">
         <li class="active"><a href="#">Link</a></li>
         <li><a href="#">Link</a></li>
@@ -31,6 +33,9 @@
       <ul class="nav navbar-nav navbar-right">
         <li><a href="#">Link</a></li>
       </ul>
+
+      </div>
+
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
