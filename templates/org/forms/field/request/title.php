f78eed743f0c8b3f75679f033bf4d53eb0c386fe
Sean Girard <seangirard@yahoo.com>
2014-03-20 12:53:36 -0700

    request

--- templates/org/forms/field/request/title.php
+++ templates/org/forms/field/request/title.php
@@ -1,43 +1,32 @@
 
-<div class="btn-group btn-group-justified" data-toggle="buttons">
+<nav class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Brand</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link</a></li>
+        <li><a href="#">Link</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Link</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
 
 
-  <label class="btn btn-primary">
-    <input type="checkbox"> Option 1
-  </label>
 
-  <label class="btn btn-primary">
-    <input type="checkbox"> Option 2
-  </label>
-
-  <label class="btn btn-primary">
-    <input type="checkbox"> Option 3
-  </label>
-
-  <label class="btn btn-primary">
-    <input type="checkbox"> Option 3
-  </label>
-
-  <br >
-
-  <label class="btn btn-primary">
-    <input type="checkbox"> Option 1
-  </label>
-
-  <label class="btn btn-primary">
-    <input type="checkbox"> Option 2
-  </label>
-
-  <label class="btn btn-primary">
-    <input type="checkbox"> Option 3
-  </label>
-
-  <label class="btn btn-primary">
-    <input type="checkbox"> Option 3
-  </label>
-
-
-</div>
 
 
 <div class="row">
