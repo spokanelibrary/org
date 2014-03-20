70650bcfdc17925a786c7e78801ec80b8c8be026
Sean Girard <seangirard@yahoo.com>
2014-03-20 13:29:45 -0700

    request

--- templates/org/forms/field/request/title.php
+++ templates/org/forms/field/request/title.php
@@ -14,9 +14,9 @@
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      
+      <br>
       <div class="row">
-        <div class="col-sm-8 col-sm-offset-0">
+        <div class="col-sm-8">
           <button type="button" 
                   class="btn btn-block btn-default navbar-btn" 
                   data-toggle="button">Book</button>
