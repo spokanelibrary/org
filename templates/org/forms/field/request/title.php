1c383d35e45313bfb63120d20dfac926cd838c04
Sean Girard <seangirard@yahoo.com>
2014-03-20 13:07:46 -0700

    request

--- templates/org/forms/field/request/title.php
+++ templates/org/forms/field/request/title.php
@@ -13,7 +13,7 @@
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="#bs-example-navbar-collapse-1">
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       
       <div class="row">
         <div class="col-sm-3">
