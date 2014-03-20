e2e8d35f7f50c8b3015a05dcdc0b25d3d8f6c42a
Sean Girard <seangirard@yahoo.com>
2014-03-20 12:54:51 -0700

    request

--- templates/org/forms/field/request/title.php
+++ templates/org/forms/field/request/title.php
@@ -9,7 +9,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Brand</a>
+      <a class="navbar-brand visible-xs" href="#">I'm looking for a &rarr;</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
