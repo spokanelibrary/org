0c734580746d73339e4d206ab24fe0ab84d292dd
Sean Girard <seangirard@yahoo.com>
2014-03-20 11:42:40 -0700

    request

--- templates/org/forms/field/request/title.php
+++ templates/org/forms/field/request/title.php
@@ -1,22 +1,28 @@
         
 
 <div class="btn-group" data-toggle="buttons">
+
   <label class="btn btn-primary">
-    <input type="radio" name="options" id="option1"> Option 1
+    <input type="radio" name="options" id="option1"> Book
   </label>
   <label class="btn btn-primary">
-    <input type="radio" name="options" id="option2"> Option 2
+    <input type="radio" name="options" id="option2"> Book on CD
   </label>
   <label class="btn btn-primary">
-    <input type="radio" name="options" id="option3"> Option 3
+    <input type="radio" name="options" id="option3"> eBook
   </label>
+  <label class="btn btn-primary">
+    <input type="radio" name="options" id="option3"> Audio Book
+  </label>
+
 </div>
 
 
-        
+<!--        
 <div class="form-group">
   <label for="spl-form-message" class="col-sm-2 control-label">My message</label>
   <div class="col-sm-10">
     <textarea rows="6" class="form-control required" id="spl-form-message" name="spl-form[message]"></textarea>
   </div>
 </div>
+-->
