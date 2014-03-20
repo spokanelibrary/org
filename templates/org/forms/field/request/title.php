9533116fa4ed5d33f7bf0c83c2374d1eef6a499e
Sean Girard <seangirard@yahoo.com>
2014-03-20 11:39:49 -0700

    request

--- templates/org/forms/field/request/title.php
+++ templates/org/forms/field/request/title.php
@@ -1,4 +1,19 @@
         
+
+<div class="btn-group" data-toggle="buttons">
+  <label class="btn btn-primary">
+    <input type="radio" name="options" id="option1"> Option 1
+  </label>
+  <label class="btn btn-primary">
+    <input type="radio" name="options" id="option2"> Option 2
+  </label>
+  <label class="btn btn-primary">
+    <input type="radio" name="options" id="option3"> Option 3
+  </label>
+</div>
+
+
+        
 <div class="form-group">
   <label for="spl-form-message" class="col-sm-2 control-label">My message</label>
   <div class="col-sm-10">
