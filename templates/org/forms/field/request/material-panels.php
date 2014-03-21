0654fb42d6109ef1ca142017a9b1727e708b6754
Sean Girard <seangirard@yahoo.com>
2014-03-21 15:37:50 -0700

    request

--- templates/org/forms/field/request/material-panels.php
+++ templates/org/forms/field/request/material-panels.php
@@ -245,11 +245,20 @@
       <div class="row">
         <div class="col-sm-6">
           <label for="spl-form-content" class="control-label">Content</label>
-          <input type="text" class="form-control" id="spl-form-content" name="spl-form[content]">
+          <select class="form-control" name="spl-form[content]" id="spl-form-content">
+            <option value="">Optional...</option>
+            <option value="fiction">Fiction</option>
+            <option value="non-fiction">Non-Fiction</option>
+          </select>
         </div>
         <div class="col-sm-6">
           <label for="spl-form-audience" class="control-label">Age Group</label>
-          <input type="text" class="form-control" id="spl-form-audience" name="spl-form[audience]">
+          <select class="form-control" name="spl-form[audience]" id="spl-form-audience">
+            <option value="">Optional...</option>
+            <option value="adult">Adult</option>
+            <option value="yount-adult">Young Adult</option>
+            <option value="juvenile">Juvenile</option>
+          </select>
         </div>
       </div>
 
