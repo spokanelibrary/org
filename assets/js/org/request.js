684570192176a2156f8a3da7c5648854d8d35d8c
Sean Girard <seangirard@yahoo.com>
2014-03-21 10:01:45 -0700

    request

--- assets/js/org/request.js
+++ assets/js/org/request.js
@@ -29,13 +29,18 @@ var org = {
 }
 
 , initRequestItem: function() {
+
+		$('body').on('change', 'input:radio[name = "spl-form[material]"]', function() {
+			var materialType = $(this).val();
+			console.log(materialType);
+		});
 		
 		$('body').on('show.bs.collapse', '#spl-form-request-panels', function() {
-			$('#spl-form-request-panel-choose').collapse('hide');
+			//$('#spl-form-request-panel-choose').collapse('hide');
 			
-			var materialType = $('input:radio[name = "spl-form[material]"]:checked' , '#spl-form-request-panel-choose').val()
+			//var materialType = $('input:radio[name = "spl-form[material]"]:checked' , '#spl-form-request-panel-choose').val()
 
-			console.log(materialType);
+			//console.log(materialType);
 
 		});
 
