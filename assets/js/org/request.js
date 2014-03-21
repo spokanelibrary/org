e1c5d034dfdc8d1031c306a8a6be0afb2b51e049
Sean Girard <seangirard@yahoo.com>
2014-03-21 13:47:23 -0700

    request

--- assets/js/org/request.js
+++ assets/js/org/request.js
@@ -45,22 +45,13 @@ var org = {
 		
 		// show generic request form when requested
 		$('body').on('show.bs.collapse', '.spl-form-request-generic', function() {
-			/*
+			
 			if ( $(this).hasClass('spl-form-request-download') ) {
 				$('#spl-form-request-submit-item').hide();
 				$('#spl-form-request-submit-download').show();
 			} else {
 				$('#spl-form-request-submit-item').show();
 				$('#spl-form-request-submit-download').hide();
-			}
-			*/
-
-			if ( $(this).hasClass('spl-form-request-download') ) {
-				$('#spl-form-request-submit-item').collapse('hide');
-				$('#spl-form-request-submit-download').collaspe('show');
-			} else {
-				$('#spl-form-request-submit-item').collapse('show');
-				$('#spl-form-request-submit-download').collapse('hide');
 			}
 
 			$('#spl-form-panel-request').collapse('show');
