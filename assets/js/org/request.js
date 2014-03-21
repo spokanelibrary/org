b4e2c6826feedcbbe5aedd25733e1bc9b79b3844
Sean Girard <seangirard@yahoo.com>
2014-03-21 11:41:01 -0700

    request

--- assets/js/org/request.js
+++ assets/js/org/request.js
@@ -42,13 +42,10 @@ var org = {
 			$('#spl-form-request-panel-choose').collapse('hide');			
 		});
 
-		$('body').on('hide.bs.collapse', '#spl-form-request-panels', function() {
-			//$('#spl-form-panel-request').collapse('hide');
-			$('#spl-form-request-panel-choose').collapse('show');
+		$('body').on('shown.bs.collapse', '.spl-form-request-material', function() {
+			$('#spl-form-panel-request').collapse('show');
 		});
 
-
-		
 		/*
 		$('body').on('show.bs.collapse', '#spl-form-request-panel-choose', function() {
 			console.log('hide panels');
@@ -61,18 +58,7 @@ var org = {
 		/*
 		
 		*/
-		/*
 		
-
-		
-
-
-
-		$('body').on('shown.bs.collapse', '.spl-form-request-material', function() {
-			$('#spl-form-panel-request').collapse('show');
-		});
-		*/
-
   } // initRequestItem()
 
 
