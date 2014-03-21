40442850672744017628a17ce3bc30d11ce27b1f
Sean Girard <seangirard@yahoo.com>
2014-03-21 11:02:36 -0700

    request

--- templates/org/forms/field/request/material-panels.php
+++ templates/org/forms/field/request/material-panels.php
@@ -4,9 +4,21 @@
 
     <div class="panel-collapse collapse spl-form-request-material"
           id="spl-form-panel-request-book">
-
+      <div class="panel panel-primary">
+        <div class="panel-heading">
+          <h4 class="panel-title">
             Book
 
+            <button type="button" 
+                    class="btn btn-success pull-right"
+                    data-toggle="collapse"
+                    data-target="#spl-form-request-panel-choose">Change</button>
+          </h4>
+        </div><!-- /.panel-heading -->   
+        <div class="panel-body">
+          Request.
+        </div><!-- /.panel-body -->
+      </div><!-- /.panel -->
     </div><!-- /.collapse -->
 
     <div class="panel-collapse collapse spl-form-request-material"
