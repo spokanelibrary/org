380f18692101b248f6d28484dc0e0cccfc8c9c97
Sean Girard <seangirard@yahoo.com>
2014-03-21 09:35:58 -0700

    request

--- templates/org/forms/field/request/material-panels.php
+++ templates/org/forms/field/request/material-panels.php
@@ -2,33 +2,35 @@
 <div class="panel-group" id="spl-form-request-panels">
   <div class="panel" style="border:none;">
 
-    <div id="spl-form-panel-book" class="panel-collapse collapse in">
-      <div class="panel panel-default">
+    <div class="panel-collapse collapse"
+          id="spl-form-panel-book">
+      <div class="panel panel-primary">
         <div class="panel-heading">
           <h4 class="panel-title">
             Book
           </h4>
         </div><!-- /.panel-heading -->   
         <div class="panel-body">
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+          Book form.
         </div><!-- /.panel-body -->
       </div><!-- /.panel -->
     </div><!-- /.collapse -->
 
-    <div id="spl-form-panel-cd-audio-book" class="panel-collapse collapse">
-      <div class="panel panel-default">
+    <div class="panel-collapse collapse"
+          id="spl-form-panel-cd-audio-book">
+      <div class="panel panel-primary">
         <div class="panel-heading">
           <h4 class="panel-title">
             Book on CD
           </h4>
         </div><!-- /.panel-heading -->  
         <div class="panel-body">
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable DVD.
+          Book on CD form.
         </div><!-- /.panel-body -->
       </div><!-- /.panel -->
     </div><!-- /.collapse -->
 
-  </div>
+  </div><!-- /.panel - required here for bs3 -->
 </div><!-- /.panel-group -->
 
 
