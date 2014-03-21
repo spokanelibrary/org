5b7c96706872b2eafcdf29ae49518d8b66145292
Sean Girard <seangirard@yahoo.com>
2014-03-21 09:14:35 -0700

    request

--- templates/org/forms/field/request/material-panels.php
+++ templates/org/forms/field/request/material-panels.php
@@ -1,3 +1,28 @@
+<div class='panel-group' id='accordionNav' >
+  <div class='panel'>
+    <div class='panel-heading'>
+      <a data-parent='#accordionNav' data-toggle='collapse' href="#navCollapse1">  <h3>About</h3></a>
+    </div>
+    <div class='panel-collapse collapse in'  id='navCollapse1'>
+      <div class='panel-body'>
+        <p>Test test ets ejkfbduskfgsalf jydasl</p>
+      </div>
+    </div>
+  </div>
+  <div class='panel'>
+    <div class='panel-heading'>
+      <a data-parent='#accordionNav' data-toggle='collapse' href="#navCollapse2"><h3>Donors</h3></a>
+    </div>
+    <div class='panel-collapse collapse' id='navCollapse2'>
+      <div class='panel-body'>
+        <p>Test test ets ejkfbduskfgsalf jydasl</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
 
 <div class="panel-group" id="spl-form-request-panels">
 
