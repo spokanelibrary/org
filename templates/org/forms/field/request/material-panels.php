ba699d40b2d6b6488a6d5c3efea4a8bfeb5bac80
Sean Girard <seangirard@yahoo.com>
2014-03-21 09:17:15 -0700

    request

--- templates/org/forms/field/request/material-panels.php
+++ templates/org/forms/field/request/material-panels.php
@@ -1,34 +1,8 @@
-<div class='panel-group' id='accordionNav' >
-  <div class='panel'>
-    <div class='panel-heading'>
-      <a data-parent='#accordionNav' data-toggle='collapse' href="#navCollapse1">  <h3>About</h3></a>
-    </div>
-    <div class='panel-collapse collapse in'  id='navCollapse1'>
-      <div class='panel-body'>
-        <p>Test test ets ejkfbduskfgsalf jydasl</p>
-      </div>
-    </div>
-  </div>
-  <div class='panel'>
-    <div class='panel-heading'>
-      <a data-parent='#accordionNav' data-toggle='collapse' href="#navCollapse2"><h3>Donors</h3></a>
-    </div>
-    <div class='panel-collapse collapse' id='navCollapse2'>
-      <div class='panel-body'>
-        <p>Test test ets ejkfbduskfgsalf jydasl</p>
-      </div>
-    </div>
-  </div>
-</div>
-
-
-
 
 <div class="panel-group" id="spl-form-request-panels">
-
-  
-  <div id="spl-form-panel-book" class="panel panel-default" data-parent="#spl-form-request-panels">
-    <div  class="panel-collapse collapse">
+<div class="panel">
+  <div id="spl-form-panel-book" class="panel-collapse collapse">
+    <div class="panel panel-default">
       <div class="panel-heading">
         <h4 class="panel-title">
           Book
@@ -37,11 +11,11 @@
       <div class="panel-body">
         Book form.
       </div><!-- /.panel-body -->
-    </div><!-- /.panel-collapse -->
-  </div><!-- /.panel -->
+    </div><!-- /.panel -->
+  </div><!-- /.collapse -->
 
-  <div id="spl-form-panel-cd-audio-book" class="panel panel-default" data-parent="#spl-form-request-panels">
-    <div  class="panel-collapse collapse">
+  <div id="spl-form-panel-cd-audio-book" class="panel-collapse collapse">
+    <div class="panel panel-default">
       <div class="panel-heading">
         <h4 class="panel-title">
           Book on CD
@@ -50,8 +24,8 @@
       <div class="panel-body">
         Book on CD form.
       </div><!-- /.panel-body -->
-    </div><!-- /.panel-collapse -->
-  </div><!-- /.panel -->
-
+    </div><!-- /.panel -->
+  </div><!-- /.collapse -->
+</div>
 </div><!-- /.panel-group -->
 
