5aef84606d643e4f896c9f43f897803e92d6da81
Sean Girard <seangirard@yahoo.com>
2014-03-21 09:22:08 -0700

    request

--- templates/org/forms/field/request/material-panels.php
+++ templates/org/forms/field/request/material-panels.php
@@ -9,15 +9,16 @@ Collapsible Group Item #2
 
 <div class="panel-group" id="accordion">
   
-  <div class="panel panel-default">
-    <div id="collapseOne" class="panel-collapse collapse in">
+  <div id="collapseOne" class="panel-collapse collapse in">
+    <div class="panel panel-default">
       <div class="panel-body">
         Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
       </div>
     </div>
   </div>
-  <div class="panel panel-default">
-    <div id="collapseTwo" class="panel-collapse collapse">
+
+  <div id="collapseTwo" class="panel-collapse collapse">
+    <div class="panel panel-default">
       <div class="panel-body">
         Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable DVD.
       </div>
