379e82b5c3821e58ff612509cc6db5f08cf91ee9
Sean Girard <seangirard@yahoo.com>
2014-03-21 09:59:23 -0700

    request

--- assets/js/org/request.js
+++ assets/js/org/request.js
@@ -33,7 +33,7 @@ var org = {
 		$('body').on('show.bs.collapse', '#spl-form-request-panels', function() {
 			$('#spl-form-request-panel-choose').collapse('hide');
 			
-			var materialType = $('input[name = "spl-form[material]"]:checked' , '#spl-form-request-panel-choose').val()
+			var materialType = $('input:radio[name = "spl-form[material]"]:checked' , '#spl-form-request-panel-choose').val()
 
 			console.log(materialType);
 
