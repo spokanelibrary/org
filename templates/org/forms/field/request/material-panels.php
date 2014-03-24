e44e45c25a1ad87ae8896ab701ed7102749a697b
Sean Girard <seangirard@yahoo.com>
2014-03-24 10:05:34 -0700

    request

--- templates/org/forms/field/request/material-panels.php
+++ templates/org/forms/field/request/material-panels.php
@@ -209,15 +209,42 @@
           </p>
           <div id="spl-form-request-search" class="collapse">
             <div class="input-group">
-              <input type="text" class="form-control">
+              <input type="text" class="form-control" id="spl-form-search" name="spl-form[search]" title="Don't forget to enter a search term!">
               <span class="input-group-btn">
-                <button class="btn btn-success" type="button">
+                <button type="button" class="btn btn-success" id="spl-form-oclc-search">
                   <small class="glyphicon glyphicon-search hidden-xs"></small>
                   Search
                 </button>
               </span>
             </div><!-- /input-group -->
-            index, sort
+            <div class="row">
+              <div class="col-md-6">
+                <label for="spl-form-search-index" class="control-label">Search For:</label>
+                <select class="form-control" name="spl-form[search-index]" id="spl-form-search-index">
+                  <option value="kw" selected="">Keyword</option>
+                  <option value="ti">Title</option>
+                  <option value="au">Author</option>
+                  <option value="se">Series</option>
+                  <option value="su">Subject</option>
+                </select>
+              </div><!-- /.row -->
+              <div class="col-md-6">
+                <label for="spl-form-search-sort" class="control-label">Sort By:</label>
+                <select class="form-control" name="spl-form[search-sort]" id="spl-form-search-sort">
+                  <option value="relevance" selected="">Relevance</option>
+                  <option value="LibraryCount,,0">Library Count</option>
+                  <option value="Author">Author</option>
+                  <option value="Title">Title</option>
+                  <option value="Date,,0">Pub. Date (Newest First)</option>
+                  <option value="Date">Pub. Date (Oldest First)</option>
+                  <!--
+                  <option value="LibraryCount">Library Count (Fewest First)</option>
+                  <option value="Score">Score Ascending</option>
+                  <option value="Score,,0">Score Descending</option>
+                  -->
+                </select>
+              </div><!-- /.row -->
+            </div><!-- /.row -->
             <span class="help-block">
               We want to respond to your request as quickly and accurately as possible. 
               You can search for a keyword (or title, author, subject, etc.) in <a href="http://www.worldcat.org/" title="WorldCat">WorldCat</a>, the world's largest bibliographic database. 
