923643726ca4a048378dbf6a9fcfaedbe6a17f30
Sean Girard <seangirard@yahoo.com>
2014-03-24 15:27:34 -0700

    request

--- templates/org/forms/field/request/material-panels.php
+++ templates/org/forms/field/request/material-panels.php
@@ -252,7 +252,7 @@
                 If you find the title you are looking for press Select and we will automatically fill out the author, ISBN, and more.
                 If you do not find the title you are looking for, try an <a href="http://www.worldcat.org/advancedsearch" title="Advanced WorldCat Search">Advanced WorldCat Search</a>.
               </span>
-              <div id="#spl-form-request-holding"></div>
+              <div id="spl-form-request-holding"></div>
             </div>
           </div>
         </div>
