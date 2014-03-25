23febd0b1558223d0ce89e3437e5a9877ebad701
Sean Girard <seangirard@yahoo.com>
2014-03-25 14:55:06 -0700

    request

---
+++ templates/org/forms/field/request/material-panel-author.php
@@ -0,0 +1,7 @@
+<!-- author -->
+<div class="form-group">
+  <label for="spl-form-author" class="col-sm-4 col-md-3 control-label">Author or Artist</label>
+  <div class="col-sm-8 col-md-9">
+    <input type="text" class="form-control" id="spl-form-author" name="spl-form[author]">
+  </div>
+</div>
---
+++ templates/org/forms/field/request/material-panel-url.php
@@ -0,0 +1,14 @@
+<!-- url -->
+<div class="form-group">
+  <label for="spl-form-url" class="col-sm-4 col-md-3 control-label">Link on the Web</label>
+  <div class="col-sm-8 col-md-9">
+    <p class="form-control-static">
+      You can send us a link with detailed information about this title (for example, on Amazon).
+    </p>
+    <input type="text" class="form-control" id="spl-form-url" name="spl-form[url]">
+    <span class="help-block">
+      <b>Note:</b> The page you link to must contain enough information for us to clearly identify the item you are requesting. 
+      Otherwise we may not be able to process your request. 
+    </span>
+  </div>
+</div>
