ff8f9d0fad5e6e7e4f76b5866b2512e76a18b045
Sean Girard <seangirard@yahoo.com>
2014-03-25 09:38:35 -0700

    request

--- templates/org/forms/field/request-title.php
+++ templates/org/forms/field/request-title.php
@@ -50,8 +50,9 @@
             <?php 
             if ( !empty($bib) ) {
               echo 'CDA for bib# '.$bib;
-
+              echo '<pre>';
               print_r( SPL_Widget::curlPostProxy('http://api.spokanelibrary.org/v2/hzws/lookup?params[bib]=12345') );
+              echo '</pre>';
             } else {
               include 'request/title.php'; 
             }
