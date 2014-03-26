bf3315153c9c791d015de740c38020dcca5dd190
Sean Girard <seangirard@yahoo.com>
2014-03-26 08:47:08 -0700

    request

--- templates/org/forms/field/request/material-panel-microfilm.php
+++ templates/org/forms/field/request/material-panel-microfilm.php
@@ -1,19 +1,28 @@
-
-<!-- city/st -->
 <div class="row">
-  <div class="col-sm-6">
-    <label for="spl-form-news-city" class="control-label"><span class="text-danger">*</span> City of Publication</label>
-    <input type="text" class="form-control required" id="spl-form-news-city" name="spl-form[news][city]">
-  </div><!-- /.col -->
-  <div class="col-sm-6">
-    <label for="spl-form-news-state" class="control-label"><span class="text-danger">*</span> State of Publication</label>
-    <input type="text" class="form-control required" id="spl-form-news-state" name="spl-form[news][state]">
+	<div class="col-md-9 col-md-offset-3">
+
+		<div class="alert alert-info">
+			<h4>Important Notice</h4>
+			<p>
+				Microfilm or microfiche is only available to view at the <strong>Downtown Library</strong>. 
+			</p>
+		</div>
+
+		<!-- city/st -->
+		<div class="row">
+		  <div class="col-sm-6">
+		    <p>
+		  	  <label for="spl-form-news-city" class="control-label"><span class="text-danger">*</span> City of Publication</label>
+			    <input type="text" class="form-control required" id="spl-form-news-city" name="spl-form[news][city]">
+		  	</p>
+		  </div><!-- /.col -->
+		  <div class="col-sm-6">
+		    <p>
+		  	  <label for="spl-form-news-state" class="control-label"><span class="text-danger">*</span> State of Publication</label>
+			    <input type="text" class="form-control required" id="spl-form-news-state" name="spl-form[news][state]">
+		  	</p>
+		  </div><!-- /.col -->
+		</div><!-- /.row -->
+
   </div><!-- /.col -->
 </div><!-- /.row -->
-
-<div class="alert alert-info">
-	<h5>Important Notice</h5>
-	<p>
-		Microfilm or microfiche is only available to view at the <strong>Downtown Library</strong>. 
-	</p>
-</div>
