5619a96b73626dff3cbbf657d7dd2b8c5084746c
Sean Girard <seangirard@yahoo.com>
2014-03-26 08:52:30 -0700

    request

--- templates/org/forms/field/request/material-panel-microfilm.php
+++ templates/org/forms/field/request/material-panel-microfilm.php
@@ -7,7 +7,7 @@
 
 <!-- city -->
 <div class="form-group">
-  <label for="spl-form-news-city" class="control-label"><span class="text-danger">*</span> City of Publication</label>
+  <label for="spl-form-news-city" class="col-sm-4 col-md-3 control-label"><span class="text-danger">*</span> City of Publication</label>
   <div class="col-sm-8 col-md-9">
     <input type="text" class="form-control required" id="spl-form-news-city" name="spl-form[news][city]">
   </div>
@@ -15,7 +15,7 @@
 
 <!-- state -->
 <div class="form-group">
-  <label for="spl-form-news-state" class="control-label"><span class="text-danger">*</span> State of Publication</label>
+  <label for="spl-form-news-state" class="col-sm-4 col-md-3 control-label"><span class="text-danger">*</span> State of Publication</label>
   <div class="col-sm-8 col-md-9">
     <input type="text" class="form-control required" id="spl-form-news-state" name="spl-form[news][state]">
   </div>
