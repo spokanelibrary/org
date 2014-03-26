5ca790d9a6758bbd9ab4efb0f112bdbaf5e3060b
Sean Girard <seangirard@yahoo.com>
2014-03-26 15:17:07 -0700

    request

--- templates/org/forms/field/request/material-panel-message-ill.php
+++ templates/org/forms/field/request/material-panel-message-ill.php
@@ -3,7 +3,7 @@
   <label for="spl-form-message" class="col-sm-4 col-md-3 control-label">Comments or Questions</label>
   <div class="col-sm-8 col-md-9">
     <p class="form-control-static">
-      Let us know if you have additional information that might help us locate this item.
+      Please let us know if you have additional information that might help us locate this item.
     </p>
     <textarea rows="4" class="form-control required" id="spl-form-message" name="spl-form[message]"></textarea>
   </div>
--- templates/org/forms/field/request/material-panel-pub-ill.php
+++ templates/org/forms/field/request/material-panel-pub-ill.php
@@ -1,11 +1,11 @@
 <!-- publisher/pubdate-->
 <div class="row">
+	<div class="col-sm-6">
+    <label for="spl-form-pubdate" class="control-label"><span class="text-danger">*</span> Date</label>
+    <input type="text" class="required form-control" id="spl-form-pubdate" name="spl-form[pubdate]">
+  </div><!-- /.col -->
   <div class="col-sm-6">
     <label for="spl-form-publisher" class="control-label">Publisher</label>
     <input type="text" class="form-control" id="spl-form-publisher" name="spl-form[publisher]">
-  </div><!-- /.col -->
-  <div class="col-sm-6">
-    <label for="spl-form-pubdate" class="control-label"><span class="text-danger">*</span> Date</label>
-    <input type="text" class="required form-control" id="spl-form-pubdate" name="spl-form[pubdate]">
   </div><!-- /.col -->
 </div><!-- /.row -->
