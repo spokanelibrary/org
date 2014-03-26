85c46c80b2a8554a96c2c715f6f65adb80fb9309
Sean Girard <seangirard@yahoo.com>
2014-03-26 10:38:22 -0700

    request

--- templates/org/forms/field/request/material-panel-periodical.php
+++ templates/org/forms/field/request/material-panel-periodical.php
@@ -46,10 +46,8 @@
 
 <div class="col-sm-8 col-md-9 col-sm-offset-4 col-md-offset-3">
   <div class="row">
-    <div class="col-sm-3">
-      <div class="form-group">
+    <div class="col-sm-4">
         <input type="text" class="required form-control" id="spl-form-journal-pages" name="spl-form[journal][pages]">
-      </div><!-- /.form-group -->
     </div><!-- /.col -->
   </div>
 </div>
