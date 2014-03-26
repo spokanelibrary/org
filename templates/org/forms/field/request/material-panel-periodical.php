e52f7e8863e9a0358991debff1edcaae907bbb46
Sean Girard <seangirard@yahoo.com>
2014-03-26 10:50:28 -0700

    request

--- templates/org/forms/field/request/material-panel-periodical.php
+++ templates/org/forms/field/request/material-panel-periodical.php
@@ -46,24 +46,18 @@
   <!-- pages -->  
   <div class="col-sm-4">
     <div class="form-group">
-      <label for="spl-form-journal-pages" class="">Page Numbers</label>
-      <br>
       <input type="text" class="required form-control" id="spl-form-journal-pages" name="spl-form[journal][pages]">
     </div><!-- /.form-group -->
   </div><!-- /.col -->
   <!-- volume -->
   <div class="col-sm-4">
     <div class="form-group">
-      <label for="spl-form-journal-volume" class="">Volume</label>
-      <br>
       <input type="text" class="required form-control" id="spl-form-journal-volume" name="spl-form[journal][volume]">
     </div><!-- /.form-group -->
   </div><!-- /.col -->
   <!-- issue -->
   <div class="col-sm-4">
     <div class="form-group">
-      <label for="spl-form-journal-number" class="">Issue #</label>
-      <br>
       <input type="text" class="required form-control" id="spl-form-journal-number" name="spl-form[journal][number]">
     </div><!-- /.form-group -->
   </div><!-- /.col -->
