f16f6b83fe4ea9507237496d44bf041f5c49c95b
Sean Girard <seangirard@yahoo.com>
2014-03-26 13:13:47 -0700

    request

--- templates/org/forms/field/request/material-panels.php
+++ templates/org/forms/field/request/material-panels.php
@@ -180,7 +180,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h4 class="modal-title" id="spl-ill-policy-title">Interlibary Loan Policy</h4>
+        <h3 class="modal-title" id="spl-ill-policy-title">Interlibary Loan Policy</h3>
       </div>
       <div class="modal-body">
         <?php include 'material-request-ill-policy.php'; ?>
--- templates/org/forms/field/request/material-request-ill-policy.php
+++ templates/org/forms/field/request/material-request-ill-policy.php
@@ -16,130 +16,71 @@
   Interlibrary Loan is the process by which Spokane Public Library requests materials from, or supplies materials to, libraries outside of our library system.  
 </p>
 
-
 <h4>Who Can Borrow?</h4>
 <p>
-Library card holders who reside within the city limits of Spokane and people who own businesses within the city of Spokane, if they have a business library card. In order to process a request <strong>ALL</strong> of the following must apply:
+Library card holders who reside within the city limits of Spokane and people who own businesses within the city of Spokane, if they have a business library card. In order to process a request <b>ALL</b> of the following must apply:
 </p>
 <ul>
-  <li>Card must be <strong>verified</strong> (proof of address and identity must be shown).</li>
-  <li>Fines must be under <strong>$10.00</strong>.</li>
-  <li>Card must be <strong>current</strong> (no expired cards, please).</li>
+  <li>Card must be <b>verified</b> (proof of address and identity must be shown).</li>
+  <li>Fines must be under <b>$10.00</b>.</li>
+  <li>Card must be <b>current</b> (no expired cards, please).</li>
 </ul>
 <p>
   Non-resident cardholders should contact their home library system for Interlibrary Loan Service.
 </p>
-    
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
 
-
-<ul class="nav nav-list">
-
-  <li class="nav-header">Who Can Borrow?</li>
-  <li>
-    <p>
-    Library card holders who reside within the city limits of Spokane and people who own businesses within the city of Spokane, if they have a business library card. In order to process a request <strong>ALL</strong> of the following must apply:
-    </p>
-    <ul>
-      <li>Card must be <strong>verified</strong> (proof of address and identity must be shown).</li>
-      <li>Fines must be under <strong>$10.00</strong>.</li>
-      <li>Card must be <strong>current</strong> (no expired cards, please).</li>
-    </ul>
-  </li>
-  <li>
-    <p>
-      Non-resident cardholders should contact their home library system for Interlibrary Loan Service.
-    </p>
-  </li>
-  <li class="divider"></li>
-
-  <li class="nav-header">What Can I Borrow?</li>
-  <li>
-    <p>
-      Customers may only request the same title <strong>twice</strong> per calendar year. 
-    </p>
-  </li>
-  <li>
-    <ul>
-      <li>Books (including Audiobooks on cassette or CD, but NOT downloadable) published prior to the last 6 months.</li>
-      <li>Photocopies of journal/magazine articles or genealogical information.</li>
-      <li>Microfilm or fiche of newspapers.</li>
-      <li>Theses or dissertations.</li>
-    </ul>
-  </li>
-  <li>
-    <p>
-      The owning library will make the decision as to whether or not they will lend the material.
-    </p>
-  </li>
-  <li class="divider"></li>
-
-  <li class="nav-header">What Am I Unable to Request?</li>
-  <li>
-    <ul>
-      <li>Videos, DVDs, music cassettes and CDs, downloadable Audiobooks, downloadable eBooks and CD-Roms, genealogy books (though we can request photocopies from genealogy items).</li>
-      <li>Books already owned by Spokane Public Library (including items located within our Reference, Northwest Room, and Genealogy collections).</li>
-    </ul>
-  </li>
-  <li class="divider"></li>
-
-  <li class="nav-header">How Many Items Can I Request?</li>
-  <li>
-    <ul>
-      <li>Only <strong>four items</strong> can be in process at a time. This includes items that could only be used in the library and microfilm.  For each item that has been returned another item can be requested.</li>
-      <li>Microfilm requests must be for a reasonable date range in order to be processed.</li>
-      <li>Requests must be submitted on Spokane Public Library forms. No photocopies of the form will be accepted.</li>
-    </ul>
-  </li>
-  <li class="divider"></li>
-
-  <li class="nav-header">How Long Will It Take?</li>
-  <li>
-    <p>
-      Interlibrary Loan items are borrowed from across the United States and Canada. 
-    </p>
-    <p>
-      It can take <strong>4&ndash;6 weeks</strong> to receive an item.
-    </p>
-  </li>
-  <li class="divider"></li>
-
-  <li class="nav-header">How Do I Find Out When the Item Arrives?</li>
-  <li>
-    <ul>
-      <li>When the item arrives a hold will be placed on the item and a <strong>hold notice</strong> (mail, e-mail or telephone) <strong>will be generated as soon as the item reaches the branch</strong> specified as the pick-up location.</li>
-      <li>Since only 20 items can be "On Hold" at any given time, please reserve space on your hold list for interlibrary loan items.</li>
-      <li>Interlibrary loan materials will be held <strong>behind the circulation desk</strong>, not on the regular hold shelf. </li>
-      <li>You will have <strong>five days</strong> to pick up your items before they will be sent back to the lending library.</li>
-    </ul>
-  </li>
-  <li class="divider"></li>
-
-  <li class="nav-header">How Long Can I Keep an Interlibrary Loan?</li>
-  <li>
-    <p>
-      Interlibrary loan materials will check out for a <strong>maximum of three weeks</strong> from the day you pick up the book. 
-    </p>
-    <p>
-      Because Spokane Public Library does not own items borrowed using the Interlibrary Loan system,  <strong>renewals are not allowed</strong>. 
-    </p>
-  </li>
-  <li class="divider"></li>
-
-  <li class="nav-header">Will I Find Out If You Cannot Locate an Item?</li>
-  <li>
-    <p>
-      The Interlibrary Loan department will let you know <strong>by placing a block on the library card used to request the item</strong> if we are unable to borrow the item you requested.
-    </p>
-  </li>
-  <li class="divider"></li>
-
+<h4>What Can I Borrow?</h4>
+<p>
+  Customers may only request the same title <b>twice</b> per calendar year. 
+</p>
+<ul>
+  <li>Books (including Audiobooks on cassette or CD, but NOT downloadable) published prior to the last 6 months.</li>
+  <li>Photocopies of journal/magazine articles or genealogical information.</li>
+  <li>Microfilm or fiche of newspapers.</li>
+  <li>Theses or dissertations.</li>
 </ul>
+<p>
+  The owning library will make the decision as to whether or not they will lend the material.
+</p>
+
+<h4>What Am I Unable to Request?</h4>
+<ul>
+  <li>Videos, DVDs, music cassettes and CDs, downloadable Audiobooks, downloadable eBooks and CD-Roms, genealogy books (though we can request photocopies from genealogy items).</li>
+  <li>Books already owned by Spokane Public Library (including items located within our Reference, Northwest Room, and Genealogy collections).</li>
+</ul>
+
+<h4>How Many Items Can I Request?</h4>
+<ul>
+  <li>Only <b>four items</b> can be in process at a time. This includes items that could only be used in the library and microfilm.  For each item that has been returned another item can be requested.</li>
+  <li>Microfilm requests must be for a reasonable date range in order to be processed.</li>
+  <li>Requests must be submitted on Spokane Public Library forms. No photocopies of the form will be accepted.</li>
+</ul>
+
+<h4>How Long Will It Take?</h4>
+<p>
+  Interlibrary Loan items are borrowed from across the United States and Canada. 
+</p>
+<p>
+  It can take <b>4&ndash;6 weeks</b> to receive an item.
+</p>
+
+<h4>How Do I Find Out When the Item Arrives?</h4>
+<ul>
+  <li>When the item arrives a hold will be placed on the item and a <b>hold notice</b> (mail, e-mail or telephone) <b>will be generated as soon as the item reaches the branch</b> specified as the pick-up location.</li>
+  <li>Since only 20 items can be "On Hold" at any given time, please reserve space on your hold list for interlibrary loan items.</li>
+  <li>Interlibrary loan materials will be held <b>behind the circulation desk</b>, not on the regular hold shelf. </li>
+  <li>You will have <b>five days</b> to pick up your items before they will be sent back to the lending library.</li>
+</ul>
+
+<h4>How Long Can I Keep an Interlibrary Loan?</h4>
+<p>
+  Interlibrary loan materials will check out for a <b>maximum of three weeks</b> from the day you pick up the book. 
+</p>
+<p>
+  Because Spokane Public Library does not own items borrowed using the Interlibrary Loan system,  <b>renewals are not allowed</b>. 
+</p>
+
+<h4>Will I Find Out If You Cannot Locate an Item?</h4>
+<p>
+  The Interlibrary Loan department will let you know <b>by placing a block on the library card used to request the item</b> if we are unable to borrow the item you requested.
+</p>
