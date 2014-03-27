39ceb1c9a41a77ca062e887596ec18a54566e2ed
Sean Girard <seangirard@yahoo.com>
2014-03-27 08:10:41 -0700

    request

--- templates/org/forms/field/contact-us-response.php
+++ templates/org/forms/field/contact-us-response.php
@@ -1,74 +1,45 @@
-<div class="alert alert-success">
-  <h3>Thank You!</h3>
-</div>
+<div class="panel panel-success">
+  <div class="panel-heading">
+    <h3>Thank You!</h3>
+  </div><!-- /.panel-heading -->
 
-<h4>We received your message</h4>
+  <div class="panel-body">
+  <h4>We received your message</h4>
 
-<dl class="dl-horizontal">
+  <dl class="dl-horizontal">
 
-  <dt>Your Name</dt>
-  <dd>
-    <p>
-      {{name}}
-    </p>
-  </dd>
+    <dt>Your Name</dt>
+    <dd>
+      <p>
+        {{name}}
+      </p>
+    </dd>
 
-  <dt>Your Barcode</dt>
-  <dd>
-    <p>
-      {{barcode}}
-    </p>
-  </dd>
+    <dt>Your Barcode</dt>
+    <dd>
+      <p>
+        {{barcode}}
+      </p>
+    </dd>
 
-  <dt>Your Email</dt>
-  <dd>
-    <p>
-      {{email}}
-    </p>
-  </dd>
+    <dt>Your Email</dt>
+    <dd>
+      <p>
+        {{email}}
+      </p>
+    </dd>
 
-  <dt>Your Message</dt>
-  <dd>
-    <p>
-      {{message}}
-    </p>
-  </dd>
+    <dt>Your Message</dt>
+    <dd>
+      <p>
+        {{message}}
+      </p>
+    </dd>
 
-</dl>
+  </dl>
 
-<script id="spl-form-contact-response-tmpl" type="text/x-handlebars-template">
-
-
-
-{{#with request}}
-
-  <div class="row-fluid">
-    <dl class="dl-horizontal">
-
-    {{#if name}}
-      
-    {{/if}}
-
-    {{#if barcode}}
-      
-    {{/if}}
-
-    {{#if email}}
-      
-    {{/if}}
-
-    {{#if message}}
-      
-    {{/if}}
-
-    </dl>
-
-
-  </div><!-- /.row-fluid -->
-
-{{/with}}
-</script>
-
+  </div><!-- /.panel-body -->
+</div><!-- /.panel -->
 <?php
 /*
 echo '<pre>';
