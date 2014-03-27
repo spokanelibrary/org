bc2e7ff4f07e1e142c39b5cac45c4b05a8a8f4da
Sean Girard <seangirard@yahoo.com>
2014-03-27 08:01:35 -0700

    contact

--- templates/org/forms/form-contact-us.php
+++ templates/org/forms/form-contact-us.php
@@ -2,7 +2,8 @@
 if ( !empty($GLOBALS['crass_response']->request) ) {
   include 'field/contact-us-response.php';
   } else {
-  include 'field/contact-us.php';
+  include 'field/contact-us-response.php';
+  //include 'field/contact-us.php';
 }
 ?>
 
