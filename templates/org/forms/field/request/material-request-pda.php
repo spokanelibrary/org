9bbf3257a52d239a4a5f3e45033140c912c49e39
Sean Girard <seangirard@yahoo.com>
2014-03-28 11:06:02 -0700

    request

--- templates/org/forms/field/request/material-request-pda.php
+++ templates/org/forms/field/request/material-request-pda.php
@@ -44,17 +44,20 @@ if ( is_array($title->titleInfo->ISBN) ) {
         </div><!-- /.col -->
       </div><!-- /.row -->
 
-      <div class="form-group">
-        <label for="spl-form-message" class="col-sm-4 col-md-3 control-label">Comments or Questions</label>
-        <div class="col-sm-8 col-md-9">
-          <p class="form-control-static">
-            How did you hear about this title?
-          </p>
-          <textarea rows="4" class="form-control required" id="spl-form-message" name="spl-form[message]"></textarea>
-        </div>
-      </div><!-- /.form-group -->
+      <div class="row">
+        <div class="col-sm-9">
+          <div class="form-group">
+            <label for="spl-form-message" class="col-sm-4 col-md-3 control-label">Comments or Questions</label>
+            <div class="col-sm-8 col-md-9">
+              <p class="form-control-static">
+                How did you hear about this title?
+              </p>
+              <textarea rows="4" class="form-control required" id="spl-form-message" name="spl-form[message]"></textarea>
+            </div>
+          </div><!-- /.form-group -->
+        </div><!-- /.col -->
+      </div><!-- /.row -->
 
-      
     </div><!-- /.panel-body -->
     <div class="panel-footer">
       <div class="row">
