85208b3530f96ede1e6ee524944797be454d3847
Sean Girard <seangirard@yahoo.com>
2014-03-28 09:24:06 -0700

    request

--- templates/org/forms/field/request/material-request-pda.php
+++ templates/org/forms/field/request/material-request-pda.php
@@ -1,4 +1,12 @@
-<h3>Patron Driven Acq</h3>
+<fieldset>
+  <legend class="text-muted">
+    <i class="glyphicon glyphicon-check"></i>
+    Finalize this request:
+  </legend>
+
+  <h3>Patron Driven Acq</h3>
+
+</fieldset>
 
 <?php
 echo '<pre>';
