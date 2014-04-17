2e5cc2dee55106f17d69ae33a8a23416547ed599
Sean Girard <seangirard@yahoo.com>
2014-04-17 14:12:38 -0700

    subpages

--- template-subpages.php
+++ template-subpages.php
@@ -14,14 +14,14 @@ Template Name: Scrollspy (Child Pages)
 		<div class="row visible-xs">
 			<div class="col-xs-12">
 				<div class="panel">
-					<?php echo do_shortcode('[spl_subpages dropdown]'); ?>
+					<?php echo do_shortcode('[spl_tutorial dropdown]'); ?>
 				</div>
 			</div>
 		</div>
 		
 		<div class="row">
 			<div class="col-xs-12">
-				<?php echo do_shortcode('[spl_subpages content]'); ?>
+				<?php echo do_shortcode('[spl_tutorial content]'); ?>
 			</div>
 		</div>
 	</div>
@@ -30,7 +30,7 @@ Template Name: Scrollspy (Child Pages)
 		<div class="scrollspy-nav" data-spy="affix" style="z-index:1">
 			<div class="panel panel-warning panel-callout panel-callout-warning">
 					<h4 class="text-warning">Choose a Topic <small>&rarr;</small></h4>
-					<?php echo do_shortcode('[spl_subpages nav]'); ?>
+					<?php echo do_shortcode('[spl_tutorial nav]'); ?>
 					<hr />
 					<div class="text-right">
 						<p>
