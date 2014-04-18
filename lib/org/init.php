add3325d2fadc0aafda4b3e6ec6dc42818f39560
Sean Girard <seangirard@yahoo.com>
2014-04-18 12:05:17 -0700

    format

--- lib/org/init.php
+++ lib/org/init.php
@@ -113,7 +113,7 @@ function spl_resources( $params ) {
                       </h4>
                       </div>
                       <div class="panel-body">
-                      '. apply_filters('the_content',do_shortcode( get_the_content() )).''.PHP_EOL.'
+                      '. get_the_content().''.PHP_EOL.'
                       </div>
                       <div class="panel-footer clearfix">
                       <a class="btn btn-sm btn-primary pull-right" href="#top">Top <small class="glyphicon glyphicon-arrow-up"></small></a>
