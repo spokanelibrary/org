30b124415cae4801d28a3fdfe2ac998c9eb00ed6
Sean Girard <seangirard@yahoo.com>
2014-04-18 12:42:12 -0700

    kiosk

--- templates/org/header-top-navbar.php
+++ templates/org/header-top-navbar.php
@@ -1,3 +1,4 @@
+<?php if ( !isset($_REQUEST['kiosk']) ) : ?>
 <script>/*(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
@@ -97,7 +98,7 @@
   </div>
 </header><!-- /.navbar -->
 
-<?php if ( !isset($_REQUEST['kiosk']) ) : ?>
+
 <header class="navbar-primary banner navbar navbar-inverse navbar-static-top" role="banner">
   <div class="container">
     <div class="navbar-header">
