2862ea74dff333df30b416ea25fdab0d27af2e0c
Sean Girard <seangirard@yahoo.com>
2014-04-21 15:08:25 -0700

    kiosk

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8636f86a9357a5a5dc8daa8d69f53706');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '120a1751112cd25f3a2b4912551c68be');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
