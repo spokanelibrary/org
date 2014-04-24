148fc6b982436c6f9e909bb12ba15ba3386754d6
Sean Girard <seangirard@yahoo.com>
2014-04-24 15:21:42 -0700

    indent

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '120a1751112cd25f3a2b4912551c68be');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '14366fc74a4cea38a79b820c6ad435e9');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
