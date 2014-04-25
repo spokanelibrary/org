7774ed30a8f0ab5e20a7602023cf3d5db4b60752
Sean Girard <seangirard@yahoo.com>
2014-04-25 08:39:55 -0700

    navbar

--- templates/org/header-top-navbar.php
+++ templates/org/header-top-navbar.php
@@ -99,7 +99,7 @@
 
 
 <header class="navbar-primary banner navbar navbar-inverse navbar-static-top" role="banner">
-  <div class="container" style="background:red; padding-left:0; padding-right:0;">
+  <div class="container" style="background:red;">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
         <span class="sr-only">Toggle navigation</span>
