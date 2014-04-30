8df247e55bdc1ca60c511f2c43e36384858bede6
Sean Girard <seangirard@yahoo.com>
2014-04-30 15:33:00 -0700

    dynatable

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5f50b01114b59c31ff39d33a5dc65da6');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '2fc75d1a09770db8b05192c9e1be454a');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
