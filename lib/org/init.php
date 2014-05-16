99afb801704f7eade876990106a77fa435a8dbb7
Sean Girard <seangirard@yahoo.com>
2014-05-16 14:36:16 -0700

    img

--- lib/org/init.php
+++ lib/org/init.php
@@ -105,6 +105,7 @@ function spl_resources( $params ) {
                       '</div>'.PHP_EOL.
                       '</div>'.PHP_EOL;  
           */
+          $image_url = null;
           $image_url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
           if ( !empty($image_url) ) {
             $img = '<a href="'.$link['url'].'" class="thumbnail">
