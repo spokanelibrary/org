fb5870ec22aa7f8a396b49ffb40827927d70c83a
Sean Girard <seangirard@yahoo.com>
2014-05-16 14:06:13 -0700

    business

---
+++ templates/org/pages/business.php
@@ -0,0 +1,511 @@
+<!-- BUSINESS SOURCE -->
+<div class="panel panel-primary">
+	<div class="panel-body">
+		<p>
+			<img class="img-responsive center-block" src="./assets/img/logos/bscomplete.png">
+		</p>
+		<p>
+			Company profiles, industry reports, case studies, access to nearly 2,000 peer-reviewed business journals.
+		</p>
+		<p>
+			<button type="button" class="btn btn-block btn-warning"
+					data-toggle="collapse"
+					data-target="#about-bussource">
+					<b>Read more</b> <span class="hidden-xs">about Business Source Complete</span>
+					<span class="caret"> </span>
+			</button>
+		</p>
+
+		<div class="collapse" id="about-bussource">
+			<p>
+				A scholarly business database providing a collection of bibliographic and full text content in all disciplines of business, including marketing, management, MIS, POM, accounting, finance and economics.
+			</p>
+			<p>
+				Updated monthly, <em>Business Source Complete</em> offers comprehensive full-text coverage plus indexing and abstracts for the most important scholarly business journals, dating back as far as 1886.
+			</p>
+			<p>
+				Additional full text, non-journal content includes financial data, books, monographs, major reference works, book digests, conference proceedings, case studies, investment research reports, industry reports, market research reports, country reports, company profiles, and SWOT analyses.
+			</p>
+		</div>
+
+		<h5 class="text-center text-muted uppercase">
+			Access this resource:
+		</h5>
+		
+		<div class="row">
+			<div class="col-md-6">
+				<p class="text-muted">
+					<a class="btn btn-block btn-info" href="http://www.scld.org/scripts/ntlinktrack.exe?http://proxy.scld.org/login?url=http://search.ebscohost.com/login.aspx?authtype=ip,uid&profile=bth" data-lib-system="scld" data-lib-title="SCLD - Business Source Complete">
+						<img src="/assets/img/SCLD-favicon.png" class="img-rounded favicon-scld">
+						Spokane County Library District
+						<small class="glyphicon glyphicon-arrow-right hidden-xs"> </small>
+					</a>
+				</p>
+			</div><!-- /.col -->
+			<div class="col-md-6">
+				<p class="text-muted">
+					<a class="btn btn-block btn-success" href="http://search.ebscohost.com/login.aspx?authtype=ip,cpid&custid=s8427805&profile=ehost&defaultdb=bth" data-lib-system="spl" data-lib-title="SPL - Business Source Complete">
+						<img src="/assets/img/SPL-favicon.png" class="img-rounded favicon-spl">
+						Spokane Public Library
+						<small class="glyphicon glyphicon-arrow-right hidden-xs"> </small>
+					</a>	
+				</p>
+			</div><!-- /.col -->
+		</div><!-- /.row -->
+
+	</div><!-- /.panel-body -->
+</div><!-- /.panel -->
+
+<!-- DEMOGRAPHICS NOW -->
+<div class="panel panel-primary">
+	<div class="panel-body">
+		<p>
+			<img class="img-responsive center-block" src="./assets/img/logos/demographicsnow.png">
+		</p>
+		<p>
+			Demographics, consumer expenditures, interactive maps, and more.
+		</p>
+		<p>
+			<button type="button" class="btn btn-block btn-warning"
+					data-toggle="collapse"
+					data-target="#about-demnow">
+					<b>Read more</b> <span class="hidden-xs">about Demographics Now</span>
+					<span class="caret"> </span>
+			</button>
+		</p>
+
+		<div class="collapse" id="about-demnow">
+			<p>
+				“Who are my customers?” 
+				“Where do they live?” 
+				“What do they do?” 
+				And perhaps most importantly, “Who are my competitors?”
+			</p>
+			<p>
+				Access demographic information, such as:
+				<b>
+				Population,
+				Income,
+				Housing,
+				Age,
+				Education,
+				Businesses,
+				Retail sales,
+				Consumer expenditures.
+				</b>
+			</p>
+
+			<ul>
+				<li>Identify household buying patterns</li>
+				<li>Create and analyze trade areas</li>
+				<li>Evaluate market penetration</li>
+				<li>Identify business locations</li>
+				<li>Target messaging, media buys and direct marketing campaigns</li>
+				<li>Find new customers — and reach them more effectively </li>
+				<li>Reveal untapped markets</li>
+				<li>Select the right merchandise mix</li>
+				<li>Expand into appropriate markets</li>
+			</ul>
+			<p>
+				<em>Demographics Now</em> includes the Dun &amp; Bradstreet business database. 
+			</p>
+		</div>
+
+		<h5 class="text-center text-muted uppercase">
+			Access this resource:
+		</h5>
+		
+		<div class="row">
+			<div class="col-md-6">
+				<p class="text-muted">
+					<a class="btn btn-block btn-info" href="http://www.scld.org/scripts/ntlinktrack.exe?http://proxy.scld.org/login?url=http://infotrac.galegroup.com/itweb/spok67112?db=DNOW" data-lib-system="scld" data-lib-title="SCLD - Demographics Now">
+						<img src="/assets/img/SCLD-favicon.png" class="img-rounded favicon-scld">
+						Spokane County Library District 
+						<small class="glyphicon glyphicon-arrow-right hidden-xs"> </small>
+					</a>
+				</p>
+			</div><!-- /.col -->
+			<div class="col-md-6">
+				<p class="text-muted">
+					<a class="btn btn-block btn-success" href="http://www2.spokanelibrary.org/rpa/webauth.exe?rs=demonow" data-lib-system="spl" data-lib-title="SPL - Demographics Now">
+						<img src="/assets/img/SPL-favicon.png" class="img-rounded favicon-spl">
+						Spokane Public Library
+						<small class="glyphicon glyphicon-arrow-right hidden-xs"> </small>
+					</a>	
+				</p>
+			</div><!-- /.col -->
+		</div><!-- /.row -->
+
+	</div><!-- /.panel-body -->
+</div><!-- /.panel -->
+
+<!-- IBISWORLD -->
+<div class="panel panel-primary">
+	<div class="panel-body">
+		<p>
+			<img class="img-responsive center-block" src="./assets/img/logos/ibisworld.jpg">
+		</p>
+		<p>
+			Industry research reports and risk ratings on every U.S. industry.
+		</p>
+		<p>
+			<button type="button" class="btn btn-block btn-warning"
+					data-toggle="collapse"
+					data-target="#about-ibisworld">
+					<b>Read more</b> <span class="hidden-xs">about IBISWorld</span>
+					<span class="caret"> </span>
+			</button>
+		</p>
+
+		<div class="collapse" id="about-ibisworld">
+			<p>
+				<em>IBISWorld</em> employs a team of dedicated expert analysts to research economic, demographic and government data to provide valuable insight into America’s 700+ industries, so you can make better business decisions, faster.
+			</p>
+			<p>
+				Each report consists of 30 to 40 pages of key statistics and analysis on market characteristics, operating conditions, current and forecast performance, major industry participants and more. 
+			</p>
+			<p>
+				Whether your company requires a better understanding of market conditions and forecasts; whether you need a clearer picture of a supply chain, or whether you need to keep abreast of competitor activity in your industry, IBISWorld’s comprehensive reports will keep you informed.
+			</p>
+		</div>
+
+		<h5 class="text-center text-muted uppercase">
+			Access this resource:
+		</h5>
+		<p class="text-center text-danger">
+			<small class="glyphicon glyphicon-exclamation-sign text-danger"> </small>
+			<b>IBIS</b><em>World</em> is only available <b>on site</b> at the Downtown Library.
+		</p>
+		<p>
+			Everyone is welcome to use <b>IBIS</b><em>World</em> at the Downtown Library.
+			You do not need a library card.
+			Just bring your computer (or use one of ours).
+		</p>
+		<p>
+			<a class="btn btn-block btn-success" href="http://www.ibisworld.com/" data-lib-system="spl" data-lib-title="SPL - IBIS World">
+				<img src="/assets/img/SPL-favicon.png" class="img-rounded favicon-spl">
+				Access <span class="hidden-xs">from the Downtown Library</span> now 
+				<small class="glyphicon glyphicon-arrow-right hidden-xs"> </small>
+			</a>	
+		</p>
+	</div><!-- /.panel-body -->
+</div><!-- /.panel -->
+
+<!-- GALE COURSES -->
+<div class="panel panel-primary">
+	<div class="panel-body">
+		<p>
+			<img class="img-responsive center-block" src="./assets/img/logos/galecourses.png">
+		</p>
+		<p>
+			Hundreds of instructor-led online courses.
+		</p>
+		<p>
+			<button type="button" class="btn btn-block btn-warning"
+					data-toggle="collapse"
+					data-target="#about-learn4life">
+					<b>Read more</b> <span class="hidden-xs">about Gale Courses</span>
+					<span class="caret"> </span>
+			</button>
+		</p>
+
+		<div class="collapse" id="about-learn4life">
+			<p>
+				<em>Gale Courses</em> offer a wide range of highly interactive courses taught entirely online by college instructors and experts in the field, ensuring a high-quality, high-impact educational experience.
+			</p>
+			<ul>
+				<li>Courses to support business professionals, such as <b>business law, human resources, management, manufacturing, and marketing</b></li>
+				<li><b>Computer and technology</b> courses that suit skill levels from beginner to advanced</li>
+			</ul>
+			<p>
+				Courses run for six weeks and new sessions begin every month.
+			</p>
+		</div>
+
+		<h5 class="text-center text-muted uppercase">
+			Access this resource:
+		</h5>
+		
+		<div class="row">
+			<div class="col-md-6">
+				<p class="text-muted">
+					<a class="btn btn-block btn-info" href="http://www.scld.org/scripts/ntlinktrack.exe?http://www.ed2go.com/l-spok67112/" data-lib-system="scld" data-lib-title="SCLD - Gale Courses">
+						<img src="/assets/img/SCLD-favicon.png" class="img-rounded favicon-scld">
+						Spokane County Library District 
+						<small class="glyphicon glyphicon-arrow-right hidden-xs"> </small>
+					</a>
+				</p>
+			</div><!-- /.col -->
+			<div class="col-md-6">
+				<p class="text-muted">
+					<a class="btn btn-block btn-success" href="http://www.ed2go.com/l-splbt_main" data-lib-system="spl" data-lib-title="SPL - Gale Courses">
+						<img src="/assets/img/SPL-favicon.png" class="img-rounded favicon-spl">
+						Spokane Public Library
+						<small class="glyphicon glyphicon-arrow-right hidden-xs"> </small>
+					</a>	
+				</p>
+			</div><!-- /.col -->
+		</div><!-- /.row -->
+
+	</div><!-- /.panel-body -->
+</div><!-- /.panel -->
+
+<!-- MS IT ACADEMY -->
+<div class="panel panel-primary">
+	<div class="panel-body">
+		<p>
+			<img class="img-responsive center-block" src="./assets/img/logos/itacademy.png">
+		</p>
+		<p>
+			Online courseware for Microsoft technology training. 
+		</p>
+		<p>
+			<button type="button" class="btn btn-block btn-warning"
+					data-toggle="collapse"
+					data-target="#about-itacademy">
+					<b>Read more</b> <span class="hidden-xs">about IT Academy</span>
+					<span class="caret"> </span>
+			</button>
+		</p>
+
+		<div class="collapse" id="about-itacademy">
+			<p>
+				Courses cover a variety of Microsoft products and skill levels – ranging from beginner users to advanced technical courses that qualify for Microsoft Certification testing.
+			</p>
+			<p>
+				Courses include:
+			</p>
+			
+			<ul>
+
+        <li>Windows</li>
+        <li>Microsoft Dynamics</li>
+
+        <li>Office</li>
+        <li>Office 365</li>
+
+        <li>SQL Server</li>
+
+        <li>Windows Server</li>
+        <li>Exchange Server</li>
+        <li>Lync Server</li>
+        <li>Sharepoint</li>
+        <li>Virtualization</li>
+        <li>System Center</li>
+
+        <li>Visual Studio</li>
+        <li>Windows Azure</li>
+        <li>Windows Phone</li>
+
+      </ul>
+
+			<p>
+				Provided freely by Microsoft and the Washington State Library.
+			</p>
+		</div>
+
+		<h5 class="text-center text-muted uppercase">
+			Access this resource:
+		</h5>
+		
+		<div class="row">
+			<div class="col-md-6">
+				<p class="text-muted">
+					<a class="btn btn-block btn-info" href="http://www.scld.org/scripts/ntlinktrack.exe?http://www.scld.org/microsoft-it-academy/" data-lib-system="scld" data-lib-title="SCLD - IT Academy">
+						<img src="/assets/img/SCLD-favicon.png" class="img-rounded favicon-scld">
+						Spokane County Library District 
+						<small class="glyphicon glyphicon-arrow-right hidden-xs"> </small>
+					</a>
+				</p>
+			</div><!-- /.col -->
+			<div class="col-md-6">
+				<p class="text-muted">
+					<a class="btn btn-block btn-success" href="http://connect.spokanelibrary.org/it-academy/" data-lib-system="spl" data-lib-title="SPL - IT Academy">
+						<img src="/assets/img/SPL-favicon.png" class="img-rounded favicon-spl">
+						Spokane Public Library
+						<small class="glyphicon glyphicon-arrow-right hidden-xs"> </small>
+					</a>	
+				</p>
+			</div><!-- /.col -->
+		</div><!-- /.row -->
+
+	</div><!-- /.panel-body -->
+</div><!-- /.panel -->
+
+<!-- PROQUEST -->
+<div class="panel panel-primary">
+	<div class="panel-body">
+		<p>
+			<img class="img-responsive center-block" src="./assets/img/logos/proquest.jpg">
+		</p>
+		<p>
+			Full-text access to the Spokesman Review going back to 1991.
+		</p>
+		<p>
+			<button type="button" class="btn btn-block btn-warning"
+					data-toggle="collapse"
+					data-target="#about-proquest">
+					<b>Read more</b> <span class="hidden-xs">about ProQuest</span>
+					<span class="caret"> </span>
+			</button>
+		</p>
+
+		<div class="collapse" id="about-proquest">
+			<p>
+				<b>Great for local research!</b> <em>Proquest</em> provides full-text access to the Spokesman Review going back to 1991.
+			</p>
+			<p>
+				Full-text access to thousands of other newspapers and other publications is available as well.
+			</p>
+		</div>
+
+		<h5 class="text-center text-muted uppercase ">
+			Access this resource:
+		</h5>
+		
+		<div class="row">
+			<div class="col-md-6">
+				<p class="text-muted">
+					<a class="btn btn-block btn-info" href="http://www.scld.org/scripts/ntlinktrack.exe?http://proxy.scld.org/login?name=ProQuest&url=http://search.proquest.com/?accountid=1631" data-lib-system="scld" data-lib-title="SCLD - ProQuest">
+						<img src="/assets/img/SCLD-favicon.png" class="img-rounded favicon-scld">
+						Spokane County Library District 
+						<small class="glyphicon glyphicon-arrow-right hidden-xs"> </small>
+					</a>
+				</p>
+			</div><!-- /.col -->
+			<div class="col-md-6">
+				<p class="text-muted">
+					<a class="btn btn-block btn-success" href="http://www2.spokanelibrary.org/rpa/webauth.exe?rs=Proquest" data-lib-system="spl" data-lib-title="SPL - ProQuest">
+						<img src="/assets/img/SPL-favicon.png" class="img-rounded favicon-spl">
+						Spokane Public Library
+						<small class="glyphicon glyphicon-arrow-right hidden-xs"> </small>
+					</a>	
+				</p>
+			</div><!-- /.col -->
+		</div><!-- /.row -->
+
+	</div><!-- /.panel-body -->
+</div><!-- /.panel -->
+
+<!-- REFERENCE USA -->
+<div class="panel panel-primary">
+	<div class="panel-body">
+		<p>
+			<img class="img-responsive center-block" src="./assets/img/logos/referenceusa.gif">
+		</p>
+		<p>
+			Market research, competitive analysis, and sales lead list generation. 
+		</p>
+		<p>
+			<button type="button" class="btn btn-block btn-warning"
+					data-toggle="collapse"
+					data-target="#about-refusa">
+					<b>Read more</b> <span class="hidden-xs">about Reference USA</span>
+					<span class="caret"> </span>
+			</button>
+		</p>
+
+		<div class="collapse" id="about-refusa">
+			<p>
+				<em>ReferenceUSA</em> is the leading provider in business and consumer research! 
+				Offering a full-service platform, ReferenceUSA helps users create marketing plans, conduct competitive analysis, raise funds and locate people.
+			</p>
+			<p>
+				 Reference USA database selections and real-time access to more than 20 Million Businesses, 222 Million Consumers, U.S. New Businesses, U.S. New Homeowners and Movers, make research easy and fast!
+			</p>
+		</div>
+
+		<h5 class="text-center text-muted uppercase">
+			Access this resource:
+		</h5>
+		
+		<div class="row">
+			<div class="col-md-6">
+				<p class="text-muted">
+					<a class="btn btn-block btn-info" href="http://www.scld.org/scripts/ntlinktrack.exe?http://proxy.scld.org/login?name=ReferenceUSA&url=http://www.referenceusa.com/" data-lib-system="scld" data-lib-title="SCLD - Reference USA">
+						<img src="/assets/img/SCLD-favicon.png" class="img-rounded favicon-scld">
+						Spokane County Library District 
+						<small class="glyphicon glyphicon-arrow-right hidden-xs"> </small>
+					</a>
+				</p>
+			</div><!-- /.col -->
+			<div class="col-md-6">
+				<p class="text-muted">
+					<a class="btn btn-block btn-success" href="http://www2.spokanelibrary.org/rpa/webauth.exe?rs=refusa" data-lib-system="spl" data-lib-title="SPL - Reference USA">
+						<img src="/assets/img/SPL-favicon.png" class="img-rounded favicon-spl">
+						Spokane Public Library
+						<small class="glyphicon glyphicon-arrow-right hidden-xs"> </small>
+					</a>	
+				</p>
+			</div><!-- /.col -->
+		</div><!-- /.row -->
+
+	</div><!-- /.panel-body -->
+</div><!-- /.panel -->
+
+<!-- WA LEGAL FORMS -->
+<div class="panel panel-primary">
+	<div class="panel-body">
+		<p>
+			<img class="img-responsive center-block" src="./assets/img/logos/walegalforms.gif">
+		</p>
+		<p>
+			Washington State and federal legal forms covering many important business use cases.
+		</p>
+		<p>
+			<button type="button" class="btn btn-block btn-warning"
+					data-toggle="collapse"
+					data-target="#about-walegal">
+					<b>Read more</b> <span class="hidden-xs">about WA State Legal Forms</span>
+					<span class="caret"> </span>
+			</button>
+		</p>
+
+		<div class="collapse" id="about-walegal">
+			<p>
+				The <em>Washington State Legal Forms Library</em> offers access to hundreds of legal forms in several document formats, such as MS Word and Adobe PDF.
+			</p>
+			<p>
+				 Some examples of the forms available include:
+			</p>
+			<ul>
+				<li>Partnership Agreements</li>
+				<li>Buy Sell Agreements</li>
+				<li>Articles of Incorporation</li>
+				<li>Nondisclosure Agreements</li>
+				<li>SWOT Analysis Worksheets</li>
+				<li>End User License Agreements</li>
+				<li>Software Contracts</li>
+				<li>Online Return and Exchange Policies</li>
+				<li>Indemnification Guaranties</li>
+				<li>Liability Waivers</li>
+			</ul>
+		</div>
+
+		<h5 class="text-center text-muted uppercase">
+			Access this resource:
+		</h5>
+		
+		<div class="row">
+			<div class="col-md-6">
+				<p class="text-muted">
+					<a class="btn btn-block btn-info" href="http://www.scld.org/scripts/ntlinktrack.exe?http://proxy.scld.org/login?name=GaleLegalForms&url=http://infotrac.galegroup.com/itweb/spok67112?db=TGLF" data-lib-system="scld" data-lib-title="SCLD - WA Stage Legal Forms">
+						<img src="/assets/img/SCLD-favicon.png" class="img-rounded favicon-scld">
+						Spokane County Library District 
+						<small class="glyphicon glyphicon-arrow-right hidden-xs"> </small>
+					</a>
+				</p>
+			</div><!-- /.col -->
+			<div class="col-md-6">
+				<p class="text-muted">
+					<a class="btn btn-block btn-success" href="http://www2.spokanelibrary.org/rpa/webauth.exe?rs=walegal" data-lib-system="spl" data-lib-title="SPL - WA State Legal Forms">
+						<img src="/assets/img/SPL-favicon.png" class="img-rounded favicon-spl">
+						Spokane Public Library
+						<small class="glyphicon glyphicon-arrow-right hidden-xs"> </small>
+					</a>	
+				</p>
+			</div><!-- /.col -->
+		</div><!-- /.row -->
+
+	</div><!-- /.panel-body -->
+</div><!-- /.panel -->
