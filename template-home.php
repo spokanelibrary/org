0d104417765622f759974e90beacdf1afa85f95f
Sean Girard <seangirard@yahoo.com>
2014-05-20 14:28:55 -0700

    home

--- template-home.php
+++ template-home.php
@@ -125,7 +125,7 @@ Template Name: Home Page
         Happening soon
       </div>
       <div class="panel-body">
-        <?php echo do_shortcode('[events_list scope="year" limit="4" country="US"]#_EVENTLINK <br> <small class="muted">@ #_LOCATIONLINK</small> <br> <small><b>#_EVENTDATES</b> #_EVENTTIMES</small> <br>  <small>#_EVENTEXCERPT{20,...}</small> <hr>[/events_list]'); ?>
+        <?php echo do_shortcode('[events_list scope="month" limit="5" country="US"]#_EVENTLINK <br> <small class="muted">@ #_LOCATIONLINK</small> <br> <small><b>#_EVENTDATES</b> #_EVENTTIMES</small> <br>  <small>#_EVENTEXCERPT{20,...}</small> <hr>[/events_list]'); ?>
       </div>
       <div class="panel-footer text-center">
         <a href="/calendar/">View events calendar</a> &rarr;
