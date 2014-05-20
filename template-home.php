ce5e8eb2c1062aea5993f089ade6ea28c2763af3
Sean Girard <seangirard@yahoo.com>
2014-05-20 11:20:26 -0700

    home

--- template-home.php
+++ template-home.php
@@ -28,7 +28,7 @@ Template Name: Home Page
         This week at the library
       </div>
       <div class="panel-body">
-        <?php echo do_shortcode('[events_list scope="month" country="US"]<p>#_EVENTLINK will take place at #_LOCATIONLINK on #_EVENTDATES at #_EVENTTIMES</p>[/events_list]'); ?>
+        <?php echo do_shortcode('[events_list scope="month" country="US"]<p>#_EVENTLINK @ #_LOCATIONLINK <br> #_EVENTDATES at #_EVENTTIMES</p>[/events_list]'); ?>
       </div>
       <div class="panel-footer text-right">
         <a href="/calendar/">Check out the events calendar</a> &rarr;
