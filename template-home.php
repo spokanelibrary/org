74c3d5d7d4fecf7c1599367569c13cc6519305d9
Sean Girard <seangirard@yahoo.com>
2014-05-20 07:20:10 -0700

    home

--- template-home.php
+++ template-home.php
@@ -8,7 +8,7 @@ Template Name: Home Page
   <div class="col-md-6">
     <?php echo do_shortcode('[spl_widget search-catalog]'); ?>
   </div><!-- /.col -->
-  <div class="col-md-6">
+  <div class="col-md-6 hidden-xs">
     <?php echo do_shortcode('[spl_widget login-form]'); ?>
   </div><!-- /.col -->
 </div><!-- /.row -->
