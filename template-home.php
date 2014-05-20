23313ffa4b0e74d5519b52582e2117fa3f3cc1a2
Sean Girard <seangirard@yahoo.com>
2014-05-20 12:30:33 -0700

    home

--- template-home.php
+++ template-home.php
@@ -26,7 +26,7 @@ Template Name: Home Page
     <div class="panel panel-warning" style="border-left-width:5px;">
       <div class="panel-heading">
         <i class="glyphicon glyphicon-cloud-download"></i>
-        Discover the <a href="/digital/">Digital Branch</a>
+        At the <a href="/digital/">Digital Branch</a>
       </div>
       <div class="panel-body">
       <?php echo do_shortcode('[spl_widget custom-page page=download-widget]'); ?>
