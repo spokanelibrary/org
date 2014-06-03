6e1cdab238b7e625ab853e32ff3fe7e1ba6ee35b
Sean Girard <seangirard@yahoo.com>
2014-06-03 13:48:55 -0700

    panels

--- lib/org/init.php
+++ lib/org/init.php
@@ -128,7 +128,7 @@ function spl_resources( $params ) {
 
 
           $panels .= '<h3>';
-          $panels .= '<a href="#" class="btn btn-default" data-toggle="collapse" data-target="#'.$post->post_name.'">'.$link['text'].'</a>' . PHP_EOL;
+          $panels .= '<a href="#" class="btn btn-lg btn-default" data-toggle="collapse" data-target="#'.$post->post_name.'">'.$link['text'].'</a>' . PHP_EOL;
           $panels .= '</h3>';
 
           
