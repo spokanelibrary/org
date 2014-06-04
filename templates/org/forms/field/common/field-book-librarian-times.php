cc0321c4574632df60f7873384e782968ba7954c
Sean Girard <seangirard@yahoo.com>
2014-06-04 14:23:52 -0700

    book

--- templates/org/forms/field/common/field-book-librarian-times.php
+++ templates/org/forms/field/common/field-book-librarian-times.php
@@ -1,2 +1,11 @@
 <option value="">Choose a time:</option>
-<option value="10am-11am">10 am - 12 am</option>
+<option value="10am-11am">10 am - 11 am</option>
+<option value="11am-12pm">11 am - 12 pm</option>
+<option value="12pm-1pm">12 pm - 1 pm</option>
+<option value="1pm-2pm">1 pm - 2 pm</option>
+<option value="2pm-3pm">2 pm - 3 pm</option>
+<option value="3pm-4pm">3 pm - 4 pm</option>
+<option value="4pm-5pm">4 pm - 5 pm</option>
+<option value="5pm-6pm">5 pm - 6 pm</option>
+<option value="6pm-7pm">6 pm - 7 pm</option>
+<option value="7pm-8pm">7 pm - 8 pm</option>
