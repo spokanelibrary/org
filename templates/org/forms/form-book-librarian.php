cfbdb73710bb59ee1ef598366951347bb32fc0bf
Sean Girard <seangirard@yahoo.com>
2014-06-04 09:22:11 -0700

    book

--- templates/org/forms/form-book-librarian.php
+++ templates/org/forms/form-book-librarian.php
@@ -1,9 +1,9 @@
 <?php
 $crass = wp_get_crass_response();
 if ( !empty($crass->request) ) {
-  include 'field/contact-us-response.php';
+  include 'field/book-librarian-response.php';
   } else {
-  include 'field/contact-us.php';
+  include 'field/book-librarian.php';
 }
 ?>
 
