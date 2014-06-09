14be14b3d96cdfff8fa63eccc68545af88de4cb9
Sean Girard <seangirard@yahoo.com>
2014-06-09 14:40:26 -0700

    common navbar

--- templates/org/header-top-navbar.php
+++ templates/org/header-top-navbar.php
@@ -23,6 +23,28 @@
   </ul>
 </header>
 
+<?php
+$menu_name = 'common-navigation';
+
+  if ( ( $locations = get_nav_menu_locations() ) && isset( $locations[ $menu_name ] ) ) {
+$menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
+
+$menu_items = wp_get_nav_menu_items($menu->term_id);
+
+$menu_list = '<ul id="menu-' . $menu_name . '">';
+
+foreach ( (array) $menu_items as $key => $menu_item ) {
+    $title = $menu_item->title;
+    $url = $menu_item->url;
+    $menu_list .= '<li><a href="' . $url . '">' . $title . '</a></li>';
+}
+$menu_list .= '</ul>';
+  } else {
+$menu_list = '<ul><li>Menu "' . $menu_name . '" not defined.</li></ul>';
+  }
+echo $menu_list;
+?>
+
 <!--<header class="navbar-common banner navbar navbar-fixed-top" role="navigation">
   <ul class="nav nav-justified">
     <li>
