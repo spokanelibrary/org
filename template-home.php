5139f85a5de8c4a57cf6b27e9c7b88ee778765f6
Sean Girard <seangirard@yahoo.com>
2014-06-09 14:20:32 -0700

    beta

--- template-home.php
+++ template-home.php
@@ -18,8 +18,8 @@ Template Name: Home Page
 <div class="alert alert-info">
   <b>Welcome to beta.spokanelibrary.org!</b>
   <p>
-    We are transitioning Spokane Public Library to a new website.
-    <a href="/contact">Please contact us</a> with comments, questions, or suggestions.
+    We are transitioning Spokane Public Library to a new website, and appreciate our patience during this period.
+    We'd welcome any comments, questions, or suggestions. Feel free to <a href="/contact">contact us</a>.
     <br>
     <a class="btn btn-info" href="http://www.spokanelibrary.org">
       Please take me to the old website &rarr;
