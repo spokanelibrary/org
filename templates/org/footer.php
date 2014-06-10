270f8280d3717bf3b16d150f0f6336b6ca4d865a
Sean Girard <seangirard@yahoo.com>
2014-06-10 09:10:33 -0700

    footer

--- templates/org/footer.php
+++ templates/org/footer.php
@@ -2,7 +2,7 @@
 
   <!-- <a name="spl-more">&nbsp;</a> -->
 
-  <footer class="content-info container" role="contentinfo">
+  <footer class="content-info container" role="contentinfo" style="border-top:1px solid rgb(100,150,75);">
 
     <?php //include('footer-bottom-navbar.php'); ?>
 
