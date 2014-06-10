f4efdd523c2b0fb059ba9ca235f71f246e5c0575
Sean Girard <seangirard@yahoo.com>
2014-06-10 16:24:57 -0700

    msit

--- templates/org/pages/it-academy.php
+++ templates/org/pages/it-academy.php
@@ -1,4 +1,73 @@
 <?php
+$spl_network_source = spl_get_network_source($_SERVER['REMOTE_ADDR']);
+?>
+
+<div class="row">
+	<div class="col-sm-6">
+		<div class="alert alert-info">
+			<p class="lead">
+				<b>Returning Users:</b> IT Academy Login
+			</p>
+			<p>
+        <a href="https://itacademy.microsoft.com?whr=uri:MicrosoftAccount"
+          rel="external"
+          class="btn btn-block btn-info"
+          title="For returning users"
+          ><b>I have already signed up for Microsoft IT Academy</b>
+          &rarr;
+        </a>
+      </p>
+      <span class="help-block">
+        Use this link <b>only</b> if you have <em>already</em> signed up.
+      </span>
+		</div><!-- ./alert -->
+	</div><!-- ./col -->
+	<div class="col-sm-6">
+		<div class="alert alert-success">
+			<p class="lead">
+        <b>Returning Users:</b> IT Academy Registration
+      </p>
+      <div class="it-academy-login">
+        <p class="spl-login" data-callback-method="splLoadITAcademyLinks">
+          <button type="button" class="btn btn-block btn-success" data-toggle="modal" data-target="#spl-login-modal">
+            <i class="glyphicon glyphicon-user"></i>
+            <b>Login to My Account &rarr;</b>
+          </button>
+        </p>
+        <span class="help-block">Login to your Library account to get started.</span>
+      </div>
+      <div class="it-academy-links">
+        Ok, here is your link:
+      </div>
+
+      <div id="spl-network-source" data-source="<?php echo $spl_network_source; ?>"></div>
+
+      <script id="spl-it-academy-link-tmpl" type="text/x-handlebars-template">
+        <p class="lead">
+        <strong>New Users:</strong> IT Academy Registration
+        </p>
+        {{#each codes}}
+          <p>
+            <a href="https://itacademy.microsoft.com/AccessCodeRedemption/EnrollmentCode?whr=uri:MicrosoftAccount&key={{code}}"
+            rel="external"
+            class="btn btn-block btn-success"
+            title="Microsoft IT Academy Login"
+            ><strong>I would like to register for Microsoft IT Academy</strong>
+            <i class="icon-white icon-arrow-right"></i>
+            </a>
+          </p>
+        {{/each}}
+        <span class="help-block">
+          Use this link <strong>only</strong> if you have <em>not yet</em> signed up.
+        </span>
+      </script>
+
+			<?php echo $spl_network_source; ?>
+		</div><!-- ./alert -->
+	</div><!-- ./col -->
+</div><!-- ./row -->
+
+<?php
 
 function spl_get_network_source($ip=null) {
   $source = null;
@@ -56,46 +125,3 @@ function spl_get_network_source($ip=null) {
 
 
 ?>
-
-<div class="row">
-	<div class="col-sm-6">
-		<div class="alert alert-info">
-			<p class="lead">
-				<b>Returning Users:</b> IT Academy Login
-			</p>
-			<p>
-        <a href="https://itacademy.microsoft.com?whr=uri:MicrosoftAccount"
-          rel="external"
-          class="btn btn-block btn-info"
-          title="For returning users"
-          ><b>I have already signed up for Microsoft IT Academy</b>
-          &rarr;
-        </a>
-      </p>
-      <span class="help-block">
-        Use this link <b>only</b> if you have <em>already</em> signed up.
-      </span>
-		</div><!-- ./alert -->
-	</div><!-- ./col -->
-	<div class="col-sm-6">
-		<div class="alert alert-success">
-			<p class="lead">
-        <b>Returning Users:</b> IT Academy Registration
-      </p>
-      <div class="it-academy-login">
-        <p class="spl-login" data-callback-method="splLoadITAcademyLinks">
-          <button type="button" class="btn btn-block btn-success" data-toggle="modal" data-target="#spl-login-modal">
-            <i class="glyphicon glyphicon-user"></i>
-            Login to My Account
-          </button>
-        </p>
-        <span class="help-block">Login to your Library account to get started.</span>
-      </div>
-      <div class="it-academy-links">
-        Ok, here is your link:
-      </div>
-
-			<?php echo ucfirst(spl_get_network_source($_SERVER['REMOTE_ADDR'])); ?>
-		</div><!-- ./alert -->
-	</div><!-- ./col -->
-</div><!-- ./row -->
