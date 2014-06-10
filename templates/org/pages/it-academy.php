b4ae2196496918add282d5b9a6effd5804bf464b
Sean Girard <seangirard@yahoo.com>
2014-06-10 11:12:26 -0700

    msit

--- templates/org/pages/it-academy.php
+++ templates/org/pages/it-academy.php
@@ -1,15 +1,24 @@
-<div class="panel panel-warning">
-	<div class="panel-heading">
-		<small class="glyphicon glyphicon-exclamation-sign"></small>
-		Use of Library computers constitutes an agreement that you accept the following terms and conditions:
-	</div>
-	<div class="panel-body">
-		<p>
-			All Library customers are expected to access the Internet in compliance with the <a href="http://www.spokanelibrary.org/index.php?page=internet_policy">Internet Use Policy</a> and <a href="http://www.spokanelibrary.org/admin/uploads/files/Policies/rules_of_conduct.pdf">Rules of Conduct</a>, and within the boundaries of the law.
-  		Viewing or downloading illegal or copyright-protected materials, obscenity, child pornography, or materials harmful to minors is prohibited.
-	  	<em>Please be considerate of other Library patrons.</em>
-		</p>
-		<a class="btn btn-success" href="http://www.spokanelibrary.org"><i class="glyphicon glyphicon-home"></i> Go to the Library website &rarr;</a>
-	</div>
-</div>
+<div class="row">
+	<div class="col-sm-6">
+		<div class="alert alert-info">
+			<p class="lead">
+				<b>Returning Users:</b> IT Academy Login
+			</p>
+			<p>
+      <a href="https://itacademy.microsoft.com?whr=uri:MicrosoftAccount"
+        rel="external"
+        class="btn btn-block btn-info"
+        title="For returning users"
+        ><b>I have already signed up for Microsoft IT Academy</b>
+        &rarr;
+      </a>
+      </p>
+      <span class="help-block">
+        Use this link <b>only</b> if you have <em>already</em> signed up.
+      </span>
+		</div>
+	</div><!-- ./col -->
+	<div class="col-sm-6">
 
+	</div><!-- ./col -->
+</div><!-- ./row -->
