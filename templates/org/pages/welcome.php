dece0d310f5d3e2a745eb5093573e52c506abfcc
Sean Girard <seangirard@yahoo.com>
2014-06-10 09:22:23 -0700

    welcome

---
+++ templates/org/pages/welcome.php
@@ -0,0 +1,168 @@
+
+<div class="alert alert-warning">
+	<h5>
+  	Use of Library computers constitutes an agreement that you accept the following terms and conditions:
+  </h5>
+	<p class="">
+  	<span class="help-block">
+  	All Library customers are expected to access the Internet in compliance with the <a href="http://www.spokanelibrary.org/index.php?page=internet_policy">Internet Use Policy</a> and <a href="http://www.spokanelibrary.org/admin/uploads/files/Policies/rules_of_conduct.pdf">Rules of Conduct</a>, and within the boundaries of the law.
+  	Viewing or downloading illegal or copyright-protected materials, obscenity, child pornography, or materials harmful to minors is prohibited.
+  	<em>Please be considerate of other Library patrons.</em>
+  	</span>
+  </p>
+  <a class="btn btn-success" href="http://www.spokanelibrary.org"><i class="icon-home icon-white"></i> Go to the Library website &rarr;</a>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+<div class="row-fluid">
+
+	<div class="span6">
+		<h3 class="muted normal">Popular Websites <small>Search & Social</small></h3>
+		<ul class="nav nav-pills">
+			<li>
+				<a href="http://google.com"><img src="../wordpress/assets/img/icons/16px/google.png"> Google</a>
+			</li>
+			<li>
+				<a href="http://yahoo.com"><img src="../wordpress/assets/img/icons/16px/yahoo.png"> Yahoo!</a>
+			</li>
+			<li>
+				<a href="http://bing.com"><img src="../wordpress/assets/img/icons/16px/bing.png"> Bing</a>
+			</li>
+			<li>
+				<a href="http://facebook.com/spokanelibrary"><img src="../wordpress/assets/img/icons/16px/facebook.png"> Facebook</a>
+			</li>
+		</ul>
+	</div>
+
+	<div class="span6">
+		<h3 class="muted normal">Check Your Email</h3>
+		<ul class="nav nav-pills">
+			<li>
+				<a href="http://gmail.com"><img src="../wordpress/assets/img/icons/16px/gmail.png"> Gmail</a>
+			</li>
+			<li>
+				<a href="http://mail.yahoo.com"><img src="../wordpress/assets/img/icons/16px/yahoo.png"> Yahoo! Mail</a>
+			</li>
+			<li>
+				<a href="http://outlook.com"><img src="../wordpress/assets/img/icons/16px/outlook.png"> Hotmail / Live / Outlook</a>
+			</li>
+		</ul>
+	</div>
+
+</div><!-- ./row-fluid -->
+
+<div class="row-fluid">
+
+	<div class="span4">
+		<h4 class="text-success">Use the Library</h4>
+		<ul class="nav nav-stacked nav-pills">
+			<li>
+				<a href="http://www.spokanelibrary.org/librarycards"><i class="icon-barcode"></i> Get a Library Card &rarr;</a>
+			</li>
+			<li>
+				<a href="http://www.spokanelibrary.org/printing"><i class="icon-print"></i> How to use Library printers &rarr;</a>
+			</li>
+			<li>
+				<a href="http://www.spokanelibrary.org/branches"><i class="icon-time"></i> When is the Library open &rarr;</a>
+			</li>
+			<li>
+				<a href="http://www.spokanelibrary.org/worksource"><i class="icon-question-sign"></i> WorkSource Career Center &rarr;</a>
+			</li>
+		</ul>
+	</div>
+
+	<div class="span4">
+		<h4 class="text-success">Search the Library</h4>
+		<ul class="nav nav-stacked nav-pills">
+			<li>
+				<a href="http://hzportal.spokanelibrary.org"><i class="icon-search"></i> <strong>Open the Library Catalog</strong> &rarr;</a>
+			</li>
+		</ul>
+		<form method="get" action="http://hzportal.spokanelibrary.org/ipac20/ipac.jsp?=">
+			<input type="hidden" name="index" value="PDVDTI">
+			<strong class="muted">Search for DVDs</strong>
+			<div class="input-append">
+			  <input class="span10" name="term" type="text">
+			  <button class="btn" type="submit">Go</button>
+			</div>
+		</form>
+		<!--
+		<form method="get" action="http://hzportal.spokanelibrary.org/ipac20/ipac.jsp?=">
+			<input type="hidden" name="index" value=".GW">
+			<input type="hidden" name="limitbox_2" value="<?php echo urldecode('FM01+%3D+fo_ebook'); ?>">
+			<strong class="muted">Search for eBooks</strong>
+			<div class="input-append">
+			  <input class="span10" name="term" type="text">
+			  <button class="btn" type="submit">Go</button>
+			</div>
+		</form>
+	 	-->
+	</div>
+
+	<div class="span4">
+		<h4 class="text-success">Download from the Library</h4>
+		<ul class="nav nav-stacked nav-pills">
+			<li>
+				<a href="http://www.spokanelibrary.org/download-music"><i class="icon-music"></i> Download Music <small>(.mp3)</small> &rarr;</a>
+			</li>
+			<li>
+				<a href="http://www.spokanelibrary.org/download-magazines"><i class="icon-picture"></i> Download Magazines &rarr;</a>
+			</li>
+			<li>
+				<a href="http://www.spokanelibrary.org/ebooks"><i class="icon-book"></i> Download eBooks &rarr;</a>
+			</li>
+			<li>
+				<a href="http://www.spokanelibrary.org/eaudiobooks"><i class="icon-headphones"></i> Download Audio Books &rarr;</a>
+			</li>
+		</ul>
+	</div>
+
+</div><!-- ./row-fluid -->
+
+<div class="row-fluid">
+	
+	<div class="span12"></h3>
+		<h4 class="text-error">Spokane Area News &amp; Information</h4>
+		<ul class="nav nav-pills">
+			<li>
+				<a href="http://beta.spokanecity.org">Spokane City</a>
+			</li>
+			<li>
+				<a href="http://spokane.craigslist.org">Spokane Craigslist</a>
+			</li>
+			<li>
+				<a href="http://www.spokesman.com">Spokesman Review</a>
+			</li>
+			<li>
+				<a href="http://www.inlander.com">Pacific NW Inlander</a>
+			</li>
+			<li>
+				<a href="http://www.krem.com">KREM</a>
+			</li>
+			<li>
+				<a href="http://www.kxly.com">KXLY</a>
+			</li>
+			<li>
+				<a href="http://www.khq.com">KHQ</a>
+			</li>
+			<li>
+				<a href="http://www.ksps.org">KSPS</a>
+			</li>
+			<li>
+				<a href="http://www.ksfc.org">KSFC &amp; KPBX</a>
+			</li>
+		</ul>
+	</div>
+
+</div><!-- ./row-fluid -->
+
+
