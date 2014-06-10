f70d2d5534c882062a67917114025d5a430e985c
Sean Girard <seangirard@yahoo.com>
2014-06-10 09:25:55 -0700

    welcome

--- templates/org/pages/welcome.php
+++ templates/org/pages/welcome.php
@@ -1,12 +1,18 @@
 <div class="panel panel-warning">
 	<div class="panel-heading">
-		Terms and conditions
+		Use of Library computers constitutes an agreement that you accept the following terms and conditions:
 	</div>
 	<div class="panel-body">
-		;asdjf;j
+		<p>
+			All Library customers are expected to access the Internet in compliance with the <a href="http://www.spokanelibrary.org/index.php?page=internet_policy">Internet Use Policy</a> and <a href="http://www.spokanelibrary.org/admin/uploads/files/Policies/rules_of_conduct.pdf">Rules of Conduct</a>, and within the boundaries of the law.
+  		Viewing or downloading illegal or copyright-protected materials, obscenity, child pornography, or materials harmful to minors is prohibited.
+	  	<em>Please be considerate of other Library patrons.</em>
+		</p>
 	</div>
 </div>
 
+
+<hr>
 
 <div class="alert alert-warning">
 	<h5>
