df90c0bd1494d7a99e486a59583cc1317a384a52
Sean Girard <seangirard@yahoo.com>
2014-06-10 14:11:30 -0700

    msit

--- assets/js/org/it-academy.js
+++ assets/js/org/it-academy.js
@@ -3,19 +3,19 @@
 var org = {
 
 	// added on script load
-	/*
+	
 	config: {
 						api: { hzws: 'http://api.spokanelibrary.org/v2/request/'
 									,oclc: 'http://api.spokanelibrary.org/oclc/'
 								 }
 					 }
-	*/
+	
 	// added on authentication
-	//user: {}
+	user: {}
 
 	// called on script load
 	init: function() {
-    console.log('test');
+
 		_self = this;
 		
 		this.config = ORG.config;
