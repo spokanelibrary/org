96e4cd4d77e09340af49b6cc156eeb963e6be2cd
Sean Girard <seangirard@yahoo.com>
2014-06-11 10:03:13 -0700

    msit

--- templates/org/pages/it-academy.php
+++ templates/org/pages/it-academy.php
@@ -28,7 +28,7 @@ $spl_network_source = spl_get_network_source($_SERVER['REMOTE_ADDR']);
         <b>Returning Users:</b> IT Academy Registration
       </p>
       <div class="it-academy-login">
-        <p class="spl-login" data-callback-method="splLoadITAcademyLinks">
+        <p class="" data-callback-method="splLoadITAcademyLinks">
           <button type="button" class="btn btn-block btn-success" data-toggle="modal" data-target="#spl-login-modal">
             <i class="glyphicon glyphicon-user"></i>
             <b>Login to My Account &rarr;</b>
