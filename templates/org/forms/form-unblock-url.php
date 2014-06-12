5063c301723962f05d75e0d6a138cf9c1003b46f
Sean Girard <seangirard@yahoo.com>
2014-06-12 11:27:52 -0700

    unblock

---
+++ templates/org/forms/form-unblock-url.php
@@ -0,0 +1,105 @@
+<div class="row-fluid">
+  <div class="span6">
+    <p>
+      In an effort to maintain a safe and comfortable environment as well as compliance with the <a href="http://www.fcc.gov/guides/childrens-internet-protection-act">Children's Internet Protection Act of 2000</a>, Spokane Public Library automatically filters pornography and other objectionable content in accordance with our <a href="http://www.spokanelibrary.org/internet-policy">Internet Use Policy</a>.
+    </p>
+  </div><!-- /.span6 -->
+  <div class="span6">
+    <p>
+      However, sometimes our software makes the wrong call. 
+      You may use the form on this page to request that a specific website be unblocked. 
+      These requests are anonymous. 
+      You may provide an email address if you wish to be informed about the status of your request.
+    </p>
+  </div><!-- /.span6 -->
+</div><!-- /.row-fluid -->
+
+<!--
+<p>
+  <a href="#">
+    <strong>
+      More information about Internet content filtering at the Library &rarr;
+    </strong>
+  </a>
+</p>
+-->
+
+<?php 
+$spl['crass']['response'] = wp_get_crass_response();
+if ( !empty($spl['crass']['response']->request['unblock']) ) {
+  include 'field/unblock-url-response.php';
+} else {
+  $spl['unblock'] = spl_parse_unblock_request();
+
+  // log blocked page
+  spl_log_unblock_request($spl['unblock']);
+  
+  include 'field/unblock-url.php';
+}
+
+/*
+echo '<pre>';
+var_export($_SERVER);
+var_export($spl['crass']['response']->status);
+echo '</pre>';
+*/
+
+function spl_log_unblock_request($request) {
+
+  $api = array();
+  $api['endpoint'] = 'http://api.spokanelibrary.org/connect/unblock';
+  
+  $params = array();
+  $params['unblock'] = $request;
+  $params['action'] = 'log';
+  if ( isset($_SERVER['HTTP_X_FORWARDED_FOR']) ) {
+    $params['ip'] = $_SERVER['HTTP_X_FORWARDED_FOR'];
+  } else {
+    $params['ip'] = $_SERVER['REMOTE_ADDR'];
+  }
+  $api['params'] = $params;
+
+  require_once('/var/web/hosts/---/apps/api/init.php');
+  $api['key'] = $init['api']['key'];
+  unset ($init);
+
+  //print_r($api);
+  
+  
+  $crass = new Crass_Response;
+  $result = $crass->jsonCurl($api['endpoint']
+                                      , array('apikey'=>$api['key'], 
+                                              'params'=>$api['params'])
+                                      );
+
+  return $result['id'];
+}
+
+function spl_parse_unblock_request() {
+  /*
+    Reconstruct the original querystring
+    WARNING: this is a little bit magic and could be broken by URL handling in future versions of PHP
+    In a URI such as http://www.proxy.org/?category=My Category&url=http://www.example.com/path/?a=b&x=y
+    ?a=b willl not become part of the $_GET array
+    So, we start with $_GET, strip out the 'category' and 'url' paramaters we are working with
+    then reparse the 'url' to recover the first query parameter in 'url' (immediately following ?)
+  */
+  $querystring = $_GET;
+  unset($querystring['url']);
+  unset($querystring['category']);
+  $uri = parse_url($_GET['url']);
+  parse_str($uri['query'], $additional);
+  $querystring += $additional;
+  // build request
+  $unblock = array();
+  $unblock['url'] = $uri;
+  // replace with reconstructed 'query'
+  $unblock['url']['query'] = http_build_query($querystring);
+  $unblock['category'] = $_GET['category'];
+
+  return $unblock;
+}
+
+?>
+
+
