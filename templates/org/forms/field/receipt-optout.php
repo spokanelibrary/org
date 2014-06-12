6172728885a140be1be369ae6b89fbbabd62ecbd
Sean Girard <seangirard@yahoo.com>
2014-06-12 10:17:51 -0700

    ok

--- templates/org/forms/field/receipt-optout.php
+++ templates/org/forms/field/receipt-optout.php
@@ -1,12 +1,31 @@
 <?php
-
-
 $endpoint = 'http://api.spokanelibrary.org/connect/receipt-optout/lookup';
 $borrower = Crass_Response::jsonCurl($endpoint, array( 'apikey'=>getenv('SPL_KEY'), 
                                                                         'params'=>array( 'batch'=>$_REQUEST['b']
                                                                                         ,'hash'=>$_REQUEST['h'])
                                                                       ));
-
-print_r($borrower);
-
 ?>
+
+<?php
+if ( array_key_exists('number', $borrower) ) :
+?>
+
+OK
+
+<?php else: ?>
+
+<div class="alert alert-error">
+
+  <h4>Oops!</h4>
+
+  <p>
+   We apologize for the inconvenience, but we are unable find you in our system.
+  </p>
+
+  <p>
+    If you landed here by clicking a link in a receipt email please call us at 509.444.5333 or <a href="/contact/">send us an email</a>.
+  </p>
+
+</div>
+
+<?php endif; ?>
