52df789fdac60b120e2aa840b253d534d26490b0
Sean Girard <seangirard@yahoo.com>
2014-06-12 09:43:19 -0700

    optout

--- templates/org/forms/field/receipt-optout.php
+++ templates/org/forms/field/receipt-optout.php
@@ -1,2 +1,10 @@
+<?php
 
-Outout!
+$endpoint = 'http://api.spokanelibrary.org/connect/receipt-optout/lookup';
+$borrower = json_decode(Crass_Response::curlPostProxy($endpoint, array( 'apikey'=>getenv('SPL_KEY'), 
+                                                                        'params'=>array( 'batch'=>$_REQUEST['b']
+                                                                                        ,'hash'=>$_REQUEST['h'])
+                                                                      )));
+
+print_r($borrower);
+?>
