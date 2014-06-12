90864df67098f0e75fc96bacd8b519733ccb4385
Sean Girard <seangirard@yahoo.com>
2014-06-12 08:27:04 -0700

    spl_get_network_source

--- templates/org/pages/it-academy.php
+++ templates/org/pages/it-academy.php
@@ -1,56 +1,6 @@
 <?php
 
-function spl_get_network_source($ip=null) {
-  $source = null;
 
-  $ip = trim($ip);
-
-  if ( strpos ($ip, '199.237.16') === 0 ) {
-    $source = 'library'; // default
-    if ( '199.237.16.1' == $ip ) {
-      $source = 'minor'; // kids proxy
-    } else {
-      $source = 'public'; // dt public
-    }
-  } elseif ( strpos ($ip, '10.14') === 0 ) {
-    $source = 'library'; // default
-    if (strpos ($ip, '10.14.1') === 0) {
-      if (strpos ($ip, '10.14.18') === 0) {
-        $source = 'staff'; // hy staff
-      } elseif (strpos ($ip, '10.14.19') === 0) {
-        $source = 'so'; // public/staff combined
-      } elseif (strpos ($ip, '10.14.118') === 0) {
-        $source = 'hy'; // hy public
-      } elseif (strpos ($ip, '10.14.121') === 0) {
-        $source = 'es'; // es public
-      } elseif (strpos ($ip, '10.14.122') === 0) {
-        $source = 'sh'; // sh public
-      } 
-    } elseif (strpos ($ip, '10.14.2') === 0) {
-      if (strpos ($ip, '10.14.200') === 0) {
-        $source = 'wireless'; // all wireless networks
-      } elseif (strpos ($ip, '10.14.21') === 0) {
-        $source = 'staff'; // es staff
-      } elseif (strpos ($ip, '10.14.22') === 0) {
-        $source = 'staff'; // sh staff
-      } elseif (strpos ($ip, '10.14.23') === 0) {
-        $source = 'it'; // it public/staff combined
-      } 
-    } elseif (strpos ($ip, '10.14.5') === 0) {
-      if (strpos ($ip, '10.14.50') === 0) {
-        $source = 'staff';
-      } elseif (strpos ($ip, '10.14.51') === 0) {
-        $source = 'staff';
-      }
-    }
-  }
-
-  if ( is_null($source) || ('external' == $_REQUEST['rel']) ) {
-    $source = 'external';
-  }
-
-  return $source;
-}
 
 ?>
 
