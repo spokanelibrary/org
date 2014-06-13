799b7fcf2e4d30a7c926fb702916992815337c44
Sean Girard <seangirard@yahoo.com>
2014-06-13 13:10:44 -0700

    home

--- template-home.php
+++ template-home.php
@@ -17,14 +17,14 @@ Template Name: Home Page
     During this time a few pages, including <a href="/contact/">contact us</a>, <a href="/book/">book a librarian</a>, and <a href="/connect/">other interactive forms</a>, are only available here on beta.spokanelibrary.org.
     </b>
   </p>
-  <p>
-    All other pages are still located on <a href="http://www.spokanelibrary.org">our old website</a>.
-    We understand this may be somewhat confusing, and we really appreciate your patience during this period.
-  </p>
-  <p>
-    Please feel free to <a href="/contact">contact us</a> with any comments, questions, or suggestions.
-  </p>
   <blockquote>
+    <p>
+      All other pages are still located on <a href="http://www.spokanelibrary.org">our old website</a>.
+      We understand this may be somewhat confusing, and we really appreciate your patience during this period.
+    </p>
+    <p>
+      Please feel free to <a href="/contact">contact us</a> with any comments, questions, or suggestions.
+    </p>
     <p>
       Thanks,
     </p>
