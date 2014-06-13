83db4f71edfc658b58264f713d7d4192a1eb0e89
Sean Girard <seangirard@yahoo.com>
2014-06-13 12:43:09 -0700

    home

--- template-home.php
+++ template-home.php
@@ -26,7 +26,7 @@ Template Name: Home Page
   </p>
   <hr>
   <p>
-    <a class="btn btn-lg btn-lg btn-primary" href="http://www.spokanelibrary.org">
+    <a class="btn btn-lg btn-block btn-primary" href="http://www.spokanelibrary.org">
       Please take me to the old website &rarr;
     </a>
   </p>
