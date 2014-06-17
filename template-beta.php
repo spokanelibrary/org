551dfc2067fcd13e873dd6d6c8f1520938f71d04
Sean Girard <seangirard@yahoo.com>
2014-06-17 13:07:06 -0700

    beta

--- template-beta.php
+++ template-beta.php
@@ -9,34 +9,39 @@ Template Name: Beta Home Page
   </h1>
 </div>
 
-<p class="lead">
-  <b>
-  We are transitioning Spokane Public Library to a new website.
-  During this time a few pages, including <a href="/contact/">contact us</a>, <a href="/book/">book a librarian</a>, and <a href="/connect/">other interactive forms</a>, are only available here on beta.spokanelibrary.org.
-  </b>
-</p>
-<blockquote>
-  <p>
-    All other pages are still located on <a href="http://www.spokanelibrary.org">our old website</a>.
-    We understand this may be somewhat confusing, and we really appreciate your patience during this period.
-  </p>
-  <p>
-    Please feel free to <a href="/contact">contact us</a> with any comments, questions, or suggestions.
-  </p>
-  <p>
-    Thanks,
-  </p>
-  <small>The SPL Web Team</small>
-</blockquote>
-<hr>
-<p>
-  <a class="btn btn-lg btn-block btn-primary" href="http://www.spokanelibrary.org">
-    <span class="visible-xs">Take me to the old website &rarr;</span>
-    <span class="hidden-xs">Please take me to the old website now &rarr;</span>
-  </a>
-</p>
-
-<img class="img-responsive img-rounded" alt="Spokane Public Library Bookmobile" src="/assets/img/jpg/spl-bookmobile-1.jpg">
+<div class="row">
+  <div class="col-md-6">
+    <p class="lead">
+      <b>
+      We are transitioning Spokane Public Library to a new website.
+      During this time a few pages, including <a href="/contact/">contact us</a>, <a href="/book/">book a librarian</a>, and <a href="/connect/">other interactive forms</a>, are only available here on beta.spokanelibrary.org.
+      </b>
+    </p>
+    <blockquote>
+      <p>
+        All other pages are still located on <a href="http://www.spokanelibrary.org">our old website</a>.
+        We understand this may be somewhat confusing, and we really appreciate your patience during this period.
+      </p>
+      <p>
+        Please feel free to <a href="/contact">contact us</a> with any comments, questions, or suggestions.
+      </p>
+      <p>
+        Thanks,
+      </p>
+      <small>The SPL Web Team</small>
+    </blockquote>
+    <hr>
+    <p>
+      <a class="btn btn-lg btn-block btn-primary" href="http://www.spokanelibrary.org">
+        <span class="visible-xs">Take me to the old website &rarr;</span>
+        <span class="hidden-xs">Please take me to the old website now &rarr;</span>
+      </a>
+    </p>
+  </div><!-- /.col -->
+  <div class="col-md-6">
+    <img class="img-responsive img-rounded" alt="Spokane Public Library Bookmobile" src="/assets/img/jpg/spl-bookmobile-1.jpg">
+  </div><!-- /.col -->
+</div><!-- /.row -->
 
 <div class="alert alert-info">
 
