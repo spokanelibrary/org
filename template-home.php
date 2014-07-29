50e02f061be046b1a4c6d69d9a90f31705a6ce90
Sean Girard <seangirard@yahoo.com>
2014-07-29 15:25:57 -0700

    home

--- template-home.php
+++ template-home.php
@@ -106,7 +106,7 @@ Template Name: Home Page
           if ( ! is_wp_error( $rss ) ) : // Checks that the object is created correctly
 
               // Figure out how many total items there are, but limit it to 5. 
-              $maxitems = $rss->get_item_quantity( 3 ); 
+              $maxitems = $rss->get_item_quantity( 9 ); 
 
               // Build an array of all the items, starting with element 0 (first element).
               $rss_items = $rss->get_items( 0, $maxitems );
@@ -133,7 +133,7 @@ Template Name: Home Page
                 <?php endforeach; ?>
             <?php endif; ?>
         </ul>
-        
+
           <small>
             <a href="/calendar/">View events calendar</a> &rarr;
           </small>
