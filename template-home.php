6cb6c36046b56682a3df55b8700bf59433fde068
Sean Girard <seangirard@yahoo.com>
2014-07-29 15:18:27 -0700

    home

--- template-home.php
+++ template-home.php
@@ -102,18 +102,18 @@ Template Name: Home Page
           
           <?php
           $rss = fetch_feed( 'http://www.trumba.com/calendars/spls-calendar.rss' );
-          /*
+          
           if ( ! is_wp_error( $rss ) ) : // Checks that the object is created correctly
 
               // Figure out how many total items there are, but limit it to 5. 
               $maxitems = $rss->get_item_quantity( 9 ); 
 
               // Build an array of all the items, starting with element 0 (first element).
-              $rss_items = $rss->get_items( 0, $maxitems );
+              //$rss_items = $rss->get_items( 0, $maxitems );
 
           endif;
-          */
-          print_r($rss);
+          
+          print_r($maxitems);
 
           ?>
           <small>
