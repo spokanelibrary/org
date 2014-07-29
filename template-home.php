08756dbae48b7f4de5d5f070d8309530c9ed57f5
Sean Girard <seangirard@yahoo.com>
2014-07-29 15:24:40 -0700

    home

--- template-home.php
+++ template-home.php
@@ -113,9 +113,27 @@ Template Name: Home Page
 
           endif;
           
-          print_r($rss_items);
+          //print_r($rss_items);
 
           ?>
+          <ul class="nav nav-pills">
+            <?php if ( $maxitems == 0 ) : ?>
+                <li><?php _e( 'No items', 'my-text-domain' ); ?></li>
+            <?php else : ?>
+                <?php // Loop through each feed item and display each item as a hyperlink. ?>
+                <?php foreach ( $rss_items as $item ) : ?>
+                    <li>
+                        <?php //print_r($item); ?>
+                        <a href="<?php echo esc_url( $item->get_permalink() ); ?>"
+                            title="<?php printf( __( 'Posted %s', 'my-text-domain' ), $item->get_date('j F Y | g:i a') ); ?>">
+                            <small class="glyphicon glyphicon-bullhorn text-muted"></small>
+                            <?php echo esc_html( $item->get_title() ); ?>
+                        </a>
+                    </li>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </ul>
+        
           <small>
             <a href="/calendar/">View events calendar</a> &rarr;
           </small>
@@ -139,25 +157,6 @@ Template Name: Home Page
 
     endif;
     ?>
-
-
-    <ul class="nav nav-pills">
-        <?php if ( $maxitems == 0 ) : ?>
-            <li><?php _e( 'No items', 'my-text-domain' ); ?></li>
-        <?php else : ?>
-            <?php // Loop through each feed item and display each item as a hyperlink. ?>
-            <?php foreach ( $rss_items as $item ) : ?>
-                <li>
-                    <?php //print_r($item); ?>
-                    <a href="<?php echo esc_url( $item->get_permalink() ); ?>"
-                        title="<?php printf( __( 'Posted %s', 'my-text-domain' ), $item->get_date('j F Y | g:i a') ); ?>">
-                        <small class="glyphicon glyphicon-bullhorn text-muted"></small>
-                        <?php echo esc_html( $item->get_title() ); ?>
-                    </a>
-                </li>
-            <?php endforeach; ?>
-        <?php endif; ?>
-    </ul>
 
     <div class="hidden-xs">
     
