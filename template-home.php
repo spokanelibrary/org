4e5a49874fcedd522269452dceaa66422090cb5f
Sean Girard <seangirard@yahoo.com>
2014-07-29 15:45:52 -0700

    home

--- template-home.php
+++ template-home.php
@@ -131,7 +131,7 @@ Template Name: Home Page
                         </a>
                         <?php 
                         $location = $item->get_item_tags('urn:ietf:params:xml:ns:xcal', 'location');
-                        print_r($location);
+                        print_r($location[0]);
                         //echo $location[0]->data;
                         //$cap = $enc ->get_caption();
                         //echo $cap->get_starttime(); 
