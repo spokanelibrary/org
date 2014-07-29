e470064d91c96be0bd904fa40a5ab9840f9429a1
Sean Girard <seangirard@yahoo.com>
2014-07-29 15:53:28 -0700

    home

--- template-home.php
+++ template-home.php
@@ -123,15 +123,18 @@ Template Name: Home Page
                 <?php // Loop through each feed item and display each item as a hyperlink. ?>
                 <?php foreach ( $rss_items as $item ) : ?> 
                     <?php $location = $item->get_item_tags('urn:ietf:params:xml:ns:xcal', 'location'); ?>
+                    <?php $formatteddatetime = $item->get_item_tags('http://schemas.trumba.com/rss/x-trumba', 'formatteddatetime'); ?>
                     <tr>
                         <?php //print_r($item); ?>
                         <td>
                           <?php echo esc_html( $location[0]['data'] ); ?>
                         </td>
                         <td>
+                          <?php echo esc_html( $formatteddatetime[0]['data'] ); ?>
+                        </td>
+                        <td>
                         <a href="<?php echo esc_url( $item->get_permalink() ); ?>"
                             title="<?php printf( __( 'Posted %s', 'my-text-domain' ), $item->get_date('j F Y | g:i a') ); ?>">
-                            <small class="glyphicon glyphicon-bullhorn text-muted"></small>
                             <?php echo esc_html( $item->get_title() ); ?>
                         </a>
                         </td>
