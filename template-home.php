ba6eaae103f3197ba6ccb63d7ec407b8f71b7953
Sean Girard <seangirard@yahoo.com>
2014-07-30 09:59:15 -0700

    home

--- template-home.php
+++ template-home.php
@@ -44,7 +44,7 @@ Template Name: Home Page
 
   </div><!-- /.col -->
   <div class="col-md-3">
-    <div class="panel panel-primary" style="border-left-width:5px;">
+    <div class="panel panel-primary panel-callout" style="border-left-width:5px;">
       <div class="panel-heading">
         <i class="glyphicon glyphicon-cloud-download"></i>
         Quick links
