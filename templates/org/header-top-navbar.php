ebe484b31af88b20f31e1d37ba188fe8eabed16d
Sean Girard <seangirard@yahoo.com>
2014-07-30 09:51:54 -0700

    home

--- templates/org/header-top-navbar.php
+++ templates/org/header-top-navbar.php
@@ -70,7 +70,7 @@ function spl_get_home_url() {
 </header>--><!-- /.navbar -->
 
 <header class="navbar-inverse">
-<div class="container">
+<div class="container" style="margin:bottom:0;">
       <?php echo do_shortcode('[spl_widget search-catalog]'); ?>
   </div>
 </header>
