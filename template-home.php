252e0f13571531b75a33bd8df2bc9f7f55584b67
Sean Girard <seangirard@yahoo.com>
2014-07-30 09:36:26 -0700

    home

--- template-home.php
+++ template-home.php
@@ -52,7 +52,7 @@ Template Name: Home Page
         Live chat
       </a>
       -->
-      <a href="/account/" class="list-group-item">
+      <a href="/account/" class="">
         My account
       </a>
       <a href="/search/" class="list-group-item">
