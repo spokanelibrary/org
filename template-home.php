12210da8e689f9f2ff0a469ebe5c782c66d72d6a
Sean Girard <seangirard@yahoo.com>
2014-08-01 11:55:44 -0700

    home

--- template-home.php
+++ template-home.php
@@ -44,7 +44,7 @@ Template Name: Home Page
 
   </div><!-- /.col -->
   <div class="col-md-3">
-    <div class="panel panel-default" style="border-left-width:5px;">
+    <div class="panel panel-primary" style="border-left-width:5px;">
       <div class="list-group">
         <!--
         <a href="/chat/" class="list-group-item active">
@@ -93,7 +93,8 @@ Template Name: Home Page
 	<div class="col-md-6">
 
     <div class="panel panel-default" style="border-left-width:5px;">
-      <div class="panel-heading">
+      <div class="panel-heading text-muted">
+        <i class="glyphicon glyphicon-bullhorn"></i>
         From the library blog
       </div>
       <div class="panel-body">
