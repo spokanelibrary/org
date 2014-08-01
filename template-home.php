3a9ac2a0fc19637c03a83325b6d3690e3cdeb3ac
Sean Girard <seangirard@yahoo.com>
2014-08-01 15:16:27 -0700

    home

--- template-home.php
+++ template-home.php
@@ -122,7 +122,7 @@ Template Name: Home Page
     <div class="panel panel-default" style="border-left-width:5px;">
       <div class="panel-heading">
         <i class="glyphicon glyphicon-comment text-muted"></i>
-        <span class="text-muted">Recently posted on the <a href="/blog/">library blog</a></span>
+        <span class="text-muted">Recently posted on the <a href="/blog/">library blog</a> &rarr;</span>
       </div>
       <div class="panel-body">
         <?php echo do_shortcode('[spl_widget recent-posts]'); ?>
