781b795cdd5d9aa87f625a1417acad544f493307
Sean Girard <seangirard@yahoo.com>
2014-09-24 13:41:58 -0700

    search

--- templates/org/header-top-navbar.php
+++ templates/org/header-top-navbar.php
@@ -146,7 +146,7 @@ function spl_get_home_url() {
 </header><!-- /.navbar -->
 
 
-<header class="navbar-primary banner navbar navbar-inverse navbar-static-top" style="margin-bottom:0;" role="banner">
+<header class="navbar-primary banner navbar navbar-inverse navbar-static-top" style="margin-bottom:0; border-bottom:0;" role="banner">
   <div class="container">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
@@ -169,7 +169,7 @@ function spl_get_home_url() {
   </div>
 </header><!-- /.navbar -->
 
-<header class="navbar-primary banner navbar navbar-inverse navbar-static-top" style="border-top:0; padding-top: 2px;" role="search">
+<header class="navbar-primary banner navbar navbar-inverse navbar-static-top" style="padding-top: 2px;" role="search">
   <div class="container">
     <?php echo do_shortcode('[spl_widget enterprise-search]'); ?>
   </div>
