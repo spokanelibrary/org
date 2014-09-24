b458baf776d4c077bb7bab15d62b5c2e9ce30972
Sean Girard <seangirard@yahoo.com>
2014-09-24 15:10:01 -0700

    header

--- templates/org/header-top-navbar.php
+++ templates/org/header-top-navbar.php
@@ -104,7 +104,7 @@ function spl_get_home_url() {
           <a class="btn btn-block btn-default btn-sm text-info" 
             href="/about/" 
             title="">
-            <small class="text-muted glyphicon glyphicon-info-sign"></small>
+            <i class="glyphicon glyphicon-info-sign"></i>
             <b>About Us</b></a>
         </p>
       </div><!-- /.col -->
