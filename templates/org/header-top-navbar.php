b50e976aa530403860c67cf58edd849d73166a6c
Sean Girard <seangirard@yahoo.com>
2014-09-24 13:01:11 -0700

    search

--- templates/org/header-top-navbar.php
+++ templates/org/header-top-navbar.php
@@ -146,7 +146,7 @@ function spl_get_home_url() {
 </header><!-- /.navbar -->
 
 
-<header class="navbar-primary banner navbar navbar-inverse navbar-static-top" role="banner">
+<header class="navbar-primary banner navbar navbar-inverse navbar-static-top" style="margin-bottom:0;" role="banner">
   <div class="container">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
