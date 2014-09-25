1a48c2f9871635f8f975fc781c1a2d82d6b59b21
Sean Girard <seangirard@yahoo.com>
2014-09-25 16:01:37 -0700

    home

--- template-home.php
+++ template-home.php
@@ -23,7 +23,7 @@ Template Name: Home Page
 
 <div class="visible-sm">
   <p>
-    <b>Tip:</b> Use the <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+    <b>Tip:</b> Use the <button type="button" class="navbar-toggle" data-toggle="collapse in" data-target=".navbar-collapse">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
