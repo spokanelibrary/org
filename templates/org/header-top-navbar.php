7d3f41c001ccbd1ce2b945964cc00922bdb0a9f1
Sean Girard <seangirard@yahoo.com>
2014-09-25 11:05:22 -0700

    header

--- templates/org/header-top-navbar.php
+++ templates/org/header-top-navbar.php
@@ -180,7 +180,7 @@ function spl_get_home_url() {
 <!--
 <header class="navbar-primary banner navbar navbar-default navbar-static-top" style="padding-top: 2px;" role="search">
 -->
-<header class="navbar navbar-default">
+<header class="navbar navbar-default" style="padding-top: 2px;">
   <div class="container">
     <?php echo do_shortcode('[spl_widget enterprise-search]'); ?>
   </div>
