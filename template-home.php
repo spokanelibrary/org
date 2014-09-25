839f8ad31ab811bf814a4b6f01475cb8859575a9
Sean Girard <seangirard@yahoo.com>
2014-09-25 16:09:50 -0700

    home

--- template-home.php
+++ template-home.php
@@ -26,6 +26,7 @@ Template Name: Home Page
     <b>Tip:</b>
 
 <header class="navbar-primary banner navbar navbar-inverse navbar-static-top" style="margin-bottom:0;" role="banner">
+   Tip
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
         <span class="sr-only">Toggle navigation</span>
