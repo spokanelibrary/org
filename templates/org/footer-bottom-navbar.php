c611273e0d90bb2ff7f714bff19b1fecf922ec7a
Sean Girard <seangirard@yahoo.com>
2014-09-29 13:37:32 -0700

    footer

--- templates/org/footer-bottom-navbar.php
+++ templates/org/footer-bottom-navbar.php
@@ -8,7 +8,112 @@
 
         <?php //echo do_shortcode('[spl_widget search-catalog]'); ?>
 
-        
+        <div class="row">
+
+          <div class="col-md-3 col-sm-6">
+            <h4>
+              <a href="#">Get Connected</a> <small>&rarr;</small>
+            </h4>
+            <ul class="nav nav-pills nav-stacked">
+              <!--
+              <li class="active">
+                <a href="#">Get Connected</a>
+              </li>
+              -->
+              <li>
+                <a href="#">Send Us an Email</a>
+              </li>
+              <li>
+                <a href="#">Suggest a New Title</a>
+              </li>
+              <li>
+                <a href="#">Ask Us a Question</a>
+              </li>
+              <li>
+                <a href="#">Get a Library Card</a>
+              </li>
+            </ul>
+          </div><!-- /.col -->
+
+          <div class="col-md-3 col-sm-6">
+            <h4 class="text-muted">
+              <a href="#">Stay Current</a>
+              <small>&rarr;</small>
+            </h4>
+            <ul class="nav nav-pills nav-stacked">
+              <!--
+              <li class="active">
+                <a href="#">Stay Current</a>
+              </li>
+              -->
+              <li>
+                <a href="#">Upcoming Events</a>
+              </li>
+              <li>
+                <a href="#">Read Our Blog</a>
+              </li>
+              <li>
+                <a href="#">Your Local Branch</a>
+              </li>
+              <li>
+                <a href="#">Library Tips</a>
+              </li>
+            </ul>
+          </div><!-- /.col -->
+
+          <div class="col-md-3 col-sm-6">
+            <h4 class="text-muted">
+              <a href="#">In the Library</a>
+              <small>&rarr;</small>
+            </h4>
+            <ul class="nav nav-pills nav-stacked">
+              <!--
+              <li class="active">
+                <a href="#">In the Library</a>
+              </li>
+              -->
+              <li>
+                <a href="#">Free WiFi</a>
+              </li>
+              <li>
+                <a href="#">Reserve a Computer</a>
+              </li>
+              <li>
+                <a href="#">Printing <small>(10&#162; per page)</small></a>
+              </li>
+              <li>
+                <a href="#">Public Meeting Rooms</a>
+              </li>
+            </ul>
+          </div><!-- /.col -->
+
+          <div class="col-md-3 col-sm-6">
+            <h4 class="text-muted">
+              <a href="#">Digital Branch</a>
+              <small>&rarr;</small>
+            </h4>
+            <ul class="nav nav-pills nav-stacked">
+              <!--
+              <li class="active">
+                <a href="#">Download Something</a>
+              </li>
+              -->
+              <li>
+                <a href="#">Music <small>(.mp3)</small></a>
+              </li>
+              <li>
+                <a href="#">Magazines</a>
+              </li>
+              <li>
+                <a href="#">eBooks</a>
+              </li>
+              <li>
+                <a href="#">Audio Books</a>
+              </li>
+            </ul>
+          </div><!-- /.col -->
+
+        </div><!-- /.row -->
       </div><!-- /.panel-body -->
 
       <div class="panel-footer">
