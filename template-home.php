26189e580f3d43c551bdf2e3db2b91ab0201d8cb
Sean Girard <seangirard@yahoo.com>
2014-09-29 13:01:21 -0700

    news

--- template-home.php
+++ template-home.php
@@ -148,13 +148,12 @@ Template Name: Home Page
 
 <hr>
 
-<h3 class="text-success">
-  <i class="glyphicon glyphicon-pencil"></i> 
-  Library News
-</h3>
-
 <div class="row">
   <div class="col-md-6">
+    <h3 class="text-success">
+      <i class="glyphicon glyphicon-pencil"></i> 
+      Library News
+    </h3>
     <h2>
       <a href="#">September - Back to School, Digital Literacy, Trivia and Spokane is Reading!</a>
     </h2>
