d58fb2ddde288b2ef265da2f4f39082ee30fadd7
Sean Girard <seangirard@yahoo.com>
2014-10-07 10:43:43 -0700

    digital

--- templates/org/pages/digital-widget.php
+++ templates/org/pages/digital-widget.php
@@ -2,7 +2,7 @@
 
 <div class="media">
   <div class="media-body">
-    <a class="btn btn-block btn-success" href="/subscriptions/">
+    <a class="btn btn-block btn-warning" href="/subscriptions/">
       <i class="glyphicon glyphicon-sort-by-alphabet"></i>
       Research databases</a>
   </div>
