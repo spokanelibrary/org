e69837ead97db0e07e153c62b1509bd1aa61ec4b
Sean Girard <seangirard@yahoo.com>
2014-10-07 13:26:10 -0700

    sub

--- templates/org/pages/subscriptions.php
+++ templates/org/pages/subscriptions.php
@@ -1,5 +1,21 @@
-<p class="lead">
-	This is a subsciptions page.
-</p>
+<?php
+$cat = array(
+			 'bus'=>'Business &amp; Career'
+			,'dir'=>'Directories'
+			,'gen'=>'Genealogy'
+			,'ref'=>'General Reference'
+			,'fun'=>'Grants &amp; Fundraising'
+			,'hom'=>'Homework Help'
+			,'law'=>'Law'
+			,'mag'=>'Magazines &amp; Newspapers'
+			,'rep'=>'Repair'
+			);
 
+$sub = new stdClass();
+
+?>
+
+<pre>
+<?php print_r($cat, true); ?>
+</pre>
 
