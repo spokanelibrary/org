c4e7b6bcb617f13a456df2ab4b055d63dbd5d03c
Sean Girard <seangirard@yahoo.com>
2014-10-07 15:40:44 -0700

    sub

--- templates/org/pages/subscriptions.php
+++ templates/org/pages/subscriptions.php
@@ -91,7 +91,7 @@ function spl_get_subscriptions() {
 			$subjects = null;
 			foreach ( $db->category as $subject ) {
 				$class .= $subject . ' spl-database-subjects ';
-				$label = ('aaa' == $subject) ? 'label-info' : 'label-default';
+				$label = ('aaa' == $subject) ? 'label-danger' : 'label-info';
 				$subjects .='<p><span class="label '.$label.'">'.$cat[$subject].'</span></p>';
 			}
 
