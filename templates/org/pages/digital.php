e5a6b6da3e552f622233d2084d9a4de743760fbc
Sean Girard <seangirard@yahoo.com>
2014-10-07 10:21:38 -0700

    digital

--- templates/org/pages/digital.php
+++ templates/org/pages/digital.php
@@ -28,7 +28,7 @@
 			<div class="panel-body">
 				<hr>
 				<p class="text-center">
-					<a href="#"><b>More about online research &amp; learning</b></a> &rarr;
+					<a href="#"><b>More about online resources</b></a> &rarr;
 				</p>
 			</div><!-- /.panel-body -->
 		</div><!-- /.panel -->
