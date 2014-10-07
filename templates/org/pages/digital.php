2a34385fbc044283afcf0568b9c6e245df6090c4
Sean Girard <seangirard@yahoo.com>
2014-10-07 10:00:18 -0700

    digital

--- templates/org/pages/digital.php
+++ templates/org/pages/digital.php
@@ -11,6 +11,8 @@
 			</div><!-- /.panel-heading -->
 			<div class="panel-body">
 				<?php echo do_shortcode('[spl_widget custom-page page=download-widget]'); ?>
+				<hr>
+				<a href="#">Learn more about downloadable material</a> &rarr;
 			</div><!-- /.panel-body -->
 		</div><!-- /.panel -->
 	</div><!-- /.col -->
