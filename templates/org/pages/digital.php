0cf1178251688706a8492e2ef900a3d967dfc4d5
Sean Girard <seangirard@yahoo.com>
2014-10-09 14:36:14 -0700

    digital

--- templates/org/pages/digital.php
+++ templates/org/pages/digital.php
@@ -21,7 +21,7 @@
 		</div><!-- /.panel -->
 	</div><!-- /.col -->
 	<div class="col-sm-6">
-		<div class="panel panel-default">
+		<div class="panel panel-warning">
 			<div class="panel-heading">
 				<h4>
 					<i class="glyphicon glyphicon-globe"></i> Research &amp; education
