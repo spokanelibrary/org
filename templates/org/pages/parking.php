eae195174acb150187e60582506bb52357f032bd
Sean Girard <seangirard@yahoo.com>
2014-10-09 13:41:29 -0700

    parking

--- templates/org/pages/parking.php
+++ templates/org/pages/parking.php
@@ -6,6 +6,6 @@
 		Parking is available under the library for $.75 per half-hour for the first two half-hours and $1.00 per half-hour after that with a daily maximum of $8.00 (cash or check only, no debit or credit cards accepted). 
 	</p>
 	<p>
-		For alternative parking options, look at this downtown Spokane parking map. 
+		For alternative parking options, look at <a href="http://www.downtownspokane.org/uploads/documents/parking/downtown_spokane_parking_map.pdf">this downtown Spokane parking map</a>. 
 	</p>
 </div>
