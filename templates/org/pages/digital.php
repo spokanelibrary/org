27d9baac32a666f759e0f0537465db96200efd38
Sean Girard <seangirard@yahoo.com>
2014-10-09 12:09:10 -0700

    digital

--- templates/org/pages/digital.php
+++ templates/org/pages/digital.php
@@ -34,7 +34,7 @@
 				
 				<div class="media">
 				  <div class="media-body">
-				    <a class="btn btn-block btn-inverse" href="/subscriptions/">
+				    <a class="btn btn-block btn-default" href="/subscriptions/">
 				      <i class="glyphicon glyphicon-sort-by-alphabet"></i>
 				      Research databases</a>
 				    <small>
