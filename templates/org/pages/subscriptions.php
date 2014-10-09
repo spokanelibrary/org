38a1793fe7d3607ae40be78132d55b2c86d176f3
Sean Girard <seangirard@yahoo.com>
2014-10-09 09:18:41 -0700

    sub

--- templates/org/pages/subscriptions.php
+++ templates/org/pages/subscriptions.php
@@ -2,7 +2,7 @@
 	<dt>In-library use</dt>
 		<dd>No library card required</dd>
 	<dt>Outside the library</dt>
-		<dd>A valid library card is required, open to city residents or paying non-residents</dd>
+		<dd>A valid library card is required (open to city residents and paying non-residents)</dd>
 </dl>
 <p>
 	Some databases are restricted to in-library use as <span class="text-danger">indicated in red</span>.
