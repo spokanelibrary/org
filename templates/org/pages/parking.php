a28aac9fd7e5cc5fd625b4e81d0d674bac9a1555
Sean Girard <seangirard@yahoo.com>
2014-10-09 13:37:57 -0700

    parking

--- templates/org/pages/parking.php
+++ templates/org/pages/parking.php
@@ -1,6 +1,12 @@
 <div class="alert alert-warning">
-	<b>Note:</b>
-	The parking garage below the Downtown Library is operated by Diamond Parking.
-	Parking is available under the library for $.75 per half-hour for the first two half-hours and $1.00 per half-hour after that with a daily maximum of $8.00 (cash or check only, no debit or credit cards accepted). 
-	For alternative parking options, look at this downtown Spokane parking map. 
+	<p>
+		<b>Note:</b>
+		The parking garage below the Downtown Library is operated by Diamond Parking.
+	</p>
+	<p>
+		Parking is available under the library for $.75 per half-hour for the first two half-hours and $1.00 per half-hour after that with a daily maximum of $8.00 (cash or check only, no debit or credit cards accepted). 
+	</p>
+	<p>
+		For alternative parking options, look at this downtown Spokane parking map. 
+	</p>
 </div>
