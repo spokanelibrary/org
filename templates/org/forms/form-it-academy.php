da6846767d6935402aa08c9ae50abd2b12135f46
Sean Girard <seangirard@yahoo.com>
2014-10-09 11:24:40 -0700

    it

--- templates/org/forms/form-it-academy.php
+++ templates/org/forms/form-it-academy.php
@@ -3,7 +3,7 @@ $crass = wp_get_crass_response();
 if ( !empty($crass->request) ) {
   //include 'field/it-academy-response.php';
   } else {
-  include 'field/it-academy.php';
+  // /include 'field/it-academy.php';
 }
 ?>
 
