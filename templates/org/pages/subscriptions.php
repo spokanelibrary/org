da3d633eb58b884614dbb6c1dfd41c18c2d10e7d
Sean Girard <seangirard@yahoo.com>
2014-10-09 09:13:05 -0700

    sub

--- templates/org/pages/subscriptions.php
+++ templates/org/pages/subscriptions.php
@@ -1,6 +1,6 @@
 <dl class="dl-horizontal">
 <dt>In-library use</dt>
 <dd></dd>
-<dt>Use outside the library</d
+<dt>Use outside the library</dt>
 </dl>
 These databases are restricted to use within the library (no library card required) or, <b>outside the library</b>, to city residents or paying non-residents <b>with valid library cards</b>.</p>
