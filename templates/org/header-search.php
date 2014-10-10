2133a84b7358c7008305e4e60a066044738de5da
Sean Girard <seangirard@yahoo.com>
2014-10-10 15:29:58 -0700

    header

--- templates/org/header-search.php
+++ templates/org/header-search.php
@@ -79,7 +79,40 @@ function spl_get_home_url() {
 
     <div id="spl-account-profile"></div>
 
-    <?php include('header-social.php'); ?>
+    <div class="pull-right" style="width:360px; margin-top: 10px; margin-right: 10px;">
+    <div class="row">
+      <div class="col-xs-4">
+        <p>  
+          <a class="btn btn-block btn-default btn-sm"
+            href="http://facebook.com/spokanelibrary" 
+            style="opacity:.9;"
+            title="">
+            <img src="/assets/img/icons/16px/facebook.png">
+            <b>Facebook</b></a>
+        </p>
+      </div><!-- /.col -->
+      <div class="col-xs-4">
+        <p>
+          <a class="btn btn-block btn-default btn-sm text-info" 
+            href="http://twitter.com/spokanelibrary" 
+            style="opacity:.9;"
+            title="">
+            <img src="/assets/img/icons/16px/twitter.png">
+            <b>Twitter</b></a>
+        </p>
+      </div><!-- /.col -->
+      <div class="col-xs-4">
+        <p>
+          <a class="btn btn-block btn-default btn-sm text-info" 
+            href="/support/" 
+            style="opacity:.9;"
+            title="">
+            <i style="border:0px solid #ccc !important; border-radius:4px; padding:2px 3px;  color:#fff; background: #f0ad4e;" class="glyphicon glyphicon-hand-right"></i>
+            <b>Support Us</b></a>
+        </p>
+      </div><!-- /.col -->
+    </div><!-- /.row -->
+    </div>
 
 
     <script id="spl-account-profile-tmpl" type="text/x-handlebars-template">
