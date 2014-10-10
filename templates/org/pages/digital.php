afea16d42db06b438d6a3ad4b298672b8c5c094f
Sean Girard <seangirard@yahoo.com>
2014-10-10 08:38:58 -0700

    digital

--- templates/org/pages/digital.php
+++ templates/org/pages/digital.php
@@ -1,6 +1,6 @@
 <div class="row">
 	<div class="col-sm-5 col-md-4 pull-right clearfix" style="background:red;">
-		<div style="float:none">A</div>
+		<div style="float:none" class="clearfix">A</div>
 	</div>
 	<div class="col-sm-7 col-md-8" style="background:blue;">
 		B
