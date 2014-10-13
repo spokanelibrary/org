a84183d7d3372fc1bebf0fb922cf0235b8670158
Sean Girard <seangirard@yahoo.com>
2014-10-13 11:03:38 -0700

    blog

--- templates/content.php
+++ templates/content.php
@@ -5,31 +5,31 @@
   </header>
   <div class="entry-summary">
   	<div class="row">
-  	<div class="pull-right">
-	  	<aside>
-	  				<ul class="nav nav-pills">
-	  				<?php
-				  	$categories = get_the_category($post->ID);
-						if ( is_array($categories)) { 
-							foreach ( $categories as $category ) {
-								echo '<li>';
-								echo '<a href="' . get_category_link($category->cat_ID) . '">';
-								echo '<small>';
-								echo '<i class="glyphicon glyphicon-pushpin text-success"></i> ';
-								echo $category->name;
-								echo '</small>';
-								echo '</a>';
-								echo '</li>';
-							}
-						}		
-						?>
-						</ul>
-	  	</aside>
-	  </div>
-	  <div class="">
-    	<?php the_excerpt(); ?>
-  	</div>
-	  </div>
-  </div>
+	  	<div class="col-sm-4 col-sm-push-8">
+		  	<aside>
+  				<ul class="nav nav-pills">
+  				<?php
+			  	$categories = get_the_category($post->ID);
+					if ( is_array($categories)) { 
+						foreach ( $categories as $category ) {
+							echo '<li>';
+							echo '<a href="' . get_category_link($category->cat_ID) . '">';
+							echo '<small>';
+							echo '<i class="glyphicon glyphicon-pushpin text-success"></i> ';
+							echo $category->name;
+							echo '</small>';
+							echo '</a>';
+							echo '</li>';
+						}
+					}		
+					?>
+					</ul>
+		  	</aside>
+		  </div><!-- /.col -->
+		  <div class="col-sm-8 col-sm-pull-4">
+	    	<?php the_excerpt(); ?>
+	  	</div>
+	  </div><!-- /.col -->
+  </div><!-- /.row -->
   <hr>
 </article>
