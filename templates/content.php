2c24f2d1dcfb8e5887093c666d1f1e16d066fe24
Sean Girard <seangirard@yahoo.com>
2014-10-13 10:41:07 -0700

    blog

--- templates/content.php
+++ templates/content.php
@@ -1,7 +1,6 @@
 <article <?php post_class(); ?>>
   <header>
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <?php get_template_part('templates/entry-meta'); ?>
   </header>
   <div class="entry-summary">
   	<div class="row">
@@ -33,4 +32,5 @@
   	</div>
 	  </div>
   </div>
+  <?php get_template_part('templates/entry-meta'); ?>
 </article>
