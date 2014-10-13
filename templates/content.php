3ba68c70d754477f28ccc75888747fa101786ec2
Sean Girard <seangirard@yahoo.com>
2014-10-13 10:36:35 -0700

    blog

--- templates/content.php
+++ templates/content.php
@@ -4,27 +4,31 @@
     <?php get_template_part('templates/entry-meta'); ?>
   </header>
   <div class="entry-summary">
-  	<aside class="col-sm-4 col-md-push-8">
-  		<div class="panel panel-primary">
-  			<div class="panel-heading">
-	  			<h4 class="panel-title">
-	  				Posted in
-	  			</h4>
-  			</div>
-  			<div class="panel-body">
-  				<ul class="nav nav-list nav-stacked">
-  				<?php
-			  	$categories = get_the_category($post->ID);
-					if ( is_array($categories)) { 
-						foreach ( $categories as $category ) {
-							echo '<li>'.$category->name.'</li>';
-						}
-					}		
-					?>
-					</ul>
-  			</div>
-	  	</div>
-  	</aside>
-    <?php the_excerpt(); ?>
+  	<div class="col-sm-4 col-sm-push-8">
+	  	<aside>
+	  		<div class="panel panel-primary">
+	  			<div class="panel-heading">
+		  			<h4 class="panel-title">
+		  				Posted in
+		  			</h4>
+	  			</div>
+	  			<div class="panel-body">
+	  				<ul class="nav nav-list nav-stacked">
+	  				<?php
+				  	$categories = get_the_category($post->ID);
+						if ( is_array($categories)) { 
+							foreach ( $categories as $category ) {
+								echo '<li>'.$category->name.'</li>';
+							}
+						}		
+						?>
+						</ul>
+	  			</div>
+		  	</div>
+	  	</aside>
+	  </div>
+	  <div class="col-sm-8 col-sm-pull-4">
+    	<?php the_excerpt(); ?>
+  	</div>
   </div>
 </article>
