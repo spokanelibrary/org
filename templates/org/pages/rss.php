cd003f197a66b8fd60fc4290819a9de31d4bb5d2
Sean Girard <seangirard@yahoo.com>
2014-10-14 14:30:45 -0700

    rss

---
+++ templates/org/pages/rss.php
@@ -0,0 +1,20 @@
+<h3>What are RSS feeds?</h3>
+
+<div class="row">
+
+<div class="col-sm-4 col-md-3">
+<div class="panel panel-warning">
+<div class="panel-body">
+Some feeds
+</div><!-- /.panel-body -->
+</div><!-- /.panel -->
+</div><!-- /.col -->
+<div class="col-sm-8 col-md-9">
+Also known as news feeds, RSS stands for <em>Really Simple Syndication</em>. It is a way to publish frequently-updated content such as blogs or news articles. You will usually see the RSS feed icon if you can subscribe to something - <img src="/assets/img/icons/16px/social-rss.png" style="display:inline" alt="" /> .
+
+RSS feeds allow you to keep up-to-date on your favorite websites by "subscribing" to their feeds. New information added to those sites can be easily browsed by looking at an "aggregator." The aggregator automatically checks for updates to the feed and displays them for you. RSS aggregators are often included with your internet browser (on Internet Explorer it can be found under Favorites), your operating system (newer versions of Windows have one), or can be a separate program you load on your computer. See this <a href="http://en.wikipedia.org/wiki/List_of_feed_aggregators" target="_blank">Wikipedia article</a> for a list of available aggregators.
+
+Most aggregators allow you to subscribe by copying a link from your browser and pasting it into the aggregator. Some aggregators automatically detect the feed when you click on the link, making subscribing even easier! If in doubt, read your aggregator's help guide on how to subscribe to news feeds. See this <a href="http://en.wikipedia.org/wiki/RSS_%28file_format%29" target="_blank">Wikipedia article</a> for more information on RSS.
+</div><!-- /.col -->
+
+</div><!-- /.row -->
