3ffcdf30db0ebd7d95e142a6fe0a02a3faeab6e6
Sean Girard <seangirard@yahoo.com>
2014-10-14 15:09:35 -0700

    header

--- templates/org/header-top-navbar.php
+++ templates/org/header-top-navbar.php
@@ -28,47 +28,6 @@ function spl_get_home_url() {
   </ul>
 </header>
 
-<!--<header class="navbar-common banner navbar navbar-fixed-top" role="navigation">
-  <ul class="nav nav-justified">
-    <li>
-      <a href="<?php echo spl_get_home_url(); ?>/">
-        <span class="visible-xs"><i class="glyphicon glyphicon-home"></i></span>
-        <span class="hidden-xs"><i class="text-success glyphicon glyphicon-home"></i> Home</span>
-      </a>
-    </li>
-    <li>
-      <a href="/account/">
-        <span class="visible-xs"><i class="glyphicon glyphicon-user"></i></span>
-        <span class="hidden-xs"><i class="text-success glyphicon glyphicon-user"></i> My Account</span>
-      </a>
-    </li>
-    <li>
-      <a href="/search/">
-        <span class="visible-xs"><i class="glyphicon glyphicon-search"></i></span>
-        <span class="hidden-xs"><i class="text-success glyphicon glyphicon-search"></i> Search</span>
-      </a>
-    </li>
-    <li>
-      <a href="/browse/">
-        <span class="visible-xs"><i class="glyphicon glyphicon-book"></i></span>
-        <span class="hidden-xs"><i class="text-success glyphicon glyphicon-book"></i> Browse</span>
-      </a>
-    </li>
-    <li>
-      <a href="/download/">
-        <span class="visible-xs"><i class="glyphicon glyphicon-download-alt"></i></span>
-        <span class="hidden-xs"><i class="text-success glyphicon glyphicon-download-alt"></i> Download</span>
-      </a>
-    </li>
-    <li>
-      <a href="/connect/">
-        <span class="visible-xs"><i class="glyphicon glyphicon-envelope"></i></span>
-        <span class="hidden-xs"><i class="text-success glyphicon glyphicon-envelope"></i> Contact Us</span>
-      </a>
-    </li>
-  </ul>
-</header>--><!-- /.navbar -->
-
 <header class="navbar-masthead banner navbar navbar-inverse navbar-static-top visible-md visible-lg" role="banner">
   <div class="container">
     <div class="navbar-header">
@@ -79,8 +38,7 @@ function spl_get_home_url() {
 
     <div id="spl-account-profile"></div>
 
-    <?php //include('header-social.php'); ?>
-
+    <?php include('header-social.php'); ?>
 
     <script id="spl-account-profile-tmpl" type="text/x-handlebars-template">
       {{#if user.sessionToken}}
