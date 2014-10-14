a85cf835f45ecdbf6e530aae097a333f007c5300
Sean Girard <seangirard@yahoo.com>
2014-10-14 14:40:56 -0700

    rss

--- templates/org/pages/rss.php
+++ templates/org/pages/rss.php
@@ -19,13 +19,21 @@
 	<div class="col-sm-6 col-lg-8">
 		<h3>What are RSS feeds?</h3>
 		<p>
-			Also known as news feeds, RSS stands for <em>Really Simple Syndication</em>. It is a way to publish frequently-updated content such as blogs or news articles. You will usually see the RSS feed icon if you can subscribe to something - <img src="/assets/img/icons/16px/social-rss.png" style="display:inline" alt="" /> .
+			Also known as news feeds, RSS stands for <em>Really Simple Syndication</em>. 
+			It is a way to publish frequently-updated content such as blogs or news articles. 
+			You will usually see the RSS feed icon <img src="/assets/img/icons/16px/social-rss.png" style="display:inline" alt="" /> if you can subscribe to something.
 		</p>
 		<p>
-			RSS feeds allow you to keep up-to-date on your favorite websites by "subscribing" to their feeds. New information added to those sites can be easily browsed by looking at an "aggregator." The aggregator automatically checks for updates to the feed and displays them for you. RSS aggregators are often included with your internet browser (on Internet Explorer it can be found under Favorites), your operating system (newer versions of Windows have one), or can be a separate program you load on your computer. See this <a href="http://en.wikipedia.org/wiki/List_of_feed_aggregators" target="_blank">Wikipedia article</a> for a list of available aggregators.
+			RSS feeds allow you to keep up-to-date on your favorite websites by "subscribing" to their feeds. 
+			New information added to those sites can be easily browsed by looking at an "aggregator." 
+			The aggregator automatically checks for updates to the feed and displays them for you. 
+			RSS aggregators are often included with your internet browser (on Internet Explorer it can be found under Favorites), your operating system (newer versions of Windows have one), or can be a separate program you load on your computer. 
+			See this <a href="http://en.wikipedia.org/wiki/List_of_feed_aggregators" target="_blank">Wikipedia article</a> for a list of available aggregators.
 		</p>
 		<p>
-			Most aggregators allow you to subscribe by copying a link from your browser and pasting it into the aggregator. Some aggregators automatically detect the feed when you click on the link, making subscribing even easier! If in doubt, read your aggregator's help guide on how to subscribe to news feeds. See this <a href="http://en.wikipedia.org/wiki/RSS_%28file_format%29" target="_blank">Wikipedia article</a> for more information on RSS.
+			Most aggregators allow you to subscribe by copying a link from your browser and pasting it into the aggregator. 
+			Some aggregators automatically detect the feed when you click on the link, making subscribing even easier! If in doubt, read your aggregator's help guide on how to subscribe to news feeds. 
+			See this <a href="http://en.wikipedia.org/wiki/RSS_%28file_format%29" target="_blank">Wikipedia article</a> for more information on RSS.
 		</p>
 	</div><!-- /.col -->
 
