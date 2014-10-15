79953c465dc254a5f67616ddc3774e6e287668e9
Sean Girard <seangirard@yahoo.com>
2014-10-15 15:52:02 -0700

    digicol

--- templates/org/pages/nw-digicol.php
+++ templates/org/pages/nw-digicol.php
@@ -7,3 +7,33 @@
 <p>
 	The original items are available for use and study in the <a href="/northwest-room/">Northwest Room</a>.
 </p>
+
+<?php
+function spl_get_digicol() {
+	$digicol = array();
+
+	$digicol[] = array('url' => 'http://cdm15223.contentdm.oclc.org/cdm/landingpage/collection/p15223coll1'
+										 'img' => ''
+										 'label' => 'Spokane Fire (1889)'
+										 );
+	$html = null; 
+	foreach ( $digicol as $col ) {
+		$html .= $col['label'];
+	}
+
+	return $html;
+}
+?>
+
+<div class="row">
+
+	<div class="col-md-4">
+		<div class="panel panel-default">
+			<div class="panel-body">
+				<?php echo spl_get_digicol(); ?>
+			</div>
+		</div>
+	</div><!-- /. col -->
+
+</div><!-- /. row -->
+
