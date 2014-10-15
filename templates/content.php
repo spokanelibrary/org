937b523bcffd34a32c51dfc16afa0918a8a0cb74
Sean Girard <seangirard@yahoo.com>
2014-10-15 08:35:27 -0700

    blog

--- templates/content.php
+++ templates/content.php
@@ -1,10 +1,26 @@
-<article <?php post_class(); ?>>
-  <header>
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-  </header>
-  <div class="entry-summary">
-    <?php the_excerpt(); ?>
-  	<?php get_template_part('templates/entry-meta'); ?>
+<div class="pull-right">
+  <img src="/assets/img/icons/16px/social-rss.png" style="margin-top: -4px;" alt="RSS feeds"> <a href="./feed/"><b>RSS</b></a>
+  &nbsp;
+  <img src="/assets/img/icons/16px/social-rss.png" style="margin-top: -4px;" alt="RSS feeds"> <a href="/rss-feeds/"><b>More feeds</b></a>
+</div>
+<?php get_template_part('templates/page', 'header'); ?>
+
+<?php if (!have_posts()) : ?>
+  <div class="alert alert-warning">
+    <?php _e('Sorry, no results were found.', 'roots'); ?>
   </div>
-  <hr>
-</article>
+  <?php get_search_form(); ?>
+<?php endif; ?>
+
+<?php while (have_posts()) : the_post(); ?>
+  <?php get_template_part('templates/blog', get_post_format()); ?>
+<?php endwhile; ?>
+
+<?php if ($wp_query->max_num_pages > 1) : ?>
+  <nav class="post-nav">
+    <ul class="pager">
+      <li class="previous"><?php next_posts_link(__('&larr; Older posts', 'roots')); ?></li>
+      <li class="next"><?php previous_posts_link(__('Newer posts &rarr;', 'roots')); ?></li>
+    </ul>
+  </nav>
+<?php endif; ?>
