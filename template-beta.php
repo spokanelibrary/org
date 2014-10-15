1def6254a10b7dcaa4e9faf90ba2d96c3b14a37a
Sean Girard <seangirard@yahoo.com>
2014-10-15 09:58:50 -0700

    beta

--- template-beta.php
+++ template-beta.php
@@ -43,7 +43,7 @@ Template Name: Beta Home Page
 </p>
 
 <div class="row" style="padding-top: 20px; border-width: 4px 0; border-style: solid; border-color: rgb(100,150,75);">
-  &nbsp;
+  <p>&nbsp;</p>
 </p>
 
 <div class="row">
