0320b21216ae20b19699662d689d684b4b2a1f0d
Sean Girard <seangirard@yahoo.com>
2014-10-15 14:44:38 -0700

    nw

--- templates/org/pages/nw-resources.php
+++ templates/org/pages/nw-resources.php
@@ -1,10 +1,19 @@
+<p>
+	<a href="/northwest-room/">Northwest Room</a>
+</p>
+<p>
+	<a href="/yearbooks/">High school yearbooks</a>
+</p>
+<p>
+	<a href="/northwest-room/resources/">Northwest Room resources</a>
+</p>
 <ul>
-	<li>Biographical files</li>
-	<li>House history</li>
-	<li>Photo archive</li>
-	<li>Map collection</li>
-	<li>Full collection</li>
-	<li>Digital collections</li>
-	<li>Exhibits</li>
-	<li>High school yearbooks</li>
+	<li><a href="/northwest-room/biofiles/">Biographical files</a></li>
+	<li><a href="">House history</a></li>
+	<li><a href="">Photo archive</a></li>
+	<li><a href="">Map collection</a></li>
+	<li><a href="">Full collection</a></li>
+	<li><a href="">Digital collections</a></li>
+	<li><a href="">Exhibits</li>
+	<li><a href="">High school yearbooks</a></li>
 </ul>
