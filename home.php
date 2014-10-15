89e23c98a181c42d979844f1b6abd045d4540f5c
Sean Girard <seangirard@yahoo.com>
2014-10-15 08:58:00 -0700

    rss

--- home.php
+++ home.php
@@ -3,6 +3,7 @@
   &nbsp;
   <a class="btn btn-sm btn-default" href="/rss-feeds/"><img src="/assets/img/icons/16px/social-rss.png" style="margin-top: -2px;" alt="RSS feeds"> <b>More feeds</b></a>
 </div>
+<?php get_template_part('templates/rss'); ?>
 <?php get_template_part('templates/page', 'header'); ?>
 <hr>
 <?php if (!have_posts()) : ?>
