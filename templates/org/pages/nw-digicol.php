ee5f40decd1c0657bc792261f4ce2f53ae643b48
Sean Girard <seangirard@yahoo.com>
2014-10-15 15:35:38 -0700

    nw

--- templates/org/pages/nw-digicol.php
+++ templates/org/pages/nw-digicol.php
@@ -1,1 +1,9 @@
-NW Digital Collections
+<p class="lead">
+	The Ned M. Barnes Northwest Room of Spokane Public Library is pleased to introduce a digital collections page of photographs selected from collections in the Northwest Room. 
+</p>
+<p>
+	These collections emphasize the most frequently requested subjects in the Northwest Room &ndash; the homes, buildings, streets and activities in and around Spokane. 
+</p>
+<p>
+	The original items are available for use and study in the Northwest Room of
+</p>
