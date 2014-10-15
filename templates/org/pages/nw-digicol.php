0160710fd3144db5b7a5c3f10f2433f1759aa42f
Sean Girard <seangirard@yahoo.com>
2014-10-15 16:13:46 -0700

    digicol

--- templates/org/pages/nw-digicol.php
+++ templates/org/pages/nw-digicol.php
@@ -3,8 +3,6 @@
 </p>
 <p>
 	These collections emphasize the most frequently requested subjects in the Northwest Room &ndash; the homes, buildings, streets and activities in and around Spokane. 
-</p>
-<p>
 	The original items are available for use and study in the <a href="/northwest-room/">Northwest Room</a>.
 </p>
 
