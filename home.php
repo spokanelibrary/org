aab087bb14a22f6c13c80320bbf1ae77f368d2a5
Sean Girard <seangirard@yahoo.com>
2014-10-15 08:04:20 -0700

    blog

--- home.php
+++ home.php
@@ -1,5 +1,5 @@
 <?php get_template_part('templates/page', 'header'); ?>
-<div class="text-right">
+<div class="pull-right">
 <img src="/assets/img/icons/16px/social-rss.png" style="margin-top: -4px;" alt="RSS feeds"> <a href="/blog/feed/"><b>RSS</b></a>
 &nbsp;
 <img src="/assets/img/icons/16px/social-rss.png" style="margin-top: -4px;" alt="RSS feeds"> <a href="/rss-feeds/"><b>More feeds</b></a>
