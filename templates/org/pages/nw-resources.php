adbb24bde43cb8407e5b98021b1742d83eac8468
Sean Girard <seangirard@yahoo.com>
2014-10-15 14:37:33 -0700

    nw

--- templates/org/pages/nw-resources.php
+++ templates/org/pages/nw-resources.php
@@ -1,27 +1,10 @@
-<div class="alert alert-warning">
-	<b>Parking downtown</b>
-	<p>
-		The parking garage below the Downtown Library is operated by Diamond Parking.
-	</p>
-	<p>
-		Parking is available under the library for $.75 per half-hour for the first two half-hours and $1.00 per half-hour after that with a daily maximum of $8.00 (cash or check only, no debit or credit cards accepted). 
-	</p>
-	<p>
-		For alternative parking options, look at <a href="http://www.downtownspokane.org/uploads/documents/parking/downtown_spokane_parking_map.pdf">this downtown Spokane parking map</a>. 
-	</p>
-</div>
-
-<h3>
-	Find a library location
-	<small class="glyphicon glyphicon-flag"></small>
-</h3>
-
-<div class="btn-group">
-	<a class="btn btn-primary" href="/downtown/">Downtown</a>
-	<a class="btn btn-primary" href="/northwest-room/">Northwest Room</a>
-	<a class="btn btn-primary" href="/east-side/">East Side</a>
-	<a class="btn btn-primary" href="/hillyard/">Hillyard</a>
-	<a class="btn btn-primary" href="/indian-trail/">Indian Trail</a>
-	<a class="btn btn-primary" href="/shadle/">Shadle</a>
-	<a class="btn btn-primary" href="/south-hill/">South Hill</a>
-</div>
+<ul>
+	<li>Biographical files</li>
+	<li>House history</li>
+	<li>Photo archive</li>
+	<li>Map collection</li>
+	<li>Full collection</li>
+	<li>Digital collections</li>
+	<li>Exhibits</li>
+	<li>High school yearbooks</li>
+</ul>
