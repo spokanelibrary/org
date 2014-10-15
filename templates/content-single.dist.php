8f6a9d9140b20e29c8d651dacc76488629af844d
Sean Girard <seangirard@yahoo.com>
2014-10-15 08:21:21 -0700

    meta

--- templates/content-single.dist.php
+++ templates/content-single.dist.php
@@ -2,10 +2,10 @@
   <article <?php post_class(); ?>>
     <header>
       <h1 class="entry-title"><?php the_title(); ?></h1>
+      <?php get_template_part('templates/entry-meta'); ?>
     </header>
     <div class="entry-content">
       <?php the_content(); ?>
-      <?php get_template_part('templates/entry-meta'); ?>
     </div>
     <footer>
       <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
