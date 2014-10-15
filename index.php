87a213d7ff7f1bb63a08bae438757103779de307
Sean Girard <seangirard@yahoo.com>
2014-10-15 08:56:31 -0700

    rss

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <div class="pull-right" style="margin-top:10px;">
-  <a class="btn btn-sm btn-default" href="./feed/"><img src="/assets/img/icons/16px/social-rss.png" style="margin-top: -4px;" alt="RSS feeds"> <b>RSS</b></a>
+  <a class="btn btn-sm btn-default" href="./feed/"><img src="/assets/img/icons/16px/social-rss.png" style="margin-top: -2px;" alt="RSS feeds"> <b>RSS</b></a>
   &nbsp;
-  <a class="btn btn-sm btn-default" href="/rss-feeds/"><img src="/assets/img/icons/16px/social-rss.png" style="margin-top: -4px;" alt="RSS feeds"> <b>More feeds</b></a>
+  <a class="btn btn-sm btn-default" href="/rss-feeds/"><img src="/assets/img/icons/16px/social-rss.png" style="margin-top: -2px;" alt="RSS feeds"> <b>More feeds</b></a>
 </div>
 <?php get_template_part('templates/page', 'header'); ?>
 <hr>
