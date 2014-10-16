6c2ef60f177361249eab4ec0e4ce57ab418b60f9
Sean Girard <seangirard@yahoo.com>
2014-10-16 09:23:11 -0700

    menu

--- lib/org/init.php
+++ lib/org/init.php
@@ -496,6 +496,9 @@ function spl_menupage() {
               <div class="panel-body">
               '.get_the_content().'
               </div>
+              </div class="panel-footer">
+                <a class="btn btn-default" href="#top">Top <small>&uarr;</small></a>
+              </div>
             </div>';  
         endwhile;  
         //$output .= '</ul>';  
