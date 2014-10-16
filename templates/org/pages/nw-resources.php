cd224b3b19d9e4bf608217463c14af8d2d379148
Sean Girard <seangirard@yahoo.com>
2014-10-16 13:01:12 -0700

    nw

--- templates/org/pages/nw-resources.php
+++ templates/org/pages/nw-resources.php
@@ -1,3 +1,58 @@
+<div id="nw-room-detail">
+<hr>
+<div class="panel panel-primary">
+	<div class="panel-heading">
+		<h3 class="panel-title text-center">
+			<i class="glyphicon glyphicon-book"></i>
+			High School Yearbooks from around Spokane
+		</h3>
+	</div>
+	<div class="panel-body">
+		<p class="lead">
+			Several hundred Spokane high scool yearbooks are now available online.
+		</p>
+		<p>
+			This collection is made available with the cooperation of the <a href="http://www.spokaneschools.org/"><b>Spokane Public Schools</b></a> and the <a href="https://archive.org/details/spokanehighschoolyearbooks"><b>Internet Archive</b></a>.
+		</p>
+		<p>
+			<a class="btn btn-default btn-block" href="http://beta.spokanelibrary.org/yearbooks/">
+			View Spokane Yearbooks &rarr;</a>
+		</p>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-6">
+		<h4>Photo Archives</h4>
+		<p>
+		These collections, available online, emphasize the most frequently requested subjects in the Northwest Room – the homes, buildings, streets and activities in and around Spokane.
+		</p>
+		<p>
+		<a href="http://cdm15223.contentdm.oclc.org/cdm/">
+		<img class="img-responsive img-rounded" alt="dc-intro" src="http://dev.spokanelibrary.org/wordpress/wp-content/uploads/dc-intro.jpg" />
+		</a>
+		<p>
+		<p>
+		<a class="btn btn-default btn-block" href="http://cdm15223.contentdm.oclc.org/cdm/">Learn more &rarr;</a>
+		</p>
+	</div>
+	<div class="col-md-6">
+		<h4>Biographical Files</h4>
+		<a href="http://www.spokanelibrary.org/nw/biofiles/">
+		<img class="img-responsive img-rounded alignleft" alt="nw-bio-thumb" src="http://dev.spokanelibrary.org/wordpress/wp-content/uploads/nw-bio-thumb.jpg" />
+		</a>
+		<p>
+		For over 100 years, librarians at SPL clipped the newspapers for biographical information. These clippings include obituaries on pioneers, but they also contain all kinds of biographical information from papers in Spokane and around the Northwest. The Northwest Room has created a database for every name in these files which includes over five thousand names.
+		</p>
+		<p>
+		<a class="btn btn-default btn-block" href="http://www.spokanelibrary.org/nw/biofiles/">Learn more &rarr;</a>
+		</p>
+	</div>
+</div>
+<hr>
+</div>
+
+
+<hr>
 <b>
 	<a href="/northwest-room/">Northwest Room</a>
 </b>
@@ -22,3 +77,4 @@
 	<li><a href="/northwest-room/about/"><b>About the Northwest Room</b></li>
 	<li><a href="/northwest-room/ned-barnes/"><b>Ned M. Barnes</b></li>
 </ul>
+
