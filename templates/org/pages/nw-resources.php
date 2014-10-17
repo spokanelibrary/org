9dd7b98de0271aaf6a7850d6018c4e4c590d3717
Sean Girard <seangirard@yahoo.com>
2014-10-17 08:11:30 -0700

    nw

--- templates/org/pages/nw-resources.php
+++ templates/org/pages/nw-resources.php
@@ -17,7 +17,7 @@
 		</div>
 	</div>
 	<div class="col-md-6">
-	<?php do_shortcode('[spl_widget branch-hours branch=nw]' ?>
+	<?php do_shortcode('[spl_widget branch-hours branch=nw]'); ?>
 	</div>
 </div>
 
