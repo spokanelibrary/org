ad93599106469ce51c043db80521a35799d796dc
Sean Girard <seangirard@yahoo.com>
2014-10-17 09:54:24 -0700

    nw

--- templates/org/pages/nw-resources.php
+++ templates/org/pages/nw-resources.php
@@ -3,10 +3,12 @@
 </h3>
 
 <div class="row">
+
 	<div class="col-md-6">
 		<hr>
-		<h4>High school yearbooks</h4>
-
+		<h4>
+			<a href="/yearbooks/">High school yearbooks</a>
+		</h4>
 		<p class="clearfix">
 			<a href="/yearbooks/">
 				<img class="img-responsive img-rounded alignleft" alt="Yearbooks" src="/assets/img/digicol/nw-yearbook-thumb.jpg" />
@@ -17,10 +19,13 @@
 		<p>
 			<a class="btn btn-default btn-block" href="/yearbooks/">More &rarr;</a>
 		</p>
-	</div>
+	</div><!-- /.col -->
+
 	<div class="col-md-6">
 		<hr>
-		<h4>Biographical files</h4>
+		<h4>
+			Biographical files
+		</h4>
 		<p class="clearfix">
 			<a href="/northwest-room/biofiles/">
 				<img class="img-responsive img-rounded alignright" alt="Biofiles" src="/assets/img/digicol/nw-bio-thumb.jpg" />
@@ -31,87 +36,106 @@
 		<p>
 			<a class="btn btn-default btn-block" href="/northwest-room/biofiles/">More &rarr;</a>
 		</p>
-	</div>
-</div>
+	</div><!-- /.col -->
 
+</div><!-- /.row -->
 <div class="row">
-	<div class="col-md-6">
-		<hr>
-		<h4>Digital archives</h4>
-		<p>
-		These collections, available online, emphasize the most frequently requested subjects in the Northwest Room – the homes, buildings, streets and activities in and around Spokane.
-		</p>
-		<p>
-		<a href="/northwest-room/digital-collections/">
-		<img class="img-responsive img-rounded" alt="Digital Collections" src="/assets/img/digicol/dc-intro.jpg" />
-		</a>
-		<p>
-		<p>
-		<a class="btn btn-default btn-block" href="/northwest-room/digital-collections/">More &rarr;</a>
-		</p>
-	</div>
-	<div class="col-md-6">
-		<hr>
-		<h4>Northwest exhibit schedule</h4>
-		<p>
-		The Northwest Room displays permanent art as well as exhibits which rotate several times a year.
-		</p>
-		<p>
-		<a href="/northwest-room/exhibits/">
-		<img class="img-responsive img-rounded" alt="Exhibits" src="/assets/img/digicol/nw-exhibit-intro.jpg" />
-		</a>
-		<p>
-		<p>
-		<a class="btn btn-default btn-block" href="/northwest-room/exhibitss/">More &rarr;</a>
-		</p>
-	</div>
-</div>
 
-<div class="row">
 	<div class="col-md-6">
 		<hr>
-		<h4>House history</h4>
+		<h4>
+			Digital archives
+		</h4>
+		<p class="clearfix">
+			These collections, available online, emphasize the most frequently requested subjects in the Northwest Room – the homes, buildings, streets and activities in and around Spokane.
+		</p>
 		<p>
+			<a href="/northwest-room/digital-collections/">
+			<img class="img-responsive img-rounded" alt="Digital Collections" src="/assets/img/digicol/dc-intro.jpg" />
+			</a>
+		</p>
+		<p>
+			<a class="btn btn-default btn-block" href="/northwest-room/digital-collections/">More &rarr;</a>
+		</p>
+	</div><!-- /.col -->
+
+	<div class="col-md-6">
+		<hr>
+		<h4>
+			Northwest exhibit schedule
+		</h4>
+		<p class="clearfix">
+			The Northwest Room displays permanent art as well as exhibits which rotate several times a year.
+		</p>
+		<p>
+			<a href="/northwest-room/exhibits/">
+			<img class="img-responsive img-rounded" alt="Exhibits" src="/assets/img/digicol/nw-exhibit-intro.jpg" />
+			</a>
+		</p>
+		<p>
+			<a class="btn btn-default btn-block" href="/northwest-room/exhibitss/">More &rarr;</a>
+		</p>
+	</div><!-- /.col -->
+
+</div><!-- /.row -->
+<div class="row">
+
+	<div class="col-md-6">
+		<hr>
+		<h4>
+			House history
+		</h4>
+		<p class="clearfix">
 			Getting to know the history of your house can be a fun and fascinating process.
 		</p>
 		<p>
 			<a class="btn btn-default btn-block" href="/northwest-room/house-history/">More &rarr;</a>
 		</p>
-	</div>
+	</div><!-- /.col -->
+
 	<div class="col-md-6">
 		<hr>
-		<h4>The Fuller Collection</h4>
-		<p>
+		<h4>
+			The Fuller Collection
+		</h4>
+		<p class="clearfix">
 			The Fuller Collection consists of rare materials relating to the history of writing and printing.
 		</p>
 		<p>
 			<a class="btn btn-default btn-block" href="/northwest-room/fuller/">More &rarr;</a>
 		</p>
-	</div>
-</div>
+	</div><!-- /.col -->
 
+</div><!-- /.row -->
 <div class="row">
+
 	<div class="col-md-6">
 		<hr>
-		<h4>Northwest Map Collection</h4>
-		<p>
+		<h4>
+			Northwest Map Collection
+		</h4>
+		<p class="clearfix">
 			Maps offer a whole different dimension to studying and understanding the history of the Pacific Northwest.
 		</p>
 		<p>
 			<a class="btn btn-default btn-block" href="/northwest-room/maps/">More &rarr;</a>
 		</p>
-	</div>
+	</div><!-- /.col -->
+
 	<div class="col-md-6">
 		<hr>
-		<h4>Northwest Room Photo archive</h4>
-		<p>
+		<h4>
+			Northwest Room Photo archive
+		</h4>
+		<p class="clearfix">
 			Spokane Public Library’s small, but impressive, photograph collection contains several hundred prints offering glimpses of life in the Pacific Northwest.
 		</p>
 		<p>
 			<a class="btn btn-default btn-block" href="/northwest-room/photo-archive/">More &rarr;</a>
 		</p>
-	</div>
-</div>
+	</div><!-- /.col -->
+
+</div><!-- /.row -->
 
 
 <!--
