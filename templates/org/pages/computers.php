f0395ede4d472473f97f48d38ceae14a662736c4
Sean Girard <seangirard@yahoo.com>
2014-10-28 14:06:17 -0700

    comp

--- templates/org/pages/computers.php
+++ templates/org/pages/computers.php
@@ -8,18 +8,16 @@
 	</p>
 </div>
 
-<div class="center-block">
 <h3>
 	Reserve a computer
 	<small class="glyphicon glyphicon-time"></small>
 </h3>
 
-<div class="btn-group">
+<div class="btn-group btn-justified">
 	<a class="btn btn-info" href="http://sam.spokanelibrary.org/sam/sampatronreservationmanager.asp?branch=DT_3">Downtown <small>&rarr;</small></a>
 	<a class="btn btn-info" href="http://sam.spokanelibrary.org/sam/sampatronreservationmanager.asp?branch=ES">East Side <small>&rarr;</small></a>
 	<a class="btn btn-info" href="http://sam.spokanelibrary.org/sam/sampatronreservationmanager.asp?branch=HY">Hillyard <small>&rarr;</small></a>
 	<a class="btn btn-info" href="http://sam.spokanelibrary.org/sam/sampatronreservationmanager.asp?branch=IT">Indian Trail <small>&rarr;</small></a>
 	<a class="btn btn-info" href="http://sam.spokanelibrary.org/sam/sampatronreservationmanager.asp?branch=SH">Shadle <small>&rarr;</small></a>
 	<a class="btn btn-info" href="http://sam.spokanelibrary.org/sam/sampatronreservationmanager.asp?branch=SO">South Hill <small>&rarr;</small></a>
-</div>
 </div>
