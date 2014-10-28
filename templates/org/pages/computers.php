34c27ca8fba7fd5edb55b575d207a7e9069158f2
Sean Girard <seangirard@yahoo.com>
2014-10-28 14:12:59 -0700

    comp

--- templates/org/pages/computers.php
+++ templates/org/pages/computers.php
@@ -3,15 +3,18 @@
 		Internet access
 	</div>
 	<div class="panel-body">
-		<div class="alert alert-warning">
-			<p>
-				90-minute appointments available to valid card holders. 
-				Travelers or visitors to the area may obtain a temporary visitor's pass; ask at any <a href="/reference/">Reference and Information Desk</a>.
-			</p>
-			<p>
-				We ask that all users accessing Spokane Public Library's computer network familiarize themselves with our <a href="/policies/inet/">Internet Use Policy</a>.
-			</p>
-		</div>
+
+		<p>
+			90-minute appointments available to valid card holders. 
+			Travelers or visitors to the area may obtain a temporary visitor's pass; ask at any <a href="/reference/">Reference and Information Desk</a>.
+		</p>
+		<p>
+			Unlimited WiFi is available at all <a href="/branches/">library locations</a>.
+		</p>
+		<p>
+			We ask that all users accessing Spokane Public Library's computer network familiarize themselves with our <a href="/policies/inet/">Internet Use Policy</a>.
+		</p>
+
 
 		<h3>
 			Reserve a computer
