fcc4236930e9f07ca3e3db27537d28a4d460d47a
Sean Girard <seangirard@yahoo.com>
2014-10-28 13:23:59 -0700

    branches

---
+++ templates/org/pages/branches.php
@@ -0,0 +1,14 @@
+<h3>
+	Find a library location
+	<small class="glyphicon glyphicon-flag"></small>
+</h3>
+
+<div class="btn-group">
+	<a class="btn btn-primary" href="/downtown/">Downtown</a>
+	<a class="btn btn-primary" href="/northwest-room/">Northwest Room</a>
+	<a class="btn btn-primary" href="/east-side/">East Side</a>
+	<a class="btn btn-primary" href="/hillyard/">Hillyard</a>
+	<a class="btn btn-primary" href="/indian-trail/">Indian Trail</a>
+	<a class="btn btn-primary" href="/shadle/">Shadle</a>
+	<a class="btn btn-primary" href="/south-hill/">South Hill</a>
+</div>
