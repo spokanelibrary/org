1db4bf76a92b5de5787dfb3ce7b650c03251f86b
Sean Girard <seangirard@yahoo.com>
2014-10-28 13:40:04 -0700

    computers

---
+++ templates/org/pages/computers.php
@@ -0,0 +1,14 @@
+<h3>
+	Reserve a computer
+	<small class="glyphicon glyphicon-time"></small>
+</h3>
+
+<div class="btn-group">
+	<a class="btn btn-warning" href="/downtown/">Downtown</a>
+	<a class="btn btn-warning" href="/northwest-room/">Northwest Room</a>
+	<a class="btn btn-warning" href="/east-side/">East Side</a>
+	<a class="btn btn-warning" href="/hillyard/">Hillyard</a>
+	<a class="btn btn-warning" href="/indian-trail/">Indian Trail</a>
+	<a class="btn btn-warning" href="/shadle/">Shadle</a>
+	<a class="btn btn-warning" href="/south-hill/">South Hill</a>
+</div>
