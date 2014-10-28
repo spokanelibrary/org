44f68307bd72b79726130fc0bdb9c22e92baa6a7
Sean Girard <seangirard@yahoo.com>
2014-10-28 14:11:31 -0700

    comp

--- templates/org/pages/computers.php
+++ templates/org/pages/computers.php
@@ -1,28 +1,34 @@
-<h2>Internet access</h2>
-<div class="alert alert-warning">
-	<p>
-		90-minute appointments available to valid card holders. 
-		Travelers or visitors to the area may obtain a temporary visitor's pass; ask at any <a href="/reference/">Reference and Information Desk</a>.
-	</p>
-	<p>
-		We ask that all users accessing Spokane Public Library's computer network familiarize themselves with our <a href="/policies/inet/">Internet Use Policy</a>.
-	</p>
+<div class="panel panel-primary">
+	<div class="panel-heading">
+		Internet access
+	</div>
+	<div class="panel-body">
+		<div class="alert alert-warning">
+			<p>
+				90-minute appointments available to valid card holders. 
+				Travelers or visitors to the area may obtain a temporary visitor's pass; ask at any <a href="/reference/">Reference and Information Desk</a>.
+			</p>
+			<p>
+				We ask that all users accessing Spokane Public Library's computer network familiarize themselves with our <a href="/policies/inet/">Internet Use Policy</a>.
+			</p>
+		</div>
+
+		<h3>
+			Reserve a computer
+			<small class="glyphicon glyphicon-time"></small>
+		</h3>
+
+		<div class="btn-group">
+			<a class="btn btn-info" href="http://sam.spokanelibrary.org/sam/sampatronreservationmanager.asp?branch=DT_3">Downtown <small>&rarr;</small></a>
+			<a class="btn btn-info" href="http://sam.spokanelibrary.org/sam/sampatronreservationmanager.asp?branch=ES">East Side <small>&rarr;</small></a>
+			<a class="btn btn-info" href="http://sam.spokanelibrary.org/sam/sampatronreservationmanager.asp?branch=HY">Hillyard <small>&rarr;</small></a>
+			<a class="btn btn-info" href="http://sam.spokanelibrary.org/sam/sampatronreservationmanager.asp?branch=IT">Indian Trail <small>&rarr;</small></a>
+			<a class="btn btn-info" href="http://sam.spokanelibrary.org/sam/sampatronreservationmanager.asp?branch=SH">Shadle <small>&rarr;</small></a>
+			<a class="btn btn-info" href="http://sam.spokanelibrary.org/sam/sampatronreservationmanager.asp?branch=SO">South Hill <small>&rarr;</small></a>
+		</div>
+
+		<h4>A note about wifi</h4>
+	</div>
 </div>
-
-<h3>
-	Reserve a computer
-	<small class="glyphicon glyphicon-time"></small>
-</h3>
-
-<div class="btn-group">
-	<a class="btn btn-info" href="http://sam.spokanelibrary.org/sam/sampatronreservationmanager.asp?branch=DT_3">Downtown <small>&rarr;</small></a>
-	<a class="btn btn-info" href="http://sam.spokanelibrary.org/sam/sampatronreservationmanager.asp?branch=ES">East Side <small>&rarr;</small></a>
-	<a class="btn btn-info" href="http://sam.spokanelibrary.org/sam/sampatronreservationmanager.asp?branch=HY">Hillyard <small>&rarr;</small></a>
-	<a class="btn btn-info" href="http://sam.spokanelibrary.org/sam/sampatronreservationmanager.asp?branch=IT">Indian Trail <small>&rarr;</small></a>
-	<a class="btn btn-info" href="http://sam.spokanelibrary.org/sam/sampatronreservationmanager.asp?branch=SH">Shadle <small>&rarr;</small></a>
-	<a class="btn btn-info" href="http://sam.spokanelibrary.org/sam/sampatronreservationmanager.asp?branch=SO">South Hill <small>&rarr;</small></a>
-</div>
-
-<h4>A note about wifi</h4>
 
 <h2>Word processing computers</h2>
