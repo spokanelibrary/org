c3bca11a1324ea0956f18661bc31f7fb4553ea5e
Sean Girard <seangirard@yahoo.com>
2014-10-29 10:52:32 -0700

    wireless

--- templates/org/forms/form-wireless-login.php
+++ templates/org/forms/form-wireless-login.php
@@ -1,3 +1,20 @@
 <div class="page-header">
 	<h1>Welcome to the library <small>we're glad you're here</small></h1>
 </div>
+
+<div class="panel panel-warning">
+	<div class="panel-heading">
+		<small class="glyphicon glyphicon-exclamation-sign"></small>
+		Use of Library computers constitutes an agreement that you accept the following terms and conditions:
+	</div>
+	<div class="panel-body">
+		<p>
+			All Library customers are expected to access the Internet in compliance with the <a href="/policies/inet/">Internet Use Policy</a> and <a href="/uploads/rules_of_conduct.pdf">Rules of Conduct</a>, and within the boundaries of the law.
+  		Viewing or downloading illegal or copyright-protected materials, obscenity, child pornography, or materials harmful to minors is prohibited.
+	  	<em>Please be considerate of other Library patrons.</em>
+		</p>
+
+
+
+	</div>
+</div>
