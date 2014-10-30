bb228700839af4c0b4aea7def3b08c8da16475db
Sean Girard <seangirard@yahoo.com>
2014-10-30 11:31:29 -0700

    storytime

---
+++ templates/org/pages/storytime.php
@@ -0,0 +1,9 @@
+
+<div class="btn-group">
+	<a class="btn btn-primary" href="/calendar/storytime/dt/">Downtown</a>
+	<a class="btn btn-primary" href="/calendar/storytime/es/">East Side</a>
+	<a class="btn btn-primary" href="/calendar/storytime/hy/">Hillyard</a>
+	<a class="btn btn-primary" href="/calendar/storytime/it/">Indian Trail</a>
+	<a class="btn btn-primary" href="/calendar/storytime/sh/">Shadle</a>
+	<a class="btn btn-primary" href="/calendar/storytime/so/">South Hill</a>
+</div>
