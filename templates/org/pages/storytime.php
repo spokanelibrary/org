69c2c41d7d81a8cd04acf909ef531ff685c7d4d1
Sean Girard <seangirard@yahoo.com>
2014-10-30 11:53:09 -0700

    storytime

--- templates/org/pages/storytime.php
+++ templates/org/pages/storytime.php
@@ -1,9 +1,9 @@
 
 <div class="btn-group btn-group-justified">
-	<a class="btn btn-success" href="/calendar/storytime/dt/">Downtown</a>
-	<a class="btn btn-success" href="/calendar/storytime/es/">East Side</a>
-	<a class="btn btn-success" href="/calendar/storytime/hy/">Hillyard</a>
-	<a class="btn btn-success" href="/calendar/storytime/it/">Indian Trail</a>
-	<a class="btn btn-success" href="/calendar/storytime/sh/">Shadle</a>
-	<a class="btn btn-success" href="/calendar/storytime/so/">South Hill</a>
+	<a class="btn btn-success" href="/calendar/storytime/dt/">Downtown &rarr;</a>
+	<a class="btn btn-success" href="/calendar/storytime/es/">East Side <small>&rarr;</small></a>
+	<a class="btn btn-success" href="/calendar/storytime/hy/">Hillyard <small>&rarr;</small></a>
+	<a class="btn btn-success" href="/calendar/storytime/it/">Indian Trail <small>&rarr;</small></a>
+	<a class="btn btn-success" href="/calendar/storytime/sh/">Shadle <small>&rarr;</small></a>
+	<a class="btn btn-success" href="/calendar/storytime/so/">South Hill <small>&rarr;</small></a>
 </div>
