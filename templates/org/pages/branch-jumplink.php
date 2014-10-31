06e659944365e3bcb51199c3c88846ac06c37367
Sean Girard <seangirard@yahoo.com>
2014-10-31 15:03:38 -0700

    branches

---
+++ templates/org/pages/branch-jumplink.php
@@ -0,0 +1,9 @@
+<div class="btn-group">
+	<a class="btn btn-primary" href="#downtown">Downtown</a>
+	<a class="btn btn-primary" href="#northwest-room">Northwest Room</a>
+	<a class="btn btn-primary" href="#east-side">East Side</a>
+	<a class="btn btn-primary" href="#hillyard">Hillyard</a>
+	<a class="btn btn-primary" href="#indian-trail">Indian Trail</a>
+	<a class="btn btn-primary" href="#shadle">Shadle</a>
+	<a class="btn btn-primary" href="#south-hill">South Hill</a>
+</div>
