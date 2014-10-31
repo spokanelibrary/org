cc310fdeea8d2897ce876dec575ae5d9177f904d
Sean Girard <seangirard@yahoo.com>
2014-10-31 15:10:53 -0700

    branches

--- templates/org/pages/branch-jumplink.php
+++ templates/org/pages/branch-jumplink.php
@@ -1,8 +1,8 @@
 <div class="btn-group btn-group-justified">
-	<a class="btn btn-primary" href="#downtown">Downtown</a>
-	<a class="btn btn-primary" href="#east-side">East Side</a>
-	<a class="btn btn-primary" href="#hillyard">Hillyard</a>
-	<a class="btn btn-primary" href="#indian-trail">Indian Trail</a>
-	<a class="btn btn-primary" href="#shadle">Shadle</a>
-	<a class="btn btn-primary" href="#south-hill">South Hill</a>
+	<a class="btn btn-default" href="#downtown">Downtown</a>
+	<a class="btn btn-default" href="#east-side">East Side</a>
+	<a class="btn btn-default" href="#hillyard">Hillyard</a>
+	<a class="btn btn-default" href="#indian-trail">Indian Trail</a>
+	<a class="btn btn-default" href="#shadle">Shadle</a>
+	<a class="btn btn-default" href="#south-hill">South Hill</a>
 </div>
