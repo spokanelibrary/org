4bc900ec5c00d0ed0b212f4831c26ce29cca2612
Sean Girard <seangirard@yahoo.com>
2014-10-31 15:04:52 -0700

    jump

--- templates/org/pages/branch-jumplink.php
+++ templates/org/pages/branch-jumplink.php
@@ -1,6 +1,5 @@
 <div class="btn-group">
 	<a class="btn btn-primary" href="#downtown">Downtown</a>
-	<a class="btn btn-primary" href="#northwest-room">Northwest Room</a>
 	<a class="btn btn-primary" href="#east-side">East Side</a>
 	<a class="btn btn-primary" href="#hillyard">Hillyard</a>
 	<a class="btn btn-primary" href="#indian-trail">Indian Trail</a>
