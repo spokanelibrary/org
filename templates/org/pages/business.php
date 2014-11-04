41b5cb066aa57c36fb951cc938515fc7dcaefb3a
Sean Girard <seangirard@yahoo.com>
2014-11-04 13:03:46 -0800

    bus

--- templates/org/pages/business.php
+++ templates/org/pages/business.php
@@ -3,8 +3,9 @@
 	Competitive <b>analysis</b>, technology <b>training</b>, and market <b>research</b>: tools and support for the Spokane regional business community.
 </p>
 
+<div class="center-block">
 <img class="img-responsive" src="/assets/img/logos/sbl-masthead.png">
-
+</div>
 
 <!-- GALE COURSES -->
 
