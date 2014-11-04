e742fe5a90c9499e0bbd2a8db86f25d597b1b0ba
Sean Girard <seangirard@yahoo.com>
2014-11-04 15:33:13 -0800

    branch

--- templates/org/pages/branches.php
+++ templates/org/pages/branches.php
@@ -3,6 +3,10 @@
 	<small class="glyphicon glyphicon-flag"></small>
 </h3>
 
+<blockquote>
+Open hours, directions, contact information, upcoming events, and other details are available on the individual branch pages listed below.
+</blockquote>
+
 <div class="btn-group">
 	<a class="btn btn-primary" href="/downtown/">Downtown</a>
 	<a class="btn btn-primary" href="/northwest-room/">Northwest Room</a>
