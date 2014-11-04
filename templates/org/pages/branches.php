603eed3724e5160d3c26acf6398a60eafeb3a989
Sean Girard <seangirard@yahoo.com>
2014-11-04 15:24:11 -0800

    branch

--- templates/org/pages/branches.php
+++ templates/org/pages/branches.php
@@ -12,3 +12,18 @@
 	<a class="btn btn-primary" href="/shadle/">Shadle</a>
 	<a class="btn btn-primary" href="/south-hill/">South Hill</a>
 </div>
+
+
+<p>&nbsp;</p>
+
+<p>
+<a class="btn btn-success" href="/open/">Library open hours &rarr;</a>
+</p>
+
+<p>
+<a class="btn btn-warning" href="/calendar/">Complete calendar of events &rarr;</a>
+</p>
+
+<p>
+<a class="btn btn-danger" href="/holiday/">Holiday schedule & closed days &rarr;</a>
+</p>
