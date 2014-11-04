b64f53734fd5dac2bed2575699f56f5264a1be8b
Sean Girard <seangirard@yahoo.com>
2014-11-04 13:08:06 -0800

    bus

--- templates/org/pages/business.php
+++ templates/org/pages/business.php
@@ -1,11 +1,21 @@
-<h3 style="color:">Spokane Business Library</h3>
 <p class="lead">
 	Competitive <b>analysis</b>, technology <b>training</b>, and market <b>research</b>: tools and support for the Spokane regional business community.
 </p>
 
+<h3 style="color:">Spokane Business Library</h3>
+
 <div class="text-center">
-<img class="img-responsive" src="/assets/img/logos/sbl-masthead.png">
+	<a rel="external" href="http://www.spokanebusiness.org"><img class="img-responsive" src="/assets/img/logos/sbl-masthead.png"></a>
 </div>
+
+<p>
+	We are a founding partner in the <a rel="external" href="http://www.spokanebusiness.org">Spokane Business Library</a>, a unified resource for all Spokane-area businesses to access library services. 
+</p>
+
+<p>
+	<a rel="external" class="btn btn-warning" 
+			href="http://www.spokanebusiness.org">Visit the Spokane Business Library &rarr;</a>
+</p>
 
 <!-- GALE COURSES -->
 
