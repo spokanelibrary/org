34cd8d148e6b749726c49b4a587f3bb97f914f5c
Sean Girard <seangirard@yahoo.com>
2014-11-05 14:02:13 -0800

    404

--- 404.php
+++ 404.php
@@ -1,6 +1,6 @@
 <?php get_template_part('templates/page', 'header'); ?>
 
-<?php get_search_form(); ?>
+<?php //get_search_form(); ?>
 
 <div class="alert alert-warning">
   <?php _e('Sorry, but the page you were trying to view does not exist.', 'roots'); ?>
@@ -12,7 +12,7 @@
   <li><?php _e('an out-of-date link', 'roots'); ?></li>
 </ul>
 -->
-<?php //get_search_form(); ?>
+<?php get_search_form(); ?>
 
 <p>
 <img style="margin:auto;" class="alignnone img-responsive img-rounded" src="/assets/img/jpg/gh.jpg">
