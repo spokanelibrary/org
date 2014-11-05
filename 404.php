a3a377dbdc4a074235d3452559ae45fafbd7cd0d
Sean Girard <seangirard@yahoo.com>
2014-11-05 14:05:52 -0800

    404

--- 404.php
+++ 404.php
@@ -6,8 +6,6 @@
 	<h4>This is one of those 404 errors.</h4>
 	<p>
   <?php _e('We could not find the page you are looking for.', 'roots'); ?>
-	</p>
-  <p>
   <?php _e('Please search again or check the <a href="/sitemap/">sitemap</a>.', 'roots'); ?>
 	</p>
   <?php //_e('Sorry, but the page you were trying to view does not exist.', 'roots'); ?>
