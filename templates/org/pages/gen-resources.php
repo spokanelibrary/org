abe454d3c3a5cc7fa15f84fb561384f4bfb60120
Sean Girard <seangirard@yahoo.com>
2014-11-06 10:41:32 -0800

    gen

--- templates/org/pages/gen-resources.php
+++ templates/org/pages/gen-resources.php
@@ -1,30 +1,5 @@
 
 
-<h3>Vital records request</h3>
-<p class="lead">
-	Are you doing research from outside the greater Spokane area?
-</p>
-<p>
-	We will search for three announcements at a time and will search the newspaper for 5 days after an event date. 
-	You may interlibrary loan a microfilm copy of the newspaper from the State Library if you wish to continue the search yourself.
-</p>
-<div class="alert alert-warning">
-	<p>
-		<b>Please note:</b> This form is for obituary and birth announcement requests for out-of-town customers only. 
-	</p>
-	<p>
-		If you live in the Spokane area, we ask that you come in to do your research. 
-		<br>
-		We'll be glad to help!
-	</p>
-</div>
-<p>
-	<a class="btn btn-block btn-primary" href="/vitals/">
-		<i class="glyphicon glyphicon-search"></i>
-		Make a vital records request &rarr;</a>
-</p>
-<hr>
-
 <h3>Directory of online genealogy resources</h3>
 <p class="lead">
 	We have collected and organized links to many of the most useful websites for exploring genealogy.
