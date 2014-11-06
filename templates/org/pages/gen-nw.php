b213dfa4ff37e0d8a720c978b86f33d80b4eae13
Sean Girard <seangirard@yahoo.com>
2014-11-06 10:54:57 -0800

    gen

---
+++ templates/org/pages/gen-nw.php
@@ -0,0 +1,86 @@
+<p class="lead">
+	Some of the digital archives curated by the Northwest Room are also useful in genealogical research.
+</p>
+
+<div class="row">
+
+	<div class="col-md-6">
+		<hr>
+		<h4>
+			<a href="/northwest-room/biofiles/">Biographical files</a>
+		</h4>
+		<p class="clearfix">
+			<a href="/northwest-room/biofiles/">
+				<img class="img-responsive img-rounded alignright" alt="Biofiles" src="/assets/img/digicol/nw-bio-thumb.jpg" />
+			</a>
+			For over 100 years, librarians at SPL clipped the newspapers for biographical information.
+		</p>
+		<p>
+			These clippings include obituaries on pioneers, but they also contain all kinds of biographical information from papers in Spokane and around the Northwest.
+		</p>
+		<p>
+			<a class="btn btn-default btn-block" href="/northwest-room/biofiles/">More &rarr;</a>
+		</p>
+	</div><!-- /.col -->
+
+	<div class="col-md-6">
+		<hr>
+		<h4>
+			<a href="/yearbooks/">High school yearbooks</a>
+		</h4>
+		<p class="clearfix">
+			<a href="/yearbooks/">
+				<img class="img-responsive img-rounded alignleft" alt="Yearbooks" src="/assets/img/digicol/nw-yearbook-thumb.jpg" />
+			</a>
+			Several hundred Spokane high scool yearbooks are now available online.
+			You can view these yearbooks in your web browser without downloading any files or additional software.
+		</p>
+		<p>
+			These yearbooks are searchable and might be useful if you have a name of someone who attended high school in Spokane.
+		</p>
+		<p>
+			<a class="btn btn-default btn-block" href="/yearbooks/">More &rarr;</a>
+		</p>
+	</div><!-- /.col -->
+
+</div><!-- /.row -->
+<div class="row">
+
+	<div class="col-md-6">
+		<hr>
+		<h4>
+			<a href="/northwest-room/house-history/">House &amp; neighborhood history</a>
+		</h4>
+		<p class="clearfix">
+			Some of the information available in our section on researching house history can be useful in uncovering biographical clues.
+		</p>
+		<p>
+			<a href="/northwest-room/house-history/">
+			<img class="img-responsive img-rounded" style="margin:auto;" alt="House History" src="/assets/img/digicol/spl-homes.jpg" />
+			</a>
+		</p>
+		<p>
+			<a class="btn btn-default btn-block" href="/northwest-room/house-history/">More &rarr;</a>
+		</p>
+	</div><!-- /.col -->
+
+	<div class="col-md-6">
+		<hr>
+		<h4>
+			<a href="/northwest-room/digital-collections/">Digitized photo archives</a>
+		</h4>
+		<p class="clearfix">
+			These collections emphasize the homes, buildings, streets and activities in and around Spokane and could provide some context to your search.
+		</p>
+		<p>
+			<a href="/northwest-room/digital-collections/">
+			<img class="img-responsive img-rounded" alt="Digital Collections" src="/assets/img/digicol/dc-intro.jpg" />
+			</a>
+		</p>
+		<p>
+			<a class="btn btn-default btn-block" href="/northwest-room/digital-collections/">More &rarr;</a>
+		</p>
+	</div><!-- /.col -->
+
+</div><!-- /.row -->
+
