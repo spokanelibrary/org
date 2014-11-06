d6bb100f4115c1648a1929ac3a26ca8f21425e7c
Sean Girard <seangirard@yahoo.com>
2014-11-06 09:10:25 -0800

    gen

--- templates/org/pages/genealogy.php
+++ templates/org/pages/genealogy.php
@@ -1,6 +1,6 @@
 <h3>Obituary search</h3>
 <p class="lead">
-	Search more than 75,000 regional obituaries dating back to the early 1900s
+	Search more than 75,000 regional obituaries dating back to the early 1900s.
 </p>
 <p>
 This search will return the person’s name, the publication the obituary appeared in, as well as the publication date and page.
