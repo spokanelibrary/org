2f09270bf0d4e5fe97c40f9c85771f3320e54771
Sean Girard <seangirard@yahoo.com>
2014-11-06 09:55:02 -0800

    gen

--- templates/org/pages/gen-obits.php
+++ templates/org/pages/gen-obits.php
@@ -1,4 +1,3 @@
-<h3>Obituary search</h3>
 <p class="lead">
 	Search more than 75,000 regional obituaries dating back to the early 1900s.
 </p>
@@ -11,3 +10,32 @@ You can then use the results to make an accurate search in the appropriate publi
 		<i class="glyphicon glyphicon-search"></i>
 		Start searching obituaries &rarr;</a>
 </p>
+
+<div class="row">
+
+	<div class="col-md-6">
+		<h4>
+			<a href="http://www.ewgsi.org/areaDeaths.php">Patchen Obituary File</a>
+		</h4>
+		<p>
+			This free database is an obituary index of the Spokane papers from approximately 1940-1979. 
+			It was compiled by Lee Patchen and made available online by the volunteers of the <a href="http://www.ewgsi.org/">Eastern Washington Genealogical Society</a>.
+		</p>
+		<p>
+			<a class="btn btn-default btn-block" href="http://www.ewgsi.org/areaDeaths.php">More &rarr;</a>
+		</p>
+	</div><!-- /.col -->
+
+	<div class="col-md-6">
+		<h4>
+			<a href="/yearbooks/">Spokesman Review Obituaries</a>
+		</h4>
+		<p>
+			These yearbooks are searchable and might be useful if you have a name of someone who attended high school in Spokane.
+		</p>
+		<p>
+			<a class="btn btn-default btn-block" href="/yearbooks/">More &rarr;</a>
+		</p>
+	</div><!-- /.col -->
+
+</div><!-- /.row -->
