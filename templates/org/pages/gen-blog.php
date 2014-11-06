b0bbe627ad1ffb1bf8617cd65f55fb87c4e01b11
Sean Girard <seangirard@yahoo.com>
2014-11-06 10:19:30 -0800

    gen

---
+++ templates/org/pages/gen-blog.php
@@ -0,0 +1,14 @@
+<h4 class="text-success">
+	<i class="glyphicon glyphicon-comment"></i> 
+	Recently posted
+</h4>
+
+<?php echo do_shortcode('[spl_widget recent-posts count=3 slug=genealogy]'); ?>
+
+<p>&nbsp;</p>
+
+<p>
+	<a class="btn btn-success" href="/blog/topic/genealogy/">
+		<i class="glyphicon glyphicon-comment"></i> 
+		More genealogy posts &rarr;</a>
+</p>
