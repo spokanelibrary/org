7e51e605bc28636967abac470ff694eb8b57b3f0
Sean Girard <seangirard@yahoo.com>
2014-11-06 15:14:05 -0800

    homework

---
+++ templates/org/pages/homework-training.php
@@ -0,0 +1,32 @@
+<p>
+	The library offers in-depth technology training to people of all ages and skill levels.
+</p>
+
+<div class="row">
+	<div class="col-md-6">
+		<p class="center-block">
+			<img style="margin-top:10px; margin-bottom:10px;" src="/assets/img/logos/itacademy.png">
+		</p>
+		<p>
+			<small>
+			Courses cover a variety of Microsoft products and skill levels – ranging from beginner users to advanced technical courses that qualify for Microsoft Certification testing. 
+			</small>
+		</p>
+	</div><!-- /.col -->
+	<div class="col-md-6">
+		<p>
+			<img src="/assets/img/logos/galecourses.png">
+		</p>
+		<p>
+			<small>
+			Gale Courses offer a wide range of highly interactive courses taught entirely online by college instructors and experts in the field, ensuring a high-quality, high-impact educational experience.
+			</small>
+		</p>
+	</div><!-- /.col -->
+</div><!-- /.row -->
+
+<p>
+	<a class="btn btn-block btn-info" 
+			href="/training/">More about <b>online technology courses</b> &rarr;</a>
+</p>
+
