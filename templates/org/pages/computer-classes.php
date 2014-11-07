9dbfdc728288ffd1dfcfc05a8df8543bb90f6739
Sean Girard <seangirard@yahoo.com>
2014-11-07 09:42:53 -0800

    computer-classes

--- templates/org/pages/computer-classes.php
+++ templates/org/pages/computer-classes.php
@@ -1,3 +1,17 @@
+<div class="alert alert-info">
+	Our classes and learning labs are free and open to the public. 
+	You do not need a library card, however we do recommend signing up in advance. 
+	Registration is required when noted.
+</div>
+
+<h4>Please call 444-5336 or the hosting <a href="/branches">library branch</a> with questions.</h4>
+
+<a class="btn btn-block btn-warning" href="#upcoming">
+	<i class="glyphicon glyphicon-calendar"></i> 
+	Schedule of all upcoming classes</a>
+
+<?php echo do_shortcode('[spl_subpages_links btnclass=success btnicon=time btntext="Find dates & times"]'; ?>
+
 <p id="upcoming">&nbsp;</p>
 
 <div class="panel panel-warning">
