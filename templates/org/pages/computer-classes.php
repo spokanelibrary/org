70e293afe00ec864aa85722e26967d242c7e13f4
Sean Girard <seangirard@yahoo.com>
2014-11-07 08:20:00 -0800

    comp

--- templates/org/pages/computer-classes.php
+++ templates/org/pages/computer-classes.php
@@ -2,22 +2,29 @@
 <ul class="list-group">
 	<li class="list-group-item">
 		<h4>
-			<a href="/reference/">Beginning Internet</a> <small>&rarr;</small>
+			<a href="/calendar/find/Beginning+Internet+Explorer/">Using a web browser</a> <small>&rarr;</small>
 		</h4>
 		<p>
-			Lorem ipsum
+			Learn basic information about the internet and how to use Internet Explorer to access it.
 		</p>
 		<p>
-			<a class="btn btn-warning" href="//">
+			<a class="btn btn-warning" href="/calendar/find/Beginning+Internet+Explorer/">
 				<i class="glyphicon glyphicon-time"></i>
 				Find dates &amp; times &rarr;</a>
 		</p>
 	</li>
 	<li class="list-group-item">
 		<h4>
-			<a href="/reference/">Beginning Internet</a> <small>&rarr;</small>
+			<a href="/calendar/find/Computer+Basics/">Computer Basics</a> <small>&rarr;</small>
 		</h4>
-		Lorem ipsum
+		<p>
+			Learn basic information about computers and have opportunities to practice what you learn.
+		</p>
+		<p>
+			<a class="btn btn-warning" href="/calendar/find/Computer+Basics/">
+				<i class="glyphicon glyphicon-time"></i>
+				Find dates &amp; times &rarr;</a>
+		</p>
 	</li>
 </ul>
 
