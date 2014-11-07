8da8927cadab61d395404604972695032a3149f6
Sean Girard <seangirard@yahoo.com>
2014-11-07 07:45:59 -0800

    comp

---
+++ templates/org/pages/computer-classes.php
@@ -0,0 +1,12 @@
+
+<div class="panel panel-warning">
+	<div class="panel-heading">
+		<h3 class="panel-title">
+			<i class="glyphicon glyphicon-calendar"></i>
+			Upcoming computer &amp; technology classes
+		</h3>
+	</div><!-- /.panel-heading -->
+	<div class="panel-body">
+		<?php echo do_shortcode('[spl_widget calendar-view computer condensed]'); ?>
+	</div><!-- /.panel-body -->
+</div><!-- /.panel -->
