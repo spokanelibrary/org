bcfa0a17a966085bd663600c277c4906c6fff15b
Sean Girard <seangirard@yahoo.com>
2014-11-07 14:56:14 -0800

    tech

--- templates/org/pages/tech-training.php
+++ templates/org/pages/tech-training.php
@@ -4,6 +4,7 @@
 		<p class="center-block">
 			<img style="margin-top:10px; margin-bottom:10px;" src="/assets/img/logos/itacademy.png">
 		</p>
+		<h5>Microsoft IT Academy</h5>
 		<p>
 			Courses cover a variety of Microsoft products and skill levels – ranging from beginner users to advanced technical courses that qualify for Microsoft Certification testing. 
 		</p>
@@ -16,6 +17,7 @@
 		<p>
 			<img src="/assets/img/logos/galecourses.png">
 		</p>
+		<h5>Gale Courses</h5>
 		<p>
 			Highly interactive courses taught entirely online by college instructors and experts in the field, ensuring a high-quality, high-impact educational experience.
 		</p>
