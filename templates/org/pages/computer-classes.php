ded6fe547da98a7c68efa1c85ee1ab116974f641
Sean Girard <seangirard@yahoo.com>
2014-11-07 08:13:24 -0800

    comp

--- templates/org/pages/computer-classes.php
+++ templates/org/pages/computer-classes.php
@@ -1,27 +1,30 @@
 <div class="panel panel-primary">
 	<div class="panel-heading">
-		<h3 class="panel-title">
-			<i class="glyphicon glyphicon-tasks"></i>
-			Find a topic
-		</h3>
+		
 	</div><!-- /.panel-heading -->
 	<div class="panel-body">
-		<ul class="list-group">
-			<li class="list-group-item">
-				<h4>
-					<a href="/reference/">Beginning Internet</a> <small>&rarr;</small>
-				</h4>
-				Lorem ipsum
-			</li>
-			<li class="list-group-item">
-				<h4>
-					<a href="/reference/">Beginning Internet</a> <small>&rarr;</small>
-				</h4>
-				Lorem ipsum
-			</li>
-		</ul>
+
 	</div><!-- /.panel-body -->
 </div><!-- /.panel -->
+
+<h3 class="text-primary">
+	<i class="glyphicon glyphicon-tasks"></i>
+	Find a topic
+</h3>
+<ul class="list-group">
+	<li class="list-group-item">
+		<h4>
+			<a href="/reference/">Beginning Internet</a> <small>&rarr;</small>
+		</h4>
+		Lorem ipsum
+	</li>
+	<li class="list-group-item">
+		<h4>
+			<a href="/reference/">Beginning Internet</a> <small>&rarr;</small>
+		</h4>
+		Lorem ipsum
+	</li>
+</ul>
 
 <div class="panel panel-warning">
 	<div class="panel-heading">
