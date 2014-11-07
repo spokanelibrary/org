16a7a91d370ef75c23b5dd8b2550a39e465152fb
Sean Girard <seangirard@yahoo.com>
2014-11-07 13:52:25 -0800

    worksource

---
+++ templates/org/pages/worksource.php
@@ -0,0 +1,46 @@
+
+<img class=="alignright" src="/uploads/img/logos/Worksource_Spokane.jpg">
+<h3 class="text-success">WorkSource open hours</h3>
+<dl class="dl-horizontal">
+<dt>Tuesday</dt>
+<dd>5&ndash;8pm</dd>
+<dt>Wednesday</dt>
+<dd>5&ndash;8pm</dd>
+<dt>Saturday</dt>
+<dd>1&ndash;6pm</dd>
+<dl>
+<p class="text-right"><a class="btn btn-warning" href="/calendar/find/WorkSource/"><i class="glyphicon glyphicon-calendar"></i> Check the WorkSource calendar &rarr;</a></p>
+
+<hr>
+
+<div class="alert alert-info">
+<p>Please direct questions about this resource to the Downtown library reference desk at 444-5336 or <a href="/ask/">ask a librarian</a>. </p>
+</div>
+
+<h4>About the WorkSource Spokane Connection site</h4>
+Our WorkSource Spokane Connection site offers computers for individuals who need them for employment-related activities. If you’re working on your resume or applying for jobs online, this is a good location to do that work. 
+
+<h4>Using the WorkSource computer lab</h4>
+Unlike the library’s other Internet computers, use of these computers has <b>no time limit</b>.
+<ul>
+    <li>Reserved for employment-related work</li>
+    <li>No library card required</li>
+    <li>No time limit on computers</li>
+    <li>No charge for printing</li>
+    <li>Individual assistance provided</li>
+    <li>First come, first served</li>
+</ul>
+
+<p id="upcoming">&nbsp;</p>
+
+<div class="panel panel-warning">
+	<div class="panel-heading">
+		<h3 class="panel-title">
+			<i class="glyphicon glyphicon-calendar"></i>
+			All upcoming computer &amp; technology classes
+		</h3>
+	</div><!-- /.panel-heading -->
+	<div class="panel-body">
+		<?php echo do_shortcode('[spl_widget calendar-view find=WorkSource condensed]'); ?>
+	</div><!-- /.panel-body -->
+</div><!-- /.panel -->
