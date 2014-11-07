53591e48a5a309ea1993f869569b0406be8f8c79
Sean Girard <seangirard@yahoo.com>
2014-11-07 09:38:39 -0800

    computer

--- templates/org/pages/computer-classes.php
+++ templates/org/pages/computer-classes.php
@@ -1,38 +1,5 @@
 
-<ul class="list-group">
-	
-
-	<!--
-	<li class="list-group-item">
-		<h4>
-			<a href="/calendar/find/Beginning+Internet+Explorer/">Using a web browser</a> <small>&rarr;</small>
-		</h4>
-		<p>
-			Learn basic information about the internet and how to use Internet Explorer to access it.
-		</p>
-		<p>
-			<a class="btn btn-warning" href="/calendar/find/Beginning+Internet+Explorer/">
-				<i class="glyphicon glyphicon-time"></i>
-				Find dates &amp; times &rarr;</a>
-		</p>
-	</li>
-	<li class="list-group-item">
-		<h4>
-			<a href="/calendar/find/Computer+Basics/">Computer Basics</a> <small>&rarr;</small>
-		</h4>
-		<p>
-			Learn basic information about computers and have opportunities to practice what you learn.
-		</p>
-		<p>
-			<a class="btn btn-warning" href="/calendar/find/Computer+Basics/">
-				<i class="glyphicon glyphicon-time"></i>
-				Find dates &amp; times &rarr;</a>
-		</p>
-	</li>
-	-->
-</ul>
-
-<div class="panel panel-warning">
+<div class="panel panel-warning" id="upcoming">
 	<div class="panel-heading">
 		<h3 class="panel-title">
 			<i class="glyphicon glyphicon-calendar"></i>
