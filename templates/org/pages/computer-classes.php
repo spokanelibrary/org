0624af3b57084acdd4792a4ed703ec18e913a0f0
Sean Girard <seangirard@yahoo.com>
2014-11-07 07:52:51 -0800

    comp

--- templates/org/pages/computer-classes.php
+++ templates/org/pages/computer-classes.php
@@ -1,3 +1,16 @@
+<div class="panel panel-primary">
+	<div class="panel-heading">
+		<h3 class="panel-title">
+			<i class="glyphicon glyphicon-tasks"></i>
+			Find a topic
+		</h3>
+	</div><!-- /.panel-heading -->
+	<div class="panel-body">
+		<ul class="nav nav-pills">
+			<li><a href="">Beginning Internet &rarr;</a></li>
+		</ul>
+	</div><!-- /.panel-body -->
+</div><!-- /.panel -->
 
 <div class="panel panel-warning">
 	<div class="panel-heading">
