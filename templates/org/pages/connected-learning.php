02fdf73ebc9aefb6abf504ee0bd9f65db3ef77af
Sean Girard <seangirard@yahoo.com>
2014-11-11 09:46:11 -0800

    con

---
+++ templates/org/pages/connected-learning.php
@@ -0,0 +1,30 @@
+
+<div class="row">
+	<div class="col-md-6">
+		<div class="alert alert-success">
+			<h4 class="text-center">
+				Headline
+			</h4>
+			<p>
+				Lorem ipsum.
+			</p>
+			<p>
+				<a class="btn btn-block btn-" href="//">Learn more &rarr;</a>
+			</p>
+		</div><!-- /.alert -->
+	</div><!-- /.col -->
+	<div class="col-md-6">
+		<div class="alert alert-info">
+			<h4 class="text-center">
+				Headline
+			</h4>
+			<p>
+				Lorem ipsum.
+			</p>
+			<p>
+				<a class="btn btn-block btn-" href="//">Learn more &rarr;</a>
+			</p>
+		</div><!-- /.alert -->
+	</div><!-- /.col -->
+</div><!-- /.row -->
+
