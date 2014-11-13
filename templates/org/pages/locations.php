5703b31e716a01201725bbb30bb2defeb873d1ab
Sean Girard <seangirard@yahoo.com>
2014-11-13 13:34:22 -0800

    locations

---
+++ templates/org/pages/locations.php
@@ -0,0 +1,17 @@
+<h3>
+	Find a library location
+	<small class="glyphicon glyphicon-flag"></small>
+</h3>
+
+<blockquote>
+Open hours, directions, contact information, upcoming events, and other details are available on the individual branch pages listed below.
+</blockquote>
+
+<div class="btn-group">
+	<a class="btn btn-primary" href="/downtown/">Downtown</a>
+	<a class="btn btn-primary" href="/east-side/">East Side</a>
+	<a class="btn btn-primary" href="/hillyard/">Hillyard</a>
+	<a class="btn btn-primary" href="/indian-trail/">Indian Trail</a>
+	<a class="btn btn-primary" href="/shadle/">Shadle</a>
+	<a class="btn btn-primary" href="/south-hill/">South Hill</a>
+</div>
