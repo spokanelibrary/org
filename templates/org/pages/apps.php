a51ddd4bfded27429cfb4fc598f038679b0721ad
Sean Girard <seangirard@yahoo.com>
2014-11-19 15:21:19 -0800

    apps

--- templates/org/pages/apps.php
+++ templates/org/pages/apps.php
@@ -20,6 +20,23 @@
 
     <hr>
 
+    <div class="media">
+        <span class="media-object pull-left">
+          <img style="width:57px; height:57px;" src="/assets/img/icons/apps/overdrive.png">
+        </span>
+      <div class="media-body">
+        <h4 class="media-heading">
+          <em>OverDrive:</em> eBooks
+        </h4>
+        <p>
+          Lorem ipsum dolor sit amet.
+        </p>
+        <a href="#">iOS</a>
+        | 
+        <a href="#">Android</a>
+      </div>
+    </div><!-- /.media -->
+
   </div><!-- /.panel-body -->
 </div><!-- /.panel -->
 
