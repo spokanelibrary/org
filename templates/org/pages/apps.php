7134aa90a4dd61b03caf8892f11de1d49122abc5
Sean Girard <seangirard@yahoo.com>
2014-11-19 15:22:42 -0800

    apps

--- templates/org/pages/apps.php
+++ templates/org/pages/apps.php
@@ -2,9 +2,9 @@
   <div class="panel-body">
     
     <div class="media">
-      	<span class="media-object pull-left">
-      		<img style="width:57px; height:57px;" src="/assets/img/icons/apps/bookmyne.png">
-      	</span>
+    	<span class="media-object pull-left">
+    		<img style="width:57px; height:57px;" src="/assets/img/icons/apps/bookmyne.png">
+    	</span>
       <div class="media-body">
         <h4 class="media-heading">
         	<em>BookMyne:</em> Mobile catalog
@@ -21,12 +21,31 @@
     <hr>
 
     <div class="media">
-        <span class="media-object pull-left">
-          <img style="width:57px; height:57px;" src="/assets/img/icons/apps/overdrive.png">
-        </span>
+      <span class="media-object pull-left">
+        <img style="width:57px; height:57px;" src="/assets/img/icons/apps/overdrive.png">
+      </span>
       <div class="media-body">
         <h4 class="media-heading">
           <em>OverDrive:</em> eBooks
+        </h4>
+        <p>
+          Lorem ipsum dolor sit amet.
+        </p>
+        <a href="#">iOS</a>
+        | 
+        <a href="#">Android</a>
+      </div>
+    </div><!-- /.media -->
+
+    <hr>
+
+    <div class="media">
+      <span class="media-object pull-left">
+        <img style="width:57px; height:57px;" src="/assets/img/icons/apps/freegal.png">
+      </span>
+      <div class="media-body">
+        <h4 class="media-heading">
+          <em>Freegal:</em> Music
         </h4>
         <p>
           Lorem ipsum dolor sit amet.
