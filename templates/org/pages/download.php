0daeecfbe194738387252a6198a85d51c71f9b09
Sean Girard <seangirard@yahoo.com>
2014-11-19 13:40:54 -0800

    dl

--- templates/org/pages/download.php
+++ templates/org/pages/download.php
@@ -22,6 +22,44 @@
 					<div class="panel-heading">
 						<h4>
 							<i class="glyphicon glyphicon-cloud-download"></i> 
+							eBooks
+						</h4>
+					</div><!-- /.panel-heading -->
+					<div class="panel-body">
+						<div class="media">
+						  <a class="pull-left btn btn-success" href="#">
+						  	<span class="media-object">
+						  		<img alt="" style="width: 50px; height: 50px;" src="/assets/img/png/spl-app-icons-ebook.png">
+						  	</span>
+							</a>
+						  <div class="media-body">
+						    <h4 class="media-heading">
+						    	<a href="#"><em>OverDrive:</em> eBook Downloads</a>
+						    </h4>
+						    <p>
+						    	We have several options for online and downloadable books you can read on your computer, smart phone, tablet, or dedicated eBook reader. 
+						  	</p>
+						  	<p>
+									eBook formats vary by publisher. 
+									When possible, we attempt to provide titles in PDF or ePUB as well as AZW (Kindle) formats. 
+		 						</p>
+						    <p>
+						    	<h4>
+						    		<a href="#">Learn more</a> 
+						    		<small>&rarr;</small>
+						    	</h4>
+						    </p>
+						  </div>
+						</div><!-- /.media -->
+					</div><!-- /.panel-body -->
+				</div><!-- /.panel -->
+			</div><!-- /.col -->
+
+			<div class="col-md-6">
+				<div class="panel panel-primary" style="border-width-left:5px;">
+					<div class="panel-heading">
+						<h4>
+							<i class="glyphicon glyphicon-cloud-download"></i> 
 							Music
 						</h4>
 					</div><!-- /.panel-heading -->
@@ -59,6 +97,44 @@
 					<div class="panel-heading">
 						<h4>
 							<i class="glyphicon glyphicon-cloud-download"></i> 
+							Audio Books
+						</h4>
+					</div><!-- /.panel-heading -->
+					<div class="panel-body">
+						<div class="media">
+						  <a class="pull-left btn btn-success" href="#">
+						  	<span class="media-object">
+						  		<img alt="" style="width: 50px; height: 50px;" src="/assets/img/png/spl-app-icons-audio.png">
+						  	</span>
+							</a>
+						  <div class="media-body">
+						    <h4 class="media-heading">
+						    	<a href="#"><em>OneClick:</em> Audio Book Downloads</a>
+						    </h4>
+						    <p>
+						    	Download audio books directly to your computer or portable music player. 
+						    	Listen at home or on the go.  
+						  	</p>
+						  	<p>
+									These recordings are a great alternative to books on CD and are compatible with virtually all mp3 players on the market today, including iPods.
+		 						</p>
+						    <p>
+						    	<h4>
+						    		<a href="#">Learn more</a> 
+						    		<small>&rarr;</small>
+						    	</h4>
+						    </p>
+						  </div>
+						</div><!-- /.media -->
+					</div><!-- /.panel-body -->
+				</div><!-- /.panel -->
+			</div><!-- /.col -->
+
+			<div class="col-md-6">
+				<div class="panel panel-primary" style="border-width-left:5px;">
+					<div class="panel-heading">
+						<h4>
+							<i class="glyphicon glyphicon-cloud-download"></i> 
 							Magazines
 						</h4>
 					</div><!-- /.panel-heading -->
@@ -91,14 +167,6 @@
 				</div><!-- /.panel -->
 			</div><!-- /.col -->
 			
-			<div class="col-md-6">
-			
-			</div><!-- /.col -->
-		
-			<div class="col-md-6">
-			
-			</div><!-- /.col -->
-		
 		</div><!-- /.row -->
 
 
