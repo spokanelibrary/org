62c39d16f8c0b7fc16e6719173246297b87215fa
Sean Girard <seangirard@yahoo.com>
2014-11-19 15:09:55 -0800

    apps

--- templates/org/pages/apps.php
+++ templates/org/pages/apps.php
@@ -1,21 +1,29 @@
-<div class="media">
-  <a class="pull-left btn btn-default btn-lg" href="#">
-  	<span class="media-object">
-  		<i class="glyphicon glyphicon-download-alt"></i>
-  	</span>
-	</a>
-  <div class="media-body">
-    <h4 class="media-heading">
-    	<em>BookMyne:</em> Mobile catalog
-    </h4>
-    <a href="#">iOS</a>
-    | 
-    <a href="#">Android</a>
-    <p>
-      Lorem ipsum dolor sit amet.
-    </p>
-  </div>
-</div><!-- /.media -->
+<div class="panel panel-primary">
+  <div class="panel-body">
+    
+    <div class="media">
+      <a class="pull-left btn btn-default btn-lg" href="#">
+      	<span class="media-object">
+      		<i class="glyphicon glyphicon-download-alt"></i>
+      	</span>
+    	</a>
+      <div class="media-body">
+        <h4 class="media-heading">
+        	<em>BookMyne:</em> Mobile catalog
+        </h4>
+        <p>
+          Lorem ipsum dolor sit amet.
+        </p>
+        <a href="#">iOS</a>
+        | 
+        <a href="#">Android</a>
+      </div>
+    </div><!-- /.media -->
+
+    <hr>
+
+  </div><!-- /.panel-body -->
+</div><!-- /.panel -->
 
 
 
