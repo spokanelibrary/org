88e92f88f40d26d3a1aac3490b16b0b38f8f8cb3
Sean Girard <seangirard@yahoo.com>
2014-11-19 14:58:55 -0800

    apps

---
+++ templates/org/pages/apps.php
@@ -0,0 +1,15 @@
+<div class="media">
+  <a class="pull-left btn btn-success btn-xs" href="#">
+  	<span class="media-object">
+  		<img alt="" style="width: 30px; height: 30px;" src="/assets/img/png/spl-app-icons-music.png">
+  	</span>
+	</a>
+  <div class="media-body">
+    <h5 class="media-heading">
+    	<a href="/emusic/"><em>Freegal:</em> Music downloads</a> <small>(.mp3)</small>
+    </h5>
+  </div>
+</div><!-- /.media -->
+
+
+
