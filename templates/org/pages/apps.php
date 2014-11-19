94852294b40b5a016c903fe4c21a57e34d410e4f
Sean Girard <seangirard@yahoo.com>
2014-11-19 15:19:42 -0800

    apps

--- templates/org/pages/apps.php
+++ templates/org/pages/apps.php
@@ -2,10 +2,10 @@
   <div class="panel-body">
     
     <div class="media">
-      <span class="media-object">
+      	<span class="media-object media-left">
       		<img style="width:57px; height:57px;" src="/assets/img/icons/apps/bookmyne.png">
-      </span>
-    	<div class="media-body">
+      	</span>
+      <div class="media-body">
         <h4 class="media-heading">
         	<em>BookMyne:</em> Mobile catalog
         </h4>
