5e8164950f988bf9fdec0979a9ead0330ac694f8
Sean Girard <seangirard@yahoo.com>
2014-11-19 14:12:32 -0800

    dl

--- templates/org/pages/download.php
+++ templates/org/pages/download.php
@@ -1,192 +1,213 @@
 
-		<div class="row">
-			<div class="col-md-8">
-				<p>
-					Digital lending is a new and rapidly evolving landscape for libraries as well as for publishers. 
-					We are working hard to offer the widest range of digital content available. 
-					Our capabilities continue to change and expand. 
-					Don't forget to check back often. 
+<div class="row">
+	<div class="col-md-8">
+		<p>
+			Digital lending is a new and rapidly evolving landscape for libraries as well as for publishers. 
+			We are working hard to offer the widest range of digital content available. 
+			Our capabilities continue to change and expand. 
+			Don't forget to check back often. 
+		</p>
+		<p>
+			Feel free to <a href="/contact/">contact us</a> with questions. 
+			We’ll do our best to help.
+		</p>
+		<p>
+			<b>
+				You can also visit our <a href="/digital/">Digital Branch</a> for online research and educational tools.
+			</b>
+		</p>
+	</div><!-- /.col -->
+	<div class="col-md-4">
+		<div class="panel panel-success">
+			<div class="panel-heading">
+				<h4 class="panel-title">
+					<i class="glyphicon glyphicon-phone"></i> Library apps
+				</h4>
+			</div><!-- /.panel-heading -->
+			<div class="panel-body">
+
+					<h4 class="text-success">Use your phone or tablet to:</h4> 
+					<ul>
+						<li><b>search</b> the catalog</li>
+						<li><b>manage</b> your account</li>
+						<li><b>download</b> books &amp; music
+					</ul>
+				<p class="text-right text-muted">
+					<small>&hellip; and much more!</small>
 				</p>
 				<p>
-					Feel free to <a href="/contact/">contact us</a> with questions. 
-					We’ll do our best to help.
+					<a class="btn btn-block btn-success" 
+						href="/apps/"><i class="glyphicon glyphicon-phone"></i> Get library apps &rarr;</a> 
 				</p>
-				<p>
-					<b>
-						You can also visit our <a href="/digital/">Digital Branch</a> for online research and educational tools.
-					</b>
-				</p>
-			</div><!-- /.col -->
-			<div class="col-md-4">
-				<p>
-					Downloads are provided by third-party vendors. 
-					<br>
-					<small>
-					In most cases you will need to create an account, and you may need to download software to access these materials. 
-					</small>
-				</p>
-			</div><!-- /.col -->
-		</div><!-- /.row -->
+			</div><!-- /.panel-body -->
+		</div><!-- /.panel -->
+	</div><!-- /.col -->
+</div><!-- /.row -->
 
 
-		<div class="row">
-			
-			<div class="col-md-6">
-				<div class="panel panel-primary" style="border-width-left:5px;">
-					<div class="panel-heading">
-						<h4 class="panel-title">
-							<i class="glyphicon glyphicon-cloud-download"></i> 
-							eBooks
-						</h4>
-					</div><!-- /.panel-heading -->
-					<div class="panel-body">
-						<div class="media">
-						  <a class="pull-left btn btn-success" href="/ebooks/">
-						  	<span class="media-object">
-						  		<img alt="" style="width: 50px; height: 50px;" src="/assets/img/png/spl-app-icons-ebook.png">
-						  	</span>
-							</a>
-						  <div class="media-body">
-						    <h4 class="media-heading">
-						    	<a href="/ebooks/"><em>OverDrive:</em> eBook Downloads</a>
-						    </h4>
-						    <p>
-						    	We have several options for books you can read on your computer, smart phone, tablet, or dedicated eBook reader. 
-						  	</p>
-						  	<p>
-									eBook formats vary by publisher. 
-									When possible, we attempt to provide titles in PDF or ePUB as well as AZW (Kindle) formats. 
-		 						</p>
-						    <p>
-						    	<h4>
-						    		<a href="/ebooks/">Learn more</a> 
-						    		<small>&rarr;</small>
-						    	</h4>
-						    </p>
-						  </div>
-						</div><!-- /.media -->
-					</div><!-- /.panel-body -->
-				</div><!-- /.panel -->
-			</div><!-- /.col -->
+<div class="row">
+	
+	<div class="col-md-6">
+		<div class="panel panel-primary" style="border-width-left:5px;">
+			<div class="panel-heading">
+				<h4 class="panel-title">
+					<i class="glyphicon glyphicon-cloud-download"></i> 
+					eBooks
+				</h4>
+			</div><!-- /.panel-heading -->
+			<div class="panel-body">
+				<div class="media">
+				  <a class="pull-left btn btn-success" href="/ebooks/">
+				  	<span class="media-object">
+				  		<img alt="" style="width: 50px; height: 50px;" src="/assets/img/png/spl-app-icons-ebook.png">
+				  	</span>
+					</a>
+				  <div class="media-body">
+				    <h4 class="media-heading">
+				    	<a href="/ebooks/"><em>OverDrive:</em> eBook Downloads</a>
+				    </h4>
+				    <p>
+				    	We have several options for books you can read on your computer, smart phone, tablet, or dedicated eBook reader. 
+				  	</p>
+				  	<p>
+							eBook formats vary by publisher. 
+							When possible, we attempt to provide titles in PDF or ePUB as well as AZW (Kindle) formats. 
+ 						</p>
+				    <p>
+				    	<h4>
+				    		<a href="/ebooks/">Learn more</a> 
+				    		<small>&rarr;</small>
+				    	</h4>
+				    </p>
+				  </div>
+				</div><!-- /.media -->
+			</div><!-- /.panel-body -->
+		</div><!-- /.panel -->
+	</div><!-- /.col -->
 
-			<div class="col-md-6">
-				<div class="panel panel-primary" style="border-width-left:5px;">
-					<div class="panel-heading">
-						<h4 class="panel-title">
-							<i class="glyphicon glyphicon-cloud-download"></i> 
-							Music
-						</h4>
-					</div><!-- /.panel-heading -->
-					<div class="panel-body">
-						<div class="media">
-						  <a class="pull-left btn btn-success" href="/emusic/">
-						  	<span class="media-object">
-						  		<img alt="" style="width: 50px; height: 50px;" src="/assets/img/png/spl-app-icons-music.png">
-						  	</span>
-							</a>
-						  <div class="media-body">
-						    <h4 class="media-heading">
-						    	<a href="/emusic/"><em>Freegal:</em> Music Downloads</a> <small>(.mp3)</small>
-						    </h4>
-						    <p>
-						    	<strong>Download 5 free songs every week</strong> – yours to keep! 
-						  	</p>
-						  	<p>
-									With over 3 million tracks from 11,000 record labels spanning more than 100 genres of music, we think everyone can find a reason (or 5) to like this service. 
-		 						</p>
-						    <p>
-						    	<h4>
-						    		<a href="/emusic/">Learn more</a> 
-						    		<small>&rarr;</small>
-						    	</h4>
-						    </p>
-						  </div>
-						</div><!-- /.media -->
-					</div><!-- /.panel-body -->
-				</div><!-- /.panel -->
-			</div><!-- /.col -->
-		
-		</div><!-- /.row -->
+	<div class="col-md-6">
+		<div class="panel panel-primary" style="border-width-left:5px;">
+			<div class="panel-heading">
+				<h4 class="panel-title">
+					<i class="glyphicon glyphicon-cloud-download"></i> 
+					Music
+				</h4>
+			</div><!-- /.panel-heading -->
+			<div class="panel-body">
+				<div class="media">
+				  <a class="pull-left btn btn-success" href="/emusic/">
+				  	<span class="media-object">
+				  		<img alt="" style="width: 50px; height: 50px;" src="/assets/img/png/spl-app-icons-music.png">
+				  	</span>
+					</a>
+				  <div class="media-body">
+				    <h4 class="media-heading">
+				    	<a href="/emusic/"><em>Freegal:</em> Music Downloads</a> <small>(.mp3)</small>
+				    </h4>
+				    <p>
+				    	<strong>Download 5 free songs every week</strong> – yours to keep! 
+				  	</p>
+				  	<p>
+							With over 3 million tracks from 11,000 record labels spanning more than 100 genres of music, we think everyone can find a reason (or 5) to like this service. 
+ 						</p>
+				    <p>
+				    	<h4>
+				    		<a href="/emusic/">Learn more</a> 
+				    		<small>&rarr;</small>
+				    	</h4>
+				    </p>
+				  </div>
+				</div><!-- /.media -->
+			</div><!-- /.panel-body -->
+		</div><!-- /.panel -->
+	</div><!-- /.col -->
 
-		<div class="row">
+</div><!-- /.row -->
 
-			<div class="col-md-6">
-				<div class="panel panel-primary" style="border-width-left:5px;">
-					<div class="panel-heading">
-						<h4 class="panel-title">
-							<i class="glyphicon glyphicon-cloud-download"></i> 
-							Audio Books
-						</h4>
-					</div><!-- /.panel-heading -->
-					<div class="panel-body">
-						<div class="media">
-						  <a class="pull-left btn btn-success" href="/eaudio/">
-						  	<span class="media-object">
-						  		<img alt="" style="width: 50px; height: 50px;" src="/assets/img/png/spl-app-icons-audio.png">
-						  	</span>
-							</a>
-						  <div class="media-body">
-						    <h4 class="media-heading">
-						    	<a href="/eaudio/"><em>OneClick:</em> Audio Book Downloads</a>
-						    </h4>
-						    <p>
-						    	Download audio books directly to your computer or portable music player. 
-						  	</p>
-						  	<p>
-									These recordings are a great alternative to books on CD and are compatible with virtually all mp3 players, smartphones, and tablets on the market today.
-		 						</p>
-						    <p>
-						    	<h4>
-						    		<a href="/eaudio/">Learn more</a> 
-						    		<small>&rarr;</small>
-						    	</h4>
-						    </p>
-						  </div>
-						</div><!-- /.media -->
-					</div><!-- /.panel-body -->
-				</div><!-- /.panel -->
-			</div><!-- /.col -->
+<div class="row">
 
-			<div class="col-md-6">
-				<div class="panel panel-primary" style="border-width-left:5px;">
-					<div class="panel-heading">
-						<h4 class="panel-title">
-							<i class="glyphicon glyphicon-cloud-download"></i> 
-							Magazines
-						</h4>
-					</div><!-- /.panel-heading -->
-					<div class="panel-body">
-						<div class="media">
-						  <a class="pull-left btn btn-success" href="/emagazines/">
-						  	<span class="media-object">
-						  		<img alt="" style="width: 50px; height: 50px;" src="/assets/img/png/spl-app-icons-magazine.png">
-						  	</span>
-							</a>
-						  <div class="media-body">
-						    <h4 class="media-heading">
-						    	<a href="/emagazines/"><em>Zinio:</em> Magazine Downloads</a>
-						    </h4>
-						    <p>
-						    	Download current magazines from the Library's digital collection using your desktop or mobile device. 
-						  	</p>
-						  	<p>
-									There are no limits or expiration dates. Apps are available for most platforms. 
-		 						</p>
-						    <p>
-						    	<h4>
-						    		<a href="/emagazines/">Learn more</a> 
-						    		<small>&rarr;</small>
-						    	</h4>
-						    </p>
-						  </div>
-						</div><!-- /.media -->
-					</div><!-- /.panel-body -->
-				</div><!-- /.panel -->
-			</div><!-- /.col -->
-			
-		</div><!-- /.row -->
+	<div class="col-md-6">
+		<div class="panel panel-primary" style="border-width-left:5px;">
+			<div class="panel-heading">
+				<h4 class="panel-title">
+					<i class="glyphicon glyphicon-cloud-download"></i> 
+					Audio Books
+				</h4>
+			</div><!-- /.panel-heading -->
+			<div class="panel-body">
+				<div class="media">
+				  <a class="pull-left btn btn-success" href="/eaudio/">
+				  	<span class="media-object">
+				  		<img alt="" style="width: 50px; height: 50px;" src="/assets/img/png/spl-app-icons-audio.png">
+				  	</span>
+					</a>
+				  <div class="media-body">
+				    <h4 class="media-heading">
+				    	<a href="/eaudio/"><em>OneClick:</em> Audio Book Downloads</a>
+				    </h4>
+				    <p>
+				    	Download audio books directly to your computer or portable music player. 
+				  	</p>
+				  	<p>
+							These recordings are a great alternative to books on CD and are compatible with virtually all mp3 players, smartphones, and tablets on the market today.
+ 						</p>
+				    <p>
+				    	<h4>
+				    		<a href="/eaudio/">Learn more</a> 
+				    		<small>&rarr;</small>
+				    	</h4>
+				    </p>
+				  </div>
+				</div><!-- /.media -->
+			</div><!-- /.panel-body -->
+		</div><!-- /.panel -->
+	</div><!-- /.col -->
 
+	<div class="col-md-6">
+		<div class="panel panel-primary" style="border-width-left:5px;">
+			<div class="panel-heading">
+				<h4 class="panel-title">
+					<i class="glyphicon glyphicon-cloud-download"></i> 
+					Magazines
+				</h4>
+			</div><!-- /.panel-heading -->
+			<div class="panel-body">
+				<div class="media">
+				  <a class="pull-left btn btn-success" href="/emagazines/">
+				  	<span class="media-object">
+				  		<img alt="" style="width: 50px; height: 50px;" src="/assets/img/png/spl-app-icons-magazine.png">
+				  	</span>
+					</a>
+				  <div class="media-body">
+				    <h4 class="media-heading">
+				    	<a href="/emagazines/"><em>Zinio:</em> Magazine Downloads</a>
+				    </h4>
+				    <p>
+				    	Download current magazines from the Library's digital collection using your desktop or mobile device. 
+				  	</p>
+				  	<p>
+							There are no limits or expiration dates. Apps are available for most platforms. 
+ 						</p>
+				    <p>
+				    	<h4>
+				    		<a href="/emagazines/">Learn more</a> 
+				    		<small>&rarr;</small>
+				    	</h4>
+				    </p>
+				  </div>
+				</div><!-- /.media -->
+			</div><!-- /.panel-body -->
+		</div><!-- /.panel -->
+	</div><!-- /.col -->
+	
+</div><!-- /.row -->
 
+<hr>
+
+<h4>Downloads are provided by third-party vendors.</h4>
+<p>
+	In most cases you will need to create an account, and you may need to download software to access these materials. 
+</p>
 
 
 
