11d6dd2e7e6747ee52701741ad6b16edda9639af
Sean Girard <seangirard@yahoo.com>
2014-11-19 14:15:42 -0800

    dl

--- templates/org/pages/download.php
+++ templates/org/pages/download.php
@@ -26,7 +26,7 @@
 			</div><!-- /.panel-heading -->
 			<div class="panel-body">
 				<p>
-					We have apps for most of our digital downloads.
+					Check it out: we have apps for most of our digital downloads.
 				</p>
 				<p>
 					<a class="btn btn-block btn-success" 
