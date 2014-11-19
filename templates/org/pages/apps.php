a5fec148987899eb6c6cd9ba97acebd53ce474d8
Sean Girard <seangirard@yahoo.com>
2014-11-19 15:24:48 -0800

    apps

--- templates/org/pages/apps.php
+++ templates/org/pages/apps.php
@@ -56,6 +56,44 @@
       </div>
     </div><!-- /.media -->
 
+    <hr>
+
+    <div class="media">
+      <span class="media-object pull-left">
+        <img style="width:57px; height:57px;" src="/assets/img/icons/apps/oneclick.png">
+      </span>
+      <div class="media-body">
+        <h4 class="media-heading">
+          <em>OneClick:</em> Audio Books
+        </h4>
+        <p>
+          Lorem ipsum dolor sit amet.
+        </p>
+        <a href="#">iOS</a>
+        | 
+        <a href="#">Android</a>
+      </div>
+    </div><!-- /.media -->
+
+    <hr>
+
+    <div class="media">
+      <span class="media-object pull-left">
+        <img style="width:57px; height:57px;" src="/assets/img/icons/apps/zinio.png">
+      </span>
+      <div class="media-body">
+        <h4 class="media-heading">
+          <em>Zinio:</em> Magazines
+        </h4>
+        <p>
+          Lorem ipsum dolor sit amet.
+        </p>
+        <a href="#">iOS</a>
+        | 
+        <a href="#">Android</a>
+      </div>
+    </div><!-- /.media -->
+
   </div><!-- /.panel-body -->
 </div><!-- /.panel -->
 
