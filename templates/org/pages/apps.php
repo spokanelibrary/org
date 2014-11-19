95b466a170c416e5e7ad3f445ff2c75f58ff929a
Sean Girard <seangirard@yahoo.com>
2014-11-19 15:17:40 -0800

    apps

--- templates/org/pages/apps.php
+++ templates/org/pages/apps.php
@@ -2,12 +2,10 @@
   <div class="panel-body">
     
     <div class="media">
-      <a class="pull-left btn btn-default btn-lg" href="#">
-      	<span class="media-object">
+      <span class="media-object">
       		<img style="width:57px; height:57px;" src="/assets/img/icons/apps/bookmyne.png">
-      	</span>
-    	</a>
-      <div class="media-body">
+      </span>
+    	<div class="media-body">
         <h4 class="media-heading">
         	<em>BookMyne:</em> Mobile catalog
         </h4>
