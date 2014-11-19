8835985213e59a7ed5f7cc606763f17f35f44dfb
Sean Girard <seangirard@yahoo.com>
2014-11-19 14:19:09 -0800

    dl

--- templates/org/pages/download.php
+++ templates/org/pages/download.php
@@ -2,7 +2,6 @@
 <div class="row">
 	<div class="col-md-8">
 		<p>
-			Digital lending is a new and rapidly evolving landscape for libraries as well as for publishers. 
 			We are working hard to offer the widest range of digital content available. 
 			Our capabilities continue to change and expand. 
 			Don't forget to check back often. 
