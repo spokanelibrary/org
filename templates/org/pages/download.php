12e2a33a64cf5e852e05d1eab0d8c29f53bd5f17
Sean Girard <seangirard@yahoo.com>
2014-11-19 13:37:53 -0800

    dl

--- templates/org/pages/download.php
+++ templates/org/pages/download.php
@@ -14,6 +14,100 @@
 			We’ll do our best to help.
 		</p>
 
+
+		<div class="row">
+			
+			<div class="col-md-6">
+				<div class="panel panel-primary" style="border-width-left:5px;">
+					<div class="panel-heading">
+						<h4>
+							<i class="glyphicon glyphicon-cloud-download"></i> 
+							Music
+						</h4>
+					</div><!-- /.panel-heading -->
+					<div class="panel-body">
+						<div class="media">
+						  <a class="pull-left btn btn-success" href="#">
+						  	<span class="media-object">
+						  		<img alt="" style="width: 50px; height: 50px;" src="/assets/img/png/spl-app-icons-music.png">
+						  	</span>
+							</a>
+						  <div class="media-body">
+						    <h4 class="media-heading">
+						    	<a href="#"><em>Freegal:</em> Music Downloads</a> <small>(.mp3)</small>
+						    </h4>
+						    <p>
+						    	<strong>Download 5 free songs every week</strong> – yours to keep! 
+						  	</p>
+						  	<p>
+									With over 3 million tracks from 11,000 record labels spanning more than 100 genres of music, we think everyone can find a reason (or 5) to like this service. 
+		 						</p>
+						    <p>
+						    	<h4>
+						    		<a href="#">Learn more</a> 
+						    		<small>&rarr;</small>
+						    	</h4>
+						    </p>
+						  </div>
+						</div><!-- /.media -->
+					</div><!-- /.panel-body -->
+				</div><!-- /.panel -->
+			</div><!-- /.col -->
+			
+			<div class="col-md-6">
+				<div class="panel panel-primary" style="border-width-left:5px;">
+					<div class="panel-heading">
+						<h4>
+							<i class="glyphicon glyphicon-cloud-download"></i> 
+							Magazines
+						</h4>
+					</div><!-- /.panel-heading -->
+					<div class="panel-body">
+						<div class="media">
+						  <a class="pull-left btn btn-success" href="#">
+						  	<span class="media-object">
+						  		<img alt="" style="width: 50px; height: 50px;" src="/assets/img/png/spl-app-icons-magazine.png">
+						  	</span>
+							</a>
+						  <div class="media-body">
+						    <h4 class="media-heading">
+						    	<a href="#"><em>Zinio:</em> Magazine Downloads</a>
+						    </h4>
+						    <p>
+						    	Download current magazines from the Library's digital collection using your desktop or mobile device. 
+						  	</p>
+						  	<p>
+									There are no limits or expiration dates. Apps are available for most platforms. 
+		 						</p>
+						    <p>
+						    	<h4>
+						    		<a href="#">Learn more</a> 
+						    		<small>&rarr;</small>
+						    	</h4>
+						    </p>
+						  </div>
+						</div><!-- /.media -->
+					</div><!-- /.panel-body -->
+				</div><!-- /.panel -->
+			</div><!-- /.col -->
+			
+			<div class="col-md-6">
+			
+			</div><!-- /.col -->
+		
+			<div class="col-md-6">
+			
+			</div><!-- /.col -->
+		
+		</div><!-- /.row -->
+
+
+
+
+
+
+
+
 		<div class="panel panel-primary" style="border-width-left:5px;">
 			<div class="panel-heading">
 				<h4>
