d57ecb37176056bea3529df79b1e09d8faccde34
Sean Girard <seangirard@yahoo.com>
2014-11-24 15:00:37 -0800

    slug

--- template-home.php
+++ template-home.php
@@ -75,7 +75,7 @@ Template Name: Home Page
 <div class="row">
   
   <div class="col-md-6 hidden-xs">
-    <?php echo do_shortcode('[spl_carousel auto slug=kiosk/dt-1]'); ?>
+    <?php echo do_shortcode('[spl_carousel auto slug=carousel]'); ?>
   </div><!-- /.col -->
 
   <div class="col-md-3">
