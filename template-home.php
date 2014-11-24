4e8ddf1bcb3cd2feaa72dad2ba46253593c57c7d
Sean Girard <seangirard@yahoo.com>
2014-11-24 13:50:35 -0800

    slides

--- template-home.php
+++ template-home.php
@@ -75,7 +75,6 @@ Template Name: Home Page
 <div class="row">
   
   <div class="col-md-6 hidden-xs">
-    <?php //echo do_shortcode('[spl_carousel auto]'); ?>
     <?php echo do_shortcode('[spl_carousel auto]'); ?>
   </div><!-- /.col -->
 
