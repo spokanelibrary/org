69fc2da7ca8ce041d9561dd9351d9ce9ad673d44
Sean Girard <seangirard@yahoo.com>
2014-11-24 14:07:27 -0800

    digi

--- templates/org/pages/digital-widget.php
+++ templates/org/pages/digital-widget.php
@@ -8,6 +8,24 @@
   </div>
 </div><!-- /.media -->
 
+
+<div class="media">
+  <a class="pull-left btn btn-success btn-xs" href="/subscriptions/">
+  	<div class="media-object">
+  		<h4 class="spl-btn-icon">
+  			<span class="label label-primary">
+  				<i class="glyphicon glyphicon-sort-by-alphabet"></i>
+  			</span>
+  		</h4>
+  	</div>
+	</a>
+
+  <div class="media-body">
+    	<a href="/subscriptions/">Subscription databases</a>
+  </div>
+</div><!-- /.media -->
+
+
 <div class="media">
   <div class="media-body">
     <a class="btn btn-block btn-link" href="/digital/">Visit the digital branch &rarr;</a>
