df6eadd220377a6c932117e94ec998980c4d9c61
Sean Girard <seangirard@yahoo.com>
2014-11-24 14:13:28 -0800

    dl

--- templates/org/pages/digital-widget.php
+++ templates/org/pages/digital-widget.php
@@ -7,7 +7,7 @@
 	</a>
   <div class="media-body">
   	<h5 class="media-heading">
-    	<a href="/subscriptions/">Online research subscriptions</a>
+    	<a href="/subscriptions/"><em>Research:</em> subscription databases</a>
     </h5>
   </div>
 </div><!-- /.media -->
