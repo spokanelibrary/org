fb4b9db94389ce9098e5736bebad90d600587073
Sean Girard <seangirard@yahoo.com>
2014-11-25 08:03:43 -0800

    digital

--- templates/org/pages/digital-widget.php
+++ templates/org/pages/digital-widget.php
@@ -1,4 +1,4 @@
-<h5 class="text-muted" style="margin-top:0;"><a href="/connected-learning/">Connected Learning<a/>. <a href="/subscriptions/">Research</a>. <a href="/apps/">Entertainment</a>.</h5>
+<h5 class="text-muted" style="margin-top:0;"><a href="/connected-learning/">Connected Learning <small>&rarr;</small></a> <a href="/subscriptions/">Research</a> <a href="/apps/">Entertainment</a></h5>
 <div class="media">
   <div class="media-body">
     <a class="btn btn-block btn-default" href="/digital/">Visit the digital branch &rarr;</a>
