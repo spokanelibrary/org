775a5d4b47fc0f7ffe6c25fe8c788531abdeba67
Sean Girard <seangirard@yahoo.com>
2014-11-25 07:57:03 -0800

    digital

--- templates/org/pages/digital-widget.php
+++ templates/org/pages/digital-widget.php
@@ -1,4 +1,4 @@
-<h5>Connected learning, research tools, &amp; entertainment</h5>
+<h5>Connected Learning. Research tools. Great entertainment</h5>
 <div class="media">
   <div class="media-body">
     <a class="btn btn-block btn-default" href="/digital/">Visit the digital branch &rarr;</a>
