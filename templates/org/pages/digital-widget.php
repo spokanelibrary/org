69f035212c7c3e7bc71c3e1fbc235c20d15012b4
Sean Girard <seangirard@yahoo.com>
2014-11-25 07:56:07 -0800

    digital

--- templates/org/pages/digital-widget.php
+++ templates/org/pages/digital-widget.php
@@ -1,3 +1,4 @@
+<h5>Connected learning, research tools, &amp; entertainment</h5>
 <div class="media">
   <div class="media-body">
     <a class="btn btn-block btn-default" href="/digital/">Visit the digital branch &rarr;</a>
@@ -5,26 +6,3 @@
 </div><!-- /.media -->
 
 <?php include('download-widget.php'); ?>
-
-<div class="media">
-  <a class="pull-left btn btn-success" href="/subscriptions/">
-  	<i class="glyphicon glyphicon-sort-by-alphabet"></i>
-	</a>
-  <div class="media-body">
-  	<h5 class="media-heading">
-    	<a href="/subscriptions/"><em>Research:</em> Library subscriptions</a>
-    </h5>
-  </div>
-</div><!-- /.media -->
-
-<div class="media">
-  <a class="pull-left btn btn-primary" href="/connected-learning/">
-  	<i class="glyphicon glyphicon-facetime-video"></i>
-	</a>
-  <div class="media-body">
-  	<h5 class="media-heading">
-    	<a href="/connected-learning/"><em>Connected Learning:</em> tech</a>
-    </h5>
-  </div>
-</div><!-- /.media -->
-
