4f9135bdb6a018d8b7db00e096dd65954548e2a3
Sean Girard <seangirard@yahoo.com>
2014-11-25 08:00:32 -0800

    digital

--- templates/org/pages/digital-widget.php
+++ templates/org/pages/digital-widget.php
@@ -6,6 +6,4 @@
 
 <?php include('download-widget.php'); ?>
 <hr>
-<h5><a href="/connected-learning/">Connected Learning<a/> <small>&rarr;</small></h5>
-<h5><a href="/subscriptions/">Research tools</a> <small>&rarr;</small></h5>
-<h5><a href="/apps/">Entertainment</a> <small>&rarr;</small></h5>
+<h5><a href="/connected-learning/">Connected Learning<a/>. <a href="/subscriptions/">Research tools</a>. <a href="/apps/">Entertainment</a>.</h5>
