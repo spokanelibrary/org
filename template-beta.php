263865f3371a90e1b87876d7da82dfefabe63ce0
Sean Girard <seangirard@yahoo.com>
2014-12-10 07:28:16 -0800

    stage

--- template-beta.php
+++ template-beta.php
@@ -93,15 +93,7 @@ Template Name: Beta Home Page
   </div><!-- /.col -->
 
   <div class="col-md-3">  
-    <div class="panel panel-primary" style="border-left-width:5px;">
-      <div class="panel-heading" style="border-radius:0;">
-        <i class="glyphicon glyphicon-pushpin"></i>
-        Announcements
-      </div>
-      <div class="panel-body">
-        <?php get_template_part('templates/content', 'page'); ?>
-      </div>
-    </div><!-- /.panel -->
+    <?php get_template_part('templates/content', 'page'); ?>
   </div><!-- /.col -->
 
 </div><!-- /.row -->
