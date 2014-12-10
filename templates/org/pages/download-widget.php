27e7003c760c1a44d59e2913b75588cffbb6671d
Sean Girard <seangirard@yahoo.com>
2014-12-10 08:44:30 -0800

    dl widget

--- templates/org/pages/download-widget.php
+++ templates/org/pages/download-widget.php
@@ -19,7 +19,7 @@
 	</a>
   <div class="media-body">
     <h5 class="media-heading">
-    	<a href="/emusic/"><em>Freegal:</em> Download music
+    	<a href="/emusic/"><em>Freegal:</em> Music downloads</a> <small>(.mp3)</small>
     </h5>
   </div>
 </div><!-- /.media -->
