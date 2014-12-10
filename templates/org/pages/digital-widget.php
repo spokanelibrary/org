1c88f77baf95dce40683b240d6bd37f224766e3f
Sean Girard <seangirard@yahoo.com>
2014-12-10 12:04:28 -0800

    stage

--- templates/org/pages/digital-widget.php
+++ templates/org/pages/digital-widget.php
@@ -8,10 +8,20 @@
 
 <?php include('download-widget.php'); ?>
 
+<div class="media-body">
+    <h4 class="media-heading">
+    	<a href="/subscriptions/">Subscription databases</a>
+    </h4>
+  	Wide ranging topics, from <a href="/subscriptions/#aps">scientific journals</a> and <a href="/subscriptions/#aarc">auto repair</a> to <a href="/subscriptions/#morn">investment research</a> and <a href="/subscriptions/#wslf">legal forms</a>.
+  	<a href="/subscriptions/#all"><b>View all</b> &rarr;</a>
+  </div>
+</div><!-- /.media -->
+
+<!--
 <div class="media">
   <div class="media-body">
     <p>
     	<a class="btn btn-block btn-link" href="/apps/">Get library apps <small>&rarr;</small></a>
   	</p>
   </div>
-</div><!-- /.media -->
+</div>--><!-- /.media -->
