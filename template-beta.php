124fdbc4b522cce4e3cc460316e82577c0c31008
Sean Girard <seangirard@yahoo.com>
2015-01-15 11:50:43 -0800

    home

--- template-beta.php
+++ template-beta.php
@@ -87,7 +87,7 @@ if ( !empty($spl_home) ): ;
 
 <div class="row">
 
-  <div class="col-md-1 hidden-xs">
+  <div class="col-md-2 hidden-xs">
     <div class="btn-group-vertical" role="group" aria-label="...">
       <button type="button" class="btn btn-default">Left</button>
       <button type="button" class="btn btn-default">Middle</button>
@@ -95,7 +95,7 @@ if ( !empty($spl_home) ): ;
     </div>
   </div><!-- /.col -->  
 
-  <div class="col-md-6 hidden-xs">
+  <div class="col-md-5 hidden-xs">
     <?php echo do_shortcode('[spl_carousel_hero auto random slug=carousel]'); ?>
   </div><!-- /.col -->
 
