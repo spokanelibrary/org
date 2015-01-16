ffc9dd57c9724f52111254b0f52ae164183d4322
Sean Girard <seangirard@yahoo.com>
2015-01-16 12:48:07 -0800

    home

--- template-beta.php
+++ template-beta.php
@@ -116,18 +116,18 @@ if ( !empty($spl_home) ): ;
 
 <div class="row" style="background: rgb(255,255,255); padding: 16px 0; margin:16px 0; border-top: 4px solid rgb(0,85,135); border-bottom: 4px solid rgb(0,85,135);">
 
-  <div class="col-md-4">
+  <div class="col-md-3">
     <h3 class="text-primary" style="margin-top:0;">
       <a href="/digital/">Digital Branch</a>
     </h3>
   </div><!-- /.col -->
 
-  <div class="col-md-4">
+  <div class="col-md-3">
       <a href="#"><i class="glyphicon glyphicon-cloud-download"></i>
         eBooks &amp; Downloads</a>
   </div><!-- /.col -->
 
-  <div class="col-md-4">
+  <div class="col-md-6">
     <a href="#"><i class="glyphicon glyphicon-cloud-download"></i>
         <b>Connected Learning:</b> Training, workshops &amp; certification</a>
   </div><!-- /.col -->
