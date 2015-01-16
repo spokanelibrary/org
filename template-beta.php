6962d13cd9251e42b99a4dac7e950f1f7e9ab350
Sean Girard <seangirard@yahoo.com>
2015-01-16 08:25:51 -0800

    home

--- template-beta.php
+++ template-beta.php
@@ -96,7 +96,7 @@ if ( !empty($spl_home) ): ;
       <button type="button" class="btn btn-default">Right</button>
     </div>
   </div><!-- /.col -->
-  <div class="col-sm-12 hidden-xs">
+  <div class="col-sm-10 hidden-xs">
     <?php echo do_shortcode('[spl_carousel_hero auto random slug=carousel]'); ?>
   </div><!-- /.col -->
 
