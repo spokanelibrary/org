006defc24047634a7a41f1dc9bda753e7ed2688d
Sean Girard <seangirard@yahoo.com>
2015-01-16 07:34:18 -0800

    home

--- template-beta.php
+++ template-beta.php
@@ -85,13 +85,23 @@ if ( !empty($spl_home) ): ;
   </div><!-- /.col -->
 </div><!-- /.row -->
 
-<div class="row"> 
 
-  <div class="col-md-12 hidden-xs">
+
+
+<div class="row"> 
+  <div class="col-md-2 hidden-xs">
+    Buttons
+  </div><!-- /.col -->
+  <div class="col-md-10 hidden-xs">
     <?php echo do_shortcode('[spl_carousel_hero auto random slug=carousel]'); ?>
   </div><!-- /.col -->
 
 </div><!-- /.row -->
+
+
+
+
+
 
 <div class="row"> 
 
