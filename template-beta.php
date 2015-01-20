54d763eed7d3d3db5c3299c9ea5dc8ceb479f0e1
Sean Girard <seangirard@yahoo.com>
2015-01-20 14:41:38 -0800

    home

--- template-beta.php
+++ template-beta.php
@@ -101,8 +101,8 @@ if ( !empty($spl_home) ): ;
 
   <div class="col-md-12">
 
-    <div class="btn-group btn-group-justified" role="group" aria-label="..." style="padding:0;">
-      <a style="border-color:rgb(0,85,135); border-bottom-left-radius:0; border-bottom-right-radius:0;" class="btn btn-lg btn-default">
+    <div class="btn-group btn-group-justified" role="group" aria-label="..." style="padding:0; margin:0;">
+      <a style="border-color:rgb(0,85,135); border-bottom-left-radius:0; border-bottom-right-radius:0; padding:0; margin:0;" class="btn btn-lg btn-default">
         <i class="glyphicon glyphicon-cloud-download"></i>
         Visit the <b>Digital Branch</b> to download eBooks, music, &amp; more
       </a>
