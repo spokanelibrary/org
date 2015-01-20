91968bf4d5182668a7c256381548c5e3f5371d21
Sean Girard <seangirard@yahoo.com>
2015-01-20 14:46:23 -0800

    home

--- template-beta.php
+++ template-beta.php
@@ -96,7 +96,7 @@ if ( !empty($spl_home) ): ;
 </div><!-- /.row -->
 
 
-<div class="" style="padding: 0px 0; margin: 16px 0; border-top: 4px solid rgb(0,85,135); border-bottom: 4px solid rgb(0,85,135);">
+<div class="" style="padding: 0px 0; margin: 16px 0; border-top: 5px solid rgb(0,85,135); border-bottom: 5px solid rgb(0,85,135);">
 
 
   <div class="">
