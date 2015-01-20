f84d19237d10cd9e0b3079ec2c0c5e8586683f51
Sean Girard <seangirard@yahoo.com>
2015-01-20 12:14:55 -0800

    home

--- template-beta.php
+++ template-beta.php
@@ -86,10 +86,10 @@ if ( !empty($spl_home) ): ;
 </div><!-- /.row -->
 
 
-<div class="row">
+<div class="row" style="border:1px solid red;">
 
 
-  <div class="col-md-12" style="border:1px solid red;">
+  <div class="col-md-12">
 
     <div class="btn-group btn-group-justified" role="group" aria-label="...">
       <a style="border:none; border-bottom-left-radius:0; border-bottom-right-radius:0;" class="btn btn-lg btn-primary spl-blue-tint-80">
