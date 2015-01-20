d27560cbc7c5a3df61cbee4ceb16d939c5b64d7b
Sean Girard <seangirard@yahoo.com>
2015-01-20 11:38:46 -0800

    home

--- template-beta.php
+++ template-beta.php
@@ -99,8 +99,31 @@ if ( !empty($spl_home) ): ;
   <div class="panel-heading">
     <h3 class="panel-title text-center">
       Visit the Digital Branch to download eBooks, music, &amp; more
-      <a class="btn btn-default" href="/digital/">Go &rarr;</a>
     </h3>
+
+    <div class="btn-group btn-group-justified" role="group" aria-label="...">
+      <a style="border:none;" class="btn btn-primary spl-blue-tint-60">
+        <i class="glyphicon glyphicon-phone"></i>
+        <br>
+        eBooks
+      </a>
+      <a style="border:none;" class="btn btn-primary spl-blue-tint-50">
+        <i class="glyphicon glyphicon-music"></i>
+        <br>
+        Music
+      </a>
+      <a style="border:none;" class="btn btn-primary spl-blue-tint-40">
+        <i class="glyphicon glyphicon-headphones"></i>
+        <br>
+        Audio Books
+      </a>
+      <a style="border:none;" class="btn btn-primary spl-blue-tint-30">
+        <i class="glyphicon glyphicon-book"></i>
+        <br>
+        Magazines
+      </a>
+    </div>
+    
   </div>
 </div>
 
