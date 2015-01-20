702c49aab756b3d603ef4b11bc41c21e65ecf664
Sean Girard <seangirard@yahoo.com>
2015-01-20 11:32:23 -0800

    home

--- template-beta.php
+++ template-beta.php
@@ -100,9 +100,9 @@ if ( !empty($spl_home) ): ;
 <div class="row" style="/*background: rgb(245,245,245);*/ /*background-image:url('http://beta.spokanelibrary.org/assets/img/png/spl-devices.png'); background-repeat:no-repeat;*/ padding: 16px 0; margin:16px 0; border-top: 4px solid rgb(0,85,135); border-bottom: 4px solid rgb(0,85,135);">
 
   <div class="col-md-12">
-    <h3 class="" style="margin:-top:0;">
+    <p class="lead">
       Visit the  <a href="/digital/">Digital Branch</a> to download eBooks, music, &amp; more
-    </h3>
+    </p>
   </div><!-- /.col -->
 
   <div class="col-md-12">
