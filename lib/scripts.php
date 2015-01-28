478f5793c53063d66118801d49acfbb1d64b6f19
Sean Girard <seangirard@yahoo.com>
2015-01-28 13:05:16 -0800

    carousel

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '264492cadad23b5983971a6d1e567347');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '3738557dd6fed60b103ac15777d73a44');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
