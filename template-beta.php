e878de232392cbc9d6ef67f49ce5dd9b43796661
Sean Girard <seangirard@yahoo.com>
2015-01-29 13:12:41 -0800

    hero

--- template-beta.php
+++ template-beta.php
@@ -104,7 +104,7 @@ if ( !empty($spl_home) ): ;
 </div><!-- /.row -->
 
 
-<div class="spl-hero-panel spl-hero-primary spl-hero-digital" style="padding:0;">
+<div class="spl-hero-panel spl-hero-primary spl-hero-digital">
 
 
   <div class="">
