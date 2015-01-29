c398dd19a4819ce415ab2503fdecbf1db62c9df0
Sean Girard <seangirard@yahoo.com>
2015-01-29 12:21:23 -0800

    search

--- templates/org/header-search.php
+++ templates/org/header-search.php
@@ -1,5 +1,5 @@
 
-<header class="navbar navbar-default" style="padding-top: 2px; background:none; border:none;">
+<header class="navbar navbar-default" style="padding-top: 2px; background:none; border-color:rgb(0,85,135);">
   <div class="container">
     <?php echo do_shortcode('[spl_widget enterprise-search]'); ?>
   </div>
