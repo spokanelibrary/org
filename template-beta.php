2de4d695f1a10ae1579d5a1a0f4e998442fd8cd4
Sean Girard <seangirard@yahoo.com>
2015-01-29 11:03:19 -0800

    home

--- template-beta.php
+++ template-beta.php
@@ -98,7 +98,7 @@ if ( !empty($spl_home) ): ;
 </div><!-- /.row -->
 
 
-<div class="" style="padding: 0px 0; margin-top: 20px; margin-bottom:20px; border-top: 5px solid rgb(0,85,135); border-bottom: 5px solid rgb(0,85,135);">
+<div class="" style="padding: 0px 0; margin-top: 20px; border-top: 5px solid rgb(0,85,135); border-bottom: 5px solid rgb(0,85,135);">
 
 
   <div class="">
@@ -140,7 +140,7 @@ if ( !empty($spl_home) ): ;
 
   <div class="col-md-5">
 
-    <div style="border-top:5px solid #f0ad4e">
+    <div class="spl-hero-panel spl-hero-calendar">
       On our calendar
     </div>
 
