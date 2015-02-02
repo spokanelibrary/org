2a32629f551c5acf9c700af85e87298792416fb4
Sean Girard <seangirard@yahoo.com>
2015-02-02 14:54:29 -0800

    header

--- templates/org/header-top-navbar.php
+++ templates/org/header-top-navbar.php
@@ -45,10 +45,6 @@ function spl_get_home_url() {
     <div id="spl-account-profile"></div>
     
     <?php if ( is_user_logged_in() ) :?>
-      <div class="text-right" style="">
-        <?php include('header-social.php'); ?>
-      </div>
-      
       <div class="row">
         <div class="col-md-12">
           <header id="spl-navbar-common" class="navbar-common navbar-static-top" style="border-left:1px solid rgb(0,85,135); border-right:1px solid rgb(0,85,135);">
@@ -66,6 +62,12 @@ function spl_get_home_url() {
             <!--</div>-->
           </header>
         </div>
+      </div>
+
+      <?php echo do_shortcode('[spl_widget enterprise-search]'); ?>
+
+      <div class="text-right" style="">
+        <?php //include('header-social.php'); ?>
       </div>
       
     <?php endif; ?>
