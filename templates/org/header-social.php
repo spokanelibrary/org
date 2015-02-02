1d32d6e7657bd0d2287f32fb4d54055fa485f55b
Sean Girard <seangirard@yahoo.com>
2015-02-02 15:37:59 -0800

    header

--- templates/org/header-social.php
+++ templates/org/header-social.php
@@ -1,17 +1,5 @@
 <div class="btn-toolbar pull-right" style="margin-top: 12px;">
   <div class="btn-group btn-group-sm" role="group" aria-label="...">
-    <a class="btn btn-default"
-            href="http://facebook.com/spokanelibrary" 
-            style="opacity:1;"
-            title="">
-            <img src="/assets/img/icons/16px/facebook.png"></a>
-    <a class="btn btn-default text-info" 
-            href="http://twitter.com/spokanelibrary" 
-            style="opacity:1;"
-            title="">
-            <img src="/assets/img/icons/16px/twitter.png"></a>
-    </div>
-    <div class="btn-group btn-group-sm" role="group" aria-label="...">
     <a class="btn btn-link" 
             href="/support/" 
             style="opacity:1;"
@@ -25,6 +13,22 @@
             <span class="label label-success"><i class="glyphicon glyphicon-info-sign"></i></span>
             <span class="">About Us</span></a>
   </div>
+
+  <div class="btn-group btn-group-sm" role="group" aria-label="...">
+    <a class="btn btn-default"
+            href="http://facebook.com/spokanelibrary" 
+            style="opacity:1;"
+            title="">
+            <img src="/assets/img/icons/16px/facebook.png"></a>
+  </div>
+  <div class="btn-group btn-group-sm" role="group" aria-label="...">
+    <a class="btn btn-default text-info" 
+            href="http://twitter.com/spokanelibrary" 
+            style="opacity:1;"
+            title="">
+            <img src="/assets/img/icons/16px/twitter.png"></a>
+  </div>
+  
 </div>
 
 
