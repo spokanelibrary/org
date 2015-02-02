99040e0ece1ddaadeac016b05b3bc06cc9e0d0c7
Sean Girard <seangirard@yahoo.com>
2015-02-02 11:05:03 -0800

    header

--- templates/org/header-social.php
+++ templates/org/header-social.php
@@ -13,7 +13,7 @@
           href="/support/" 
           style="opacity:1;"
           title="">
-          <b class="text-danger"><small class="glyphicon glyphicon-hand-right"></small> Support Us</b></a>
+          <b class=""><small class="glyphicon glyphicon-hand-right text-danger"></small> Support Us</b></a>
 
 </div>
 
