43520aef6e1d244e81cafd60dd04ba21de4512cc
Sean Girard <seangirard@yahoo.com>
2015-02-02 13:39:54 -0800

    header

--- templates/org/header-top-navbar.php
+++ templates/org/header-top-navbar.php
@@ -50,7 +50,7 @@ function spl_get_home_url() {
       </div>
       <div class="row">
         <div class="col-md-8 col-md-offset-4">
-          <header class="navbar-common navbar-fixed-top">
+          <header class="navbar-common navbar-static-top">
             <!--<div class="container">-->
             <ul class="nav nav-justified">
             <?php 
