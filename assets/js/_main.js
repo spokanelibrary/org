533c5c95614f7b56ebfef49ca2c16047946638b6
Sean Girard <seangirard@yahoo.com>
2015-02-02 14:17:07 -0800

    header

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -190,7 +190,7 @@ var ORG = {
 
       // Scroll effetc
       var waypoint = new Waypoint({
-        element: document.getElementById('#spl-navbar-primary'),
+        element: document.getElementById('spl-navbar-primary'),
         handler: function(direction) {
           console.log('Scrolled to waypoint!')
         }
