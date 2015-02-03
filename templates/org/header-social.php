e3f4cb0c891dd805234f51c8bcc277a1128b47a5
Sean Girard <seangirard@yahoo.com>
2015-02-03 09:01:23 -0800

    header

--- templates/org/header-social.php
+++ templates/org/header-social.php
@@ -1,43 +1,33 @@
 <div class="row" style="margin-top: 12px;">
-
   <div class="col-md-4">
-    <div class="btn-group btn-group-justified" role="group" aria-label="...">
+    
+    <div class="btn-group" role="group" aria-label="...">
       <a class="btn btn-link"
-              href="http://facebook.com/spokanelibrary" 
-              style="opacity:1;"
-              title="">
-              <img style="margin:auto;" class="img-responsive" src="/assets/img/icons/24px/facebook.png"></a>
+          href="http://facebook.com/spokanelibrary" 
+          style="opacity:1;"
+          title="">
+          <img style="margin:auto;" class="img-responsive" src="/assets/img/icons/24px/facebook.png"></a>
 
-      <a class="btn btn-link text-info" 
+      <a class="btn btn-link" 
               href="http://twitter.com/spokanelibrary" 
               style="opacity:1;"
               title="">
               <img style="margin:auto;" class="img-responsive" src="/assets/img/icons/24px/twitter.png"></a>
-    </div>
-  </div>
-
-  <div class="col-md-4">
-    <div class="" style="margin-bottom:0;">
-    <a class="spl-hero-link" 
+      <a class="btn btn-link" 
             href="/about/" 
             title="">
             <h5 class="text-right">
               About Us
             </h5></a>
-    </div>
-  </div>
-
-  <div class="col-md-4">
-    <div class="" style="margin-bottom:0;">
-    <a class="spl-hero-link" 
+      <a class="btn btn-link" 
             href="/support/" 
             title="">
             <h5 class="text-right text-danger">
               Support Your Library
             </h5></a>
     </div>
-  </div>
 
+  </div>
 </div>
 
 
