4614a47ee3f42faf5b2a4205feb0ebbad0ceeeb0
Sean Girard <seangirard@yahoo.com>
2015-02-03 11:30:46 -0800

    masthead

--- templates/org/header-top-navbar.php
+++ templates/org/header-top-navbar.php
@@ -40,9 +40,9 @@ function spl_get_home_url() {
         </a>
       </div>
       <?php if ( is_user_logged_in() ) :?>
-      <h3 class="text-success pull-left" style="margin-left: 10px; margin-top:50px;">
+      <h4 class="text-success pull-left" style="margin-left: 20px; margin-top:50px;">
         Read. Learn. Discover.
-      </h3>
+      </h4>
       <?php endif; ?>
     </div>
     <div class="col-md-7">
