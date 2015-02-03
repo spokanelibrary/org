4878daad250f2ee21b2fa5ac0ecf2490b62979af
Sean Girard <seangirard@yahoo.com>
2015-02-03 13:00:30 -0800

    search

--- templates/org/header-search.php
+++ templates/org/header-search.php
@@ -1,6 +1,5 @@
-<header class="navbar navbar-default spl-search-bar" style="/*padding-top: 2px; background:none; border:none; border-bottom:1px solid #ccc; border-radius:0*/">
+<header class="navbar navbar-default spl-search-bar" style="">
   <div class="container">
-  	53
     <?php echo do_shortcode('[spl_widget enterprise-search beta]'); ?>
   </div>
 </header>
