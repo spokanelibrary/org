4ab4504521cdaa4c568b760b44b9f59fe781e47f
Sean Girard <seangirard@yahoo.com>
2015-02-03 09:21:40 -0800

    header

--- templates/org/header-social.php
+++ templates/org/header-social.php
@@ -6,15 +6,17 @@
           style="opacity:1;"
           title="">
           <img style="margin:auto;" class="" src="/assets/img/icons/24px/facebook.png"></a>
+      &nbsp;
       <a class="" 
               href="http://twitter.com/spokanelibrary" 
               style="opacity:1;"
               title="">
               <img style="margin:auto;" class="" src="/assets/img/icons/24px/twitter.png"></a>
+      &nbsp;
       <a class="btn btn-sm btn-default" 
             href="/about/" 
             title=""><b class="">About Us</b></a>
-
+      &nbsp;
       <a class="btn btn-sm btn-default" 
             href="/support/" 
             title=""><b class="spl-text-alt">Support Your Library</b></a>
