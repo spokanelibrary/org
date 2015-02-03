9eae825df31f9599d99b6caffc92d6c25d27ed16
Sean Girard <seangirard@yahoo.com>
2015-02-03 09:44:14 -0800

    header

--- templates/org/header-social.php
+++ templates/org/header-social.php
@@ -13,7 +13,7 @@
               title="">
               <img style="width:30px; height:30px;" class="" src="/assets/img/icons/32px/twitter.png"></a>
       &nbsp;
-      <a class="btn btn-sm btn-primary" 
+      <a class="btn btn-sm btn-default" 
             href="/about/" 
             title=""><b class="">About Us</b></a>
       &nbsp;
