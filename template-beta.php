a67a3c6bf2fa6d7169c364cb5acdafd07a9fe02e
Sean Girard <seangirard@yahoo.com>
2015-02-03 10:34:46 -0800

    home

--- template-beta.php
+++ template-beta.php
@@ -122,22 +122,22 @@ if ( !empty($spl_home) ): ;
       <div class="spl-hero-digital btn-group btn-group-justified" role="group" aria-label="...">
         <a href="/ebooks/" style="height:50px; vertical-align:middle; border:none; border-radius:0;" class="btn btn-primary spl-blue-tint-60">
           <i class="glyphicon glyphicon-phone"></i>
-          <br>
+          
           <span class="hidden-xs">&nbsp;<b>eBooks</b> &rarr;</span>
         </a>
         <a href="/emusic/" style="height:50px; vertical-align:middle; border:none;" class="btn btn-primary spl-blue-tint-50">
           <i class="glyphicon glyphicon-music"></i>
-          <br>
+          
           <span class="hidden-xs">&nbsp;<b>Music</b> &rarr;</span>
         </a>
         <a href="/eaudio/" style="height:50px; vertical-align:middle; border:none;" class="btn btn-primary spl-blue-tint-40">
           <i class="glyphicon glyphicon-headphones"></i></span>
-          <br>
+          
           <span class="hidden-xs">&nbsp;<b>Audio Books</b> &rarr;</span>
         </a>
         <a href="/emagazines/" style="height:50px; vertical-align:middle; border:none; border:none; border-radius:0;" class="btn btn-primary spl-blue-tint-30">
           <i class="glyphicon glyphicon-book"></i>
-          <br>
+          
           <span class="hidden-xs">&nbsp;<b>Magazines</b> &rarr;</span>
         </a>
       </div>
