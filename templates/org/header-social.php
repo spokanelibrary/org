e38aca028ff556aa7241db2acbf981afe86a57d2
Sean Girard <seangirard@yahoo.com>
2015-02-03 10:13:21 -0800

    header

--- templates/org/header-social.php
+++ templates/org/header-social.php
@@ -3,25 +3,18 @@
     <div class="btn-group btn-group-sm" role="group" aria-label="Social Library Links">
       <a class="btn btn-default"
           href="http://facebook.com/spokanelibrary" 
-          style="opacity:1;"
-          title="">
-          <img style="" class="" src="/assets/img/icons/16px/facebook.png"></a>
-      <a class="btn btn-default" 
+          title=""><img style="" class="" src="/assets/img/icons/16px/facebook.png"></a>
+      <a class="btn btn-default"
           href="http://twitter.com/spokanelibrary" 
-          style="opacity:1;"
-          title="">
-          <img style="" class="" src="/assets/img/icons/16px/twitter.png"></a>
+          title=""><img style="" class="" src="/assets/img/icons/16px/twitter.png"></a>
       <a class="btn btn-default" 
-          href="/about/" 
-          style="opacity:1;"
+          href="/about/"
           title=""><b class="">About Us</b></a>
       <a class="btn btn-default" 
-          href="/card/" 
-          style="opacity:1;"
+          href="/card/"
           title=""><b class="text-success">Get a Library Card</b></a>
       <a class="btn btn-default" 
-          href="/support/" 
-          style="opacity:1;"
+          href="/support/"
           title=""><b class="text-danger">Support Your Library!</b></a>
     </div>
   </div>
