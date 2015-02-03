420a202584d1946fd529bbd6af5002983e6d09b7
Sean Girard <seangirard@yahoo.com>
2015-02-03 09:04:09 -0800

    header

--- templates/org/header-social.php
+++ templates/org/header-social.php
@@ -1,31 +1,33 @@
-<div class="text-right" style="margin-top: 12px;">
+<div class="row" style="margin-top: 12px;">
+  <div class="col-md-12  text-right">
     
-    <div class="btn-group" role="group" aria-label="...">
-      <a class="btn btn-link"
+
+      <a class=""
           href="http://facebook.com/spokanelibrary" 
           style="opacity:1;"
           title="">
           <img style="margin:auto;" class="img-responsive" src="/assets/img/icons/24px/facebook.png"></a>
 
-      <a class="btn btn-link" 
+      <a class="" 
               href="http://twitter.com/spokanelibrary" 
               style="opacity:1;"
               title="">
               <img style="margin:auto;" class="img-responsive" src="/assets/img/icons/24px/twitter.png"></a>
-      <a class="btn btn-link" 
+      <a class="" 
             href="/about/" 
             title="">
             <h5 class="text-right">
               About Us
             </h5></a>
-      <a class="btn btn-link" 
+      <a class="" 
             href="/support/" 
             title="">
             <h5 class="text-right text-danger">
               Support Your Library
             </h5></a>
-    </div>
 
+
+  </div>
 </div>
 
 
