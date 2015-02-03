ea8c58630d67bda7f5a67f2e06b19645f1d9f39b
Sean Girard <seangirard@yahoo.com>
2015-02-03 09:44:43 -0800

    header

--- templates/org/header-social.php
+++ templates/org/header-social.php
@@ -17,7 +17,7 @@
             href="/about/" 
             title=""><b class="">About Us</b></a>
       &nbsp;
-      <a class="btn btn-sm btn-alt" 
+      <a class="btn btn-sm btn-default" 
             href="/support/" 
             title=""><b class="">Support Your Library</b></a>
 
