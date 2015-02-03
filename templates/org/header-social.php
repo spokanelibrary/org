b15b6d689234db2ab52ebbbba971e502b7819b41
Sean Girard <seangirard@yahoo.com>
2015-02-02 16:03:56 -0800

    header

--- templates/org/header-social.php
+++ templates/org/header-social.php
@@ -2,13 +2,13 @@
 
   <div class="col-md-4">
     <div class="btn-group btn-group-justified" role="group" aria-label="...">
-      <a class="btn btn-default"
+      <a class="btn btn-link"
               href="http://facebook.com/spokanelibrary" 
               style="opacity:1;"
               title="">
               <img style="margin:auto;" class="img-responsive" src="/assets/img/icons/24px/facebook.png"></a>
 
-      <a class="btn btn-default text-info" 
+      <a class="btn btn-link text-info" 
               href="http://twitter.com/spokanelibrary" 
               style="opacity:1;"
               title="">
