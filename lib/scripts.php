a969867a9e1eb37d68c07e8a3d76e2aeb6febb92
Sean Girard <seangirard@yahoo.com>
2015-02-03 09:42:09 -0800

    btn-alt

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '09719c1711d4a6c6219a698e7f690993');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '780880276dfbea02996af243da313276');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
