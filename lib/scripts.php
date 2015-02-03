b5d621ff24b2025363d8e7cbdfc52b262e81149f
Sean Girard <seangirard@yahoo.com>
2015-02-03 10:25:20 -0800

    retina

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '2c954c220c391900f5a773a821b53b5b');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5f7e4e002aafdce4aba5a4aecc30f53b');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
