9fa8323e4817fe1989887a101ec917243cd48498
Sean Girard <seangirard@yahoo.com>
2015-02-03 13:56:42 -0800

    search

--- templates/org/header-search.php
+++ templates/org/header-search.php
@@ -1,6 +1,6 @@
 <header class="navbar navbar-default spl-search-bar">
   <div class="container">
-  	<span class="text-muted">Search the catalog, website, or calendar</span>
+  	<span class="text-muted">Search the catalog, website, or events calendar:</span>
     <?php echo do_shortcode('[spl_widget enterprise-search beta]'); ?>
   </div>
 </header>
