32d1b349a213d7bd37dfdb94fee53fdf1f7c26cf
Sean Girard <seangirard@yahoo.com>
2015-02-04 14:57:56 -0800

    header

--- templates/org/header-top-navbar.php
+++ templates/org/header-top-navbar.php
@@ -130,6 +130,17 @@ function spl_get_home_url() {
       <a class="navbar-brand hidden-md hidden-lg" href="<?php echo spl_get_home_url(); ?>/">
         <span class="logotype"><?php bloginfo('name'); ?></span>
       </a>
+      <div class="container visible-xs visible-sm">
+        <div class="" style="margin:10px 0;">
+          <a class=""
+            href="http://facebook.com/spokanelibrary" 
+            title=""><img style="width:auto;" class="" src="/assets/img/icons/32px/facebook.png"></a>
+          &nbsp;
+          <a class=""
+            href="http://twitter.com/spokanelibrary" 
+            title=""><img style="width:auto;" class="" src="/assets/img/icons/32px/twitter.png"></a>
+        </div>
+      </div>
     </div>
     <nav class="collapse navbar-collapse" role="navigation">
       <?php
@@ -146,16 +157,6 @@ function spl_get_home_url() {
   </div>
 </header><!-- /.navbar -->
 
-<div class="container visible-xs visible-sm">
-  <div class="" style="margin:10px 0;">
-    <a class=""
-      href="http://facebook.com/spokanelibrary" 
-      title=""><img style="width:auto;" class="" src="/assets/img/icons/32px/facebook.png"></a>
-    &nbsp;
-    <a class=""
-      href="http://twitter.com/spokanelibrary" 
-      title=""><img style="width:auto;" class="" src="/assets/img/icons/32px/twitter.png"></a>
-  </div>
-</div>
+
 
 <?php include('header-search.php'); ?>
