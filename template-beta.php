c149fd0b36951c9ee8860e989246bdd64d795ace
Sean Girard <seangirard@yahoo.com>
2015-02-04 15:49:01 -0800

    touch

--- template-beta.php
+++ template-beta.php
@@ -72,7 +72,7 @@ if ( !empty($spl_home) ): ;
       <div class="spl-hero-panel spl-hero-primary">
 
         <h4 class="text-center">
-        <i class="glyphicon glyphicon-hand-up"></i>
+        <i class="glyphicon glyphicon-hand-down"></i>
         For mobile devices
       </h4>
 
