016fc76e05204978965581fd735e7f628c340db7
Sean Girard <seangirard@yahoo.com>
2015-02-04 15:10:48 -0800

    header

---
+++ templates/org/header-social.php
@@ -0,0 +1,9 @@
+  <a class=""
+    href="http://facebook.com/spokanelibrary" 
+    title=""><img class="" src="/assets/img/icons/32px/facebook.png"></a>
+  &nbsp;
+  <a class=""
+    href="http://twitter.com/spokanelibrary" 
+    title=""><img class="" src="/assets/img/icons/32px/twitter.png"></a>
+
+
