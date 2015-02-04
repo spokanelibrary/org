824949f79f5a08b16ca1eb6ba7243747cf67e922
Sean Girard <seangirard@yahoo.com>
2015-02-04 14:59:32 -0800

    header

--- templates/org/header-top-navbar.php
+++ templates/org/header-top-navbar.php
@@ -130,17 +130,17 @@ function spl_get_home_url() {
       <a class="navbar-brand hidden-md hidden-lg" href="<?php echo spl_get_home_url(); ?>/">
         <span class="logotype"><?php bloginfo('name'); ?></span>
       </a>
-      <div class="container visible-xs visible-sm">
-        <div class="" style="margin:10px 0;">
-          <a class=""
-            href="http://facebook.com/spokanelibrary" 
-            title=""><img style="width:auto;" class="" src="/assets/img/icons/32px/facebook.png"></a>
-          &nbsp;
-          <a class=""
-            href="http://twitter.com/spokanelibrary" 
-            title=""><img style="width:auto;" class="" src="/assets/img/icons/32px/twitter.png"></a>
-        </div>
+
+      <div class="">
+        <a class="pull-right"
+          href="http://facebook.com/spokanelibrary" 
+          title=""><img style="width:auto;" class="" src="/assets/img/icons/32px/facebook.png"></a>
+        &nbsp;
+        <a class="pull-right"
+          href="http://twitter.com/spokanelibrary" 
+          title=""><img style="width:auto;" class="" src="/assets/img/icons/32px/twitter.png"></a>
       </div>
+
     </div>
     <nav class="collapse navbar-collapse" role="navigation">
       <?php
@@ -156,7 +156,17 @@ function spl_get_home_url() {
     </nav>
   </div>
 </header><!-- /.navbar -->
-
-
-
+<!--
+<div class="container visible-xs visible-sm">
+  <div class="" style="margin:10px 0;">
+    <a class=""
+      href="http://facebook.com/spokanelibrary" 
+      title=""><img style="width:auto;" class="" src="/assets/img/icons/32px/facebook.png"></a>
+    &nbsp;
+    <a class=""
+      href="http://twitter.com/spokanelibrary" 
+      title=""><img style="width:auto;" class="" src="/assets/img/icons/32px/twitter.png"></a>
+  </div>
+</div>
+-->
 <?php include('header-search.php'); ?>
