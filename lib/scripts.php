0e4acc30eb50ff1b278c24296664f87792f2ef54
Sean Girard <seangirard@yahoo.com>
2015-02-05 08:20:59 -0800

    swipe

--- lib/scripts.php
+++ lib/scripts.php
@@ -27,7 +27,7 @@ function roots_scripts() {
   }
 
   wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.7.0.min.js', array(), null, false);
-  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', array(), '302932d6c8c3c2ddbc8deb2ebd16e559', true);
+  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', array(), '159a498151528c47ce43a4e0d7a71c7e', true);
   wp_enqueue_script('modernizr');
   wp_enqueue_script('jquery');
   wp_enqueue_script('roots_scripts');
