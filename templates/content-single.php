2fc6aa8a97bd8cf1b4c15bafe7ad3a25549a3200
Sean Girard <seangirard@yahoo.com>
2015-02-05 11:43:25 -0800

    blog

--- templates/content-single.php
+++ templates/content-single.php
@@ -15,10 +15,13 @@
   </article>
   <div class="panel panel-primary">
     <div class="panel-heading">
-      <h4>Find more on the library blog</h4>
+      <h4>Find more on the library blog&hellip;</h4>
     </div>
     <div class="panel-body">
     <?php wp_tag_cloud( array('taxonomy' => array('post_tag', 'category')) ); ?>  
+    </div>
+    <div class="panel-footer">
+      <h4 class="text-right"><a href="/blog/">All blog posts &rarr;</a></h4>
     </div>
   </div>
 <?php endwhile; ?>
