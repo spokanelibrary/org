ec20c0c62f046cf9c09c1aa5212e4bb2643c7343
Sean Girard <seangirard@yahoo.com>
2015-02-11 11:27:07 -0800

    card

--- templates/org/forms/field/signup/card-type.php
+++ templates/org/forms/field/signup/card-type.php
@@ -1,59 +1,57 @@
-<div class="collapse in" id="spl-signup-card-type">
-  <div class="panel panel-primary">
+<div class="panel panel-primary">
 
-    <div class="panel-heading">
-      <h3 class="panel-title">
-        Let's find out what type of card you need
-      </h3>
-    </div><!-- /.panel-body -->
+  <div class="panel-heading">
+    <h3 class="panel-title">
+      Let's find out what type of card you need
+    </h3>
+  </div><!-- /.panel-body -->
 
-    <div class="panel-body">
-      
-      <fieldset>
+  <div class="panel-body">
+    
+    <fieldset>
 
-        <div class="form-group">
-          <label for="spl-form-card-type" class="col-sm-2 control-label">
-            <h2>I am:</h2>
-          </label>
-          <div class="col-sm-10" id="spl-form-card-type">
-            <div class="radio">
-              <label>
-                <input type="radio" name="spl-form[card][type]" id="spl-form-card-type-adult" value="adult" checked>
-                <b>an adult</b> with an address or residence in Spokane County.
-              </label>
-            </div>
-            <div class="radio">
-              <label>
-                <input type="radio" name="spl-form[card][type]" id="spl-form-card-type-minor" value="minor">
-                <b class="text-info">under 18 yeard old</b> and I live in Spokane.
-              </label>
-            </div>
-            <div class="radio">
-              <label>
-                <input type="radio" name="spl-form[card][type]" id="spl-form-card-type-teacher" value="teacher">
-                <b class="text-success">a teacher</b> and I need a card for my classroom.
-              </label>
-            </div>
-            <div class="radio">
-              <label>
-                <input type="radio" name="spl-form[card][type]" id="spl-form-card-type-business" value="business">
-                <b class="text-danger">a business or non-profit</b> and I need a card for myself or my employees.
-              </label>
-            </div>
+      <div class="form-group">
+        <label for="spl-form-card-type" class="col-sm-2 control-label">
+          <h2>I am:</h2>
+        </label>
+        <div class="col-sm-10" id="spl-form-card-type">
+          <div class="radio">
+            <label>
+              <input type="radio" name="spl-form[card][type]" id="spl-form-card-type-adult" value="adult" checked>
+              <b>an adult</b> with an address or residence in Spokane County.
+            </label>
+          </div>
+          <div class="radio">
+            <label>
+              <input type="radio" name="spl-form[card][type]" id="spl-form-card-type-minor" value="minor">
+              <b class="text-info">under 18 yeard old</b> and I live in Spokane.
+            </label>
+          </div>
+          <div class="radio">
+            <label>
+              <input type="radio" name="spl-form[card][type]" id="spl-form-card-type-teacher" value="teacher">
+              <b class="text-success">a teacher</b> and I need a card for my classroom.
+            </label>
+          </div>
+          <div class="radio">
+            <label>
+              <input type="radio" name="spl-form[card][type]" id="spl-form-card-type-business" value="business">
+              <b class="text-danger">a business or non-profit</b> and I need a card for myself or my employees.
+            </label>
           </div>
         </div>
+      </div>
 
-        <div class="form-group">
-          <div class="col-sm-offset-2 col-sm-4">
-            <button class="btn btn-block btn-primary spl-card-type-select">
-              <b>Next</b>
-              <small class="glyphicon glyphicon-arrow-right"></small>
-            </button>
-          </div>
+      <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-4">
+          <button type="submit" class="btn btn-block btn-primary">
+            <b>Next</b>
+            <small class="glyphicon glyphicon-arrow-right"></small>
+          </button>
         </div>
+      </div>
 
-      </fieldset>
+    </fieldset>
 
-    </div><!-- /.panel-body -->
-  </div><!-- /.panel -->
-</div><!-- /.collapse -->
+  </div><!-- /.panel-body -->
+</div><!-- /.panel -->
