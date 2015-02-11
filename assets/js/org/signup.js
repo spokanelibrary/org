852236aab773d8b3603d53d79ba1f0c84fa2c091
Sean Girard <seangirard@yahoo.com>
2015-02-11 09:53:44 -0800

    card

--- assets/js/org/signup.js
+++ assets/js/org/signup.js
@@ -41,6 +41,7 @@ var org = {
     $('body').on('click', '.spl-card-type-select', function(e) {
       $('#spl-signup-card-type').collapse('hide');
       $('#spl-signup-adult').collapse('show');
+      history.pushState('/signup/adult/')
     });
   
   }
