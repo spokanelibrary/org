599ee00ab65fff86f84e00b2ea9d84674f54b248
Sean Girard <seangirard@yahoo.com>
2015-02-11 11:34:11 -0800

    card

--- templates/org/forms/field/card-signup.php
+++ templates/org/forms/field/card-signup.php
@@ -15,7 +15,7 @@
   <?php
   print_r($_REQUEST);
   
-  switch ( $_REQUEST['spl-form']['card'['type'] ) {
+  switch ( $_REQUEST['spl-form']['card']['type'] ) {
     case 'adult':
       include 'signup/card-adult.php';
       break;
