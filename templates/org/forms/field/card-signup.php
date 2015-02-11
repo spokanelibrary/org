e6756d1a13acea27759091f2a7b2044c9b446622
Sean Girard <seangirard@yahoo.com>
2015-02-11 08:33:01 -0800

    signup

--- templates/org/forms/field/card-signup.php
+++ templates/org/forms/field/card-signup.php
@@ -1,153 +1,84 @@
-<div class="row">
 
-  <div class="col-md-8">
-
-  <?php 
-  $renew =  json_decode(do_shortcode('[spl_widget card-renew]'));
-  if ( isset($renew->login) )  {
-    echo do_shortcode('[spl_widget login-form label post=renew intro="Please login to get started."]');
-  } elseif ( isset($renew->process) ) {
-    // processing should get routed to card-renew-response
-  } else {
-    switch ( $renew->borrower->status ) {
-      case 'ok':
-  ?>
-
-    <div class="panel panel-default">
-      <div class="panel-body">
-        <form class="form-horizontal spl-form" id="spl-form-renew" method="post" role="form">
-          
-          <?php include 'common/crass-response.php'; ?>
+<div class="panel panel-default">
+  <div class="panel-body">
+    <form class="form-horizontal spl-form" id="spl-form-signup" method="post" role="form">
       
-          <input type="hidden" id="spl-form-id" name="spl-form[id]" value="renew" />
-          
-          <input type="hidden" 
-                id="spl-form-ip" 
-                name="spl-form[ip]" 
-                value="<?php echo $_SERVER['REMOTE_ADDR']; ?>"
-                />
+      <?php include 'common/crass-response.php'; ?>
+  
+      <input type="hidden" id="spl-form-id" name="spl-form[id]" value="signup" />
+      
+      <input type="hidden" 
+            id="spl-form-ip" 
+            name="spl-form[ip]" 
+            value="<?php echo $_SERVER['REMOTE_ADDR']; ?>"
+            />
 
-          <fieldset>
-            <legend class="text-muted">
-              <small class="glyphicon glyphicon-barcode"></small>
-              <?php echo $renew->borrower->barcode; ?>
-            </legend>
-            
-            <p>
-              Please take a moment to review your contact information and update any inaccuracies.
-            </p>
-
-            <input type="hidden" name="spl-form[previous][name]" value="<?php echo $renew->borrower->name; ?>" />
-            <input type="hidden" name="spl-form[previous][email]" value="<?php echo $renew->borrower->email; ?>" />
-            <input type="hidden" name="spl-form[previous][phone]" value="<?php echo $renew->borrower->phone; ?>" />
-            <input type="hidden" name="spl-form[previous][street]" value="<?php echo $renew->borrower->street; ?>" />
-            <input type="hidden" name="spl-form[previous][city-st]" value="<?php echo $renew->borrower->city_st; ?>" />
-            <input type="hidden" name="spl-form[previous][zip]" value="<?php echo $renew->borrower->zip; ?>" />
-
-            <input type="hidden" name="spl-form[borrower]" value="<?php echo $renew->borrower->borrower; ?>" />
-            <input type="hidden" name="spl-form[time]" value="<?php echo $renew->borrower->time; ?>" />
-
-
-            <div class="form-group">
-              <label for="spl-form-name" class="col-sm-4 control-label">My name</label>
-              <div class="col-sm-8">
-                <p class="form-control-static text-muted">
-                  Our system stores names LAST FIRST MIDDLE.
-                </p>
-                <input type="text" class="form-control required" id="spl-form-name" name="spl-form[contact][name]" value="<?php echo $renew->borrower->name; ?>" placeholder="">
-              </div>
-            </div>
-
-            <div class="form-group">
-              <label for="spl-form-email" class="col-sm-4 control-label">My email address</label>
-              <div class="col-sm-8">
-                <input type="text" class="form-control required" id="spl-form-email" name="spl-form[contact][email]" value="<?php echo $renew->borrower->email; ?>" placeholder="">
-              </div>
-            </div>
-
-            <div class="form-group">
-              <label for="spl-form-phone" class="col-sm-4 control-label">My phone number</label>
-              <div class="col-sm-8">
-                <input type="text" class="form-control required" id="spl-form-phone" name="spl-form[contact][phone]" value="<?php echo $renew->borrower->phone; ?>" placeholder="">
-              </div>
-            </div>
-
-            <div class="form-group">
-              <label for="spl-form-street" class="col-sm-4 control-label">Street address</label>
-              <div class="col-sm-8">
-                <textarea rows="3" class="form-control required" id="spl-form-street" name="spl-form[contact][street]"><?php echo $renew->borrower->street; ?></textarea>
-              </div>
-            </div>
-
-            <div class="form-group">
-              <label for="spl-form-city-st" class="col-sm-4 control-label">City, ST</label>
-              <div class="col-sm-8">
-                <input type="text" class="form-control required" id="spl-form-city-st" name="spl-form[contact][city-st]" value="<?php echo $renew->borrower->city_st; ?>" placeholder="">
-              </div>
-            </div>
-
-            <div class="form-group">
-              <label for="spl-form-zip" class="col-sm-4 control-label">ZIP Code</label>
-              <div class="col-sm-8">
-                <input type="text" class="form-control required" id="spl-form-zip" name="spl-form[contact][zip]" value="<?php echo $renew->borrower->zip; ?>" placeholder="">
-              </div>
-            </div>
-
-            <div class="form-group">
-              <div class="col-sm-offset-4 col-sm-8">
-                <button type="submit" class="btn btn-block btn-success">
-                  <small class="glyphicon glyphicon-check"></small>
-                  Renew my library card now &rarr;
-                </button>
-              </div>
-            </div>
-
-            
-            <?php //print_r('<pre>'.$renew->borrower.'</pre>'); ?>
-            
-            <?php //include 'common/field-contact-method.php'; ?>
-            <?php //include 'common/field-login.php'; ?>
-            <?php //include 'common/field-contact.php'; ?>
-            <?php //include 'common/field-submit.php'; ?>
-            
-          </fieldset>
-        </form>
-      </div><!-- /.panel-body -->
-    </div><!-- /.panel -->
+      <fieldset>
+        <legend class="text-muted">
+          <small class="glyphicon glyphicon-"></small>
+          Let's get started
+        </legend>
         
-  <?php
-        break;
-      case 'problem':
-        echo '<div class="alert alert-warning">';
-        echo $renew->borrower->status_msg;
-        echo '</div>';
-        break;
-      case 'bad':
-        echo '<div class="alert alert-danger">';
-        echo $renew->borrower->status_msg;
-        echo '</div>';
-      default:
-        echo '<div class="alert alert-danger">';
-        echo 'An unknown error occurred. Please call our Circulation Desk at 509-444-5333 or <a href="http://www.spokanelibrary.org/contact">contact us</a> for assistance.';
-        echo '</div>';
-        break;
-    }
-  } 
-  ?>
-    
-  </div><!-- /.row -->
 
-  <div class="col-md-4">
-    <?php echo do_shortcode('[spl_widget custom-page page=contact-info]'); ?>
-    <h4 class="text-muted">
-      Asked &amp; answered
-    </h4>
-    <p>
-      <a class="btn btn-block btn-primary" title="How do I?" href="/faq/">
-        <i class="glyphicon glyphicon-question-sign"></i>
-        Frequently Asked Questions &rarr;
-      </a>
-    </p>
-  </div><!-- /.col -->
+        <div class="form-group">
+          <label for="spl-form-name" class="col-sm-4 control-label">My name</label>
+          <div class="col-sm-8">
+            <p class="form-control-static text-muted">
+              Our system stores names LAST FIRST MIDDLE.
+            </p>
+            <input type="text" class="form-control required" id="spl-form-name" name="spl-form[contact][name]" value="<?php echo $renew->borrower->name; ?>" placeholder="">
+          </div>
+        </div>
 
-</div><!-- /.row -->
+        <div class="form-group">
+          <label for="spl-form-email" class="col-sm-4 control-label">My email address</label>
+          <div class="col-sm-8">
+            <input type="text" class="form-control required" id="spl-form-email" name="spl-form[contact][email]" value="<?php echo $renew->borrower->email; ?>" placeholder="">
+          </div>
+        </div>
+
+        <div class="form-group">
+          <label for="spl-form-phone" class="col-sm-4 control-label">My phone number</label>
+          <div class="col-sm-8">
+            <input type="text" class="form-control required" id="spl-form-phone" name="spl-form[contact][phone]" value="<?php echo $renew->borrower->phone; ?>" placeholder="">
+          </div>
+        </div>
+
+        <div class="form-group">
+          <label for="spl-form-street" class="col-sm-4 control-label">Street address</label>
+          <div class="col-sm-8">
+            <textarea rows="3" class="form-control required" id="spl-form-street" name="spl-form[contact][street]"><?php echo $renew->borrower->street; ?></textarea>
+          </div>
+        </div>
+
+        <div class="form-group">
+          <label for="spl-form-city-st" class="col-sm-4 control-label">City, ST</label>
+          <div class="col-sm-8">
+            <input type="text" class="form-control required" id="spl-form-city-st" name="spl-form[contact][city-st]" value="<?php echo $renew->borrower->city_st; ?>" placeholder="">
+          </div>
+        </div>
+
+        <div class="form-group">
+          <label for="spl-form-zip" class="col-sm-4 control-label">ZIP Code</label>
+          <div class="col-sm-8">
+            <input type="text" class="form-control required" id="spl-form-zip" name="spl-form[contact][zip]" value="<?php echo $renew->borrower->zip; ?>" placeholder="">
+          </div>
+        </div>
+
+        <div class="form-group">
+          <div class="col-sm-offset-4 col-sm-8">
+            <button type="submit" class="btn btn-block btn-success">
+              <small class="glyphicon glyphicon-check"></small>
+              Renew my library card now &rarr;
+            </button>
+          </div>
+        </div>
+
+        
+        <?php //print_r('<pre>'.$renew->borrower.'</pre>'); ?>
+
+      </fieldset>
+    </form>
+  </div><!-- /.panel-body -->
+</div><!-- /.panel -->
+        
