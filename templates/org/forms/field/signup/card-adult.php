17a62a66a053e2cce40b6218531ff4099fe2de11
Sean Girard <seangirard@yahoo.com>
2015-02-11 13:32:41 -0800

    card

---
+++ templates/org/forms/field/signup/card-adult.php
@@ -0,0 +1,14 @@
+<div class="panel panel-default">
+  <div class="panel-body">
+
+    <fieldset>
+      <legend class="text-muted">
+        <small class="glyphicon glyphicon-"></small>
+        Let's get started
+      </legend>
+
+      <?php //include 'card-signup.php'; ?>
+
+    </fieldset>
+  </div><!-- /.panel-body -->
+</div><!-- /.panel -->
