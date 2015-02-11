cc6db2c009d263afaa8db4c9bda3f89656644d2c
Sean Girard <seangirard@yahoo.com>
2015-02-11 11:16:44 -0800

    card

--- templates/org/forms/form-library-card.php
+++ templates/org/forms/form-library-card.php
@@ -3,7 +3,8 @@ $crass = wp_get_crass_response();
 if ( !empty($crass->request) ) {
   include 'field/card-signup-response.php';
   } else {
-  print_r($crass);
+  //print_r($crass);
+  echo 'test';
   include 'field/card-signup.php';
 }
 ?>
