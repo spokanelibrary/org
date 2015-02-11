dab1f8ca06f5289a8ffb875abe5cdb020a992e89
Sean Girard <seangirard@yahoo.com>
2015-02-11 09:29:48 -0800

    card

--- templates/org/forms/field/card-signup.php
+++ templates/org/forms/field/card-signup.php
@@ -1,3 +1,5 @@
+<?php $spl_network_source = spl_get_network_source($_SERVER['REMOTE_ADDR']); ?>
+<div id="spl-network-source" data-source="<?php echo $spl_network_source; ?>"></div>
 <form class="form-horizontal spl-form" id="spl-form-signup" method="post" role="form">
   
   <?php include 'common/crass-response.php'; ?>
