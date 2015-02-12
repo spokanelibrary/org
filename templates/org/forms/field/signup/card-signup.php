f6ce7f97ea3a50671b50b11d588144bea14f1ff8
Sean Girard <seangirard@yahoo.com>
2015-02-12 12:39:26 -0800

    card

--- templates/org/forms/field/signup/card-signup.php
+++ templates/org/forms/field/signup/card-signup.php
@@ -4,7 +4,7 @@ $spl_network_source = spl_get_network_source($_SERVER['REMOTE_ADDR']);
 <div id="spl-network-source" data-source="<?php echo $spl_network_source; ?>"></div>
 <form class="form-horizontal spl-form" id="spl-form-signup" method="post" role="form">
   
-  <?php include '../common/crass-response.php'; ?>
+  <?php //include '../common/crass-response.php'; ?>
 
   <input type="hidden" id="spl-form-id" name="spl-form[id]" value="signup" />
   
