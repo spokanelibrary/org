668d03ae6d09a7f209f09ee89c132aa19abcdf3a
Sean Girard <seangirard@yahoo.com>
2015-02-12 15:24:32 -0800

    card

--- templates/org/forms/field/signup/shared/card-contact.php
+++ templates/org/forms/field/signup/shared/card-contact.php
@@ -1,10 +1,27 @@
 <div class="form-group">
   <label for="spl-form-email" class="col-sm-3 control-label">My email address</label>
   <div class="col-sm-3">
-    <input type="text" class="form-control required" id="spl-form-email" name="spl-form[contact][email]" value="<?php echo $renew->borrower->email; ?>" placeholder="">
+    <input type="text" class="form-control" id="spl-form-email" name="spl-form[signup][email]" value="" placeholder="">
   </div>
     <label for="spl-form-phone" class="col-sm-3 control-label">My phone number</label>
   <div class="col-sm-3">
-    <input type="text" class="form-control required" id="spl-form-phone" name="spl-form[contact][phone]" value="<?php echo $renew->borrower->phone; ?>" placeholder="">
+    <input type="text" class="form-control" id="spl-form-phone" name="spl-form[signup][phone]" value="" placeholder="">
   </div>
 </div>
+
+<div class="form-group">
+  <label for="spl-form-notice" class="col-sm-3 control-label">Send notices via</label>
+  <div class="col-sm-3 radio">
+    <label>
+      <input type="radio" id="spl-form-notice-email" name="spl-form[signup][notice]" value="email" checked>
+      Email
+    </label>
+  </div>
+  <div class="col-sm-3 col-sm-offset-3 radio">
+    <label>
+      <input type="radio" id="spl-form-notice-phone" name="spl-form[signup][notice]" value="phone" checked>
+      Email
+    </label>
+  </div>
+</div>
+
