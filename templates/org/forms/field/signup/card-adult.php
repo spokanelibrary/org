e37c9ff5f291aecdd396971433f882c05f5ca406
Sean Girard <seangirard@yahoo.com>
2015-02-12 13:07:46 -0800

    card

--- templates/org/forms/field/signup/card-adult.php
+++ templates/org/forms/field/signup/card-adult.php
@@ -7,12 +7,31 @@
         Let's get started
       </legend>
 
-      
+
       <div class="form-group">
         <label for="spl-form-birthdate" class="col-sm-4 control-label">My date of birth</label>
         <div class="col-sm-3">
           <input type="text" class="form-control required" id="spl-form-birthdate" name="spl-form[signup][birthdate]" value="" placeholder="">
         </div>
+      </div>
+
+            <div class="form-group">
+      <label class="control-label col-sm-2">Block name/Number</label>
+      <div class="col-md-4">
+      <input type="text" class="form-control">
+      </div>
+      <label class="control-label col-sm-1">Entrance</label>
+      <div class="col-md-1">
+      <input type="text" class="form-control">
+      </div>
+      <label class="control-label col-sm-1">Floor</label>
+      <div class="col-md-1">
+      <input type="text" class="form-control">
+      </div>
+      <label class="control-label col-sm-1">Apartment</label>
+      <div class="col-md-1">
+      <input type="text" class="form-control">
+      </div>
       </div>
 
       <?php //include 'card-contact.php'; ?>
