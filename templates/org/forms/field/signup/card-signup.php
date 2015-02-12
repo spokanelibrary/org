cd31d173d3a8c1da7e70756b7042be600bc07668
Sean Girard <seangirard@yahoo.com>
2015-02-12 11:56:10 -0800

    card

--- templates/org/forms/field/signup/card-signup.php
+++ templates/org/forms/field/signup/card-signup.php
@@ -13,8 +13,12 @@
 
 <div class="form-group">
   <label for="spl-form-email" class="col-sm-4 control-label">My email address</label>
-  <div class="col-sm-8">
+  <div class="col-sm-4">
     <input type="text" class="form-control required" id="spl-form-email" name="spl-form[contact][email]" value="<?php echo $renew->borrower->email; ?>" placeholder="">
+  </div>
+    <label for="spl-form-phone" class="col-sm-4 control-label">My phone number</label>
+  <div class="col-sm-4">
+    <input type="text" class="form-control required" id="spl-form-phone" name="spl-form[contact][phone]" value="<?php echo $renew->borrower->phone; ?>" placeholder="">
   </div>
 </div>
 
