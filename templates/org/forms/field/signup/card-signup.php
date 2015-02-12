2523383463fc7dc51f27d65d0fa606ad571db377
Sean Girard <seangirard@yahoo.com>
2015-02-12 12:59:14 -0800

    card

--- templates/org/forms/field/signup/card-signup.php
+++ templates/org/forms/field/signup/card-signup.php
@@ -4,16 +4,8 @@ $spl_network_source = spl_get_network_source($_SERVER['REMOTE_ADDR']);
 <div id="spl-network-source" data-source="<?php echo $spl_network_source; ?>"></div>
 <form class="form-inline spl-form" id="spl-form-signup" method="post" role="form">
     <?php //include '../common/crass-response.php'; ?>  
-  a
-    <?php include '../common/field-login.php'; ?>
-  b
-  <?php include 'common/field-login.php'; ?>
-  c
-  <?php include '/common/field-login.php'; ?>
-  d
-  <?php include 'field/common/field-login.php'; ?>
-  e
-  <?php echo realpath(__DIR__.'/../common/'); ?>
+
+  <?php include realpath(__DIR__.'/../common/field-login'); ?>
 
   <input type="hidden" id="spl-form-id" name="spl-form[id]" value="signup" />
   
