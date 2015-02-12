04b368a48df335ee3a867d740970a2e5daf59797
Sean Girard <seangirard@yahoo.com>
2015-02-12 14:51:24 -0800

    card

--- templates/org/forms/field/signup/shared/card-address.php
+++ templates/org/forms/field/signup/shared/card-address.php
@@ -1,21 +1,21 @@
 
 <div class="form-group">
-  <label for="spl-form-street" class="col-sm-4 control-label">Street address</label>
-  <div class="col-sm-8">
+  <label for="spl-form-street" class="col-sm-3 control-label">Street address</label>
+  <div class="col-sm-3">
     <textarea rows="3" class="form-control required" id="spl-form-street" name="spl-form[contact][street]"><?php echo $renew->borrower->street; ?></textarea>
   </div>
 </div>
 
 <div class="form-group">
-  <label for="spl-form-city-st" class="col-sm-4 control-label">City, ST</label>
-  <div class="col-sm-8">
+  <label for="spl-form-city-st" class="col-sm-3 control-label">City, ST</label>
+  <div class="col-sm-3">
     <input type="text" class="form-control required" id="spl-form-city-st" name="spl-form[contact][city-st]" value="<?php echo $renew->borrower->city_st; ?>" placeholder="">
   </div>
 </div>
 
 <div class="form-group">
-  <label for="spl-form-zip" class="col-sm-4 control-label">ZIP Code</label>
-  <div class="col-sm-8">
+  <label for="spl-form-zip" class="col-sm-3 control-label">ZIP Code</label>
+  <div class="col-sm-3">
     <input type="text" class="form-control required" id="spl-form-zip" name="spl-form[contact][zip]" value="<?php echo $renew->borrower->zip; ?>" placeholder="">
   </div>
 </div>
