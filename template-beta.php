0540e3ffb32f819f7207228c096bc4888b40fb5e
Sean Girard <seangirard@yahoo.com>
2015-02-16 13:54:02 -0800

    home

--- template-beta.php
+++ template-beta.php
@@ -126,7 +126,7 @@ if ( !empty($spl_home) ): ;
 <div class="row" id="spl-hero"> 
   
   <div class="col-sm-12">
-    <?php echo do_shortcode('[spl_carousel_beta pause auto random shuffle news posts pages calendar browse promo=tech slug=carousel title="" ]'); ?>
+    <?php echo do_shortcode('[spl_carousel_beta pause auto random shuffle news posts pages calendar promo=tech slug=carousel title="" ]'); ?>
   </div><!-- /.col -->
 
   <div class="col-sm-12">
@@ -194,7 +194,7 @@ if ( !empty($spl_home) ): ;
         </h4>
       </a>
     </div>
-    <?php //echo do_shortcode('[spl_widget browse-list list=star widget limit=7]'); ?>
+    <?php echo do_shortcode('[spl_widget browse-list list=star widget limit=7]'); ?>
     <!--
     <p class="text-right">
       <a href="/browse/"><b>More staff picks</b> <small>&rarr;</small></a>
