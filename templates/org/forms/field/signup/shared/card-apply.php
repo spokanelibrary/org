0ba8cc1a7f190631a5ba0f56d68fcef91913a16a
Sean Girard <seangirard@yahoo.com>
2015-02-16 13:45:14 -0800

    card

--- templates/org/forms/field/signup/shared/card-apply.php
+++ templates/org/forms/field/signup/shared/card-apply.php
@@ -2,7 +2,7 @@
   <div class="col-sm-offset-3 col-sm-6">
     <button type="submit" class="btn btn-block btn-success">
       <small class="glyphicon glyphicon-check"></small>
-      Apply for library card now
+      Get your library card now
       <small class="glyphicon glyphicon-arrow-right"></small>
     </button>
   </div>
