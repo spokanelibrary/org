2a0a7dd37fe475fd66cfa52fb45c32611fe47070
Sean Girard <seangirard@yahoo.com>
2015-02-16 13:58:29 -0800

    home

--- template-beta.php
+++ template-beta.php
@@ -164,6 +164,41 @@ if ( !empty($spl_home) ): ;
 
 </div><!-- /.row -->
 
+<div class="col-sm-12">
+
+    <div class="spl-hero-panel spl-hero-warning">
+
+      <a href="/digital/" class="spl-hero-link">
+        <h4 class="text-center normal">
+          <i class="glyphicon glyphicon-cloud-calendar"></i>
+          On our calendar &rarr;
+        </h4>
+      </a>
+
+      <div class="spl-hero-digital btn-group btn-group-justified" role="group" aria-label="...">
+        <a href="/ebooks/" style="height:40px; vertical-align:middle; border:none; border-radius:0;" class="btn btn-primary spl-blue-tint-60">
+          <i class="glyphicon glyphicon-phone"></i>
+          <span class="hidden-xs">&nbsp;<b>eBooks</b></span>
+        </a>
+        <a href="/emusic/" style="height:40px; vertical-align:middle; border:none;" class="btn btn-primary spl-blue-tint-50">
+          <i class="glyphicon glyphicon-music"></i>
+          <span class="hidden-xs">&nbsp;<b>Music</b></span>
+        </a>
+        <a href="/eaudio/" style="height:40px; vertical-align:middle; border:none;" class="btn btn-primary spl-blue-tint-40">
+          <i class="glyphicon glyphicon-headphones"></i></span>
+          <span class="hidden-xs">&nbsp;<b>Audio Books</b></span>
+        </a>
+        <a href="/emagazines/" style="height:40px; vertical-align:middle; border:none; border:none; border-radius:0;" class="btn btn-primary spl-blue-tint-30">
+          <i class="glyphicon glyphicon-book"></i>
+          <span class="hidden-xs">&nbsp;<b>Magazines</b></span>
+        </a>
+      </div>
+
+    </div>
+  </div><!-- /.col -->
+
+</div><!-- /.row -->
+
 
 <div class="row">
 
