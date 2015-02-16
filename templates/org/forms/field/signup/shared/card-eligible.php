fea84e6483fad76201b2262844b2f538f88b62ec
Sean Girard <seangirard@yahoo.com>
2015-02-16 12:24:44 -0800

    card

--- templates/org/forms/field/signup/shared/card-eligible.php
+++ templates/org/forms/field/signup/shared/card-eligible.php
@@ -2,7 +2,7 @@
   <label for="spl-form-local" class="col-sm-3 control-label">My address is in</label>
   <div class="col-sm-3">
     <select class="form-control required" id="spl-form-local" name="spl-form[signup][local]">
-      <option value="city">The City of Spokane</option>
+      <option value="city" selected>The City of Spokane</option>
       <option value="county">Spokane County</option>
       <option value="other">Outside the area</option>
       <option value="unsure">I'm not sure</option>
