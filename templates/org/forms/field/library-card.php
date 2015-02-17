66ce5364a28a5eef90f1b4f48679ea6b0d6e9b8d
Sean Girard <seangirard@yahoo.com>
2015-02-17 10:22:32 -0800

    card

--- templates/org/forms/field/library-card.php
+++ templates/org/forms/field/library-card.php
@@ -2,6 +2,7 @@
 if ( isset($_REQUEST['spl-form']['card']['type']) ) {
   include 'signup/card-signup.php';
 } else {
-  include 'signup/card-type.php';
+  //include 'signup/card-type.php';
+  include 'signup/card-signup.php';
 }
 ?>
