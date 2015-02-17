db6c25c96c75954d6b78a90ea3097665b761afb5
Sean Girard <seangirard@yahoo.com>
2015-02-17 14:32:29 -0800

    panel

--- template-alpha.php
+++ template-alpha.php
@@ -4,5 +4,29 @@ Template Name: Alpha Home Page
 */
 ?>
 
+<?php 
+  function get_alpha_panel($h=100) {
+    $html = '';
+    $html .= '
+              <div class="panel panel-default">
+              <div class="panel-body" style="height='.$h.'px;">
+                &nbsp;
+              </div>
+              </div>
+    ';
+
+    return $html;
+  }
+?>
 
 
+<div class="row">
+
+  <div class="col-sm-5">
+    <?php echo get_alpha_panel(); ?>
+  </div><!-- /.col -->
+  <div class="col-sm-7">
+      <?php echo get_alpha_panel(); ?>
+  </div><!-- /.col -->
+
+</div><!-- /.row -->
