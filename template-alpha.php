dbd92aad2e0237a50b859331a15acf89e5202397
Sean Girard <seangirard@yahoo.com>
2015-02-18 13:52:38 -0800

    alpha

--- template-alpha.php
+++ template-alpha.php
@@ -98,7 +98,7 @@ Template Name: Alpha Home Page
         </div>
         <div class="col-sm-6">
           <blockquote>
-          <ul class="listy-unstyled">
+          <ul class="list-unstyled">
             <li>Video editing</li>
             <li>Social media</li>
             <li>Online job search</li>
