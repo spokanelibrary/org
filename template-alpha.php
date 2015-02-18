ca4944dab4981eab64ff22d6291511c6968a451a
Sean Girard <seangirard@yahoo.com>
2015-02-18 15:10:47 -0800

    alpha

--- template-alpha.php
+++ template-alpha.php
@@ -143,20 +143,11 @@ Template Name: Alpha Home Page
 
 </div><!-- /.row -->
 
+<h4 class="text-center">
+  <i class="glyphicon glyphicon-"></i>
+  <span class="hidden-xs">On our reading list:</span> <b>Staff picks &amp; popular titles</b> <small class="glyphicon glyphicon-arrow-right"></small>
+</h4>
+<?php echo do_shortcode('[spl_widget browse-list list=star widget-alpha limit=4]'); ?>
 
-<div class="row">
-  
-  <div class="col-md-12">
-    <div class="spl-hero-panel spl-hero-default">
-      <a class="spl-hero-link" href="/browse/">
-        <h4 class="text-center">
-          <i class="glyphicon glyphicon-"></i>
-          <span class="hidden-xs">On our reading list:</span> <b>Staff picks &amp; popular titles</b> <small class="glyphicon glyphicon-arrow-right"></small>
-        </h4>
-      </a>
-    </div>
-    <?php echo do_shortcode('[spl_widget browse-list list=star widget-alpha limit=4]'); ?>
-  </div><!-- /.col -->
 
-</div><!-- /.row -->
 
