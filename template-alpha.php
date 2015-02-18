7094243f4ff1a908dc13447bcb69299f35551525
Sean Girard <seangirard@yahoo.com>
2015-02-18 13:08:06 -0800

    alpha

--- template-alpha.php
+++ template-alpha.php
@@ -28,7 +28,35 @@ Template Name: Alpha Home Page
     <?php echo get_alpha_panel('Calendar'); ?>
   </div><!-- /.col -->
   <div class="col-sm-7">
-    <?php echo get_alpha_panel('Digital Branch'); ?>
+    <div class="spl-hero-panel spl-hero-primary">
+
+      <a href="/digital/" class="spl-hero-link">
+        <h4 class="text-center normal">
+          <i class="glyphicon glyphicon-cloud-download"></i>
+          Visit the <b>Digital Branch</b> <span class="hidden-xs">to <b>download</b> eBooks, music, &amp; more</span> <small class="glyphicon glyphicon-arrow-right"></small>
+        </h4>
+      </a>
+
+      <div class="spl-hero-digital btn-group btn-group-justified" role="group" aria-label="...">
+        <a href="/ebooks/" style="height:40px; vertical-align:middle; border:none; border-radius:0;" class="btn btn-primary spl-blue-tint-60">
+          <i class="glyphicon glyphicon-phone"></i>
+          <span class="hidden-xs">&nbsp;<b>eBooks</b></span>
+        </a>
+        <a href="/emusic/" style="height:40px; vertical-align:middle; border:none;" class="btn btn-primary spl-blue-tint-50">
+          <i class="glyphicon glyphicon-music"></i>
+          <span class="hidden-xs">&nbsp;<b>Music</b></span>
+        </a>
+        <a href="/eaudio/" style="height:40px; vertical-align:middle; border:none;" class="btn btn-primary spl-blue-tint-40">
+          <i class="glyphicon glyphicon-headphones"></i></span>
+          <span class="hidden-xs">&nbsp;<b>Audio Books</b></span>
+        </a>
+        <a href="/emagazines/" style="height:40px; vertical-align:middle; border:none; border:none; border-radius:0;" class="btn btn-primary spl-blue-tint-30">
+          <i class="glyphicon glyphicon-book"></i>
+          <span class="hidden-xs">&nbsp;<b>Magazines</b></span>
+        </a>
+      </div>
+
+    </div>
   </div><!-- /.col -->
 
   <div class="col-sm-6">
