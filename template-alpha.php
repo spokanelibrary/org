36ed6468f1b775f8b8b345b187bdbe2832aa0ca6
Sean Girard <seangirard@yahoo.com>
2015-02-18 13:40:40 -0800

    alpha

--- template-alpha.php
+++ template-alpha.php
@@ -123,7 +123,7 @@ Template Name: Alpha Home Page
           <span class="hidden-xs"><b>Coming up</b> on the</span> <b>Library Calendar</b> <small class="glyphicon glyphicon-arrow-right"></small>
         </h4>
       </a>
-      <img class="img-responsive img-rounded" style="max-height:240px;" src="/assets/img/promos/spl-yearbooks-promo.png">
+      <img class="img-responsive" style="max-height:240px;" src="/assets/img/promos/spl-yearbooks-promo.png">
     </div>
   </div><!-- /.col -->
 
