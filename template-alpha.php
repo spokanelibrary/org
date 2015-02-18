36a661ca9ff5e38956f234d2b3f01b5ed107ae30
Sean Girard <seangirard@yahoo.com>
2015-02-18 13:33:26 -0800

    alpha

--- template-alpha.php
+++ template-alpha.php
@@ -61,7 +61,7 @@ Template Name: Alpha Home Page
       <a href="/calendar/" class="spl-hero-link">
         <h4 class="text-center normal">
           <i class="glyphicon glyphicon-calendar"></i>
-          <span class="hidden-xs"><b>Coming up soon</b> on the</span> <b>Library Calendar</b> <small class="glyphicon glyphicon-arrow-right"></small>
+          <span class="hidden-xs"><b>Coming up</b> on the</span> <b>Library Calendar</b> <small class="glyphicon glyphicon-arrow-right"></small>
         </h4>
       </a>
       
