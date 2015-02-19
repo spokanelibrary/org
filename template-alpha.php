096dfa6604beca6e905a1b109ffd938e0cd4f97a
Sean Girard <seangirard@yahoo.com>
2015-02-19 09:45:51 -0800

    home

--- template-alpha.php
+++ template-alpha.php
@@ -59,7 +59,7 @@ Template Name: Alpha Home Page
         <?php echo do_shortcode('[spl_widget home-page posts]'); ?>
       </div><!-- /.col -->
       <div class="col-sm-8">
-        <?php echo do_shortcode('[spl_widget home-page shuffle slides slug=carousel]'); ?>
+        <?php echo do_shortcode('[spl_widget home-page shuffle posts pages slides slug=carousel]'); ?>
       </div><!-- /.col -->
     </div><!-- /.row -->
 
