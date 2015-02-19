cd51513e1d8e1d3eca34d7f4152e2fa6b64657b0
Sean Girard <seangirard@yahoo.com>
2015-02-19 09:20:17 -0800

    home

--- template-alpha.php
+++ template-alpha.php
@@ -29,19 +29,19 @@ Template Name: Alpha Home Page
 
     <div class="spl-hero-panel spl-hero-primary">
       <a href="/digital/" class="spl-hero-link">
-        <h5 class="text-center normal">
+        <h4 class="text-center normal">
           <i class="glyphicon glyphicon-cloud-download"></i>
           Visit the <b>Digital Branch</b> <span class="hidden-xs">to <b>download</b> eBooks, music, &amp; more</span> <small class="glyphicon glyphicon-arrow-right"></small>
-        </h5>
+        </h4>
       </a>
     </div>
 
     <div class="spl-hero-panel spl-hero-success">
       <a href="/calendar/" class="spl-hero-link">
-        <h5 class="text-center normal">
+        <h4 class="text-center normal">
           <i class="glyphicon glyphicon-calendar"></i>
           <span class="hidden-xs"><b>Coming up soon</b> on the</span> <b>Library Calendar</b> <small class="glyphicon glyphicon-arrow-right"></small>
-        </h5>
+        </h4>
       </a>
     </div>
 
