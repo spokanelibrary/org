9bf8e460237f90d2f2eec44576b9b77a80b0867f
Sean Girard <seangirard@yahoo.com>
2015-02-19 12:59:48 -0800

    home

--- template-alpha.php
+++ template-alpha.php
@@ -24,7 +24,7 @@ Template Name: Alpha Home Page
 <div class="row">
   <div class="col-md-3 col-md-push-9">
     <!-- digital -->
-    <div class="spl-hero-panel spl-hero-primary">
+    <div class="spl-hero-panel spl-hero-primary" style="margin-bottom: 4px;">
       <a href="/digital/" class="spl-hero-link">
         <h4 class="text-center normal">
           <i class="glyphicon glyphicon-cloud-download"></i>
