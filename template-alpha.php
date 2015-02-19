6a5e492af3d3a2cd348b7013ee26de68955b9f1b
Sean Girard <seangirard@yahoo.com>
2015-02-19 09:32:50 -0800

    home

--- template-alpha.php
+++ template-alpha.php
@@ -68,6 +68,14 @@ Template Name: Alpha Home Page
 
 <div class="row">
   <div class="col-md-12">
+
+    <div class="spl-hero-panel spl-hero-default">
+      <a class="spl-hero-link" href="/browse/">
+        <h4 class="text-center">
+          <i class="glyphicon glyphicon-"></i>
+          <span class="hidden-xs">On our reading list:</span> <b>Staff picks &amp; popular titles</b> <small class="glyphicon glyphicon-arrow-right"></small>
+        </h4>
+      </a>
     <?php echo do_shortcode('[spl_widget home-page browse]'); ?>
   </div>
 </div><!-- /.row -->
