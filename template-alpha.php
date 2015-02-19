47a2c5a6e623766d022629187252491544f96c04
Sean Girard <seangirard@yahoo.com>
2015-02-18 16:27:11 -0800

    home

--- template-alpha.php
+++ template-alpha.php
@@ -29,12 +29,13 @@ Template Name: Alpha Home Page
     <?php echo get_alpha_panel('&nbsp;', 200); ?>
   </div>
   <div class="col-sm-4">
-    <?php echo do_shortcode('[spl_widget home-page calendar]'); ?>
+    <?php echo get_alpha_panel(); ?>
+    
     <?php echo get_alpha_panel('&nbsp;', 400); ?>
     <?php echo get_alpha_panel(); ?>
   </div>
   <div class="col-sm-4">
-    <?php echo get_alpha_panel(); ?>
+    <?php echo do_shortcode('[spl_widget home-page calendar]'); ?>
     <?php echo get_alpha_panel('&nbsp;', 200); ?>
     <?php echo get_alpha_panel(); ?>
   </div>
