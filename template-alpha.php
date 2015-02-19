fa871547ada131c6b550df91a9d775919c42b885
Sean Girard <seangirard@yahoo.com>
2015-02-19 09:23:23 -0800

    home

--- template-alpha.php
+++ template-alpha.php
@@ -30,7 +30,7 @@ Template Name: Alpha Home Page
     <div class="spl-hero-panel spl-hero-primary">
       <a href="/digital/" class="spl-hero-link">
         <h4 class="text-center normal">
-          <i class="glyphicon glyphicon-cloud-download text-primary"></i>
+          <i class="glyphicon glyphicon-cloud-download spl-blue"></i>
           Visit the <b>Digital Branch</b> <span class="hidden-xs">to download eBooks and more</span> <small class="glyphicon glyphicon-arrow-right"></small>
         </h4>
       </a>
