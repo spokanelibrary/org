5aa071535515923b80ee9678d95ae0600549b154
Sean Girard <seangirard@yahoo.com>
2015-02-24 09:39:41 -0800

    home

--- template-alpha.php
+++ template-alpha.php
@@ -71,7 +71,7 @@ Template Name: Alpha Home Page
     
     <div class="row">
       <div class="col-sm-9 col-sm-push-3">
-        <?php echo do_shortcode('[spl_widget home-page shuffle posts pages limit=2]'); ?>
+        <?php echo do_shortcode('[spl_widget home-page shuffle posts pages]'); ?>
       </div><!-- /.col -->
       <div class="col-sm-3 col-sm-pull-9">
         <?php echo do_shortcode('[spl_widget home-page browse]'); ?>
