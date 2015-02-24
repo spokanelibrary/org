bd455aa441412bc79a0ed0d2d7d1d18015ffafde
Sean Girard <seangirard@yahoo.com>
2015-02-24 10:00:56 -0800

    holme

--- template-alpha.php
+++ template-alpha.php
@@ -69,11 +69,13 @@ Template Name: Alpha Home Page
     
     <?php echo do_shortcode('[spl_widget home-page shuffle slides slug=carousel limit=2]'); ?>
     
+    <div style="border:1px solid #ccc;">
     <div class="spl-tile spl-tile-default">
       <h5 class="text-muted uppercase">Reading list</h5>
       <div class="row">
         <?php echo do_shortcode('[spl_widget home-page browse]'); ?>
       </div><!-- /.row -->
+    </div>
     </div>
 
     <div class="row">
