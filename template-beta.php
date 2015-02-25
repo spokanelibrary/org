bc1511f99094cc518933a41504521cf1c47d477f
Sean Girard <seangirard@yahoo.com>
2015-02-25 14:59:11 -0800

    home

--- template-beta.php
+++ template-beta.php
@@ -173,8 +173,8 @@ if ( !empty($spl_home) ): ;
         </h4>
       </a>
       
-      <div class="spl-hero-calendar btn-group btn-group-justified" role="group" aria-label="...">
-        <a href="/storytime/" style="height:40px; vertical-align:middle; border:none; border-radius:0;" class="btn btn-warning spl-orange-tint-70">
+      <div class="btn-group btn-group-justified" role="group" aria-label="...">
+        <a href="/storytime/" style="height:40px; vertical-align:middle; border:none; border-radius:0;" class="btn btn-warning spl-green-tint-70">
           <i class="glyphicon glyphicon-picture"></i>
           <span class="hidden-xs">&nbsp;<b>Storytimes</b></span>
         </a>
