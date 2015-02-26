c28108b3e5ff933c3edaf39ec458975af4136d06
Sean Girard <seangirard@yahoo.com>
2015-02-26 07:36:54 -0800

    header

--- templates/org/header-top-navbar.php
+++ templates/org/header-top-navbar.php
@@ -30,99 +30,93 @@ function spl_get_home_url() {
   <!--</div>-->
 </header>
 
+<?php //if ( is_user_logged_in() ) :?>
 <header class="navbar-masthead banner navbar navbar-inverse navbar-static-top visible-md visible-lg" role="banner">
   <div class="container">
-
-
-  <?php //if ( is_user_logged_in() ) :?>
-  <div class="row">
-    <div class="col-md-2">
-      <div class="navbar-header">
-        <a class="navbar-brand" href="<?php echo spl_get_home_url(); ?>/">
-          <span class="text-hide"><?php bloginfo('name'); ?></span>
-        </a>
-      </div>
-    </div>
-    <div class="col-md-10">
-        
-        <div class="row">
-          <div class="col-md-11 col-md-offset-1">
-            <header id="spl-navbar-common" class="navbar-common navbar-static-top" style="border-left:1px solid rgb(0,85,135); border-right:1px solid rgb(0,85,135);">
-              <!--<div class="container">-->
-              <ul class="nav nav-justified">
-              <?php 
-              wp_nav_menu( 
-              array(
-                'menu' => 'Common Navigation'
-              , 'container' => false
-              , 'items_wrap' => '%3$s'
-              )); 
-              ?>
-              </ul>
-              <!--</div>-->
-            </header>
-          </div>
+    <div class="row">
+      <div class="col-md-2">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="<?php echo spl_get_home_url(); ?>/">
+            <span class="text-hide"><?php bloginfo('name'); ?></span>
+          </a>
         </div>
+      </div><!-- /.col -->
+      <div class="col-md-10">
+          
+          <div class="row">
+            <div class="col-md-11 col-md-offset-1">
+              <header id="spl-navbar-common" class="navbar-common navbar-static-top" style="border-left:1px solid rgb(0,85,135); border-right:1px solid rgb(0,85,135);">
+                <!--<div class="container">-->
+                <ul class="nav nav-justified">
+                <?php 
+                wp_nav_menu( 
+                array(
+                  'menu' => 'Common Navigation'
+                , 'container' => false
+                , 'items_wrap' => '%3$s'
+                )); 
+                ?>
+                </ul>
+                <!--</div>-->
+              </header>
+            </div><!-- /.col -->
+          </div><!-- /.row -->
 
-        <div class="row">
-          <div class="col-md-12">
-            <div id="spl-account-profile"></div>
-            <h4 class="text-success normal pull-left" style="opacity:.9; margin-left: 10px; margin-top:20px;">
-              <?php bloginfo('description'); ?>
-            </h4>
-            <div class="pull-right" style="margin-top: 12px;">
-              <?php include('header-social-connect.php'); ?>
-            </div>
-          </div>
-        </div>
-    
-    </div>
-  </div>
-  <script id="spl-account-profile-tmpl" type="text/x-handlebars-template">
-      {{#if user.sessionToken}}
-      <div class="pull-right" id="spl-account-profile">
-        <div class="well well-sm clearfix" style="margin: 6px 0 0 0;  opacity:.9; border-left-width:5px;">
-            <i class="glyphicon glyphicon-user text-muted"></i>
-            <strong class="text-muted">{{user.firstName}}</strong>
-            <a href="/account/"><small><strong>My Account</strong></small></a>
-            <span class="text-muted">&nbsp;&nbsp;</span>
-            
-            <span class="">
-              <i class="glyphicon glyphicon-log-out text-danger"></i>
-              <a href="./?logout" class="text-danger"><small><strong>Logout</strong></small></a>
-            </span>
-          <!--  
-            <br>
-        
-          {{#if user.holdRequests}}
-            {{#if user.holdRequests.ready}}
-            <a href="/account#holds" class="btn btn-sm btn-default">
-              Ready to pickup
-              <small class="badge">{{user.holdRequests.ready}}</small>
-            </a>
-            {{/if}}
-          {{/if}}
-          &nbsp;
-          {{#if user.itemsOut}}
-            {{#if user.itemsOut.overdue}}
-            <a href="/account#cko" class="btn btn-sm btn-default">
-              Overdue 
-              <small class="badge">{{user.itemsOut.overdue}}</small>
-            </a>
-            {{/if}}
-          {{/if}}
-          -->
-        </div>
-      </div>
-      {{/if}}
-    </script>
-  <?php //endif; ?>
-
-
-  
-  </div>
+          <div class="row">
+            <div class="col-md-12">
+              <h4 class="text-success normal pull-left" style="opacity:.9; margin-left: 10px; margin-top:20px;">
+                <?php bloginfo('description'); ?>
+              </h4>
+              <div class="pull-right" style="margin-top: 12px;">
+                <?php include('header-social-connect.php'); ?>
+              </div>
+              <div id="spl-account-profile"></div>
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+      
+      </div><!-- /.col -->
+    </div><!-- /.row -->
+  </div><!-- /.container -->
 </header><!-- /.navbar -->
-
+<script id="spl-account-profile-tmpl" type="text/x-handlebars-template">
+  {{#if user.sessionToken}}
+  <div class="pull-right" id="spl-account-profile">
+    <div class="well well-sm clearfix" style="margin: 6px 0 0 0;  opacity:.9; border-left-width:5px;">
+        <i class="glyphicon glyphicon-user text-muted"></i>
+        <strong class="text-muted">{{user.firstName}}</strong>
+        <a href="/account/"><small><strong>My Account</strong></small></a>
+        <span class="text-muted">&nbsp;&nbsp;</span>
+        
+        <span class="">
+          <i class="glyphicon glyphicon-log-out text-danger"></i>
+          <a href="./?logout" class="text-danger"><small><strong>Logout</strong></small></a>
+        </span>
+      <!--  
+        <br>
+    
+      {{#if user.holdRequests}}
+        {{#if user.holdRequests.ready}}
+        <a href="/account#holds" class="btn btn-sm btn-default">
+          Ready to pickup
+          <small class="badge">{{user.holdRequests.ready}}</small>
+        </a>
+        {{/if}}
+      {{/if}}
+      &nbsp;
+      {{#if user.itemsOut}}
+        {{#if user.itemsOut.overdue}}
+        <a href="/account#cko" class="btn btn-sm btn-default">
+          Overdue 
+          <small class="badge">{{user.itemsOut.overdue}}</small>
+        </a>
+        {{/if}}
+      {{/if}}
+      -->
+    </div>
+  </div>
+  {{/if}}
+</script>
+<?php //endif; ?>
 
 <header id="spl-navbar-primary" class="navbar-primary banner navbar navbar-inverse navbar-static-top spl-ribbon" style="margin-bottom:0px;" role="banner">
   <div class="container">
