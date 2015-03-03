1919a0dca221e9a739f1c9a18b78e49f46b12047
Sean Girard <seangirard@yahoo.com>
2015-03-03 12:44:46 -0800

    home

--- template-alpha.php
+++ template-alpha.php
@@ -98,7 +98,7 @@ Template Name: Alpha Home Page
         </div>
       </div>
     </div>
-    <div class="spl-tile spl-tile-boxed">
+    <div class="spl-tile spl-tile-boxed spl-tile-body">
     <?php echo do_shortcode('[spl_widget home-page posts limit=3]'); ?>
     </div>
   </div><!-- /.col -->
