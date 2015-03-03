166b771b7a5eddcde49cd319c5fc729eaa045626
Sean Girard <seangirard@yahoo.com>
2015-03-03 12:06:43 -0800

    home

--- template-alpha.php
+++ template-alpha.php
@@ -80,7 +80,7 @@ Template Name: Alpha Home Page
 <div class="row">
   <div class="col-sm-9">
     <div class="spl-tile">
-      <h4 class="text-warning">The Fiction Queen</h4>
+      <h4 class="text-warning">The Fiction Queen <small>and her subjects</small></h4>
       <h6 class="text-success uppercase">Reviews and recomendations from Spokane Public Library's <b>Susan Creed</b></h6>
       <div class="spl-tile-boxed spl-tile-body">
         <div class="serif">
