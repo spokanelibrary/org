2f9eef2cf9423667f909df2c5917d48ee2c7a05a
Sean Girard <seangirard@yahoo.com>
2015-03-03 12:35:43 -0800

    home

--- template-alpha.php
+++ template-alpha.php
@@ -59,7 +59,7 @@ Template Name: Alpha Home Page
   <!--</div>--><!-- /.col -->
   <div class="col-md-9">
     <div class="spl-tile">
-      <div class="spl-hero-panel spl-hero-success">
+      <div class="spl-hero-panel spl-hero-warning">
       <?php echo do_shortcode('[spl_widget home-page news]'); ?>
       </div>
     </div>
