81f3e2b6bdb84f0c1a6cde5a55e65a5b4d33bab1
Sean Girard <seangirard@yahoo.com>
2015-03-03 13:02:31 -0800

    home

--- template-alpha.php
+++ template-alpha.php
@@ -100,12 +100,12 @@ Template Name: Alpha Home Page
     </div>
 
     <div class="row">
-      <div class="col-md-2">
+      <div class="col-md-3">
         <div class="spl-hero-panel spl-hero-success">
           <h4>Some stuff</h4>
         </div>
       </div><!-- /.col -->
-      <div class="col-md-10">
+      <div class="col-md-9">
         <?php echo do_shortcode('[spl_widget home-page posts limit=3]'); ?>
       </div><!-- /.col -->      
     </div><!-- /.row -->
