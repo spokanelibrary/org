809c6d5d6f7171a95fdae5108029b9341875de13
Sean Girard <seangirard@yahoo.com>
2015-03-04 13:00:25 -0800

    home

--- templates/org/pages/fiction-queen.php
+++ templates/org/pages/fiction-queen.php
@@ -20,7 +20,9 @@
   if ($posts->have_posts()) : 
   	$i = 1;
     while ($posts->have_posts()) : $posts->the_post(); 
-			$html .= '
+
+    	if ( 1 == $i ) {
+    		$html .= '
     		<div class="spl-tile spl-tile-boxed">
 				  <div class="spl-tile-body">
 				    
@@ -32,7 +34,21 @@
 				  </div>
 				</div>
     		';
+    	} else {
+    		$html .= '
+    		<div class="spl-tile spl-tile-boxed">
+				  <div class="spl-tile-body">
+				    <div class="serif">
+				      <img style="width:120px; height:120px; margin-right:10px; margin-bottom:6px;" class="pull-left" src="/assets/img/promos/spl-fiction-queen.jpg">
+				      
+				      <h4>'.get_the_title().'</h4>
+				      '.preg_replace('/<img[^>]+./','', apply_filters('the_content', get_the_content())).'
+				    </div>
+				  </div>
+				</div>
+    		';
     	}
+
       $i++;
     endwhile; 
   endif;
