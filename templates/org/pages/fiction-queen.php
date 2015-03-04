3e4697e6ff76b5f829876230db4de80dc8ae5bc9
Sean Girard <seangirard@yahoo.com>
2015-03-04 13:00:05 -0800

    home

--- templates/org/pages/fiction-queen.php
+++ templates/org/pages/fiction-queen.php
@@ -20,9 +20,7 @@
   if ($posts->have_posts()) : 
   	$i = 1;
     while ($posts->have_posts()) : $posts->the_post(); 
-
-    	if ( 1 == $i ) {
-    		$html .= '
+			$html .= '
     		<div class="spl-tile spl-tile-boxed">
 				  <div class="spl-tile-body">
 				    
@@ -34,36 +32,7 @@
 				  </div>
 				</div>
     		';
-    	} else {
-    		$html .= '
-    		<div class="spl-tile spl-tile-boxed">
-				  <div class="spl-tile-body">
-				    <div class="serif">
-				      <img style="width:120px; height:120px; margin-right:10px; margin-bottom:6px;" class="pull-left" src="/assets/img/promos/spl-fiction-queen.jpg">
-				      
-				      <h4>'.get_the_title().'</h4>
-				      '.preg_replace('/<img[^>]+./','', apply_filters('the_content', get_the_content())).'
-				    </div>
-				  </div>
-				</div>
-    		';
     	}
-    	/*
-      $html .= '<div class="media">'.PHP_EOL;
-			
-			$html .= '<h4 class="media-heading"><a href="'.get_permalink().'">'.get_the_title().'</a></h4>'.PHP_EOL;
-			
-			//$html .= '<div class="media-body">';
-			if ( has_post_thumbnail() ) { 
-      	$html .= '<a class="pull-left" href="'.get_permalink().'">'.PHP_EOL;
-			  $html .= get_the_post_thumbnail($page->ID, 'thumbnail', array('class'=>'media-object img-responsive spl-featured-thumbnail')).PHP_EOL;
-			  //$html .= get_the_post_thumbnail($page->ID, 'thumbnail', array('class'=>'img-responsive spl-featured-thumbnail')).PHP_EOL;
-			  $html .= '</a>'.PHP_EOL;
-      }
-      $html .= '<div class="media-body">'.get_the_excerpt().'</div>'.PHP_EOL;
-
-      $html .= '</div>'.PHP_EOL;
-			*/
       $i++;
     endwhile; 
   endif;
@@ -76,12 +45,9 @@
 ?>
 
 <h4 class="text-warning">The Fiction Queen <small>and her subjects</small></h4>
-				    <h6 class="text-success uppercase">Reviews and recomendations from Spokane Public Library\'s <b>Susan Creed</b></h6>
-				  
-				  
-<?php //echo do_shortcode('[spl_widget recent-posts count=3 slug=readers-corner]'); ?>
+<h6 class="text-success uppercase">Reviews and recomendations from Spokane Public Library's <b>Susan Creed</b></h6>
+
 <?php echo spl_get_fiction_queen(); ?>
-<p>&nbsp;</p>
 
 <p>
 	<a class="btn btn-success" href="/blog/topic/readers-corner/">
