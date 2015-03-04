c681e46ee65d16cabf8b4d1214663ceb0111c7d9
Sean Girard <seangirard@yahoo.com>
2015-03-04 13:02:48 -0800

    home

--- templates/org/pages/fiction-queen.php
+++ templates/org/pages/fiction-queen.php
@@ -60,13 +60,30 @@
 }
 ?>
 
-<h3 class="text-warning">The Fiction Queen <small>and her subjects</small></h3>
-<h6 class="text-success uppercase">Reviews and recomendations from Spokane Public Library's <b>Susan Creed</b></h6>
+<div class="row">
+	<div class="col-md-9">
+	<h3 class="text-warning">The Fiction Queen <small>and her subjects</small></h3>
+	<h6 class="text-success uppercase">Reviews and recomendations from Spokane Public Library's <b>Susan Creed</b></h6>
 
-<?php echo spl_get_fiction_queen(); ?>
+	<?php echo spl_get_fiction_queen(); ?>
 
-<p>
-	<a class="btn btn-success" href="/blog/topic/readers-corner/">
-		<i class="glyphicon glyphicon-comment"></i> 
-		More posts by the Fiction Queen &rarr;</a>
-</p>
+	<p>
+		<a class="btn btn-success" href="/blog/topic/readers-corner/">
+			<i class="glyphicon glyphicon-comment"></i> 
+			More posts by the Fiction Queen &rarr;</a>
+	</p>
+	</div>
+	<div class="col-md-3">
+		<div class="panel panel-primary">
+	    <div class="panel-heading">
+	      <h4>Find more on the library blog&hellip;</h4>
+	    </div>
+	    <div class="panel-body">
+	    <?php wp_tag_cloud( array('taxonomy' => array('post_tag', 'category')) ); ?>  
+	    </div>
+	    <div class="panel-footer">
+	      <h4 class="text-right"><a href="/blog/">All blog posts &rarr;</a></h4>
+	    </div>
+	  </div>
+	</div>
+</div>
