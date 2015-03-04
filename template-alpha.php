c06d0f278f6238fe5ad353fba223ade9a0553b02
Sean Girard <seangirard@yahoo.com>
2015-03-04 08:39:31 -0800

    home

--- template-alpha.php
+++ template-alpha.php
@@ -75,9 +75,7 @@ Template Name: Alpha Home Page
 
     <div class="row">
       <div class="col-md-12">
-        <div class="spl-tile-boxed">
         <?php echo do_shortcode('[spl_widget home-page auto pause posts limit=3]'); ?>
-        </div>
       </div><!-- /.col -->      
     </div><!-- /.row -->
 
