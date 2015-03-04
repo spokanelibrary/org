ba5f72c329956b1e815dfeda66fb2d43c29f7ee1
Sean Girard <seangirard@yahoo.com>
2015-03-04 13:49:38 -0800

    fq

--- templates/org/pages/fiction-queen.php
+++ templates/org/pages/fiction-queen.php
@@ -71,7 +71,7 @@
 	<div class="col-md-4">
 		<blockquote>
 			<p>
-				<img class="pull-left" style="width:100px; height:100px; margin-right:10px;" class="pull-left" src="/assets/img/jpg/screed.jpg">
+				<img class="pull-left" style="width:80px; height:80px; margin-right:10px;" class="pull-left" src="/assets/img/jpg/screed.jpg">
 				<b>Susan Creed</b> is an Assistant Manager at the Downtown Library.
 			</p>
 			<p>
