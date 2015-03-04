de64a2f139dc818e0dd868767dcc6b31cb617fb2
Sean Girard <seangirard@yahoo.com>
2015-03-04 11:00:53 -0800

    home

--- template-alpha.php
+++ template-alpha.php
@@ -87,7 +87,7 @@ Template Name: Alpha Home Page
         <div class="spl-hero-panel spl-hero-success" style="margin-bottom:10px;">
           <h4>What's new?</h4>
         </div>
-        <?php echo do_shortcode('[spl_widget home-page  browse limit=3]'); ?>
+        <?php echo do_shortcode('[spl_widget home-page carousel auto pause shuffle browse limit=3]'); ?>
         
       </div><!-- /.col -->      
     </div><!-- /.row -->
