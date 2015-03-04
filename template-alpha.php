4129946124950d92378d99428145c4eec99b4de9
Sean Girard <seangirard@yahoo.com>
2015-03-04 14:30:24 -0800

    home

--- template-alpha.php
+++ template-alpha.php
@@ -58,12 +58,12 @@ Template Name: Alpha Home Page
         <h4 class="text-warning">The Fiction Queen <small>and her subjects</small></h4>
         <h6 class="text-success uppercase">Reviews and recomendations from Spokane Public Library's <b>Susan Creed</b></h6>
       
-        <div class="serif">
+        <div class="serif spl-ficiton-queen">
           <img style="width:120px; height:120px; margin-right:10px; margin-bottom:6px;" class="pull-left" src="/assets/img/promos/spl-fiction-queen.jpg">
           <h4>Emperors &amp; ninjas in 1920's Japan</h4>
           <p>
-            Mary Russell and Sherlock Holmes meet a young ninja in their latest suspense novel set in Japan, <a href="/bib/553606"><i>Dreaming Spies</i></a> by Laurie King. 
-            If you’re not familiar with this series you’ll want to start with <a href="/bib/65255"><i>The Beekeeper’s Apprentice</i></a>, in which the young Miss Russell meets the retired Mr. Holmes. 
+            Mary Russell and Sherlock Holmes meet a young ninja in their latest suspense novel set in Japan, <a href="/bib/553606">Dreaming Spies</a> by Laurie King. 
+            If you’re not familiar with this series you’ll want to start with <a href="/bib/65255">The Beekeeper’s Apprentice</a>, in which the young Miss Russell meets the retired Mr. Holmes. 
           </p>
           <p>
             Reading this book has reminded me of one of my favorite fantasy series set in Japan and also involving a young ninja:
