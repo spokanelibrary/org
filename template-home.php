f33e7446084e5c27499666de4cbf93c46e1596bc
Sean Girard <seangirard@yahoo.com>
2015-03-04 15:55:18 -0800

    home

--- template-home.php
+++ template-home.php
@@ -36,25 +36,8 @@ Template Name: Home Page
       <?php echo do_shortcode('[spl_widget home-page facebook limit=2]'); ?>
   </div><!-- /.col --> 
   <div class="col-sm-8 col-sm-pull-4 col-md-9 col-md-pull-3">
-    <div class="spl-tile spl-tile-boxed">
-      <div class="spl-tile-body">
-        <h4 class="text-warning">The Fiction Queen <small>and her subjects</small></h4>
-        <h6 class="text-success uppercase">Reviews and recomendations from Spokane Public Library's <b>Susan Creed</b></h6>
-      
-        <div class="serif">
-          <img style="width:120px; height:120px; margin-right:10px; margin-bottom:6px;" class="pull-left" src="/assets/img/promos/spl-fiction-queen.jpg">
-          <h4>Emperors &amp; ninjas in 1920's Japan</h4>
-          <p>
-            Mary Russell and Sherlock Holmes meet a young ninja in their latest suspense novel set in Japan, <a href="/bib/553606"><i>Dreaming Spies</i></a> by Laurie King. 
-            If you’re not familiar with this series you’ll want to start with <a href="/bib/65255"><i>The Beekeeper’s Apprentice</i></a>, in which the young Miss Russell meets the retired Mr. Holmes. 
-          </p>
-          <p>
-            Reading this book has reminded me of one of my favorite fantasy series set in Japan and also involving a young ninja:
-            Author Lian Hearn begins his trilogy with <a href="/bib/537347"><i>Across the Nightingale Floor</i></a>.
-          </p>
-        </div>
-      </div>
-    </div>
+    
+    <?php echo do_shortcode('[spl_widget home-page fiction-queen]'); ?>
 
     <div class="row">
       <div class="col-md-8 col-md-push-4">
