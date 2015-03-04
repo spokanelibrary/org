56af48209f9a55203706334a9b12067bd5035754
Sean Girard <seangirard@yahoo.com>
2015-03-04 13:26:44 -0800

    home

--- templates/org/pages/fiction-queen.php
+++ templates/org/pages/fiction-queen.php
@@ -69,13 +69,15 @@
 	</div>
 
 	<div class="col-md-4">
-		<p>
+		<blockquote>
 		<img class="pull-left" style="width:100px; height:100px; margin-right:10px;" class="pull-left" src="/assets/img/promos/spl-fiction-queen.jpg">
 		<b>Susan Creed</b> is a Librarian and Assistant Manager at the Downtown Library.
 		She is sometimes the voice of Spokane Public Radio's production, <a href="http://spokanepublicradio.org/programs/bookshelf">The Bookshelf</a>.
-		</p>
-		<p>
+		<br>
 		<a href="/blog/topic/readers-corner/"><b>More from the Fiction Queen</b> &rarr;</a>
+		
+		</blockquote>
+		<p>
 		</p>
 	
 		<div class="panel panel-default">
