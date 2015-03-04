383f582d8af6fbe1360e53edb933f23e9caac124
Sean Girard <seangirard@yahoo.com>
2015-03-04 12:21:29 -0800

    home

---
+++ templates/org/pages/fiction-queen.php
@@ -0,0 +1,14 @@
+<h4 class="text-success">
+	<i class="glyphicon glyphicon-comment"></i> 
+	Recently posted
+</h4>
+
+<?php echo do_shortcode('[spl_widget recent-posts count=3 slug=readers-corner]'); ?>
+
+<p>&nbsp;</p>
+
+<p>
+	<a class="btn btn-success" href="/blog/topic/readers-corner/">
+		<i class="glyphicon glyphicon-comment"></i> 
+		More posts by the Fiction Queen &rarr;</a>
+</p>
