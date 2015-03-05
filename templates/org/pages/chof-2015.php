32c8b8df0cd9e149ef6ae23ef40a557a101c260d
Sean Girard <seangirard@yahoo.com>
2015-03-05 09:14:53 -0800

    home

--- templates/org/pages/chof-2015.php
+++ templates/org/pages/chof-2015.php
@@ -1,7 +1,11 @@
 <div class="row">
 
 	<div class="col-md-2">
-		panel
+		<div class="spl-tile spl-tile-boxed">		
+			<div class="spl-tile-body">
+				panel
+			</div>
+		</div>
 	</div>
 
 	<div class="col-md-2">
