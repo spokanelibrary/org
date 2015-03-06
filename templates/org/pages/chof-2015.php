833cb81a6d346861d07d2b9c36d9f0aa463a7e05
Sean Girard <seangirard@yahoo.com>
2015-03-06 11:41:57 -0800

    chof

--- templates/org/pages/chof-2015.php
+++ templates/org/pages/chof-2015.php
@@ -4,7 +4,7 @@
 			$img = '<img class="img-responsive" src="'.$img.'">';
 
 			$link = '
-						<div style="position:absolute; top:0; right:0; opacity:.5;">
+						<div style="position:absolute; top:4px; right:4px; opacity:.5;">
 	    			<small class="glyphicon glyphicon-share-alt" style="color:#fff;"></small>
 	    			</div>
 	    			';
