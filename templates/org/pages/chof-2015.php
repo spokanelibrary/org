8836029e6bd644ca9c1fb3748e7de15f7addbae6
Sean Girard <seangirard@yahoo.com>
2015-03-06 14:28:56 -0800

    chof

--- templates/org/pages/chof-2015.php
+++ templates/org/pages/chof-2015.php
@@ -345,11 +345,9 @@
 <div class="modal fade" id="chof-modal" tabindex="-1" role="dialog" aria-labelledby="chof-modal-label" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content chof-orange">
-      <div class="modal-header">
+      <div class="modal-body">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
         <h5 class="modal-title uppercase" id="chof-modal-label">Harold Balazs</h5>
-      </div>
-      <div class="modal-body">
         Induction
       </div>
 
