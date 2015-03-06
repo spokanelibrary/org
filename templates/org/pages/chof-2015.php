a04f345c440604d059249912a1f692c816365dda
Sean Girard <seangirard@yahoo.com>
2015-03-06 14:25:31 -0800

    chof

--- templates/org/pages/chof-2015.php
+++ templates/org/pages/chof-2015.php
@@ -342,12 +342,12 @@
 
 
 <!-- Modal -->
-<div class="modal fade" id="chof-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
+<div class="modal fade" id="chof-modal" tabindex="-1" role="dialog" aria-labelledby="chof-modal-label" aria-hidden="true">
+  <div class="modal-dialog chof-orange">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+        <h4 class="modal-title" id="chof-modal-label">Modal title</h4>
       </div>
       <div class="modal-body">
         ...
