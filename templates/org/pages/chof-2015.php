68360cbde0c5df3de3b4b9099ee404767da9ac0d
Sean Girard <seangirard@yahoo.com>
2015-03-06 10:03:40 -0800

    chof

--- templates/org/pages/chof-2015.php
+++ templates/org/pages/chof-2015.php
@@ -60,8 +60,7 @@
 	<?php echo get_chof_panel('Dr. Elizabeth Welty', 'chof-blue chof-text-light', 'http://lorempixel.com/g/200/200/people?10'); ?>
 	<?php echo get_chof_panel('', 'chof-red chof-text-light'); ?>
 	<?php echo get_chof_panel('Vicki McNeill', 'chof-yellow', 'http://lorempixel.com/g/200/200/people?11'); ?>
-	<?php echo get_chof_panel('Peg Hopkins', 'chof-green col-lg-push-10', 'http://lorempixel.com/g/200/200/people?12'); ?>
-
+	
 	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-8 col-lg-pull-2">
 		<div class="chof-panel" style="border:1px solid #666; height:170px;">
 			<div class="panel-body">		
@@ -73,6 +72,8 @@
 			</div>
 		</div>
 	</div>
+
+	<?php echo get_chof_panel('Peg Hopkins', 'chof-green col-lg-push-10', 'http://lorempixel.com/g/200/200/people?12'); ?>
 
 </div>
 
