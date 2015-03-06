ad9670c7e0e469076e31232a68d043e81d47b254
Sean Girard <seangirard@yahoo.com>
2015-03-06 14:40:32 -0800

    chof

--- templates/org/pages/chof-2015.php
+++ templates/org/pages/chof-2015.php
@@ -348,8 +348,8 @@
       <div class="modal-body">
         <div class="panel-body" style="background:#fff;">  
   	    	<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-      	  <h5 class="modal-title uppercase" id="chof-modal-label">Harold Balazs</h5>
-  	      Induction
+      	  <h3 class="modal-title uppercase" id="chof-modal-label">Harold Balazs</h3>
+  	      <h4>Arts &amp; Letters</h4>
 	      </div>
       
       </div>
