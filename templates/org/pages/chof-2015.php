dc07ea7ea66a1b889fa71eee4e06ba157124c17f
Sean Girard <seangirard@yahoo.com>
2015-03-06 09:14:48 -0800

    chof

--- templates/org/pages/chof-2015.php
+++ templates/org/pages/chof-2015.php
@@ -18,7 +18,7 @@
 					<div class="panel-body">	
 						'.$img.'
 						<div style="position:relative;">					
-							<div style="position:absolute; bottom:0; width:100%; background:#000; opacity:.5; color:#fff;">
+							<div style="position:absolute; bottom:0; width:100%; background:#000; opacity:1; color:#fff;">
 								<div style="opacity:1; color:#fff;">
 								<h6 class="uppercase" style="margin:6px 4px;">'.$title.'</h6>
 								</div>
