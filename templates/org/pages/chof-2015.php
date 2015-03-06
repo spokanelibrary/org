d0bb8fefb6fc0f962417891dd8d58d1a2a3cd79d
Sean Girard <seangirard@yahoo.com>
2015-03-06 13:14:29 -0800

    chof

--- templates/org/pages/chof-2015.php
+++ templates/org/pages/chof-2015.php
@@ -165,7 +165,7 @@
 	<div class="col-xs-6 col-sm-3 col-md-3 col-lg-2">
 		<div class="chof-panel chof-panel-sized chof-yellow chof-text-dark">
 			<div class="panel-body">
-				<img class="img-responsive" src="http://lorempixel.com/g/200/200/people/8">
+				<img class="img-responsive" src="http://lorempixel.com/g/200/200/people/1">
 				<div class="chof-text-wrapper">			
 					<div class="chof-text-mask">
 						<h6 class="chof-title">Donald F. Kardong</h6>
