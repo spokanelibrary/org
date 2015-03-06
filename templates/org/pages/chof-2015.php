a53121816858a0384bfe1d27c2e80d35386ccbec
Sean Girard <seangirard@yahoo.com>
2015-03-06 09:41:59 -0800

    chof

--- templates/org/pages/chof-2015.php
+++ templates/org/pages/chof-2015.php
@@ -17,8 +17,8 @@
 		if ($img) {
 			$html .= '<div class="panel-body">'.PHP_EOL;
 			$html .= $img.PHP_EOL;
-			$html .= '<div style="position:relative; background:#000; opacity:.5;">'.PHP_EOL;				
-			$html .= '<div style="position:absolute; bottom:0; width:100%;">'.PHP_EOL;
+			$html .= '<div style="position:relative;">'.PHP_EOL;				
+			$html .= '<div style="position:absolute; bottom:0; width:100%; background:#000; opacity:.5;">'.PHP_EOL;
 			$html .= '<div>'.PHP_EOL;
 			$html .= '<h6 class="uppercase" style="margin:6px 4px;">'.$title.'</h6>'.PHP_EOL;
 			$html .= '</div>'.PHP_EOL;
