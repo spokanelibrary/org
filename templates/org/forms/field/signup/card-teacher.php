202e628e7c34c4e22cd62177e49e8ca0d0a1785b
Sean Girard <seangirard@yahoo.com>
2015-03-10 14:52:49 -0700

    card

--- templates/org/forms/field/signup/card-teacher.php
+++ templates/org/forms/field/signup/card-teacher.php
@@ -4,10 +4,42 @@
     <fieldset>
       <legend class="text-muted">
         <small class="glyphicon glyphicon-"></small>
-        Teacher
+        Let's get started
       </legend>
-
-
+      <?php include 'shared/card-name.php'; ?>
     </fieldset>
+
+    <fieldset>
+      <legend class="text-muted">
+        <small class="glyphicon glyphicon-"></small>
+        Tell us how to reach you
+      </legend>
+      <?php include 'shared/card-contact.php'; ?>
+    </fieldset>
+
+    <fieldset>
+      <legend class="text-muted">
+        <small class="glyphicon glyphicon-"></small>
+        Your primary mailing address
+      </legend>
+      <?php include 'shared/card-address.php'; ?>
+    </fieldset>
+
+    <fieldset>
+      <legend class="text-muted">
+        <small class="glyphicon glyphicon-"></small>
+        Library card eligibility
+      </legend>
+      <?php include 'shared/card-eligible.php'; ?>
+    </fieldset>
+
+    <fieldset>
+      <legend class="text-muted">
+        <small class="glyphicon glyphicon-"></small>
+        Complete your application
+      </legend>
+      <?php include 'shared/card-apply.php'; ?>
+    </fieldset>
+
   </div><!-- /.panel-body -->
 </div><!-- /.panel -->
