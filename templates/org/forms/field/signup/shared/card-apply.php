58ba3cd8d8d63912f8d4c784751615329387178e
Sean Girard <seangirard@yahoo.com>
2015-03-10 08:47:54 -0700

    signup

--- templates/org/forms/field/signup/shared/card-apply.php
+++ templates/org/forms/field/signup/shared/card-apply.php
@@ -1,10 +1,15 @@
 <div class="form-group">
   <div class="col-sm-offset-3 col-sm-3">
-    Terms &amp; Conditions
+    <div class="checkbox">
+      <label>
+        <input type="checkbox" value="">
+        I agree to Spokane Public Library Terms &amp; Conditions.
+      </label>
+    </div>
   </div>
   <div class="col-sm-6 col-md-3">
     <button type="submit" class="btn btn-block btn-primary">
-      <b>Get your library card</b>
+      <b>Get my library card</b>
       <small class="glyphicon glyphicon-arrow-right"></small>
     </button>
   </div>
