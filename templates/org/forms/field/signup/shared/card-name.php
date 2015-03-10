bfc12ac00ea148c3b08e03f1a2f553f59fa6fcd9
Sean Girard <seangirard@yahoo.com>
2015-03-10 15:58:55 -0700

    card

---
+++ templates/org/forms/field/signup/shared/card-name.php
@@ -0,0 +1,16 @@
+<div class="form-group">
+	<label for="" class="col-sm-3 control-label">My name</label>
+  <div class="col-sm-3" id="">
+    <span class="help-block">Fist</span>
+    <input type="text" class="form-control uppercase required" id="spl-form-name-first" name="spl-form[signup][name][first]" value="" placeholder="">
+  </div>
+  <div class="col-sm-3" id="">
+    <span class="help-block">Middle</span>
+    <input type="text" class="form-control uppercase required" id="spl-form-name-middle" name="spl-form[signup][name][middle]" value="" placeholder="">
+  </div>
+  <div class="col-sm-3" id="">
+    <span class="help-block">Last</span>
+    <input type="text" class="form-control uppercase required" id="spl-form-name-last" name="spl-form[signup][name][last]" value="" placeholder="">
+  </div>
+</div>
+
