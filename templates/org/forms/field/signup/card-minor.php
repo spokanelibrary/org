4145b103941737a57f9327bff46f958c6d85db2c
Sean Girard <seangirard@yahoo.com>
2015-03-10 14:42:32 -0700

    card

--- templates/org/forms/field/signup/card-minor.php
+++ templates/org/forms/field/signup/card-minor.php
@@ -1,11 +1,45 @@
 <div class="panel panel-info">
   <div class="panel-body">
 
-    <fieldset>
-      <legend class="text-muted">
-        <small class="glyphicon glyphicon-"></small>
-        Mr. Minor
-      </legend>
+        <fieldset>
+	      <legend class="text-muted">
+	        <small class="glyphicon glyphicon-"></small>
+	        Let's get started
+	      </legend>
+	      <?php include 'shared/card-name.php'; ?>
+	    </fieldset>
+
+	    <fieldset>
+	      <legend class="text-muted">
+	        <small class="glyphicon glyphicon-"></small>
+	        Tell us how to reach you
+	      </legend>
+	      <?php include 'shared/card-contact.php'; ?>
+	    </fieldset>
+
+	    <fieldset>
+	      <legend class="text-muted">
+	        <small class="glyphicon glyphicon-"></small>
+	        Your primary mailing address
+	      </legend>
+	      <?php include 'shared/card-address.php'; ?>
+	    </fieldset>
+
+	    <fieldset>
+	      <legend class="text-muted">
+	        <small class="glyphicon glyphicon-"></small>
+	        Library card eligibility
+	      </legend>
+	      <?php include 'shared/card-eligible.php'; ?>
+	    </fieldset>
+
+	    <fieldset>
+	      <legend class="text-muted">
+	        <small class="glyphicon glyphicon-"></small>
+	        Complete your application
+	      </legend>
+	      <?php include 'shared/card-apply.php'; ?>
+	    </fieldset>
 
 
     </fieldset>
