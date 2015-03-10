dc8de521dca8c3c19e6271e95a36cebcc93dd221
Sean Girard <seangirard@yahoo.com>
2015-03-10 08:31:15 -0700

    home

--- templates/org/forms/field/signup/shared/card-apply.php
+++ templates/org/forms/field/signup/shared/card-apply.php
@@ -2,9 +2,9 @@
   <div class="col-sm-offset-3 col-sm-3">
     Terms &amp; Conditions
   </div>
-  <div class="col-sm-6">
+  <div class="col-sm-3">
     <button type="submit" class="btn btn-block btn-primary">
-      <b>Get your library card now</b>
+      <b>Get your library card</b>
       <small class="glyphicon glyphicon-arrow-right"></small>
     </button>
   </div>
