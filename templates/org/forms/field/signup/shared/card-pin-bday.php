1d10e73bf7f010aaca515ba5ac97955111e8b613
Sean Girard <seangirard@yahoo.com>
2015-03-10 15:58:40 -0700

    card

--- templates/org/forms/field/signup/shared/card-pin-bday.php
+++ templates/org/forms/field/signup/shared/card-pin-bday.php
@@ -1,20 +1,4 @@
 <div class="form-group">
-	<label for="" class="col-sm-3 control-label">My name</label>
-  <div class="col-sm-3" id="">
-    <span class="help-block">Fist</span>
-    <input type="text" class="form-control uppercase required" id="spl-form-name-first" name="spl-form[signup][name][first]" value="" placeholder="">
-  </div>
-  <div class="col-sm-3" id="">
-    <span class="help-block">Middle</span>
-    <input type="text" class="form-control uppercase required" id="spl-form-name-middle" name="spl-form[signup][name][middle]" value="" placeholder="">
-  </div>
-  <div class="col-sm-3" id="">
-    <span class="help-block">Last</span>
-    <input type="text" class="form-control uppercase required" id="spl-form-name-last" name="spl-form[signup][name][last]" value="" placeholder="">
-  </div>
-</div>
-
-<div class="form-group">
   <label for="spl-form-pin" class="col-sm-3 control-label">
     <span class="hidden-sm">Choose a</span> 4-digit PIN 
     <br>
