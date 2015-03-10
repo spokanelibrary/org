420fa1b18dd7504a2220ec29fd3d9c4f38665f43
Sean Girard <seangirard@yahoo.com>
2015-03-10 10:55:04 -0700

    card

--- templates/org/forms/field/signup/shared/card-apply.php
+++ templates/org/forms/field/signup/shared/card-apply.php
@@ -3,23 +3,25 @@
     <p>
       <a href="#"><b>Spokane Public Library Terms &amp; Conditions</b></a>
     </p>
-    <div class="checkbox">
-      <label>
-        <input type="checkbox" value="">
-        I accept.
-      </label>
-    </div>
   </div>
   <div class="col-sm-3 col-sm-offset-3">
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" value="">
+            I accept.
+          </label>
+        </div>
+
+        <button type="submit" class="btn btn-block btn-primary">
+          <b>Get my library card</b>
+          <small class="glyphicon glyphicon-arrow-right"></small>
+        </button>
+
         
-        <span class="help-block">
-          Use of your library card constitutes an acceptance of our <a href="#">Terms &amp; Conditions</a>.
-        </span>
   </div>
   <div class="col-sm-6 col-md-3">
-    <button type="submit" class="btn btn-block btn-primary">
-      <b>Get my library card</b>
-      <small class="glyphicon glyphicon-arrow-right"></small>
-    </button>
+      <span class="help-block">
+          Use of your library card constitutes an acceptance of our <a href="#">Terms &amp; Conditions</a>.
+        </span>
   </div>
 </div>
