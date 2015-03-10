312b418ad13122851822556f53426a20c7fb36f0
Sean Girard <seangirard@yahoo.com>
2015-03-10 12:05:35 -0700

    jobs

--- templates/org/forms/field/signup/shared/card-apply.php
+++ templates/org/forms/field/signup/shared/card-apply.php
@@ -1,31 +1,44 @@
 <div class="form-group">
   <div class="col-sm-9 col-sm-offset-3">
     <p>
-      <a href="#"><b>Spokane Public Library <i>Terms &amp; Conditions</i></b></a>
+      <a href="#" data-toggle="modal" data-target="#spl-terms-conditions"><b>Spokane Public Library <i>Terms &amp; Conditions</i></b></a>
     </p>
   </div>
   <div class="col-sm-9 col-sm-offset-3">
-        <div class="checkbox">
-          <p>
-          <label>
-            <input type="checkbox" class="required" value="">
-            I agree to these terms and conditions.
-          </label>
-        </p>
-        </div>
-
-        
-
-        
-  </div>
-
-  <div class="col-sm-6 col-sm-offset-3 col-md-6">
-    <p>
-    <button type="submit" class="btn btn-block btn-primary">
-          <b>Get my library card</b>
-          <small class="glyphicon glyphicon-arrow-right"></small>
-        </button>
+    <div class="checkbox">
+      <p>
+        <label>
+          <input type="checkbox" class="required" value="">
+          I agree to these terms and conditions.
+        </label>
       </p>
+    </div>        
   </div>
+  <div class="col-sm-6 col-sm-offset-3">
+    <p>
+      <button type="submit" class="btn btn-block btn-primary">
+        <b>Get my library card</b>
+        <small class="glyphicon glyphicon-arrow-right"></small>
+      </button>
+    </p>
+  </div>
+</div>
 
+<!-- Modal -->
+<div class="modal fade" id="spl-terms-conditions" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
 </div>
