53c21dd60f4d29cb59d38fef40b3ac5052081905
Sean Girard <seangirard@yahoo.com>
2015-03-10 12:08:27 -0700

    card

--- templates/org/forms/field/signup/shared/card-apply.php
+++ templates/org/forms/field/signup/shared/card-apply.php
@@ -25,19 +25,18 @@
 </div>
 
 <!-- Modal -->
-<div class="modal fade" id="spl-terms-conditions" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<div class="modal fade" id="spl-terms-conditions" tabindex="-1" role="dialog" aria-labelledby="spl-terms-conditions-title" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+        <h4 class="modal-title" id="spl-terms-conditions-title">Library Card Terms &amp; Conditions</h4>
       </div>
       <div class="modal-body">
         ...
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-success">I agree!</button>
       </div>
     </div>
   </div>
