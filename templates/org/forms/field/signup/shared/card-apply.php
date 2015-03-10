ca8179507bad6c91e2c950e7664e5577a9dacdb7
Sean Girard <seangirard@yahoo.com>
2015-03-10 11:03:38 -0700

    card

--- templates/org/forms/field/signup/shared/card-apply.php
+++ templates/org/forms/field/signup/shared/card-apply.php
@@ -17,7 +17,7 @@
         
   </div>
 
-  <div class="col-sm-6 col-sm-offset-3">
+  <div class="col-sm-3 col-sm-offset-3">
     <p>
     <button type="submit" class="btn btn-block btn-primary">
           <b>Get my library card</b>
