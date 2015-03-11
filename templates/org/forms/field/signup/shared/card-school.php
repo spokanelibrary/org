e7869a546750fc4da023742e6ec8ca78d2b915c4
Sean Girard <seangirard@yahoo.com>
2015-03-11 10:40:28 -0700

    card

--- templates/org/forms/field/signup/shared/card-school.php
+++ templates/org/forms/field/signup/shared/card-school.php
@@ -3,26 +3,26 @@
   <div class="col-sm-3">
     <input type="text" class="form-control required" id="spl-form-school-email" name="spl-form[signup][school][name]" value="" placeholder="">
   </div>
-    <label for="spl-form-school-phone" class="col-sm-3 control-label">School phone number</label>
+    <label for="spl-form-school-phone" class="col-sm-3 control-label text-success">School phone number</label>
   <div class="col-sm-3">
     <input type="text" class="form-control" id="spl-form-school-phone required" name="spl-form[signup][school][phone]" value="" placeholder="">
   </div>
 </div>
 
 <div class="form-group">
-  <label for="spl-form-school-street" class="col-sm-3 control-label">School address</label>
+  <label for="spl-form-school-street" class="col-sm-3 control-label text-success">School address</label>
   <div class="col-sm-3">
     <textarea rows="3" class="form-control required spl-signup-address" id="spl-form-school-street" name="spl-form[signup][school][address][street]"></textarea>
   </div>
-  <label for="spl-form-school-city" class="col-sm-1 control-label">City</label>
+  <label for="spl-form-school-city" class="col-sm-1 control-label text-success">City</label>
   <div class="col-sm-2">
     <input type="text" class="form-control required spl-signup-address" id="spl-form-school-city" name="spl-form[signup][school][address][city]" value="" placeholder="">
   </div>
-  <label for="spl-form-school-state" class="col-sm-1 control-label">State</label>
+  <label for="spl-form-school-state" class="col-sm-1 control-label text-success">State</label>
   <div class="col-sm-2">
     <input type="text" class="form-control required spl-signup-address" id="spl-form-school-state" name="spl-form[signup][school][address][state]" value="" placeholder="">
   </div>
-  <label for="spl-form-school-zip" class="col-sm-1 col-sm-offset-3 control-label">ZIP</label>
+  <label for="spl-form-school-zip" class="col-sm-1 col-sm-offset-3 control-label text-success">ZIP</label>
   <div class="col-sm-2">
     <input type="text" class="form-control required spl-signup-address" id="spl-form-school-zip" name="spl-form[signup][school][address][zip]" value="" placeholder="">
   </div>
