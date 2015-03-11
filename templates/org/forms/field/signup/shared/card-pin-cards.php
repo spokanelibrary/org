e2604fffa9bc514e808a1729f9ca8bd66f5be1ba
Sean Girard <seangirard@yahoo.com>
2015-03-11 11:17:00 -0700

    cards

---
+++ templates/org/forms/field/signup/shared/card-pin-cards.php
@@ -0,0 +1,17 @@
+<div class="form-group">
+  <?php include 'card-pin.php'; ?>
+  <label for="spl-form-cards" class="col-sm-3 control-label">Number of cards</label>
+  <div class="col-sm-3">
+    <select class="form-control required" id="spl-form-cards" name="spl-form[signup][cards]">
+      <option value="1" selected>1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+    </select>
+    <span class="help-block">
+      PIN is the same for all cards. You can change these later.
+    </span>
+  </div>
+</div>
+
