b6adde4da7d847d76aa4e56439d47663ce2d3085
Sean Girard <seangirard@yahoo.com>
2015-03-11 14:10:34 -0700

    card

--- templates/org/forms/field/signup/shared/card-guardian.php
+++ templates/org/forms/field/signup/shared/card-guardian.php
@@ -1,15 +1,18 @@
 <div class="form-group">
-	<label for="" class="col-sm-3 control-label">Parent or guardian</label>
+	<label for="" class="col-sm-3 control-label">My parent or guardian</label>
   <div class="col-sm-3">
+    <span class="help-block">First</span>
     <input type="text" class="form-control" id="spl-form-guardian-first" name="spl-form[signup][guardian][first]" value="" placeholder="">
   </div>
   <div class="col-sm-3">
+    <span class="help-block">Middle</span>
     <input type="text" class="form-control" id="spl-form-guardian-middle" name="spl-form[signup][guardian][middle]" value="" placeholder="">
   </div>
   <div class="col-sm-3">
+    <span class="help-block">Last</span>
     <input type="text" class="form-control" id="spl-form-guardian-last" name="spl-form[signup][guardian][last]" value="" placeholder="">
   </div>
-  <div class="col-sm-12">
+  <div class="col-sm-9 col-sm-offset-3">
     <span class="help-block">
       If under 18
     </span>
