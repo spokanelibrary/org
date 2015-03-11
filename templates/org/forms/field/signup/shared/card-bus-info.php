5b20d5bf2089c55bd89fe0f659c6658504dcd1f6
Sean Girard <seangirard@yahoo.com>
2015-03-11 11:23:24 -0700

    card

---
+++ templates/org/forms/field/signup/shared/card-bus-info.php
@@ -0,0 +1,8 @@
+<div class="form-group">
+  <label for="spl-form-org" class="col-sm-3 control-label text-success">Name of business or organization</label>
+  <div class="col-sm-3">
+    <input type="text" class="form-control required" id="spl-form-org" name="spl-form[signup][org]" value="" placeholder="">
+  </div>
+</div>
+
+<?php include 'card-address.php'; ?>
