bcedcab0648cd13deebdc0f8160ca2755b90c08f
Sean Girard <seangirard@yahoo.com>
2015-03-11 11:24:40 -0700

    card

--- templates/org/forms/field/signup/shared/card-bus-info.php
+++ templates/org/forms/field/signup/shared/card-bus-info.php
@@ -1,6 +1,6 @@
 <div class="form-group">
-  <label for="spl-form-org" class="col-sm-3 control-label text-success">Name of business or organization</label>
-  <div class="col-sm-3">
+  <label for="spl-form-org" class="col-sm-6 control-label">Name of business or organization</label>
+  <div class="col-sm-6">
     <input type="text" class="form-control required" id="spl-form-org" name="spl-form[signup][org]" value="" placeholder="">
   </div>
 </div>
