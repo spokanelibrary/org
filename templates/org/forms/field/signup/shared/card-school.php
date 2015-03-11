5d33f29971de2dac81359374b3e6b0024e53fc2a
Sean Girard <seangirard@yahoo.com>
2015-03-11 10:39:17 -0700

    card

--- templates/org/forms/field/signup/shared/card-school.php
+++ templates/org/forms/field/signup/shared/card-school.php
@@ -1,11 +1,11 @@
 <div class="form-group">
-  <label for="spl-form-school-name" class="col-sm-3 control-label">Name of school</label>
+  <label for="spl-form-school-name" class="col-sm-3 control-label text-success">Name of school</label>
   <div class="col-sm-3">
-    <input type="text" class="form-control" id="spl-form-school-email" name="spl-form[signup][school][name]" value="" placeholder="">
+    <input type="text" class="form-control required" id="spl-form-school-email" name="spl-form[signup][school][name]" value="" placeholder="">
   </div>
     <label for="spl-form-school-phone" class="col-sm-3 control-label">School phone number</label>
   <div class="col-sm-3">
-    <input type="text" class="form-control" id="spl-form-school-phone" name="spl-form[signup][school][phone]" value="" placeholder="">
+    <input type="text" class="form-control" id="spl-form-school-phone required" name="spl-form[signup][school][phone]" value="" placeholder="">
   </div>
 </div>
 
