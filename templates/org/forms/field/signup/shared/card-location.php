39bb7d516506178d1a362ff836026807e6ef3307
Sean Girard <seangirard@yahoo.com>
2015-03-11 15:45:10 -0700

    card

---
+++ templates/org/forms/field/signup/shared/card-location.php
@@ -0,0 +1,21 @@
+<div class="form-group">
+  <label for="spl-form-location" class="col-sm-3 control-label">My library branch</label>
+  <div class="col-sm-2">
+    <select class="form-control required" id="spl-form-cards" name="spl-form[signup][cards]">
+      <option value="dt" selected>Downtown</option>
+      <option value="es">East Side</option>
+      <option value="hy">Hillyard</option>
+      <option value="it">Indian Trail</option>
+      <option value="sh">Shadle</option>
+      <option value="so">South Hill</option>
+    </select>
+  </div>
+  <div class="col-sm-6">
+    <span class="help-block">
+      Your library branch is the default location to pickup materials you have requested.
+      You can change this later.
+    </span>
+  </div>
+
+</div>
+
