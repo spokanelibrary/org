fbb19b1644c149fc30f48c5bea97ea32a9c49274
Sean Girard <seangirard@yahoo.com>
2015-03-11 15:02:09 -0700

    card

--- templates/org/forms/field/library-card-response.php
+++ templates/org/forms/field/library-card-response.php
@@ -1,42 +1,18 @@
-<?php
-echo '<pre>';
-print_r($crass);
-echo '</pre>';
-?>
 
-<?php if ( isset( $crass->response ) ) : ?>
+<?php if ( isset( $crass->result ) ) : ?>
 
 <div class="alert alert-success">
   
   <h2>Success!</h2>
-
-  <blockquote>
-    <p>
-      Your card has been renewed until <b><?php echo $crass->result['date']; ?></b>.
-    </p>
-    <p>
-      If you have updated your contact information, your changes will not be reflected on your account until they are reviewed by library circulation staff.
-    </p>
-  </blockquote>
 
 </div><!-- /.alert -->
 
 <?php else: ?>
 
 <div class="alert alert-error">
+
   <h2>Oops!</h2>
 
-  <blockquote>
-    <p>
-      It looks like we ran into a problem renewing your card.
-    </p>
-    <p>
-      Please give us a call at <strong>444-5333</strong> so we can take a look at this for you.
-    </p>
-    <p>
-      Thanks, and apologies for the inconvenience.
-    </p>
-  </blockquote>
 
 </div><!-- /.alert -->
 
@@ -44,10 +20,12 @@ echo '</pre>';
 
 <?php
 
-/*
+
 echo '<pre>';
+echo 'Result';
 print_r($crass->result);
-//print_r($crass->request); 
+echo 'Request';
+print_r($crass->request); 
 echo '</pre>';
-*/
+
 ?>
