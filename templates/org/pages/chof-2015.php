2d32afe578751f1b086ddf8b5bb6d5dd490521fc
Sean Girard <seangirard@yahoo.com>
2015-03-12 09:06:15 -0700

    chof

--- templates/org/pages/chof-2015.php
+++ templates/org/pages/chof-2015.php
@@ -42,7 +42,7 @@
 				<div class="panel-body">
 					<div class="chof-text-wrapper">			
 						<div class="chof-icon-mask" style="width:auto; right:0;">
-							<h6 class="chof-title">AL</h6>
+							<h6 class="chof-title chof-text-orange">AL</h6>
 						</div>
 					</div>
 					<!--
