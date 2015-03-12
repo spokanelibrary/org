549b5b85e7102c5489ce1a81fc8f8349e72ff728
Sean Girard <seangirard@yahoo.com>
2015-03-12 15:01:02 -0700

    card

---
+++ templates/org/forms/field/signup/shared/card-address-alt.php
@@ -0,0 +1,20 @@
+<div class="form-group">
+  <label for="spl-form-street" class="col-sm-3 control-label">Street address</label>
+  <div class="col-sm-3">
+    <!--<textarea rows="3" class="form-control required spl-signup-address-alt" id="spl-form-street-alt" name="spl-form[signup][address-alt][street]"></textarea>-->
+    <input type="text" class="form-control required spl-signup-address-alt" id="spl-form-street-alt-1" name="spl-form[signup][address][street-alt][1]" value="" placeholder="">
+    <input type="text" class="form-control required spl-signup-address-alt" id="spl-form-street-alt-2" name="spl-form[signup][address][street-alt][2]" value="" placeholder="">
+  </div>
+  <label for="spl-form-city" class="col-sm-1 control-label">City</label>
+  <div class="col-sm-2">
+    <input type="text" class="form-control required spl-signup-address-alt" id="spl-form-city-alt" name="spl-form[signup][address-alt][city]" value="" placeholder="">
+  </div>
+  <label for="spl-form-state" class="col-sm-1 control-label uppercase">State</label>
+  <div class="col-sm-2">
+    <input type="text" class="form-control required spl-signup-address-alt" id="spl-form-state-alt" name="spl-form[signup][address-alt][state]" value="" placeholder="">
+  </div>
+  <label for="spl-form-zip" class="col-sm-1 col-sm-offset-3 control-label">ZIP</label>
+  <div class="col-sm-2">
+    <input type="text" class="form-control required spl-signup-address-alt" id="spl-form-zip-alt" name="spl-form[signup][address-alt][zip]" value="" placeholder="">
+  </div>
+</div>
