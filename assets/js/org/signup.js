b2acfff367c3535f7911fc73ff7a3b4bb1aac3b8
Sean Girard <seangirard@yahoo.com>
2015-03-12 10:38:22 -0700

    chagne

--- assets/js/org/signup.js
+++ assets/js/org/signup.js
@@ -55,7 +55,7 @@ var org = {
     });    
 
     $('body').on('change', "[name='spl-form[card][type]']", function(e) {
-      $(this).nearest('form').submit();
+      $(this).parent('form').submit();
     });
 
     /*
