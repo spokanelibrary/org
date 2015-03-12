6720a1f77442c11f832416366a3d2e6f73ec083c
Sean Girard <seangirard@yahoo.com>
2015-03-12 10:27:39 -0700

    card

--- templates/org/forms/field/signup/card-type.php
+++ templates/org/forms/field/signup/card-type.php
@@ -21,7 +21,7 @@
           <label for="spl-form-card-type" class="col-sm-2 control-label">
             <h2>I am:</h2>
           </label>
-          <div class="col-sm-10 col-md-8" id="spl-form-card-type">
+          <div class="col-sm-10 col-md-8 col-lg-7" id="spl-form-card-type">
             <div class="alert well">
               <div class="radio">
                 <label>
@@ -66,7 +66,7 @@
         </div>
 
         <div class="form-group">
-          <div class="col-sm-offset-2 col-sm-10 col-md-8">
+          <div class="col-sm-offset-2 col-sm-10 col-md-8 col-lg-7">
             <button type="submit" class="btn btn-lg btn-block btn-primary">
               <b>Next</b>
               <small class="glyphicon glyphicon-arrow-right"></small>
