6da6ca59f532c33d45a2f1799862b7bf12a5153e
Sean Girard <seangirard@yahoo.com>
2015-03-13 09:59:48 -0700

    card

--- templates/org/header-no-chrome.php
+++ templates/org/header-no-chrome.php
@@ -1,37 +1,14 @@
 <?php
 
 function spl_get_home_url() {
-  return home_url();
+  //return home_url();
   //return 'http://www.spokanelibrary.org';
+  return './';
 }
 
 ?>
 
-<script>/*(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=289675684463099";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));*/</script>  
-
-<header class="navbar-common navbar-fixed-top visible-xs visible-sm">
-  <!--<div class="container">-->
-  <ul class="nav nav-justified">
-  <?php 
-  wp_nav_menu( 
-  array(
-    'menu' => 'Common Navigation'
-  , 'container' => false
-  , 'items_wrap' => '%3$s'
-  )); 
-  ?>
-  </ul>
-  <!--</div>-->
-</header>
-
-<?php //if ( is_user_logged_in() ) :?>
-<header style="border:none;" class="navbar-masthead banner navbar navbar-inverse navbar-static-top visible-md visible-lg" role="banner">
+<header style="border:none; margin-bottom:20px;" class="navbar-masthead banner navbar navbar-inverse navbar-static-top visible-md visible-lg" role="banner">
   <div class="container">
     <div class="row">
       <div class="col-md-2">
