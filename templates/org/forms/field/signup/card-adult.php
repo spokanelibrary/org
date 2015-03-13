b40cd5eacecb59499f3927aac4c7b5b0fcff85c7
Sean Girard <seangirard@yahoo.com>
2015-03-13 10:06:18 -0700

    CARD

--- templates/org/forms/field/signup/card-adult.php
+++ templates/org/forms/field/signup/card-adult.php
@@ -4,7 +4,7 @@
       Library Card Application
       <small class="pull-right">
         <i class="glyphicon glyphicon-refresh"></i>
-        <a href="./"><span class="text-muted">Start over</span></a>
+        <a href="<?php echo $_SERVER['REQUEST_URI'] ;?>"><span class="text-muted">Start over</span></a>
       </small>
     </h4>
   </div>
