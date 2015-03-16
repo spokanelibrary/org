c3b0625b936a32d4932f75ad63c5f29e7119e2f6
Sean Girard <seangirard@yahoo.com>
2015-03-16 11:35:14 -0700

    user

--- templates/org/header-top-navbar.php
+++ templates/org/header-top-navbar.php
@@ -26,10 +26,6 @@ function spl_get_home_url() {
   , 'items_wrap' => '%3$s'
   )); 
   ?>
-  <?php if ( isset($_SESSION['spl']['user']) && !empty($_SESSION['spl']['user']) ) {
-    echo '<li class="menu-logout"><a href="./?logout"><i class="glyphicon glyphicon-log-out text-danger"></i></a></li>';
-  }
-  ?>
   </ul>
   <!--</div>-->
 </header>
@@ -61,7 +57,7 @@ function spl_get_home_url() {
                 )); 
                 ?>
                 <?php if ( isset($_SESSION['spl']['user']) && !empty($_SESSION['spl']['user']) ) {
-                  echo '<li class="menu-logout"><a href="./?logout"><i class="glyphicon glyphicon-log-out text-danger"></i></a></li>';
+                  echo '<li class="menu-logout"><a href="./?logout"><i class="glyphicon glyphicon-log-out text-danger"></i> <span class="hidden-xs hidden-md text-danger">Logout</span></a></li>';
                 }
                 ?>
                 </ul>
