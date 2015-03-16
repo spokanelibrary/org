31ebcc9027f412661b15a9825a624c5d59052fb8
Sean Girard <seangirard@yahoo.com>
2015-03-16 12:14:00 -0700

    card

--- templates/org/forms/field/signup/shared/card-eligible.php
+++ templates/org/forms/field/signup/shared/card-eligible.php
@@ -59,7 +59,7 @@
         </ul>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-success" data-dismiss="modal">Close!</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
       </div>
     </div>
   </div>
