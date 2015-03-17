577cce70a66dfde8ac18be30d6390232df6e6f4a
Sean Girard <seangirard@yahoo.com>
2015-03-17 15:42:39 -0700

    home

--- template-alpha.php
+++ template-alpha.php
@@ -25,7 +25,7 @@ Template Name: Alpha Home Page
 <div class="row">
  
   <div class="col-sm-4 col-md-3 col-md-push-9 col-lg-2 col-lg-push-10">
-    <div class="spl-hero-panel spl-hero-primary" style="margin-bottom:10px;">
+    <div class="spl-hero-panel spl-hero-primary">
       <h5 class="text-center">Digital Branch</h5>
     </div>
     <?php echo get_alpha_panel('Digital Branch'); ?>
@@ -42,8 +42,8 @@ Template Name: Alpha Home Page
   <div class="col-sm-12 col-md-3 col-md-pull-9 col-lg-2 col-lg-pull-9">
     <div class="spl-hero-panel spl-hero-success" style="margin-bottom:10px;">
       <h5 class="text-center">Is the library open?</h5>
+      <?php echo do_shortcode('[spl_widget branch-hours]'); ?>
     </div>
-    <?php echo do_shortcode('[spl_widget branch-hours]'); ?>
   </div><!-- /.col -->
 
 </div><!-- /.row -->
