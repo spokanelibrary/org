3a56212aa529ed13f59ac3c83275b1343754cf67
Sean Girard <seangirard@yahoo.com>
2015-03-17 04:05:10 -0700

    home

--- template-home.php
+++ template-home.php
@@ -4,10 +4,111 @@ Template Name: Home Page
 */
 ?>
 
+
+<div class="row">
+
+  <div class="col-md-3 col-sm-6">
+    <div class="spl-tile spl-tile-info" style="padding-left:10px;">
+      <h4 class="uppercase">
+        <a href="/catalog/"><span class="text-info">Library Catalog</span></a>
+      </h4>
+      <ul class="list-unstyled">
+        <li>
+          <h5>
+            <a href="/catalog/"><span class="text-info">Search</span></a>
+          </h5>
+        </li>
+        <li>
+          <h5>
+            <a href="/login/"><span class="text-info">My account</span></a>
+          </h5>
+        </li>
+        <li>
+          <h5>
+            <a href="/new/"><span class="text-info">Browse new arrivals</span></a>
+          </h5>
+        </li>
+      </ul>
+    </div>
+  </div><!-- /.col -->
+
+  <div class="col-md-3 col-sm-6">
+    <div class="spl-tile spl-tile-primary" style="padding-left:10px;">
+      <h4 class="uppercase">
+        <a href="/digital/"><span class="text-primary">Digital Branch</span></a>
+      </h4>
+      <ul class="list-unstyled">
+        <li>
+          <h5>
+            <a href="/download/"><span class="text-primary">Download eBooks, music, magazines, &amp; more</span></a>
+          </h5>
+        </li>
+        <li>
+          <h5>
+            <a href="/apps/"><span class="text-primary">Library apps</span></a>
+          </h5>
+        </li>
+      </ul>
+    </div>
+  </div><!-- /.col -->
+
+  <div class="col-md-3 col-sm-6">
+    <div class="spl-tile spl-tile-success" style="padding-left:10px;">
+      <h4 class="uppercase">
+        <a href="/calendar/"><span class="text-success">Classes &amp; Events</span></a>
+      </h4>
+      <ul class="list-unstyled">
+        <li>
+          <h5>
+            <a href="/storytime/"><span class="text-success">Storytime schedules</span></a>
+          </h5>
+        </li>
+        <li>
+          <h5>
+            <a href="/connected-learning/"><span class="text-success">Computers &amp; technology</span></a>
+          </h5>
+        </li>
+        <li>
+          <h5>
+            <a href="/calendar/"><span class="text-success">Complete calendar of events</span></a>
+          </h5>
+        </li>
+      </ul>
+    </div>
+  </div><!-- /.col -->
+
+  <div class="col-md-3 col-sm-6">
+    <div class="spl-tile spl-tile-warning" style="padding-left:10px;">
+      <h4 class="uppercase">
+        <a href="/faq/"><span class="text-warning">My Library</span></a>
+      </h4>
+      <ul class="list-unstyled">
+        <li>
+          <h5>
+            <a href="/branches/"><span class="text-warning">Find a branch</span></a>
+          </h5>
+        </li>
+        <li>
+          <h5>
+            <a href="/news/"><span class="text-warning">See what's new</span></a>
+          </h5>
+        </li>
+        <li>
+          <h5>
+            <a href="/services"><span class="text-warning">Library services</span></a>
+          </h5>
+        </li>
+      </ul>
+    </div>
+  </div><!-- /.col -->
+
+</div><!-- /.row -->
+
+
 <div class="row">
  
   <div class="col-md-9">
-    <div class="spl-tile spl-tile-primary" style="padding-left:10px;">
+    <div class="spl-tile spl-tile-default" style="padding-left:10px;">
       <?php echo do_shortcode('[spl_widget home-page news]'); ?>
     </div>
   </div><!-- /.col -->
@@ -20,18 +121,9 @@ Template Name: Home Page
 </div><!-- /.row -->
 
 <div class="row">
-  <div class="col-sm-6 col-md-7">
-    <?php echo do_shortcode('[spl_widget home-page promo=tech]'); ?>
-  </div><!-- /.col -->  
-  <div class="col-sm-6 col-md-5">
-    <?php echo do_shortcode('[spl_widget home-page promo=yearbooks]'); ?>
-  </div><!-- /.col -->        
-</div><!-- /.row -->
-
-<div class="row">
   <div class="col-sm-4 col-sm-push-8 col-md-3 col-md-push-9">
     <div class="spl-tile spl-tile-boxed">
-      <?php echo do_shortcode('[spl_widget home-page carousel pause auto shuffle slides limit=3 slug=alpha]'); ?>  
+      <?php echo do_shortcode('[spl_widget home-page carousel pause auto shuffle slides limit=1 slug=alpha]'); ?>  
     </div>
       <?php echo do_shortcode('[spl_widget home-page facebook limit=2]'); ?>
   </div><!-- /.col --> 
@@ -71,4 +163,13 @@ Template Name: Home Page
 
   </div><!-- /.col -->
        
+</div><!-- /.row -->
+
+<div class="row">
+  <div class="col-sm-6 col-md-7">
+    <?php echo do_shortcode('[spl_widget home-page promo=tech]'); ?>
+  </div><!-- /.col -->  
+  <div class="col-sm-6 col-md-5">
+    <?php echo do_shortcode('[spl_widget home-page promo=yearbooks]'); ?>
+  </div><!-- /.col -->        
 </div><!-- /.row -->
