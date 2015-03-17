a2978ed0c57e549f2b4c744febc33e72f7abc1bd
Sean Girard <seangirard@yahoo.com>
2015-03-17 15:44:21 -0700

    home

--- template-alpha.php
+++ template-alpha.php
@@ -27,7 +27,7 @@ Template Name: Alpha Home Page
   <div class="col-sm-4 col-md-3 col-md-push-9 col-lg-2 col-lg-push-10">
     <div class="spl-hero-panel spl-hero-primary">
       <h5 class="text-center">Digital Branch</h5>
-      <div class="panel-body spl-blue-tint-80">
+      <div class="panel-body spl-blue-tint-60">
         <?php echo get_alpha_panel('Digital Branch'); ?>
       </div>
     </div>
