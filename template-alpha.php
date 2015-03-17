0ac0f7226ce3d4e7a200ead3e201f69a902b3e95
Sean Girard <seangirard@yahoo.com>
2015-03-17 13:02:40 -0700

    home

--- template-alpha.php
+++ template-alpha.php
@@ -24,7 +24,7 @@ Template Name: Alpha Home Page
 
 <div class="row">
  
-  <div class="col-md-2 col-md-push-8">
+  <div class="col-md-2 col-md-push-7">
     <?php echo get_alpha_panel('Digital Branch'); ?>
   </div><!-- /.col -->
 
@@ -33,7 +33,7 @@ Template Name: Alpha Home Page
     <?php echo get_alpha_panel('This week'); ?>
   </div><!-- /.col -->
 
-  <div class="col-md-3 col-md-pull-8">
+  <div class="col-md-3 col-md-pull-7">
     <?php echo do_shortcode('[spl_widget branch-hours]'); ?>
   </div><!-- /.col -->
 
