e3ad45f28e525196cd2d501837e9a8aed2a124f7
Sean Girard <seangirard@yahoo.com>
2015-03-17 15:45:10 -0700

    home

--- template-alpha.php
+++ template-alpha.php
@@ -28,7 +28,7 @@ Template Name: Alpha Home Page
     <div class="spl-hero-panel spl-hero-primary">
       <h5 class="text-center">Digital Branch</h5>
       <div class="panel-body spl-blue-tint-60">
-        <?php echo get_alpha_panel('Digital Branch'); ?>
+        <i class="glyphicon glyphicon-cloud-download"></i>
       </div>
     </div>
     
