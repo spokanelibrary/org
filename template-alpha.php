3dd53bbbd49006d7d79e4bdd286c4d5efb27e082
Sean Girard <seangirard@yahoo.com>
2015-03-17 15:51:03 -0700

    home

--- template-alpha.php
+++ template-alpha.php
@@ -50,7 +50,7 @@ Template Name: Alpha Home Page
   </div><!-- /.col -->
 
   <div class="col-sm-12 col-md-3 col-md-pull-9 col-lg-2 col-lg-pull-9">
-      <h4 class="text-center text-success">Is the library open?</h4>
+      <h4 class="text-center text-success" style="margin-top:0;">Is the library open?</h4>
     <?php echo do_shortcode('[spl_widget branch-hours]'); ?>
   </div><!-- /.col -->
 
