acd9f1271ae66e517ee8d81ea875f62e64f05fb7
Sean Girard <seangirard@yahoo.com>
2015-03-17 15:05:35 -0700

    home

--- template-alpha.php
+++ template-alpha.php
@@ -31,12 +31,12 @@ Template Name: Alpha Home Page
     </div>
   </div><!-- /.col -->
 
-  <div class="col-sm-8 col-md-6">
+  <div class="col-sm-8 col-md-6 col-lg-offset-1">
     <?php echo get_alpha_panel('Carousel'); ?>
     <?php echo get_alpha_panel('This week'); ?>
   </div><!-- /.col -->
 
-  <div class="col-sm-12 col-md-3 col-md-pull-9 col-lg-2 col-lg-pull-9">
+  <div class="col-sm-12 col-md-3 col-md-pull-9 col-lg-2 col-lg-pull-8">
     <?php echo do_shortcode('[spl_widget branch-hours]'); ?>
   </div><!-- /.col -->
 
