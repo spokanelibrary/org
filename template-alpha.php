3bf5277cac0bb8b62c4c8f8168962d896a89a0f6
Sean Girard <seangirard@yahoo.com>
2015-03-17 03:53:04 -0700

    home

--- template-alpha.php
+++ template-alpha.php
@@ -137,14 +137,7 @@ Template Name: Alpha Home Page
   </div><!-- /.col -->
 </div><!-- /.row -->
 
-<div class="row">
-  <div class="col-sm-6 col-md-7">
-    <?php echo do_shortcode('[spl_widget home-page promo=tech]'); ?>
-  </div><!-- /.col -->  
-  <div class="col-sm-6 col-md-5">
-    <?php echo do_shortcode('[spl_widget home-page promo=yearbooks]'); ?>
-  </div><!-- /.col -->        
-</div><!-- /.row -->
+
 
 <div class="row">
   <div class="col-sm-4 col-sm-push-8 col-md-3 col-md-push-9">
