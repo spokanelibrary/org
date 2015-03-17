d080cdda5d6f428320fc7a382537324c6faf5725
Sean Girard <seangirard@yahoo.com>
2015-03-17 07:38:22 -0700

    home

--- template-home.php
+++ template-home.php
@@ -32,7 +32,7 @@ Template Name: Home Page
     </div>
   </div><!-- /.col -->
 
-  <div class="col-md-3 col-sm-6 clearfix">
+  <div class="col-md-3 col-sm-6">
     <div class="spl-tile spl-tile-primary" style="padding-left:10px;">
       <h4 class="uppercase">
         <a href="/digital/"><span class="text-primary">Digital Branch</span></a>
@@ -52,7 +52,7 @@ Template Name: Home Page
     </div>
   </div><!-- /.col -->
 
-  <div class="col-md-3 col-sm-6">
+  <div class="col-md-3 col-sm-6  clearfix">
     <div class="spl-tile spl-tile-success" style="padding-left:10px;">
       <h4 class="uppercase">
         <a href="/calendar/"><span class="text-success">Classes &amp; Events</span></a>
