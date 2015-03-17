642f049ba5c1347d6dd02613434202cec0548e11
Sean Girard <seangirard@yahoo.com>
2015-03-17 15:06:13 -0700

    home

--- template-alpha.php
+++ template-alpha.php
@@ -24,7 +24,7 @@ Template Name: Alpha Home Page
 
 <div class="row">
  
-  <div class="col-sm-4 col-md-3 col-md-push-9 col-lg-2 col-lg-push-9">
+  <div class="col-sm-4 col-md-3 col-md-push-9 col-lg-2 col-lg-push-10">
     <?php echo get_alpha_panel('Digital Branch'); ?>
     <div class="hidden-xs">
       <?php echo get_alpha_panel('Staff Picks'); ?>
