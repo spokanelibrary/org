da75855c25de215bf7ce25019e3fd11bc42babba
Sean Girard <seangirard@yahoo.com>
2015-03-17 15:50:16 -0700

    home

--- template-alpha.php
+++ template-alpha.php
@@ -28,10 +28,10 @@ Template Name: Alpha Home Page
     
     <div class="spl-hero-panel spl-hero-primary">
       <a href="#">
-      <h5 class="text-center">
+      <h4 class="text-center">
         <i class="glyphicon glyphicon-cloud-download"></i>
         Digital Branch
-      </h5>
+      </h4>
       <div class="panel-body spl-blue-tint-60">
         Download ebooks, music, movies, &amp; more
       </div>
@@ -50,7 +50,7 @@ Template Name: Alpha Home Page
   </div><!-- /.col -->
 
   <div class="col-sm-12 col-md-3 col-md-pull-9 col-lg-2 col-lg-pull-9">
-      <h5 class="text-center text-success">Is the library open?</h5>
+      <h4 class="text-center text-success">Is the library open?</h4>
     <?php echo do_shortcode('[spl_widget branch-hours]'); ?>
   </div><!-- /.col -->
 
