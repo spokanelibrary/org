e9ad658ed1ced84c1305e9b763a9264f3e8ae99b
Sean Girard <seangirard@yahoo.com>
2015-03-18 15:12:32 -0700

    home

--- template-alpha.php
+++ template-alpha.php
@@ -95,7 +95,7 @@ Template Name: Alpha Home Page
 
 <div class="row">
     
-  <div class="col-sm-4 col-md-3 col-md-push-9 col-lg-2 col-lg-push-6">
+  <div class="col-sm-4 col-md-3 col-md-push-9 col-lg-2 col-lg-push-10">
     <?php echo get_alpha_panel('Digital Branch'); ?>
   </div><!-- /.col -->
 
