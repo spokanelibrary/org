a3f03311b1f4f608f5a4b973e9b12b5242cdeef3
Sean Girard <seangirard@yahoo.com>
2015-03-18 09:28:35 -0700

    home

--- template-alpha.php
+++ template-alpha.php
@@ -58,40 +58,7 @@ Template Name: Alpha Home Page
 
   <div class="col-sm-8 col-md-6 col-lg-offset-1">
     
-    <div class="spl-tile">
-
-
-      <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-        <!-- Indicators -->
-        <ol class="carousel-indicators">
-          <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-          <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-          <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-        </ol>
-
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner" role="listbox">
-          <div class="item active">
-            <img class="img-responsive" src="/assets/img/promos/photos/IMG_3272.jpg">
-            <div class="carousel-caption">
-              Some caption
-            </div>
-          </div>
-        </div>
-
-        <!-- Controls -->
-        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
-
-      
-    </div>
+    <?php echo do_shortcode('[spl_carousel auto random slug=stage]'); ?>
 
     <?php echo get_alpha_panel('This week', 240); ?>
   </div><!-- /.col -->
