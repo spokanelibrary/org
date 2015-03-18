8c3d81d9080e14c897a89ddaa591cb493df48207
Sean Girard <seangirard@yahoo.com>
2015-03-18 12:29:03 -0700

    home

--- template-alpha.php
+++ template-alpha.php
@@ -32,7 +32,7 @@ Template Name: Alpha Home Page
           <i class="glyphicon glyphicon-cloud-download"></i>
           Digital Branch
         </h4>
-        <div class="panel-body spl-blue-tint-60">
+        <div class="panel-body spl-blue-tint-60" style="background-image:url('/assets/img/promos/photos/spl-handheld.jpg');">
           <!-- <div class="panel-body spl-blue-tint-80" style="border:1px solid #fff;"> -->
             Download ebooks, music, magazines, &amp; more
           <!--</div>-->
