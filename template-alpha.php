b4b78a7a09a8361368d06b30bafdbf2686e48f4f
Sean Girard <seangirard@yahoo.com>
2015-03-18 13:01:06 -0700

    home

--- template-alpha.php
+++ template-alpha.php
@@ -34,7 +34,7 @@ Template Name: Alpha Home Page
         </h4>
           <img class="img-responsive" src="/assets/img/promos/photos/spl-handheld.jpg">
         <div class="panel-body" style="padding:6px;">
-          <b>Download</b> ebooks, music, magazines, &amp; more <small class="glyphicon glyphicon-arrow-right"></small>
+          <b>Download</b> ebooks, music, magazines, &amp; more <small>&rarr;</small>
         </div>
       </a>
     </div>
