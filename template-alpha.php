0aed2c14f6b9953e8e9d150659dc97837158fd10
Sean Girard <seangirard@yahoo.com>
2015-03-19 11:53:12 -0700

    home

--- template-alpha.php
+++ template-alpha.php
@@ -60,7 +60,9 @@ Template Name: Alpha Home Page
       </div><!-- /.col -->
 
       <div class="col-sm-4 col-md-4 col-lg-3">
-        <?php echo do_shortcode('[spl_widget home-page carousel pause auto calendar embedded refresh]'); ?>
+        <div style="background:#ececec;">
+          <?php echo do_shortcode('[spl_widget home-page carousel pause auto calendar embedded refresh]'); ?>
+        </div>
       </div><!-- /.col -->
 
     </div><!-- /.row -->
