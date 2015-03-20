455bc8e33050bc0a0e29db4a6fcf3e476a87d6bc
Sean Girard <seangirard@yahoo.com>
2015-03-20 13:57:24 -0700

    home

--- templates/org/pages/chof-2015.php
+++ templates/org/pages/chof-2015.php
@@ -162,10 +162,10 @@
 		<a href="#" data-toggle="modal" data-target="#chof-modal-e">
 			<div class="chof-panel chof-panel-sized chof-green">
 				<div class="panel-body">
-					<img class="img-responsive" src="/assets/img/promos/chof/Livingston_Gary-sm.jpg">
+					<img class="img-responsive" src="/assets/img/promos/chof/Welty_Elizabeth-sm.jpg">
 					<div class="chof-text-wrapper">			
 						<div class="chof-text-mask">
-							<h6 class="chof-title">Gary A. Livingston, Ph. D.</h6>
+							<h6 class="chof-title">Dr. Elizabeth Welty, MD</h6>
 						</div>
 					</div>
 				</div>
@@ -219,10 +219,10 @@
 		<a href="#" data-toggle="modal" data-target="#chof-modal-i">
 			<div class="chof-panel chof-panel-sized chof-yellow chof-text-dark">
 				<div class="panel-body">
-					<img class="img-responsive" src="/assets/img/promos/chof/Welty_Elizabeth-sm.jpg">
+					<img class="img-responsive" src="/assets/img/promos/chof/Livingston_Gary-sm.jpg">
 					<div class="chof-text-wrapper">			
 						<div class="chof-text-mask">
-							<h6 class="chof-title">Dr. Elizabeth Welty, MD</h6>
+							<h6 class="chof-title">Gary A. Livingston, Ph. D.</h6>
 						</div>
 					</div>
 				</div>
