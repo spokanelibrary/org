247fadec622789726602053b2ff1c3af4c86d97a
Sean Girard <seangirard@yahoo.com>
2015-03-20 09:36:00 -0700

    chof

--- templates/org/pages/chof-2015.php
+++ templates/org/pages/chof-2015.php
@@ -91,7 +91,7 @@
 			</div>
 		</a>
 	</div>
-	
+
 	<!-- PANEL -->
 	
 	<div class="col-xs-6 col-sm-3 col-md-3 col-lg-2">
@@ -124,7 +124,7 @@
 					<img class="img-responsive" src="http://lorempixel.com/g/200/200/people/8">
 					<div class="chof-text-wrapper">			
 						<div class="chof-text-mask">
-							<h6 class="chof-title"><span class="chof-salish-bold">ilmz sPQNi/</span></h6>
+							<h6 class="chof-title"><span class="chof-salish-normal">ilmz sPQNi/</span></h6>
 						</div>
 					</div>
 				</div>
