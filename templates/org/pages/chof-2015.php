a53e545d3222b8bdc87cefd28078cc92909c2136
Sean Girard <seangirard@yahoo.com>
2015-03-20 08:51:15 -0700

    chof

--- templates/org/pages/chof-2015.php
+++ templates/org/pages/chof-2015.php
@@ -213,9 +213,22 @@
 	
 	<!-- LOGOTYPE -->
 	<div class="col-xs-6 col-sm-3 col-md-3 col-lg-2">
+		<!--
 		<div class="chof-panel chof-panel-sized chof-yellow">
 			<img class="img-responsive" src="/assets/img/promos/chof-logotype.png">
 		</div>
+		-->
+		<a href="#" data-toggle="modal" data-target="#chof-modal-about">
+			<div class="chof-panel chof-panel-sized chof-yellow chof-text-dark">
+				<div class="panel-body">
+					<img class="img-responsive" src="/assets/img/promos/chof-logotype.png">
+				</div>
+				<div class="chof-glyph">
+					<small class="glyphicon glyphicon-share-alt"></small>
+				</div>
+			</div>
+		</a>
+
 	</div>
 	
 	<!-- PANEL -->
@@ -419,6 +432,36 @@
 	</div><!-- /.col -->
 
 </div><!-- /.row -->
+
+<!-- MODAL ABOUT -->
+<div class="modal fade chof-modal" id="chof-modal-about" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content chof-yellow">
+      <div class="modal-body">
+        
+        <div class="panel-body">  
+  	    	<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+      	  <h3 class="normal chof-text-yellow">Spokane's Citizen Hall of Fame</h3>
+      	  <div class="row">
+  	      	<div class="col-sm-4 col-md-4">
+  			      <p>
+  		    	  	<img class="img-responsive" src="http://lorempixel.com/g/200/200/people/7">
+	  	  	    </p>
+	  	  	  </div><!-- /.col -->
+	  	  	  <div class="col-sm-8 col-md-8">
+	  	  	  	<h3 class="modal-title uppercase" id="chof-modal-label">Lorem ipsum</h3>
+	  	  	  	<p class="lead">
+	  	  	  		Lead the development of the North South freeway and the revitalization of the Logan neighborhood. 
+	  	  	  		Born in Brown’s Addition, Mr. North also attended and later instructed at Spokane Falls Community College in the engineering department.
+	  	  	  	</p>
+  	      	</div><!-- /.col -->
+	  	    </div><!-- /.row -->
+	      </div><!-- /.panel-body -->
+
+      </div>
+    </div>
+  </div>
+</div>
 
 
 <!-- MODAL A -->
