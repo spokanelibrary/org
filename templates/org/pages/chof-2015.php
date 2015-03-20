b3d255416537c13736159a7c6f35c8dd4b5dee44
Sean Girard <seangirard@yahoo.com>
2015-03-20 14:37:47 -0700

    chof

--- templates/org/pages/chof-2015.php
+++ templates/org/pages/chof-2015.php
@@ -93,7 +93,7 @@
 
 	<!-- TILE -->
 	<div class="col-xs-6 col-sm-3 col-md-3 col-lg-2">
-		<a href="#" data-toggle="modal" data-target="#chof-modal-b">
+		<a href="#" data-toggle="modal" data-target="#chof-modal-Spokan_ChiefGarry">
 			<div class="chof-panel chof-panel-sized chof-brown">
 				<div class="panel-body">
 					<img class="img-responsive" src="/assets/img/promos/chof/Spokan_ChiefGarry-sm.jpg">
@@ -443,6 +443,36 @@
   </div>
 </div>
 
+<!-- MODAL -->
+<div class="modal fade chof-modal" id="chof-modal-Spokan_ChiefGarry" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content chof-brown">
+      <div class="modal-body">
+        
+        <div class="panel-body">  
+  	    	<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+      	  <h3 class="normal chof-text-gray">Historic Inductee <small>2015</small></h3>
+      	  <div class="row">
+  	      	<div class="col-sm-4 col-md-4">
+  			      <p>
+  		    	  	<img class="img-responsive" src="http://lorempixel.com/g/200/200/people/8">
+	  	  	    </p>
+	  	  	  </div><!-- /.col -->
+	  	  	  <div class="col-sm-8 col-md-8">
+	  	  	  	<h3 class="modal-title uppercase" id="chof-modal-label">Chief Spokane Garry</h3>
+	  	  	  	<p class="lead">
+	  	  	  		Lead the development of the North South freeway and the revitalization of the Logan neighborhood. 
+	  	  	  		Born in Brown’s Addition, Mr. North also attended and later instructed at Spokane Falls Community College in the engineering department.
+	  	  	  	</p>
+  	      	</div><!-- /.col -->
+	  	    </div><!-- /.row -->
+	      </div><!-- /.panel-body -->
+
+      </div>
+    </div>
+  </div>
+</div>
+
 <!-- MODAL A -->
 <div class="modal fade chof-modal" id="chof-modal-a" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
   <div class="modal-dialog modal-lg">
@@ -473,35 +503,7 @@
   </div>
 </div>
 
-<!-- MODAL B -->
-<div class="modal fade chof-modal" id="chof-modal-b" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
-  <div class="modal-dialog modal-lg">
-    <div class="modal-content chof-gray">
-      <div class="modal-body">
-        
-        <div class="panel-body">  
-  	    	<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-      	  <h3 class="normal chof-text-gray">Historic Inductee <small>2015</small></h3>
-      	  <div class="row">
-  	      	<div class="col-sm-4 col-md-4">
-  			      <p>
-  		    	  	<img class="img-responsive" src="http://lorempixel.com/g/200/200/people/8">
-	  	  	    </p>
-	  	  	  </div><!-- /.col -->
-	  	  	  <div class="col-sm-8 col-md-8">
-	  	  	  	<h3 class="modal-title uppercase" id="chof-modal-label">Chief Spokane Garry</h3>
-	  	  	  	<p class="lead">
-	  	  	  		Lead the development of the North South freeway and the revitalization of the Logan neighborhood. 
-	  	  	  		Born in Brown’s Addition, Mr. North also attended and later instructed at Spokane Falls Community College in the engineering department.
-	  	  	  	</p>
-  	      	</div><!-- /.col -->
-	  	    </div><!-- /.row -->
-	      </div><!-- /.panel-body -->
 
-      </div>
-    </div>
-  </div>
-</div>
 
 <!-- MODAL C -->
 <div class="modal fade chof-modal" id="chof-modal-c" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
