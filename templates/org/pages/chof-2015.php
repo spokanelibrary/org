52c75901cce52e4a18518ff15c9ffc1c4cfccfae
Sean Girard <seangirard@yahoo.com>
2015-03-20 13:04:54 -0700

    chof

--- templates/org/pages/chof-2015.php
+++ templates/org/pages/chof-2015.php
@@ -50,7 +50,7 @@
 		<a href="#" data-toggle="modal" data-target="#chof-modal-g">
 			<div class="chof-panel chof-panel-sized chof-yellow chof-text-dark">
 				<div class="panel-body">
-					<img class="img-responsive" src="http://lorempixel.com/200/200/abstract/1">
+					<img class="img-responsive" src="/assets/img/promos/chof/Kardong_Don-sm.jpg">
 					<div class="chof-text-wrapper">			
 						<div class="chof-text-mask">
 							<h6 class="chof-title">Donald F. Kardong</h6>
