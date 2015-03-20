7cae8798c76d700c862da0395818ef1967565e71
Sean Girard <seangirard@yahoo.com>
2015-03-20 14:19:46 -0700

    chof

--- templates/org/pages/chof-2015.php
+++ templates/org/pages/chof-2015.php
@@ -66,7 +66,7 @@
 
 	<!-- TILE -->
 	<div class="col-xs-6 col-sm-3 col-md-3 col-lg-2">
-		<a href="#" data-toggle="modal" data-target="#chof-modal-d">
+		<a href="#" data-toggle="modal" data-target="#chof-modal-cole-king">
 			<div class="chof-panel chof-panel-sized chof-orange">
 				<div class="panel-body">
 					<img class="img-responsive" src="/assets/img/promos/chof/Cole_King-sm.jpg">
@@ -413,6 +413,36 @@
   </div>
 </div>
 
+<!-- MODAL -->
+<div class="modal fade chof-modal" id="chof-modal-cole-king" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content chof-orange">
+      <div class="modal-body">
+        
+        <div class="panel-body">  
+  	    	<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+      	  <h3 class="normal chof-text-orange">Historic Inductee <small>2015</small></h3>
+      	  <div class="row">
+  	      	<div class="col-sm-4 col-md-4">
+  			      <p>
+  		    	  	<img class="img-responsive" src="http://lorempixel.com/g/200/200/people/3">
+	  	  	    </p>
+	  	  	  </div><!-- /.col -->
+	  	  	  <div class="col-sm-8 col-md-8">
+	  	  	  	<h3 class="modal-title uppercase" id="chof-modal-label">King Cole</h3>
+	  	  	  	<p class="lead">
+	  	  	  		Lead the development of the North South freeway and the revitalization of the Logan neighborhood. 
+	  	  	  		Born in Brown’s Addition, Mr. North also attended and later instructed at Spokane Falls Community College in the engineering department.
+	  	  	  	</p>
+  	      	</div><!-- /.col -->
+	  	    </div><!-- /.row -->
+	      </div><!-- /.panel-body -->
+
+      </div>
+    </div>
+  </div>
+</div>
+
 <!-- MODAL A -->
 <div class="modal fade chof-modal" id="chof-modal-a" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
   <div class="modal-dialog modal-lg">
@@ -490,36 +520,6 @@
 	  	  	  </div><!-- /.col -->
 	  	  	  <div class="col-sm-8 col-md-8">
 	  	  	  	<h3 class="modal-title uppercase" id="chof-modal-label">Gordon Budke</h3>
-	  	  	  	<p class="lead">
-	  	  	  		Lead the development of the North South freeway and the revitalization of the Logan neighborhood. 
-	  	  	  		Born in Brown’s Addition, Mr. North also attended and later instructed at Spokane Falls Community College in the engineering department.
-	  	  	  	</p>
-  	      	</div><!-- /.col -->
-	  	    </div><!-- /.row -->
-	      </div><!-- /.panel-body -->
-
-      </div>
-    </div>
-  </div>
-</div>
-
-<!-- MODAL D -->
-<div class="modal fade chof-modal" id="chof-modal-d" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
-  <div class="modal-dialog modal-lg">
-    <div class="modal-content chof-orange">
-      <div class="modal-body">
-        
-        <div class="panel-body">  
-  	    	<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-      	  <h3 class="normal chof-text-orange">Historic Inductee <small>2015</small></h3>
-      	  <div class="row">
-  	      	<div class="col-sm-4 col-md-4">
-  			      <p>
-  		    	  	<img class="img-responsive" src="http://lorempixel.com/g/200/200/people/3">
-	  	  	    </p>
-	  	  	  </div><!-- /.col -->
-	  	  	  <div class="col-sm-8 col-md-8">
-	  	  	  	<h3 class="modal-title uppercase" id="chof-modal-label">King Cole</h3>
 	  	  	  	<p class="lead">
 	  	  	  		Lead the development of the North South freeway and the revitalization of the Logan neighborhood. 
 	  	  	  		Born in Brown’s Addition, Mr. North also attended and later instructed at Spokane Falls Community College in the engineering department.
