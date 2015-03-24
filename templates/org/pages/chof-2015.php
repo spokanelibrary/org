70fe0b4602580b9ccb1faef1094576bd7f254eb9
Sean Girard <seangirard@yahoo.com>
2015-03-24 16:11:54 -0700

    chof

--- templates/org/pages/chof-2015.php
+++ templates/org/pages/chof-2015.php
@@ -160,7 +160,7 @@
 
 	<!-- TILE -->
 	<div class="col-xs-6 col-sm-3 col-md-3 col-lg-2">
-		<a href="#" data-toggle="modal" data-target="#chof-modal-e">
+		<a href="#" data-toggle="modal" data-target="#chof-modal-welty-elizabeth">
 			<div class="chof-panel chof-panel-sized chof-green">
 				<div class="panel-body">
 					<img class="img-responsive" src="/assets/img/promos/chof/Welty_Elizabeth-sm.jpg">
@@ -631,6 +631,36 @@
   </div>
 </div>
 
+<!-- MODAL -->
+<div class="modal fade chof-modal" id="chof-modal-welty-elizabeth" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content chof-blue">
+      <div class="modal-body">
+        
+        <div class="panel-body">  
+  	    	<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+      	  <h3 class="normal chof-text-blue">Public Service &amp; Philanthropy <small>2015</small></h3>
+      	  <div class="row">
+  	      	<div class="col-sm-4 col-md-4">
+  			      <p>
+  		    	  	<img class="img-responsive" src="http://lorempixel.com/g/200/200/people/10">
+	  	  	    </p>
+	  	  	  </div><!-- /.col -->
+	  	  	  <div class="col-sm-8 col-md-8">
+	  	  	  	<h3 class="modal-title uppercase">Dr. Elizabeth Welty</h3>
+	  	  	  	<p class="lead">
+	  	  	  		Lead the development of the North South freeway and the revitalization of the Logan neighborhood. 
+	  	  	  		Born in Brown’s Addition, Mr. North also attended and later instructed at Spokane Falls Community College in the engineering department.
+	  	  	  	</p>
+  	      	</div><!-- /.col -->
+	  	    </div><!-- /.row -->
+	      </div><!-- /.panel-body -->
+
+      </div>
+    </div>
+  </div>
+</div>
+
 <!-- MODAL E -->
 <div class="modal fade chof-modal" id="chof-modal-e" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
   <div class="modal-dialog modal-lg">
@@ -768,36 +798,6 @@
 	  	  	  </div><!-- /.col -->
 	  	  	  <div class="col-sm-8 col-md-8">
 	  	  	  	<h3 class="modal-title uppercase">May Arkwright Hutton</h3>
-	  	  	  	<p class="lead">
-	  	  	  		Lead the development of the North South freeway and the revitalization of the Logan neighborhood. 
-	  	  	  		Born in Brown’s Addition, Mr. North also attended and later instructed at Spokane Falls Community College in the engineering department.
-	  	  	  	</p>
-  	      	</div><!-- /.col -->
-	  	    </div><!-- /.row -->
-	      </div><!-- /.panel-body -->
-
-      </div>
-    </div>
-  </div>
-</div>
-
-<!-- MODAL K -->
-<div class="modal fade chof-modal" id="chof-modal-k" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
-  <div class="modal-dialog modal-lg">
-    <div class="modal-content chof-blue">
-      <div class="modal-body">
-        
-        <div class="panel-body">  
-  	    	<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-      	  <h3 class="normal chof-text-blue">Public Service &amp; Philanthropy <small>2015</small></h3>
-      	  <div class="row">
-  	      	<div class="col-sm-4 col-md-4">
-  			      <p>
-  		    	  	<img class="img-responsive" src="http://lorempixel.com/g/200/200/people/10">
-	  	  	    </p>
-	  	  	  </div><!-- /.col -->
-	  	  	  <div class="col-sm-8 col-md-8">
-	  	  	  	<h3 class="modal-title uppercase">Dr. Elizabeth Welty</h3>
 	  	  	  	<p class="lead">
 	  	  	  		Lead the development of the North South freeway and the revitalization of the Logan neighborhood. 
 	  	  	  		Born in Brown’s Addition, Mr. North also attended and later instructed at Spokane Falls Community College in the engineering department.
