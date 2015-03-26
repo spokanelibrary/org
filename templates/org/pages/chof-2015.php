ee8ed12ef05089471ad6cd071bb2f2c4f3515e93
Sean Girard <seangirard@yahoo.com>
2015-03-26 12:43:38 -0700

    chof

--- templates/org/pages/chof-2015.php
+++ templates/org/pages/chof-2015.php
@@ -380,17 +380,17 @@
 	  	    	Categories in which citizens are honored for their achievements include:
 	  	    </h3>
 	  	    <dl class="">
-		  	    <dt>Economic Development and Business</dt>
+		  	    <dt class="chof-text-red">Economic Development and Business</dt>
 						<dd class="help-block">
 							<i class="glyphicon glyphicon-chevron-right"></i>
 							For entrepreneurial activity or significant contributions toward the economic betterment of Spokane
 						</dd>
-						<dt>Education</dt>
+						<dt class="chof-text-red">Education</dt>
 						<dd class="help-block">
 							<i class="glyphicon glyphicon-chevron-right"></i>
 							For enriching and inspiring lifelong learning in our community
 						</dd>
-						<dt>Public Service and Philanthropy</dt>
+						<dt class="chof-text-red">Public Service and Philanthropy</dt>
 						<dd class="help-block">
 							<i class="glyphicon glyphicon-chevron-right"></i>
 							For exemplary voluntary and public service activities that benefit the community
