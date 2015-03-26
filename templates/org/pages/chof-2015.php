e0f305624e4144d072d56bbe281a5bc9db3c56a7
Sean Girard <seangirard@yahoo.com>
2015-03-26 14:33:44 -0700

    chof

--- templates/org/pages/chof-2015.php
+++ templates/org/pages/chof-2015.php
@@ -83,25 +83,6 @@
 
 	<!-- TILE -->
 	<div class="col-xs-6 col-sm-3 col-md-2 col-lg-2">
-		<a href="#" data-toggle="modal" data-target="#chof-modal-spokan-chief-garry">
-			<div class="chof-panel chof-panel-sized chof-brown">
-				<div class="panel-body">
-					<img class="img-responsive" src="/assets/img/promos/chof/Spokan_ChiefGarry-sm.jpg">
-					<div class="chof-text-wrapper">			
-						<div class="chof-text-mask">
-							<h6 class="chof-title">Chief Spokan Garry</h6>
-						</div>
-					</div>
-				</div>
-				<div class="chof-glyph">
-					<small class="glyphicon glyphicon-share-alt"></small>
-				</div>
-			</div>
-		</a>
-	</div>
-
-	<!-- TILE -->
-	<div class="col-xs-6 col-sm-3 col-md-2 col-lg-2">
 		<a href="#" data-toggle="modal" data-target="#chof-modal-balazs-harold">
 			<div class="chof-panel chof-panel-sized chof-blue">
 				<div class="panel-body">
@@ -168,6 +149,25 @@
 					<div class="chof-text-wrapper">			
 						<div class="chof-text-mask">
 							<h6 class="chof-title">Dr. Elizabeth Welty, MD</h6>
+						</div>
+					</div>
+				</div>
+				<div class="chof-glyph">
+					<small class="glyphicon glyphicon-share-alt"></small>
+				</div>
+			</div>
+		</a>
+	</div>
+
+	<!-- TILE -->
+	<div class="col-xs-6 col-sm-3 col-md-2 col-lg-2">
+		<a href="#" data-toggle="modal" data-target="#chof-modal-spokan-chief-garry">
+			<div class="chof-panel chof-panel-sized chof-brown">
+				<div class="panel-body">
+					<img class="img-responsive" src="/assets/img/promos/chof/Spokan_ChiefGarry-sm.jpg">
+					<div class="chof-text-wrapper">			
+						<div class="chof-text-mask">
+							<h6 class="chof-title">Chief Spokan Garry</h6>
 						</div>
 					</div>
 				</div>
