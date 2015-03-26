664ce6b32fe5b1768dc025f829f8487a5a188de3
Sean Girard <seangirard@yahoo.com>
2015-03-26 09:54:09 -0700

    chof

--- templates/org/pages/chof-2015.php
+++ templates/org/pages/chof-2015.php
@@ -387,8 +387,10 @@
 	  	  	  	<blockquote>
 	  	  	  		The Spokane Citizen Hall of Fame recognizes individuals who have made outstanding contributions to the quality of life in our city and the development of Spokane.
 	  	  	  	</blockquote>
-  	  	  		<a rel="external" href="http://www.spokanelibraryfoundation.org/citizen-hall-of-fame/"><b>Learn more about Spokane's Citizen Hall of Fame</b></a>
-  	  	  		<small class="glyphicon glyphicon-arrow-right text-muted">&rarr;</small>
+	  	  	  	<p>
+  		  	  		<a rel="external" href="http://www.spokanelibraryfoundation.org/citizen-hall-of-fame/"><b>Learn more about Spokane's Citizen Hall of Fame</b></a>
+	  	  	  		<small class="glyphicon glyphicon-arrow-right text-muted"></small>
+ 	  	  			</p>
  	  	  			<h3>
 			  	    	Categories in which citizens are honored for their achievements include:
 			  	    </h3>
