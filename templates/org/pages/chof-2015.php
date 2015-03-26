07089db0cf6b1b87bd633588c958c60c8885b581
Sean Girard <seangirard@yahoo.com>
2015-03-26 10:36:29 -0700

    chof

--- templates/org/pages/chof-2015.php
+++ templates/org/pages/chof-2015.php
@@ -397,7 +397,7 @@
  	  	  			</p>
  	  	  		</div><!-- /.col -->
 	  	    </div><!-- /.row -->
-	  	    <h3 class="chof-text-blue">
+	  	    <h3 class="normal chof-text-blue">
 	  	    	Categories in which citizens are honored for their achievements include:
 	  	    </h3>
 	  	    <dl class="">
