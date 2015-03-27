021ec6ef7c1b9ec1df4e58a219da108b3b288903
Sean Girard <seangirard@yahoo.com>
2015-03-27 09:24:07 -0700

    home

--- template-alpha.php
+++ template-alpha.php
@@ -23,101 +23,101 @@ Template Name: Alpha Home Page
 
 <div class="spl-hero-wrapper">
 
-<div class="row">
-    
-  <div class="col-md-9 col-md-push-3 col-lg-8">
-    
-    <div class="row">
+  <div class="row">
+      
+    <div class="col-md-9 col-md-push-3 col-lg-8">
+      
+      <div class="row">
 
-      <div class="col-sm-4 col-sm-push-8 col-md-4 col-md-push-8 col-lg-3 col-lg-push-9">
-        
-        <div class="panel spl-hero-panel spl-hero-primary">
-          <a class="spl-hero-link" href="/download/">
-            <h4 class="text-center">
-              <i class="glyphicon glyphicon-cloud-download"></i>
-              Digital Branch
+        <div class="col-sm-4 col-sm-push-8 col-md-4 col-md-push-8 col-lg-3 col-lg-push-9">
+          
+          <div class="panel spl-hero-panel spl-hero-primary">
+            <a class="spl-hero-link" href="/download/">
+              <h4 class="text-center">
+                <i class="glyphicon glyphicon-cloud-download"></i>
+                Digital Branch
+              </h4>
+                <img style="margin:auto;" class="img-responsive hidden-xs" src="/assets/img/promos/photos/spl-handheld.jpg">
+              <div class="panel-body" style="padding:8px;">
+                <b>Download</b> ebooks, music, magazines, and more&hellip;
+              </div>
+            </a>
+          </div>
+
+        </div><!-- /.col -->
+         <div class="col-sm-8 col-sm-pull-4 col-md-8 col-md-pull-4 col-lg-9 col-lg-pull-3">
+
+          <div class="spl-tile">
+            <?php echo do_shortcode('[spl_carousel_photo auto random slug=stage]'); ?>
+          </div>
+
+        </div><!-- /.col -->
+
+      </div><!-- /.row -->
+
+      <?php //echo do_shortcode('[spl_widget home-page refresh this-week limit=1]'); ?>
+
+      
+      <div class="row">
+        <div class="col-sm-8 col-md-8 col-lg-9">
+          <?php echo do_shortcode('[spl_widget home-page refresh this-week limit=1]'); ?>
+        </div>
+        <div class="col-sm-4 col-md-4 col-lg-3">
+          <div class="panel spl-hero-panel spl-hero-callout">
+            <h4 class="text-center" style="">
+              <i class="glyphicon glyphicon-calendar"></i> 
+              You're Invited
             </h4>
-              <img style="margin:auto;" class="img-responsive hidden-xs" src="/assets/img/promos/photos/spl-handheld.jpg">
-            <div class="panel-body" style="padding:8px;">
-              <b>Download</b> ebooks, music, magazines, and more&hellip;
-            </div>
-          </a>
-        </div>
-
-      </div><!-- /.col -->
-       <div class="col-sm-8 col-sm-pull-4 col-md-8 col-md-pull-4 col-lg-9 col-lg-pull-3">
-
-        <div class="spl-tile">
-          <?php echo do_shortcode('[spl_carousel_photo auto random slug=stage]'); ?>
-        </div>
-
-      </div><!-- /.col -->
-
-    </div><!-- /.row -->
-
-    <?php //echo do_shortcode('[spl_widget home-page refresh this-week limit=1]'); ?>
-
-    
-    <div class="row">
-      <div class="col-sm-8 col-md-8 col-lg-9">
-        <?php echo do_shortcode('[spl_widget home-page refresh this-week limit=1]'); ?>
-      </div>
-      <div class="col-sm-4 col-md-4 col-lg-3">
-        <div class="panel spl-hero-panel spl-hero-callout">
-          <h4 class="text-center" style="">
-            <i class="glyphicon glyphicon-calendar"></i> 
-            You're Invited
-          </h4>
-          <div class="panel-body">
-            <?php echo do_shortcode('[spl_widget home-page carousel pause auto calendar embedded refresh]'); ?>
-          </div>
-        </div>
-      </div>
-    </div>
-    
-  </div><!-- /.col -->
-
-  <div class="col-md-3 col-md-pull-9 col-lg-2 col-lg-pull-8 col-lg-offset-1">
-    
-    <div class="row">
-      <div class="col-sm-8 col-md-12">
-    
-        <div class="panel spl-hero-open">
-          <h4 class="text-center">
-            <i class="glyphicon glyphicon-time"></i>
-            Open Hours
-          </h4>
-          <div class="panel-body">
-            <?php echo do_shortcode('[spl_widget branch-hours]'); ?>
-          </div>
-        </div>
-
-      </div><!-- /.col -->
-
-      <div class="col-sm-4 col-md-12">
-        <div class="panel spl-hero-panel spl-hero-muted">
-          <h4 class="text-center" style="">
-            <i class="glyphicon glyphicon-thumbs-up"></i> 
-            Staff Picks
-          </h4>
-          <img class="img-responsive hidden-xs" style="margin:auto;" src="/assets/img/promos/spl-bookshelf-avatar.jpg">
-          <div class="panel-body">
-            <h5>Recently Reviewed:</h5>
-            <h5 class="serif">Emperors & ninjas in 1920’s Japan</h5>
-            <div class="text-right">
-              <a href="#"><small>Continue</small></a> <small class="text-muted">&rarr;</small>
+            <div class="panel-body">
+              <?php echo do_shortcode('[spl_widget home-page carousel pause auto calendar embedded refresh]'); ?>
             </div>
           </div>
         </div>
-      </div><!-- /.col -->
+      </div>
+      
+    </div><!-- /.col -->
 
-    </div><!-- /.row -->
+    <div class="col-md-3 col-md-pull-9 col-lg-2 col-lg-pull-8 col-lg-offset-1">
+      
+      <div class="row">
+        <div class="col-sm-8 col-md-12">
+      
+          <div class="panel spl-hero-open">
+            <h4 class="text-center">
+              <i class="glyphicon glyphicon-time"></i>
+              Open Hours
+            </h4>
+            <div class="panel-body">
+              <?php echo do_shortcode('[spl_widget branch-hours]'); ?>
+            </div>
+          </div>
 
-  </div><!-- /.col -->
+        </div><!-- /.col -->
 
-</div><!-- /.row -->
+        <div class="col-sm-4 col-md-12">
+          <div class="panel spl-hero-panel spl-hero-muted">
+            <h4 class="text-center" style="">
+              <i class="glyphicon glyphicon-thumbs-up"></i> 
+              Staff Picks
+            </h4>
+            <img class="img-responsive hidden-xs" style="margin:auto;" src="/assets/img/promos/spl-bookshelf-avatar.jpg">
+            <div class="panel-body">
+              <h5>Recently Reviewed:</h5>
+              <h5 class="serif">Emperors & ninjas in 1920’s Japan</h5>
+              <div class="text-right">
+                <a href="#"><small>Continue</small></a> <small class="text-muted">&rarr;</small>
+              </div>
+            </div>
+          </div>
+        </div><!-- /.col -->
 
-</div>
+      </div><!-- /.row -->
+
+    </div><!-- /.col -->
+
+  </div><!-- /.row -->
+
+</div><!-- /.spl-hero-wrapper -->
 
 
 <?php 
