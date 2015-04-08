a0e8fd30673f3ffe3db57edc595a5b618d90980d
Sean Girard <seangirard@yahoo.com>
2015-04-08 13:31:14 -0700

    home

--- template-beta.php
+++ template-beta.php
@@ -32,7 +32,7 @@ if ( !empty($spl_home) ): ;
     <div class="panel spl-hero-panel spl-hero-primary spl-hero-digital">
       <a class="spl-hero-link" href="/download/">
         <h4 class="text-center">
-          <i class="glyphicon glyphicon-cloud-download"></i>
+          <i class="glyphicon glyphicon-cloud-download hidden-sm"></i>
           Digital Branch
         </h4>
         <div class="hidden-xs" style="height: 140px; overflow:hidden;">
