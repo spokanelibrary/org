a09c28ce2c3e18fdfb61a8080c48d617bc1b8890
Sean Girard <seangirard@yahoo.com>
2015-04-08 12:33:56 -0700

    home

--- templates/org/header-social-connect.php
+++ templates/org/header-social-connect.php
@@ -9,6 +9,9 @@
       href="/news/"
       title=""><b>News</b></a>
   <a class="btn btn-default" 
+      href="/card/"
+      title=""><b>Get a Library Card</b></a>
+  <a class="btn btn-default" 
       href="/support/"
       title=""><b class="">Support Your Library!</b></a>
 </div>
