226cb023cb6a0da7996df6edb2712ca7c8a7a2e2
Sean Girard <seangirard@yahoo.com>
2015-04-08 14:03:17 -0700

    home

--- template-beta.php
+++ template-beta.php
@@ -35,7 +35,7 @@ if ( !empty($spl_home) ): ;
           <i class="glyphicon glyphicon-cloud hidden-sm"></i>
           Digital Branch
         </h4>
-        <div class="hidden-xs" style="height: 140px; overflow:hidden;">
+        <div class="hidden-xs" style="height: 110px; overflow:hidden;">
           <img style="margin:auto;" class="img-responsive" src="/assets/img/promos/photos/spl-handheld.jpg">
         </div>
         <div class="digital-promo">
