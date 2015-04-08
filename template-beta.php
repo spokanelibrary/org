48f92d9dca9b26024153d605362e025bdc5240ec
Sean Girard <seangirard@yahoo.com>
2015-04-08 09:04:05 -0700

    home

--- template-beta.php
+++ template-beta.php
@@ -4,23 +4,6 @@ Template Name: Beta Home Page
 */
 ?>
 
-<?php echo do_shortcode('[spl_carousel_photo auto random slug=stage]'); ?>
-
-<!--
-<div class="row">
-  <div class="col-md-6">
-    <?php //echo do_shortcode('[spl_widget search-catalog]'); ?>
-  </div>
-  <div class="col-md-6">
-    <div class="hidden-xs">
-      <?php //echo do_shortcode('[spl_widget login-form]'); ?>
-    </div>
-  </div>
-</div>
--->
-
-<?php //get_template_part('templates/content', 'page'); ?>
-
 <?php 
 // conditionally display page content
 $spl_home = $post->post_content;
@@ -38,178 +21,36 @@ if ( !empty($spl_home) ): ;
 </div>
 <?php endif; ?>
 
-<!--
-<div class="visible-sm">
-  <div class="navbar navbar-inverse" style="margin-top: 0px; margin-bottom: 0px; background:transparent; border:none;">
-    <span class="text-muted">
-      <b>Tip:</b> use the toggle (upper right) to browse our website.
-    </span>
-    <button type="button" style="float:none; margin-top:0; margin-right:0;" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-      <span class="sr-only">Toggle navigation</span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-    </button>     
-  </div>
-</div>
--->
-
-<div class="visible-xs visible-sm text-center" style="margin-top:-10px; margin-bottom: 10px;">
-  <div class="btn-group" role="group" aria-label="Social Library Links">
-    <a class="btn btn-link hidden-sm"
-        href="http://facebook.com/spokanelibrary" 
-        title=""><b><img style="width:24px;" class="" src="/assets/img/icons/32px/facebook.png"></b></a>
-    <a class="btn btn-link hidden-sm"
-        href="http://twitter.com/spokanelibrary" 
-        title=""><b><img style="width:24px;" class="" src="/assets/img/icons/32px/twitter.png"></b></a>
-    <a class="btn btn-link hidden-xs" 
-        href="/news/"
-        title=""><b>News</b></a>
-    <a class="btn btn-link hidden-xs" 
-        href="/blog/"
-        title=""><b>Blog</b></a>
-    <a class="btn btn-link hidden-xs" 
-        href="/about/"
-        title=""><b>About Us</b></a>
-    <a class="btn btn-link" 
-        href="/card/"
-        title=""><b>Get a Library Card</b></a>
-    <a class="btn btn-link hidden-xs" 
-        href="/support/"
-        title=""><b class="text-danger">Support Your Library!</b></a>
-  </div>
-</div>
-
-<div class="visible-xs visible-sm">
-  <div class="row">
-    <div class="col-xs-12">
-      <div class="list-group">
-        <a href="/branches/" class="list-group-item">
-          <span class="text-primary">Find us: Branches &amp; open hours <small>&rarr;</small></span>
-        </a>
-        <a href="/storytime/" class="list-group-item">
-          <span class="text-primary">Join us: Storytime schedules <small>&rarr;</small></span>
-        </a>
-      </div><!-- /.list-group -->
-    </div><!-- /.col -->
-  </div><!-- /.row -->
-
-  <div class="row">
-    <div class="col-sm-12">
-
-      <div class="spl-hero-panel spl-hero-primary">
-
-        <h4 class="text-center">
-        <i class="glyphicon glyphicon-hand-down"></i>
-        For mobile devices
-      </h4>
-
-        <div class="spl-hero-digital btn-group btn-group-justified" role="group" aria-label="...">
-          <a href="/search/" style="height:40px; vertical-align:middle; border:none; border-radius:0;" class="btn btn-primary spl-blue-tint-60">
-            <span class="hidden-xs">&nbsp;Catalog</span> Search &rarr;
-          </a>
-          <a href="/account/" style="height:40px; vertical-align:middle; border:none;" class="btn btn-primary spl-blue-tint-50">
-            <span class="hidden-xs">&nbsp;My</span> Account &rarr;
-          </a>
-          <a href="/apps/" style="height:40px; vertical-align:middle; border:none;" class="btn btn-primary spl-blue-tint-40">
-            <span class="hidden-xs">&nbsp;Native</span> Apps &rarr;</span>
-          </a>
-        </div>
-
-      </div>
-    </div><!-- /.col -->
 
 
-  </div><!-- /.row -->
-</div>
 
-
-<div class="row" id="spl-hero" style="margin-top:-20px;"> 
-  
-  <div class="col-sm-12">
-    <?php echo do_shortcode('[spl_carousel_beta pause auto random shuffle news posts pages calendar promo=tech slug=carousel limit=6 title="" ]'); ?>
-  </div><!-- /.col -->
-
-  <div class="col-sm-12">
-
-    <div class="spl-hero-panel spl-hero-primary">
-
-      <a href="/digital/" class="spl-hero-link">
-        <h4 class="text-center normal">
-          <i class="glyphicon glyphicon-cloud-download"></i>
-          Visit the <b>Digital Branch</b> <span class="hidden-xs">to <b>download</b> eBooks, music, &amp; more</span> <small class="glyphicon glyphicon-arrow-right"></small>
-        </h4>
-      </a>
-
-      <div class="spl-hero-digital btn-group btn-group-justified" role="group" aria-label="...">
-        <a href="/ebooks/" style="height:40px; vertical-align:middle; border:none; border-radius:0;" class="btn btn-primary spl-blue-tint-60">
-          <i class="glyphicon glyphicon-phone"></i>
-          <span class="hidden-xs">&nbsp;<b>eBooks</b></span>
-        </a>
-        <a href="/emusic/" style="height:40px; vertical-align:middle; border:none;" class="btn btn-primary spl-blue-tint-50">
-          <i class="glyphicon glyphicon-music"></i>
-          <span class="hidden-xs">&nbsp;<b>Music</b></span>
-        </a>
-        <a href="/eaudio/" style="height:40px; vertical-align:middle; border:none;" class="btn btn-primary spl-blue-tint-40">
-          <i class="glyphicon glyphicon-headphones"></i></span>
-          <span class="hidden-xs">&nbsp;<b>Audio Books</b></span>
-        </a>
-        <a href="/emagazines/" style="height:40px; vertical-align:middle; border:none; border:none; border-radius:0;" class="btn btn-primary spl-blue-tint-30">
-          <i class="glyphicon glyphicon-book"></i>
-          <span class="hidden-xs">&nbsp;<b>Magazines</b></span>
-        </a>
-      </div>
-
-    </div>
-  </div><!-- /.col -->
-
-  <div class="col-sm-12">
-
-    <div class="spl-hero-panel spl-hero-success">
-
-      <a href="/calendar/" class="spl-hero-link">
-        <h4 class="text-center normal">
-          <i class="glyphicon glyphicon-calendar"></i>
-          <span class="hidden-xs"><b>Coming up soon</b> on the</span> <b>Library Calendar</b> <small class="glyphicon glyphicon-arrow-right"></small>
-        </h4>
-      </a>
-      
-      <div class="btn-group btn-group-justified" role="group" aria-label="...">
-        <a href="/storytime/" style="height:40px; vertical-align:middle; border:none; border-radius:0;" class="btn btn-warning spl-green-tint-70">
-          <i class="glyphicon glyphicon-picture"></i>
-          <span class="hidden-xs">&nbsp;<b>Storytimes</b></span>
-        </a>
-        <a href="/calendar/find/connected+learning/" style="height:40px; vertical-align:middle; border:none;" class="btn btn-warning spl-green-tint-60">
-          <i class="glyphicon glyphicon-facetime-video"></i>
-          <span class="hidden-xs">&nbsp;<b>Connected Learning</b></span>
-        </a>
-        <a href="/calendar/find/board+meeting/" style="height:40px; vertical-align:middle; border:none;" class="btn btn-warning spl-green-tint-50">
-          <i class="glyphicon glyphicon-paperclip"></i></span>
-          <span class="hidden-xs">&nbsp;<b>Board Meetings</b></span>
-        </a>
-        <a href="/calendar/" style="height:40px; vertical-align:middle; border:none; border:none; border-radius:0;" class="btn btn-warning spl-green-tint-40">
-          <i class="glyphicon glyphicon-plus-sign"></i>
-          <span class="hidden-xs">&nbsp;<b>Much More</b></span>
-        </a>
-      </div>
-      
-    </div>
-
-  </div><!-- /.col -->
-</div><!-- /.row -->
 
 <div class="row">
-  
-  <div class="col-md-12">
-    <div class="spl-hero-panel spl-hero-default">
-      <a class="spl-hero-link" href="/browse/">
+
+  <div class="col-sm-4 col-sm-push-8 col-md-4 col-md-push-8 col-lg-3 col-lg-push-9">
+    
+    <div class="panel spl-hero-panel spl-hero-primary spl-hero-digital">
+      <a class="spl-hero-link" href="/download/">
         <h4 class="text-center">
-          <i class="glyphicon glyphicon-star"></i>
-          <span class="hidden-xs">On our reading list:</span> <b>Staff picks &amp; popular titles</b> <small class="glyphicon glyphicon-arrow-right"></small>
+          <i class="glyphicon glyphicon-cloud-download"></i>
+          Digital Branch
         </h4>
+          <img style="margin:auto;" class="img-responsive hidden-xs" src="/assets/img/promos/photos/spl-handheld.jpg">
+        <div class="panel-body">
+          <div class="digital-promo">
+            <b>Download</b> ebooks, music, magazines, and more&hellip;
+          </div>
+        </div>
       </a>
     </div>
-    <?php echo do_shortcode('[spl_widget browse-list list=star widget-beta limit=4]'); ?>
+
+  </div><!-- /.col -->
+   <div class="col-sm-8 col-sm-pull-4 col-md-8 col-md-pull-4 col-lg-9 col-lg-pull-3">
+
+    <div class="spl-tile">
+      <?php echo do_shortcode('[spl_carousel_photo auto random slug=stage]'); ?>
+    </div>
+
   </div><!-- /.col -->
 
 </div><!-- /.row -->
