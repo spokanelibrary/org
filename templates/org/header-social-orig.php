c2bd20ce869f2c5d5c3d94a0db52971d78a895aa
Sean Girard <seangirard@yahoo.com>
2015-04-08 12:10:12 -0700

    home

--- templates/org/header-social-orig.php
+++ templates/org/header-social-orig.php
@@ -1,36 +1,23 @@
-
-<div class="pull-right" style="width:360px; margin-top: 10px; margin-right: 10px;">
-  <div class="row">
-    <div class="col-xs-4">
-      <p>  
-        <a class="btn btn-block btn-default btn-sm"
-          href="http://facebook.com/spokanelibrary" 
-          style="opacity:.9;"
-          title="">
-          <img src="/assets/img/icons/16px/facebook.png">
-          <b>Facebook</b></a>
-      </p>
-    </div><!-- /.col -->
-    <div class="col-xs-4">
-      <p>
-        <a class="btn btn-block btn-default btn-sm text-info" 
-          href="http://twitter.com/spokanelibrary" 
-          style="opacity:.9;"
-          title="">
-          <img src="/assets/img/icons/16px/twitter.png">
-          <b>Twitter</b></a>
-      </p>
-    </div><!-- /.col -->
-    <div class="col-xs-4">
-      <p>
-        <a class="btn btn-block btn-default btn-sm text-info" 
-          href="/support/" 
-          style="opacity:.9;"
-          title="">
-          <i style="border:0px solid #ccc !important; border-radius:4px; padding:2px 3px;  color:#fff; background: #f0ad4e;" class="glyphicon glyphicon-hand-right"></i>
-          <b>Support Us</b></a>
-      </p>
-    </div><!-- /.col -->
-  </div><!-- /.row -->
+<div class="btn-group btn-group-sm" role="group" aria-label="Social Library Links">
+  <a class="btn btn-default"
+      href="http://facebook.com/spokanelibrary" 
+      title=""><img style="width:16px;" class="" src="/assets/img/icons/32px/facebook.png"></a>
+  <a class="btn btn-default"
+      href="http://twitter.com/spokanelibrary" 
+      title=""><img style="width:16px;" class="" src="/assets/img/icons/32px/twitter.png"></a>
+  <a class="btn btn-default" 
+      href="/news/"
+      title=""><b>News</b></a>
+  <a class="btn btn-default" 
+      href="/blog/"
+      title=""><b>Blog</b></a>
+  <a class="btn btn-default" 
+      href="/about/"
+      title=""><b>About Us</b></a>
+  <a class="btn btn-default" 
+      href="/card/"
+      title=""><b>Get a Library Card</b></a>
+  <a class="btn btn-default" 
+      href="/support/"
+      title=""><b class="text-success">Support Your Library!</b></a>
 </div>
-
