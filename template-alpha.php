043bb4e53e48a09da3c19b99da992b891f5d402b
Sean Girard <seangirard@yahoo.com>
2015-04-08 15:03:34 -0700

    home

--- template-alpha.php
+++ template-alpha.php
@@ -41,69 +41,74 @@ if ( !empty($spl_home) ): ;
 
 
 
-
-
 <div class="row">
 
-  <div class="col-sm-3">
+  <div class="col-lg-10 col-lg-offset-1">
     
-    <div class="panel spl-hero-panel spl-hero-primary spl-hero-digital">
-      <a class="spl-hero-link" href="/download/">
-        <h4 class="text-center">
-          <i class="glyphicon glyphicon-cloud hidden-sm"></i>
-          Digital Branch
-        </h4>
-        <div class="hidden-xs" style="height: 80px; overflow:hidden;">
-          <img style="margin:auto;" class="img-responsive" src="/assets/img/promos/photos/spl-handheld.jpg">
+    <div class="row">
+
+      <div class="col-sm-3">
+        
+        <div class="panel spl-hero-panel spl-hero-primary spl-hero-digital">
+          <a class="spl-hero-link" href="/download/">
+            <h4 class="text-center">
+              <i class="glyphicon glyphicon-cloud hidden-sm"></i>
+              Digital Branch
+            </h4>
+            <div class="hidden-xs" style="height: 80px; overflow:hidden;">
+              <img style="margin:auto;" class="img-responsive" src="/assets/img/promos/photos/spl-handheld.jpg">
+            </div>
+            <div class="digital-promo">
+              <h5 class="text-center uppercase">Always open, always on</h5>
+            </div>
+            <div class="panel-body hidden-xs hidden-sm" style="background-image:url('/assets/img/spl-touch-bg.png'); background-position:-20px -20px;">
+              <h5>eBooks &amp; downloads</h5>
+              <h5 class="text-right">Research databases</h5>
+              <h5>Mobile apps</h5>
+              <h5 class="text-right">Technology training</h5>        
+              <h5>Microsoft Certification</h5>
+            </div> 
+          </a>
         </div>
-        <div class="digital-promo">
-          <h5 class="text-center uppercase">Always open, always on</h5>
+
+        
+      </div><!-- /.col -->
+      <div class="col-sm-6">
+
+        <div class="spl-tile">
+          <?php echo do_shortcode('[spl_carousel_photo auto random slug=stage]'); ?>
         </div>
-        <div class="panel-body hidden-xs hidden-sm" style="background-image:url('/assets/img/spl-touch-bg.png'); background-position:-20px -20px;">
-          <h5>eBooks &amp; downloads</h5>
-          <h5 class="text-right">Research databases</h5>
-          <h5>Mobile apps</h5>
-          <h5 class="text-right">Technology training</h5>        
-          <h5>Microsoft Certification</h5>
-        </div> 
-      </a>
-    </div>
 
-    
-  </div><!-- /.col -->
-  <div class="col-sm-6">
+        
+      </div><!-- /.col -->
 
-    <div class="spl-tile">
-      <?php echo do_shortcode('[spl_carousel_photo auto random slug=stage]'); ?>
-    </div>
-
-    
-  </div><!-- /.col -->
-
-  <div class="col-sm-3">
-    
-    <div class="panel spl-hero-panel spl-hero-calendar">
-      <h4 class="text-center" style="">
-        <i class="glyphicon glyphicon-calendar"></i> 
-        Coming Up
-      </h4>
-      <div class="panel-body" style="background-image:url('/assets/img/spl-touch-bg.png'); background-position:120px 120px;">
-        <div style="height:220px;">
-          <?php echo do_shortcode('[spl_widget home-page carousel pause auto calendar embedded vertical refresh]'); ?>
+      <div class="col-sm-3">
+        
+        <div class="panel spl-hero-panel spl-hero-calendar">
+          <h4 class="text-center" style="">
+            <i class="glyphicon glyphicon-calendar"></i> 
+            Coming Up
+          </h4>
+          <div class="panel-body" style="background-image:url('/assets/img/spl-touch-bg.png'); background-position:120px 120px;">
+            <div style="height:220px;">
+              <?php echo do_shortcode('[spl_widget home-page carousel pause auto calendar embedded vertical refresh]'); ?>
+            </div>
+            <h5 class="text-center">
+              <a href="#">Calendar of Events <span class="text-muted">&rarr;</span></a>
+            </h5>      
+          </div>
         </div>
-        <h5 class="text-center">
-          <a href="#">Calendar of Events <span class="text-muted">&rarr;</span></a>
-        </h5>      
+
+      </div>
+
+    </div><!-- /.row -->
+
+
+    <div class="panel spl-hero-panel spl-hero-success">
+      <div class="panel-body" style="background:#fff !important;">
+        <?php echo do_shortcode('[spl_widget home-page news]'); ?>
       </div>
     </div>
 
-  </div>
-
+  </div><!-- /.col -->
 </div><!-- /.row -->
-
-
-<div class="panel spl-hero-panel spl-hero-success">
-  <div class="panel-body" style="background:#fff !important;">
-    <?php echo do_shortcode('[spl_widget home-page news]'); ?>
-  </div>
-</div>
