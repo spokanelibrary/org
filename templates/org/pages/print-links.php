c955e8b175f37daafc72de9cd48a475851c53bbe
Sean Girard <seangirard@yahoo.com>
2015-04-27 08:47:28 -0700

    print links

---
+++ templates/org/pages/print-links.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#spl-nav-print-links">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Brand</a>
+    </div>
+    <div class="collapse navbar-collapse" id="spl-nav-print-links">
+      <ul class="nav navbar-nav">
+        <li><a href="/printing/downtown/">Downtown</a></li>
+        <li><a href="/printing/east-side/">East Side</a></li>
+        <li><a href="/printing/hillyard/">Hillyard</a></li>
+        <li><a href="/printing/indian-trail/">Indian Trail</a></li>
+        <li><a href="/printing/shadle/">Shadle</a></li>
+        <li><a href="/printing/south-hill/">South Hill</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
