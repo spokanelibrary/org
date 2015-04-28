9e3e813b528ad3f47280defe706d586d63999497
Sean Girard <seangirard@yahoo.com>
2015-04-28 09:47:40 -0700

    footer

--- templates/org/footer.php
+++ templates/org/footer.php
@@ -36,6 +36,9 @@
         <a href="/">
           <img alt="Spokane Public Library logo" class="" style="width:170px; height:80px; margin:auto;" src="/assets/img/SPL-Logo-hidpi.png">
         </a>
+        <p>
+          Lorem ipsum dolor
+        </p>
       </div><!-- /.col -->
       <div class="col-sm-3 hidden-xs text-center">
         <a href="http://www.fdlp.gov">
@@ -45,11 +48,17 @@
                 style="width:92px; height:82px; margin:auto;" 
                 src="/assets/img/png/fdlp-emblem-color.png">
         </a>
+        <p>
+          Lorem ipsum dolor
+        </p>
       </div><!-- /.col -->
       <div class="col-xs-4 col-sm-3 text-right">
         <a href="https://my.spokanecity.org/">
           <img alt="City of Spokane logo" class="" style="width:75px; height:90px;" src="/assets/img/png/city-spokane-logo-hidpi.png">
         </a>
+        <p>
+          Lorem ipsum dolor
+        </p>
       </div><!-- /.col -->
     </div><!-- /.row -->
 
