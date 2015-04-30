4ab95811e60e194ca7d1814be2dc164dc80f56d8
Sean Girard <seangirard@yahoo.com>
2015-04-30 15:59:04 -0700

    footer

--- templates/org/footer.php
+++ templates/org/footer.php
@@ -32,12 +32,12 @@
           509.444.5300
         </address>
       </div><!-- /.col -->
-      <div class="col-sm-3 hidden-xs text-center">
-        <a href="/">
-          <img alt="Spokane Public Library logo" class="" style="width:170px; height:80px; margin:auto;" src="/assets/img/SPL-Logo-hidpi.png">
+      <div class="col-xs-4 col-sm-3 text-right">
+        <a href="https://my.spokanecity.org/">
+          <img alt="City of Spokane logo" class="" style="width:75px; height:90px;" src="/assets/img/png/city-spokane-logo-hidpi.png">
         </a>
-        <h6 class="text-success">
-          Read. Learn. Discover.
+        <h6 class="help-block">
+          <a href="https://my.spokanecity.org/">Division of the City of Spokane</a>
         </h6>
       </div><!-- /.col -->
       <div class="col-sm-3 hidden-xs text-center">
@@ -52,14 +52,14 @@
           <a href="http://www.fdlp.gov">Federal Depository Library</a>
         </h6>
       </div><!-- /.col -->
-      <div class="col-xs-4 col-sm-3 text-center">
-        <a href="https://my.spokanecity.org/">
-          <img alt="City of Spokane logo" class="" style="width:75px; height:90px;" src="/assets/img/png/city-spokane-logo-hidpi.png">
+      <div class="col-sm-3 hidden-xs text-right">
+        <a href="/">
+          <img alt="Spokane Public Library logo" class="" style="width:170px; height:80px; margin:auto;" src="/assets/img/SPL-Logo-hidpi.png">
         </a>
-        <h6 class="help-block">
-          <a href="https://my.spokanecity.org/">Division of the City of Spokane</a>
+        <h6 class="text-success">
+          Read. Learn. Discover.
         </h6>
-      </div><!-- /.col -->
+      </div><!-- /.col -->      
     </div><!-- /.row -->
 
     <div class="row">
