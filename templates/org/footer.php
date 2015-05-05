9e59a6cfaecf4d55dc0400bf97bd1299b6ddedf7
Sean Girard <seangirard@yahoo.com>
2015-05-05 14:31:39 -0700

    footer

--- templates/org/footer.php
+++ templates/org/footer.php
@@ -20,12 +20,12 @@
   <footer class="content-info container" role="contentinfo">
 
     <div class="row">
-      <div class="col-xs-8 col-sm-3">
+      <div class="col-xs-8 col-sm-6">
 
         <div class="media">
           <div class="media-left pull-left">
             <a href="/">
-              <img class="media-object" style="width:50px; height:40px; margin:auto;" src="/assets/img/spl-touch-hidpi.png" alt="Spokane Public Library logo">
+              <img class="media-object" style="width:100px; height:80px; margin:auto;" src="/assets/img/spl-touch-hidpi.png" alt="Spokane Public Library logo">
             </a>
           </div>
           <div class="media-body">
@@ -39,7 +39,24 @@
           </address>
           </div>
         </div>
+
       </div><!-- /.col -->
+
+      <div class="col-xs-8 col-sm-6">
+
+        <div class="media">
+          <div class="media-left pull-left">
+            <a href="/">
+              <img class="media-object" style="width:width:75px; height:90px; margin:auto;" src="/assets/img/png/city-spokane-logo-hidpi.png" alt="City of Spokane logo">
+            </a>
+          </div>
+          <div class="media-body">
+            <h5 class="media-heading"><a href="https://my.spokanecity.org/">A division of the<br>City of Spokane</a></h5>
+          </div>
+        </div>
+
+      </div><!-- /.col -->
+      <!--
       <div class="col-xs-4 col-sm-3 text-center">
         <a href="https://my.spokanecity.org/">
           <img alt="City of Spokane logo" class="" style="width:75px; height:90px;" src="/assets/img/png/city-spokane-logo-hidpi.png">
@@ -47,7 +64,7 @@
         <h6 class="help-block">
           <a href="https://my.spokanecity.org/">Division of the<br>City of Spokane</a>
         </h6>
-      </div><!-- /.col -->
+      </div>--><!-- /.col -->
       <!--<div class="col-sm-3 hidden-xs text-center">
         <a href="http://www.fdlp.gov">
           <img alt="Federal Depository Library Program logo" 
