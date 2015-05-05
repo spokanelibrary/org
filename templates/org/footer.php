23d39b8a1747e3a5adfa171843ab35dcf5b3e326
Sean Girard <seangirard@yahoo.com>
2015-05-05 14:47:00 -0700

    footer

--- templates/org/footer.php
+++ templates/org/footer.php
@@ -29,15 +29,15 @@
             </a>
           </div>
           <div class="media-body">
-            <h6 class="media-heading"><a href="/">Spokane Public Library</a></h6>
+            <h5 class="media-heading"><a href="/">Spokane Public Library</a></h5>
             <address class="help-block" style="">
-              <h5>
+              <h6>
                 906 West Main Avenue
                 <br>
                 Spokane, WA 99201
                 <br>
                 509.444.5300
-              </h5>
+              </h6>
           </address>
           </div>
         </div>
