b83e6405358a2ce1a3c843d96c9666d6784e4e6e
Sean Girard <seangirard@yahoo.com>
2015-05-20 12:44:40 -0700

    summer

--- templates/org/forms/form-summer-reading.php
+++ templates/org/forms/form-summer-reading.php
@@ -1,11 +1,11 @@
 <?php
 $crass = wp_get_crass_response();
-if ( !empty($crass->request) ) {
-  print_r($crass);
+print_r($crass);
   echo 'test';
+if ( !empty($crass->request) ) {
   include 'field/summer-reading-response.php';
   } else {
-  //include 'field/summer-reading.php';
+  include 'field/summer-reading.php';
 }
 ?>
 
