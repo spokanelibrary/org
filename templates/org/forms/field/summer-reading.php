48e8bcce9bf38c4aaef10539f59551773ec5b820
Sean Girard <seangirard@yahoo.com>
2015-05-20 13:22:43 -0700

    summer

--- templates/org/forms/field/summer-reading.php
+++ templates/org/forms/field/summer-reading.php
@@ -24,14 +24,14 @@
             
             <div class="form-group">
               <label for="spl-form-name" class="col-sm-4 control-label">What is your name?</label>
-              <div class="col-sm-8">
+              <div class="col-sm-8 col-md-6">
                 <input type="text" class="form-control required" id="spl-form-name" name="spl-form[name]" placeholder="">
               </div>
             </div>
 
             <div class="form-group">
               <label for="spl-form-name" class="col-sm-4 control-label">How old are you?</label>
-              <div class="col-sm-8">
+              <div class="col-sm-8 col-md-6">
                 <select 
                     name="spl-form[age]" 
                     id="spl-form-age"
