86854f74ad6c9a4538838ee8305f0ae379c48fa0
Sean Girard <seangirard@yahoo.com>
2015-05-20 13:25:20 -0700

    summer

--- templates/org/forms/field/summer-reading.php
+++ templates/org/forms/field/summer-reading.php
@@ -38,25 +38,44 @@
                     class="form-control"
                     title="Please select your age."
                     >
-                <option value="0">Your Age...</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-                <option value="11">11</option>
-                <option value="12">12</option>
-                <option value="13">13</option>
-                <option value="14">14</option>
-                <option value="15">15</option>
-                <option value="16">16</option>
-                <option value="adult">Over 16 years old</option>
-            </select>
+                    <option value="0">Your Age...</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                    <option value="13">13</option>
+                    <option value="14">14</option>
+                    <option value="15">15</option>
+                    <option value="16">16</option>
+                    <option value="adult">Over 16 years old</option>
+                </select>
+              </div>
+            </div>
 
+            <div class="form-group">
+              <label for="spl-form-name" class="col-sm-4 control-label">Which branch do you visit most?</label>
+              <div class="col-sm-8 col-md-6">
+                <select 
+                    name="spl-form[branch]" 
+                    id="spl-form-branch"
+                    class="form-control"
+                    title="Please select a library branch."
+                    >
+                    <option value="">Your Library Branch...</option>
+                    <option value="dt">Downtown</option>
+                    <option value="es">East Side</option>
+                    <option value="hy">Hillyard</option>
+                    <option value="it">Indian Trail</option>
+                    <option value="sh">Shadle</option>
+                    <option value="so">South Hill</option>
+                </select>
               </div>
             </div>
 
