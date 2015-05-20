cf18e5f6f7223ccf21a3255e5f3ee3b4d10dd28f
Sean Girard <seangirard@yahoo.com>
2015-05-20 15:40:39 -0700

    summer

--- templates/org/forms/field/summer-reading-response.php
+++ templates/org/forms/field/summer-reading-response.php
@@ -1,40 +1,17 @@
 <div class="panel panel-success">
   <div class="panel-heading">
-    <h3>Thank You <?php if( !empty($crass->request['name']) ) : ?> <?php echo ' '.$crass->request['name']; ?><?php endif; ?>!</h3>
+    <h3>Thank You <?php if( !empty($crass->request['name']) ) : ?> <?php echo ' '.ucfirst($crass->request['name']); ?><?php endif; ?>!</h3>
   </div><!-- /.panel-heading -->
 
   <div class="panel-body">
-    <h4>We received your Summer Reading Signup:</h4>
-    <dl class="dl-horizontal">
-
-      <?php if( !empty($crass->request['name']) ) : ?>
-      <dt>Your Name</dt>
-      <dd>
-        <p>
-          <?php echo $crass->request['name']; ?>
-        </p>
-      </dd>
-      <?php endif; ?>
-
-      <?php if( !empty($crass->request['age']) ) : ?>
-      <dt>Your Age</dt>
-      <dd>
-        <p>
-          <?php echo ucfirst($crass->request['age']); ?>
-        </p>
-      </dd>
-      <?php endif; ?>
-
-      <?php if( !empty($crass->request['branch']) ) : ?>
-      <dt>Your Library Branch</dt>
-      <dd>
-        <p>
-          <?php echo $crass->request['branch']; ?>
-        </p>
-      </dd>
-      <?php endif; ?>
-
-    </dl>
+    
+    <?php if( 'adult' == $crass->request['age'] ) : ?>
+    <a href="/assets/pdf/spl-summer-reading-log-2015-adult.pdf">
+    <?php else: ?>
+    <a href="/assets/pdf/spl-summer-reading-log-2015.pdf">
+    <?php endif; ?>
+    <h4>Get your summer reading log now &rarr;</h4>
+    </a>
   </div><!-- /.panel-body -->
 </div><!-- /.panel -->
 
