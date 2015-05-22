d5b3f94fb5d024abd1bb6374592a96fb1391a443
Sean Girard <seangirard@yahoo.com>
2015-05-22 10:21:16 -0700

    user

--- templates/head.php
+++ templates/head.php
@@ -6,7 +6,8 @@
   <title><?php wp_title('|', true, 'right'); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <?php //echo do_shortcode('[spl_widget my-account session]'); ?>
+  <?php echo do_shortcode('[spl_widget my-account session]'); ?>
+  
 
   <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
   <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
