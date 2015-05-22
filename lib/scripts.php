f67587ba425789f0a9276d2bdab41b13f732e53b
Sean Girard <seangirard@yahoo.com>
2015-05-22 13:49:27 -0700

    modernizr

--- lib/scripts.php
+++ lib/scripts.php
@@ -26,9 +26,9 @@ function roots_scripts() {
     wp_enqueue_script('comment-reply');
   }
 
-  wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.7.0.min.js', array(), null, false);
+  //wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.7.0.min.js', array(), null, false);
   wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', array(), 'ae37e753cf426d452c56b9e7fc508e96', true);
-  wp_enqueue_script('modernizr');
+  //wp_enqueue_script('modernizr');
   wp_enqueue_script('jquery');
   wp_enqueue_script('roots_scripts');
 }
