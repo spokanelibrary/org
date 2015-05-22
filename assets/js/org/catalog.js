22e21684367c3d46b816a60f4b59f77eac4bb773
Sean Girard <seangirard@yahoo.com>
2015-05-22 11:00:37 -0700

    user

--- assets/js/org/catalog.js
+++ assets/js/org/catalog.js
@@ -328,6 +328,9 @@ var org = {
 
 		        //$('#spl-login-modal').modal('hide');
 		        $('.spl-login-modal-response').addClass('hide');
+		        if ( console && console.log ) {
+		        		console.log( _self.user );
+		        	}
 		        if ( _self.user && _self.user.sessionToken ) {
 		        	if ( _self.login.callbackFunction && _self.login.callbackTrigger ) {
 		        		_self.login.callbackFunction(_self.login.callbackTrigger);
