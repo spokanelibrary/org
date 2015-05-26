68fcba791a05b9f272e20e4f42f8f254fda576ad
Sean Girard <seangirard@yahoo.com>
2015-05-26 14:46:52 -0700

    card

--- templates/org/forms/field/signup/card-adult.php
+++ templates/org/forms/field/signup/card-adult.php
@@ -1,6 +1,6 @@
 <div class="panel panel-default">
   <div class="panel-heading">
-    <h4 class="panel-title clearfix">
+    <h4 class="panel-title clearfix" style="margin-top:10px;">
       Library Card Application
       <a class="btn btn-sm btn-default pull-right" style="margin:0;" href="<?php echo $_SERVER['REQUEST_URI'] ;?>"><i class="glyphicon glyphicon-refresh"></i> Start over</a>
     </h4>
