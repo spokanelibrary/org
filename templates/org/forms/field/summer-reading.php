ec628e096faf36a63ed665055919f22147ddf764
Sean Girard <seangirard@yahoo.com>
2015-05-28 07:15:37 -0700

    summer

--- templates/org/forms/field/summer-reading.php
+++ templates/org/forms/field/summer-reading.php
@@ -36,6 +36,7 @@
                     title="Please select your age."
                     >
                     <option value="0">Your Age...</option>
+                    <option value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
                     <option value="4">4</option>
