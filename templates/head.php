d98968c31ea866cb3c0c691bbf5adaad4604f909
Sean Girard <seangirard@yahoo.com>
2015-05-29 07:25:49 -0700

    head

--- templates/head.php
+++ templates/head.php
@@ -1,5 +1,11 @@
 <!DOCTYPE html>
-<html class="no-js" <?php language_attributes(); ?>>
+<!--[if lt IE 7]>  <html class="ie ie6 lte9 lte8 lte7 no-js" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 7]>     <html class="ie ie7 lte9 lte8 lte7 no-js" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 8]>     <html class="ie ie8 lte9 lte8 no-js" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 9]>     <html class="ie ie9 lte9 no-js" <?php language_attributes(); ?>> <![endif]-->
+<!--[if gt IE 9]>  <html class="no-js" <?php language_attributes(); ?>> <![endif]-->
+<!--[if !IE]><!--> <html class="no-js" <?php language_attributes(); ?>><!--<![endif]--> 
+
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
