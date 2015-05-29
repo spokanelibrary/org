71e11c9f889cc09e07902dd9ba22b81f865a2b15
Sean Girard <seangirard@yahoo.com>
2015-05-29 07:31:48 -0700

    ie8

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '18346cb19dd405a3bb70c19605b74060');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e21e876d9360885c5aeb31ee27ee398c');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
