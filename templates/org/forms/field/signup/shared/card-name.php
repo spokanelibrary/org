c8f971c63156990f8f2c10ba40c84c0d0fddc391
Sean Girard <seangirard@yahoo.com>
2015-07-14 10:05:17 -0700

    middle initial

--- templates/org/forms/field/signup/shared/card-name.php
+++ templates/org/forms/field/signup/shared/card-name.php
@@ -6,7 +6,7 @@
   </div>
   <div class="col-sm-3" id="">
     <span class="help-block">Middle</span>
-    <input type="text" class="form-control uppercase required" id="spl-form-name-middle" name="spl-form[signup][name][middle]" value="" placeholder="">
+    <input type="text" class="form-control uppercase" id="spl-form-name-middle" name="spl-form[signup][name][middle]" value="" placeholder="">
   </div>
   <div class="col-sm-3" id="">
     <span class="help-block">Last</span>
