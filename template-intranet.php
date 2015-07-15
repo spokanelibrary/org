af7fcf89c0847c9bd3328d6c0932aaffecfd175a
Sean Girard <seangirard@yahoo.com>
2015-07-15 14:00:09 -0700

    intra

--- template-intranet.php
+++ template-intranet.php
@@ -32,51 +32,6 @@ Template Name: Intranet Home Page
     
     <div class="row">
 
-      <div class="col-sm-8 col-md-3">
-        
-        <div class="panel spl-hero-panel spl-hero-primary spl-hero-digital">
-          <a class="spl-hero-link" href="/digital/">
-            <h4 class="text-center hero-heading">
-              <i class="glyphicon glyphicon-cloud"></i>
-              Digital Branch
-            </h4>
-            <div class="hidden-xs hidden-sm" style="height: 80px; overflow:hidden;">
-              <img style="margin:auto;" class="img-responsive" src="/assets/img/promos/photos/spl-handheld.jpg">
-            </div>
-            <div class="digital-promo">
-              <h5 class="text-center uppercase">Always open, always on</h5>
-            </div>
-            <div class="panel-body hidden-xs hidden-sm" style="">
-              <h5>eBooks &amp; downloads</h5>
-              <h5 class="text-right">Research databases</h5>
-              <h5>Mobile apps</h5>
-              <h5 class="text-right">Technology training</h5>        
-              <h5>Microsoft Certification</h5>
-            </div> 
-          </a>
-        </div>
-        
-      </div><!-- /.col -->
-
-      <div class="col-sm-4 visible-sm visible-xs">
-
-        <div class="panel spl-hero-open">
-          <a class="spl-hero-link" href="/open/">  
-            <h4 class="text-center hero-heading">
-              <i class="glyphicon glyphicon-time"></i>
-              Open Hours
-            </h4>
-            <div class="panel-body">
-              <div class="open-promo">
-                <h5 class="text-center text-success uppercase">Check library schedule</h5>
-              </div>
-            </div>
-          </a>
-        </div>
-      </div>
-
-      <div class="clearfix visible-sm"></div>
-
       <div class="col-sm-4 col-sm-push-8 col-md-3 col-md-push-6">
         
         <?php dynamic_sidebar('sidebar-primary'); ?>
