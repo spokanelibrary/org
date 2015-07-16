4de39cb8082ca4c130c32a454ad2c0b79603e802
Sean Girard <seangirard@yahoo.com>
2015-07-16 13:33:33 -0700

    intra

--- template-intranet.php
+++ template-intranet.php
@@ -10,43 +10,49 @@ Template Name: Intranet Home Page
   <!--<div class="col-lg-10 col-lg-offset-1">-->
   <div class="col-sm-12">
 
-    <nav class="navbar navbar-default">
-      <ul class="nav nav-justified">
-      <?php 
-      wp_nav_menu( 
-      array(
-        'menu' => 'Quick Links'
-      , 'container' => false
-      , 'items_wrap' => '%3$s'
-      )); 
-      ?>
-      </ul>
-    </nav>
-
-    <div style="margin-bottom:16px;">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-    </div>
-
-    <?php 
-    // conditionally display page content
-    $spl_home = $post->post_content;
-    $spl_home = trim($spl_home);
-    if ( !empty($spl_home) ): ;
-    ?>
-    <div class="panel spl-hero-panel spl-hero-announce">
-      <?php 
-      if ( $post->post_excerpt): ; 
-      ?>
-      <h4 class="text-center hero-heading">
-        <i class="glyphicon glyphicon-bullhorn"></i>
-        <?php echo $post->post_excerpt; ?>
-      </h4>
-      <?php endif; ?>
+    <div class="panel spl-hero-panel spl-hero-primary">
       <div class="panel-body">
-        <?php echo $post->post_content; ?>
-      </div>
-    </div>
-    <?php endif; ?>
+        
+        <nav class="navbar navbar-default">
+          <ul class="nav nav-justified">
+          <?php 
+          wp_nav_menu( 
+          array(
+            'menu' => 'Quick Links'
+          , 'container' => false
+          , 'items_wrap' => '%3$s'
+          )); 
+          ?>
+          </ul>
+        </nav>
+
+        <div style="margin-bottom:16px;">
+        <?php dynamic_sidebar('sidebar-footer'); ?>
+        </div>
+
+        <?php 
+        // conditionally display page content
+        $spl_home = $post->post_content;
+        $spl_home = trim($spl_home);
+        if ( !empty($spl_home) ): ;
+        ?>
+        <div class="panel spl-hero-panel spl-hero-announce">
+          <?php 
+          if ( $post->post_excerpt): ; 
+          ?>
+          <h4 class="text-center hero-heading">
+            <i class="glyphicon glyphicon-bullhorn"></i>
+            <?php echo $post->post_excerpt; ?>
+          </h4>
+          <?php endif; ?>
+          <div class="panel-body">
+            <?php echo $post->post_content; ?>
+          </div>
+        </div>
+        <?php endif; ?>
+
+      </div><!-- /.panel-body -->
+    </div><!-- /.panel -->
     
     <div class="row">
 
