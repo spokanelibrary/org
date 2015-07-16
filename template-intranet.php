a76af99f10d120bd7b3c2c2f93f6cfa95278171c
Sean Girard <seangirard@yahoo.com>
2015-07-16 14:29:19 -0700

    intra

--- template-intranet.php
+++ template-intranet.php
@@ -96,20 +96,12 @@ Template Name: Intranet Home Page
       </div><!-- /.col -->
 
       <div class="col-md-6">
-        <div class="panel spl-hero-panel spl-hero-calendar">
-          <h4 class="text-center hero-heading" style="">
-            <i class="glyphicon glyphicon-calendar"></i> 
-            Coming Up
-          </h4>
-          <div class="panel-body" style="padding-bottom:0;">
-            <?php echo do_shortcode('[spl_widget home-page carousel control calendar embedded]'); ?>
-            <div class="calendar-promo">
-              <h5 class="text-center">
-                <a href="/calendar/">Full Calendar <span class="text-muted">&rarr;</span></a>
-              </h5>      
-            </div>
-          </div>
-        </div>
+        <div class="panel spl-hero-panel spl-hero-intranet">
+          <h4>Gold Star Thingies</h4>
+          <div class="panel-body">
+            hlhklhk
+          </div><!-- /.panel-body -->
+        </div><!-- /.panel -->
       </div><!-- /.col -->
 
       <div class="clearfix"></div>
