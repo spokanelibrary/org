6dced0a0ea77e2cb9e488c8685fb86b9875aed34
Sean Girard <seangirard@yahoo.com>
2015-07-16 13:38:17 -0700

    intra

--- template-intranet.php
+++ template-intranet.php
@@ -10,7 +10,7 @@ Template Name: Intranet Home Page
   <!--<div class="col-lg-10 col-lg-offset-1">-->
   <div class="col-sm-12">
 
-    <div class="panel spl-hero-panel spl-hero-primary">
+    <div class="panel spl-hero-panel spl-hero-success">
       <div class="panel-body">
         
         <nav class="navbar navbar-default">
