f6dfcb9b8c3dd882d79540b3cc40f600b38da7fc
Sean Girard <seangirard@yahoo.com>
2015-07-17 13:06:26 -0700

    staff

--- template-staff.php
+++ template-staff.php
@@ -44,8 +44,8 @@ class SPL_StaffDirectoryShortcode {
       
       foreach ( $categories as $c => $category ) {
         foreach ( $menu as $m => $item ) {
-          $menu['test'][] = true;
-          if ( 0 !=  $category->category_parent && $m ==  $category->cat_ID ) {
+          if ( 0 !=  $category->category_parent ) {
+            $menu['test'][] = true;
             
             $menu[$category->category_parent]['children'][$category->cat_ID][] = $category;
           }
