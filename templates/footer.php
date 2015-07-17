076e5e51b38fe50042414c600c094f72e6048858
Sean Girard <seangirard@yahoo.com>
2015-07-17 07:23:59 -0700

    footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,7 @@
 <footer class="content-info container" role="contentinfo">
   <div class="row">
     <div class="col-lg-12">
-      <?php dynamic_sidebar('sidebar-footer'); ?>
+      <?php //dynamic_sidebar('sidebar-footer'); ?>
       <!--
       <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
     	-->
