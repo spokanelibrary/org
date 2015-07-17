b2b25f17ebe1e86b64cd0b8b61c917d12448bbe0
Sean Girard <seangirard@yahoo.com>
2015-07-17 12:40:32 -0700

    staff

--- template-staff.php
+++ template-staff.php
@@ -32,7 +32,13 @@ class SPL_StaffDirectoryShortcode {
       ,'posts_per_page' => -1
     );
     $categories = get_categories($query_args);
-    return '<pre>'.print_r($categories,true).'</pre>';
+    foreach ( $categories as $c=>$category ) {
+      $menu[$category['category_parent']][] = $category; 
+    }
+
+    $menu = array();
+
+    return '<pre>'.print_r($menu,true).'</pre>'.'<pre>'.print_r($categories,true).'</pre>';
     /*
     $wp_query = new WP_Query($query_args);
     return '<pre>'.print_r($wp_query,true).'</pre>';
