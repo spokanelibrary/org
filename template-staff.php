cc8c7602eeec690d7af57fde8bcd4eaaac61ecda
Sean Girard <seangirard@yahoo.com>
2015-07-17 13:28:00 -0700

    staff

--- template-staff.php
+++ template-staff.php
@@ -60,15 +60,7 @@ class SPL_StaffDirectoryShortcode {
     
 
     return '<pre>'.print_r($menu,true).'</pre>';
-    /*
-    $wp_query = new WP_Query($query_args);
-    return '<pre>'.print_r($wp_query,true).'</pre>';
-    while($wp_query->have_posts()) {
-      $wp_query->the_post();
-      $name = get_the_title();
-      return '<pre>'.$name.'</pre>';
-    }
-    */
+
   }
 
   static function shortcode($params) {
