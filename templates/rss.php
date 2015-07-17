8ee412d882cb33380ca26e48600b618db045580f
Sean Girard <seangirard@yahoo.com>
2015-07-17 07:44:37 -0700

    rss

--- templates/rss.php
+++ templates/rss.php
@@ -1,5 +1,7 @@
+<?php if ( 'learn.spokanelibrary.org' != $_SERVER['SERVER_NAME'] && 'staff.spokanelibrary.org' != $_SERVER['SERVER_NAME'] ) : ?>
 <div class="pull-right" style="margin-top:10px;">
   <a class="btn btn-sm btn-default" href="./feed/"><img src="/assets/img/icons/16px/social-rss.png" style="margin-top: -2px;" alt="RSS feeds"> <b>RSS</b></a>
   &nbsp;
   <a class="btn btn-sm btn-default" href="/rss-feeds/"><img src="/assets/img/icons/16px/social-rss.png" style="margin-top: -2px;" alt="RSS feeds"> <b>More feeds</b></a>
 </div>
+<?php endif; ?>
