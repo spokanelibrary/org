3f633088e53c45b0ef280798f661d28e8927657d
Sean Girard <seangirard@yahoo.com>
2015-07-22 08:54:34 -0700

    archive

--- archive-newsletter.php
+++ archive-newsletter.php
@@ -5,7 +5,7 @@
   </h1>
 </div>
 
-<div class="panel panel-default" style="border-left-width:5px;">
+<div class="panel panel-default">
   <div class="panel-body">
 
 <?php while (have_posts()) : the_post(); ?>
