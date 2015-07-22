e8babe13050aa325824fb97205e93579b9c0c1bb
Sean Girard <seangirard@yahoo.com>
2015-07-22 08:50:54 -0700

    archive

--- archive-newsletter.php
+++ archive-newsletter.php
@@ -23,10 +23,12 @@
     ?>
 
     <h2 class="entry-title">
+      <!--
       <small>
         <time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date('F, Y'); ?></time>
       </small>
       <br>
+      -->
       <a class="text-success" href="<?php the_permalink(); ?>">
         <?php the_title(); ?>
       </a>
