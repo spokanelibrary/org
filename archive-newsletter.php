d4737179847176723230767c5544269854bc79bd
Sean Girard <seangirard@yahoo.com>
2015-07-22 14:51:31 -0700

    news

--- archive-newsletter.php
+++ archive-newsletter.php
@@ -1,33 +1,7 @@
 <div class="page-header">
   <h1>
-    Library News 
+    Library News
   </h1>
-</div>
-
-<div class="row">
-
-  <div class="col-md-6">
-    <p class="lead">
-      <b>We’ll keep you informed</b> of new service offerings, as well as upcoming events and activities at your local library.
-    </p>
-    <p class="lead">
-      Library staff and customers also suggest some great reading and listening ideas you won’t want to miss!
-    </p>
-  </div>
-  <div class="col-md-6">
-    <div class="panel spl-hero-panel spl-hero-news" style="">
-      <h4>
-        <i class="glyphicon glyphicon-bullhorn"></i>
-        In the current issue
-      </h4>
-      <div class="panel-body">
-      <?php echo do_shortcode('[spl_mailgun_current format=toc link_posts=true]'); ?>
-      <hr>
-      <?php echo do_shortcode('[spl_mailgun_current title="Read the current issue &rarr;" class="btn btn-block btn-success"]'); ?>
-      </div>
-    </div>
-  </div>
-
 </div>
 
 <h2 class="text-success">Read Past Issues</h2>
