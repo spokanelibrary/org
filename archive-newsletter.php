720befbd48958c228084c722b3a1539c1781b481
Sean Girard <seangirard@yahoo.com>
2015-07-28 12:13:00 -0700

    news

--- archive-newsletter.php
+++ archive-newsletter.php
@@ -99,7 +99,7 @@
           }
           $html .= '</ul>';
 
-          $html .= '<a class="btn btn-large btn-success" href="'.$article->link.'">Read this issue &rarr;</a>';
+          $html .= '<a class="btn btn-large btn-success" href="'.the_permalink().'">Read this issue &rarr;</a>';
 
           $html .= '</div>';
           $html .= '</div>';
