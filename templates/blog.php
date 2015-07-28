cc773d5d56b6a16f4cd71a63ea6ce7c028443f37
Sean Girard <seangirard@yahoo.com>
2015-07-28 10:17:30 -0700

    news

--- templates/blog.php
+++ templates/blog.php
@@ -1,3 +1,5 @@
+<?php $categories = get_the_category($post->ID); ?>
+<?php if ( !array_search('library-news', $categories) ) :; ?>
 <article <?php post_class(); ?>>
   <header>
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
@@ -9,7 +11,7 @@
 		  	<aside>
   				<ul class="nav nav-pills">
   				<?php
-			  	$categories = get_the_category($post->ID);
+			  	//$categories = get_the_category($post->ID);
 					if ( is_array($categories)) { 
 						foreach ( $categories as $category ) {
 							echo '<li>';
@@ -45,3 +47,4 @@
 	?>
   <hr>
 </article>
+<?php endif; ?>
