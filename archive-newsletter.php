256a5425c1eac65592920f740d603ee5a4c615f7
Sean Girard <seangirard@yahoo.com>
2015-07-28 15:46:45 -0700

    news

--- archive-newsletter.php
+++ archive-newsletter.php
@@ -4,7 +4,7 @@
   </h1>
 </div>
 
-<div class="panel panel-default">
+<div class="panel panel-default spl-hero-news">
   <div class="panel-heading">
     <div class="text-right">
         <i class="glyphicon glyphicon-envelope text-primary"></i>
