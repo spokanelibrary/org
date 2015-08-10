0a9fdb10b03a56d543ddd1db832ada3ad6b7c964
Sean Girard <seangirard@yahoo.com>
2015-08-10 10:49:37 -0700

    agree

--- templates/org/forms/field/signup/shared/card-apply.php
+++ templates/org/forms/field/signup/shared/card-apply.php
@@ -14,7 +14,7 @@
       <div class="checkbox">
         <p>
           <label>
-            <input type="checkbox" style="border:2px solid #222" class="required" id="spl-signup-agree" name="spl-form[signup][agree]" value="">
+            <input type="checkbox" style="transform:scale(2, 2);" class="required" id="spl-signup-agree" name="spl-form[signup][agree]" value="">
             <b class="text-success">I agree to these <i>Terms &amp; Conditions</b></i>.
           </label>
         </p>
