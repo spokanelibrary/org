701484670ce369744a69dc4b9e8cdfe9b82e1d90
Sean Girard <seangirard@yahoo.com>
2015-08-10 10:34:54 -0700

    birthday

--- assets/js/org/signup.js
+++ assets/js/org/signup.js
@@ -192,8 +192,8 @@ var org = {
       .done(function(data) {
         //console.log(data);
         if ( data.error ) {
-          $('#'+id).val('');
           $('#'+id).blur();
+          $('#'+id).val('');
         } else {
           $('#'+id).val(data);
         }
