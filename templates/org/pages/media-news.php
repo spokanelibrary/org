d17f7bc78bbbbbe3ff5f4b878a432ff381fcedd9
Sean Girard <seangirard@yahoo.com>
2015-10-09 12:41:53 -0700

    press

--- templates/org/pages/media-news.php
+++ templates/org/pages/media-news.php
@@ -53,10 +53,12 @@
 
 	<div class="col-md-3 col-lg-4">
 
+		<div class="affix">
 		<div class="alert alert-info" style="border: 4px solid #F79734;">
 			<h4 class="text-center">
 				<a href="/blog/topic/media/"><span>Library</span> News &amp; Press Archive</a> <small class="text-muted">&rarr;</small>
 			</h4>
+		</div>
 		</div>
 		
 	</div>
