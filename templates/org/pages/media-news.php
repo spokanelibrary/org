30cb1646a154eafaef3d52b73f660ec347f99641
Sean Girard <seangirard@yahoo.com>
2015-10-09 12:39:41 -0700

    pr

--- templates/org/pages/media-news.php
+++ templates/org/pages/media-news.php
@@ -54,9 +54,9 @@
 	<div class="col-md-3 col-lg-4">
 
 		<div class="">
-			<h4 class="text-center">
+			<h5 class="text-center">
 				<a href="/blog/topic/media/"><span>Library</span> News &amp; Press Archive</a> <small class="text-muted">&rarr;</small>
-			</h4>
+			</h5>
 		</div>
 		
 	</div>
