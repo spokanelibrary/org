0ee19fbe0eae61972f610050a8e9ceeb7cc89a9b
Sean Girard <seangirard@yahoo.com>
2015-10-13 15:05:28 -0700

    cp

--- template-intranet.php
+++ template-intranet.php
@@ -54,12 +54,10 @@ Template Name: Intranet Home Page
             Director's Notes</h4>
           </div>
           
-          <div class="" style="/*padding-top:20px; background-image:url('/assets/img/png/spl-tile-banner.png'); background-repeat:no-repeat;*/">
-            <div class="panel-body">
+          <div class="panel-body">
             <?php echo do_shortcode('[spl_widget category-posts slug=director count=1 format=full]'); ?>
             </div><!-- /.panel-body -->
-          </div>
-
+          
         </div><!-- /.panel -->
       </div><!-- /.col -->
 
