049ed8d93bcdb6879920249ab4661590688644c9
Sean Girard <seangirard@yahoo.com>
2015-10-14 10:58:31 -0700

    sd

--- templates/content.php
+++ templates/content.php
@@ -3,7 +3,6 @@
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
   </header>
   <div class="entry-summary">
-  	test
     <?php the_excerpt(); ?>
   	<?php //get_template_part('templates/entry-meta'); ?>
   </div>
