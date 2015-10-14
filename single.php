6424d0dbb812cdbb6d450177101a9beef6cf972c
Sean Girard <seangirard@yahoo.com>
2015-10-14 11:22:10 -0700

    sd

--- single.php
+++ single.php
@@ -1,1 +1,5 @@
+<?php if ( 'staff.spokanelibrary.org' == $_SERVER['SERVER_NAME'] ) :; ?>
+  Single post on staff site
+<?PHP else: ?>
 <?php get_template_part('templates/content', 'single'); ?>
+<?php endif;?>
