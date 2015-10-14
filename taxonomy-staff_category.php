e84eaef606e99d82e1f7d621933e93b72c8265d9
Sean Girard <seangirard@yahoo.com>
2015-10-14 12:29:32 -0700

    sd

--- taxonomy-staff_category.php
+++ taxonomy-staff_category.php
@@ -11,42 +11,58 @@
   $name = get_the_title();
   $position = get_post_meta(get_the_ID(), 'position', true);
   $bio = get_the_content();
+  
+  $staff_categories = wp_get_post_terms(get_the_ID(), 'staff_category');
+  if (count($staff_categories) > 0) {
+    //$staff_category = $staff_categories[0]->name;
+    foreach ( $staff_categories as $category ) {
+      $staff_category .= $category->name.', ';
+    }
+    $staff_category = rtrim($staff_category, ', ');
+  } else {
+    $staff_category = "";
+  }
+
   if(has_post_thumbnail()) {
     $attachment_array = wp_get_attachment_image_src(get_post_thumbnail_id());
     $photo_url = $attachment_array[0];
-    $photo_html = '<div class="photo"><img src="' . $photo_url . '" /></div>';
+    //$photo_html = '<img src='e;
   } else {
+    $photo_url = '';
     $photo_html = '';
   }
+  
   if(get_post_meta(get_the_ID(), 'email', true) != '') {
     $email = get_post_meta(get_the_ID(), 'email', true);
-    $email_html = '<div class="email">Email: <a href="mailto:' . $email . '">' . $email . '</a></div>';
+    $email_html = '<a href="mailto:' . $email . '">' . $email . '</a>';
   } else {
+    $email = '';
     $email_html = '';
   }
+  
   if(get_post_meta(get_the_ID(), 'phone_number', true) != '') {
     $phone = get_post_meta(get_the_ID(), 'phone_number', true);
   } else {
     $phone = '';
   }
+
   if(get_post_meta(get_the_ID(), 'website', true) != '') {
     $website = get_post_meta(get_the_ID(), 'website', true);
     $website_html = '<div class="website">Website: <a href="' . $website . '">' . $website . '</a></div>';
   } else {
     $website_html = '';
   }
+
   ?>
 
   <div class="staff-directory">
 
       <div class="media-object">
       <h3><a href="<?php echo $permalink; ?>"><?php echo $name; ?></a> <small>x<?php echo $phone; ?></small></h3>
-      <h4>[position]</h4>
-      [photo]
-      <h5>[category]</h5>
+      <h4><?php echo $name; ?></h4>
+      <?php echo $photo_url; ?>
+      <h5><?php echo $staff_category; ?></h5>
       </div>
-
-      <?php echo get_post_meta(get_the_ID(), 'position', true) ?>
 
   </div>
 
