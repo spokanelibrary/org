7d35311897fe7a173e08d61dad5cb084b520f3e1
Sean Girard <seangirard@yahoo.com>
2015-10-14 12:24:15 -0700

    sd

--- taxonomy-staff_category.php
+++ taxonomy-staff_category.php
@@ -6,11 +6,41 @@
 <?php while (have_posts()) : the_post(); ?>
 
 <article <?php post_class(); ?>>
+  <?php
+  $permalink = get_the_permalink();
+  $name = get_the_title();
+  $position = get_post_meta(get_the_ID(), 'position', true);
+  $bio = get_the_content();
+  if(has_post_thumbnail()) {
+    $attachment_array = wp_get_attachment_image_src(get_post_thumbnail_id());
+    $photo_url = $attachment_array[0];
+    $photo_html = '<div class="photo"><img src="' . $photo_url . '" /></div>';
+  } else {
+    $photo_html = '';
+  }
+  if(get_post_meta(get_the_ID(), 'email', true) != '') {
+    $email = get_post_meta(get_the_ID(), 'email', true);
+    $email_html = '<div class="email">Email: <a href="mailto:' . $email . '">' . $email . '</a></div>';
+  } else {
+    $email_html = '';
+  }
+  if(get_post_meta(get_the_ID(), 'phone', true) != '') {
+    $phone = get_post_meta(get_the_ID(), 'phone', true);
+  } else {
+    $phone = '';
+  }
+  if(get_post_meta(get_the_ID(), 'website', true) != '') {
+    $website = get_post_meta(get_the_ID(), 'website', true);
+    $website_html = '<div class="website">Website: <a href="' . $website . '">' . $website . '</a></div>';
+  } else {
+    $website_html = '';
+  }
+  ?>
 
   <div class="staff-directory">
 
       <div class="media-object">
-      <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> <small>x[phone_number]</small></h3>
+      <h3><a href="<?php echo $permalink; ?>"><?php echo $name; ?></a> <small>x<?php echo $phone; ?></small></h3>
       <h4>[position]</h4>
       [photo]
       <h5>[category]</h5>
@@ -18,23 +48,8 @@
 
       <?php echo get_post_meta(get_the_ID(), 'position', true) ?>
 
-
-      <?php //print_r( get_post_meta( get_the_ID(), 'phone_number', true ) ); ?>
   </div>
 
-  <header>
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-  </header>
-  <div class="entry-summary">
-  <?php if ( has_post_thumbnail() ) : ?>
-    <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-    <?php the_post_thumbnail('thumbnail', array( 'class' => 'img-rounded' )); ?>
-    </a>
-  <?php endif; ?>
-    <?php //the_excerpt(); ?>
-    <?php //get_template_part('templates/entry-meta'); ?>
-  </div>
-  <hr>
 </article>
 
 <?php endwhile; ?>
