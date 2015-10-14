df8ec53082dc7c1de1ee28599fe02883a6e14735
Sean Girard <seangirard@yahoo.com>
2015-10-14 11:55:40 -0700

    test

--- taxonomy-staff_category.php
+++ taxonomy-staff_category.php
@@ -1,5 +1,7 @@
 <?php get_template_part('templates/page', 'header'); ?>
 
+<hr>
+
 <?php while (have_posts()) : the_post(); ?>
 
 <article <?php post_class(); ?>>
@@ -8,7 +10,7 @@
   </header>
   <div class="entry-summary">
     <?php the_excerpt(); ?>
-    <?php //get_template_part('templates/entry-meta'); ?>
+    <?php get_template_part('templates/entry-meta'); ?>
   </div>
   <hr>
 </article>
