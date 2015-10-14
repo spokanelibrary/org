7b358692b8534399b414eb07a84f942806a6af43
Sean Girard <seangirard@yahoo.com>
2015-10-14 11:59:56 -0700

    sd

--- taxonomy-staff_category.php
+++ taxonomy-staff_category.php
@@ -9,11 +9,11 @@
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
   </header>
   <div class="entry-summary">
-  <?php
-    if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-  the_post_thumbnail();
-} 
-?>
+  <?php if ( has_post_thumbnail() ) : ?>
+    <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+    <?php the_post_thumbnail('thumbnail'); ?>
+    </a>
+  <?php endif; ?>
     <?php //the_excerpt(); ?>
     <?php //get_template_part('templates/entry-meta'); ?>
   </div>
