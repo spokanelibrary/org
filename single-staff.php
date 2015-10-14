ca3b3845727c107459aeafca49e31bfaa974b92e
Sean Girard <seangirard@yahoo.com>
2015-10-14 13:32:06 -0700

    sd

--- single-staff.php
+++ single-staff.php
@@ -51,7 +51,7 @@
     <div class="panel-heading" style="padding-left:0; padding-right:0;">
       <h2 class="text-primary"><?php echo $name; ?> <small class="text-success">x<b><?php echo $phone; ?></b></small></h2>
     </div>
-    <div class="panel-body" style="padding-top:0;">
+    <div class="panel-body">
       <h3><?php echo $position; ?></h3>
       <h4><?php echo $staff_category; ?></h4>
       <div class="row">
