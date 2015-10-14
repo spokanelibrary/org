4d42f59f0269206b898551559c74765084a9eccd
Sean Girard <seangirard@yahoo.com>
2015-10-14 11:25:28 -0700

    sd

--- templates/content-staff-single.php
+++ templates/content-staff-single.php
@@ -13,15 +13,5 @@
     <hr>
     <?php //comments_template('/templates/comments.php'); ?>
   </article>
-  <div class="panel panel-primary">
-    <div class="panel-heading">
-      <h4>Find more on the library blog&hellip;</h4>
-    </div>
-    <div class="panel-body">
-    <?php wp_tag_cloud( array('taxonomy' => array('post_tag', 'category')) ); ?>  
-    </div>
-    <div class="panel-footer">
-      <h4 class="text-right"><a href="/blog/">All blog posts &rarr;</a></h4>
-    </div>
-  </div>
+  
 <?php endwhile; ?>
