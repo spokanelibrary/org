adfe28b9923cf19f0b1516caa6feb2dfea8b21c8
Sean Girard <seangirard@yahoo.com>
2015-10-15 12:59:38 -0700

    kb

---
+++ wp_knowledgebase/single-kbe_knowledgebase.php
@@ -0,0 +1,14 @@
+<?php while(have_posts()) : ?>
+<?php the_post(); ?>
+<?php kbe_set_post_views(get_the_ID()); ?>
+
+<div class="page-header">
+  <h1>
+    <?php the_title(); ?>
+  </h1>
+</div>
+
+<?php the_content(); ?>
+
+<?php endwhile; ?>
+<?php kbe_get_post_views(get_the_ID()); ?>
