0490d7331a55c87d727b6dabad4d03d19567a2c9
Sean Girard <seangirard@yahoo.com>
2015-10-15 14:09:22 -0700

    kb

--- wp_knowledgebase/single-kbe_knowledgebase.php
+++ wp_knowledgebase/single-kbe_knowledgebase.php
@@ -8,11 +8,24 @@
   </h1>
 </div>
 
-<pre>
-<?php print_r(wp_get_post_terms(get_the_ID(), 'kbe_taxonomy')); ?>
-</pre>
-
-<?php the_content(); ?>
+<div class="row">
+  <div class="col-md-8 col-md-9">
+    <?php the_content(); ?>
+  </div><!-- /.col -->
+  <div class="col-md-4 col-lg-3">
+    <pre>
+    <?php print_r(wp_get_post_terms(get_the_ID(), 'kbe_taxonomy')); ?>
+    </pre>
+  </div><!-- /.col -->
+</div><!-- /.row -->
 
 <?php endwhile; ?>
 <?php kbe_get_post_views(get_the_ID()); ?>
+
+<?php
+
+function spl_kbe_get_related_articles_by_id($id) {
+
+}
+
+?>
