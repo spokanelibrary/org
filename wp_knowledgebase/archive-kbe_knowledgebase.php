b728da6d97b7188678fa11b232321ed2866d537d
Sean Girard <seangirard@yahoo.com>
2015-10-15 08:35:51 -0700

    kb

--- wp_knowledgebase/archive-kbe_knowledgebase.php
+++ wp_knowledgebase/archive-kbe_knowledgebase.php
@@ -73,7 +73,8 @@ foreach($kbe_terms as $kbe_taxonomy) :
                                                             array(
                                                                     'taxonomy' => KBE_POST_TAXONOMY,
                                                                     'field' => 'term_id',
-                                                                    'terms' => $kbe_child_term_id
+                                                                    //'terms' => $kbe_child_term_id
+                                                                    'terms' => $kbe_term_id
                                                             )
                                                     )
                                             );
