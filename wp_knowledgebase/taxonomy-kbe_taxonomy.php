5d0a66e31eb3482722e9928c8b2b45f580534eb6
Sean Girard <seangirard@yahoo.com>
2015-10-15 13:24:49 -0700

    kb

--- wp_knowledgebase/taxonomy-kbe_taxonomy.php
+++ wp_knowledgebase/taxonomy-kbe_taxonomy.php
@@ -17,7 +17,6 @@ $spl_kbe_cat_name = get_queried_object()->name;
   </div><!-- /.col -->
 </div><!-- /.row -->
 
-<?php exit; ?>
 
 <?php
 
@@ -59,44 +58,3 @@ function spl_kbe_get_kb_list_by_slug($slug) {
 }
 
 ?>
-
-
-<?php
-// Query for Category
-$kbe_cat_slug = get_queried_object()->slug;
-$kbe_cat_name = get_queried_object()->name;
-
-$kbe_tax_post_args = array(
-    'post_type' => KBE_POST_TYPE,
-    'posts_per_page' => 999,
-    'orderby' => 'menu_order',
-    'order' => 'ASC',
-    'tax_query' => array(
-        array(
-            'taxonomy' => KBE_POST_TAXONOMY,
-            'field' => 'slug',
-            'terms' => $kbe_cat_slug
-        )
-    )
-);
-$kbe_tax_post_qry = new WP_Query($kbe_tax_post_args);
-?>
-
-<h2><strong><?php echo $kbe_cat_name; ?></strong></h2>
-
-<ul>
-<?php
-if($kbe_tax_post_qry->have_posts()) :
-while($kbe_tax_post_qry->have_posts()) :
-$kbe_tax_post_qry->the_post();
-?>
-<li>
-    <a href="<?php the_permalink(); ?>">
-        <?php the_title(); ?>
-    </a>
-</li>
-<?php
-endwhile;
-endif;
-?>
-</ul>
