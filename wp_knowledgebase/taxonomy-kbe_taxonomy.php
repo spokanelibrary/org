e11bed4977703e1bc2c490984372fafaba835b8f
Sean Girard <seangirard@yahoo.com>
2015-10-15 13:04:55 -0700

    kb

--- wp_knowledgebase/taxonomy-kbe_taxonomy.php
+++ wp_knowledgebase/taxonomy-kbe_taxonomy.php
@@ -8,51 +8,41 @@
 
 
 <?php
-    //get_header();
+// Query for Category
+$kbe_cat_slug = get_queried_object()->slug;
+$kbe_cat_name = get_queried_object()->name;
 
-    // Query for Category
-    $kbe_cat_slug = get_queried_object()->slug;
-    $kbe_cat_name = get_queried_object()->name;
-    
-    $kbe_tax_post_args = array(
-        'post_type' => KBE_POST_TYPE,
-        'posts_per_page' => 999,
-        'orderby' => 'menu_order',
-        'order' => 'ASC',
-        'tax_query' => array(
-            array(
-                'taxonomy' => KBE_POST_TAXONOMY,
-                'field' => 'slug',
-                'terms' => $kbe_cat_slug
-            )
+$kbe_tax_post_args = array(
+    'post_type' => KBE_POST_TYPE,
+    'posts_per_page' => 999,
+    'orderby' => 'menu_order',
+    'order' => 'ASC',
+    'tax_query' => array(
+        array(
+            'taxonomy' => KBE_POST_TAXONOMY,
+            'field' => 'slug',
+            'terms' => $kbe_cat_slug
         )
-    );
-    $kbe_tax_post_qry = new WP_Query($kbe_tax_post_args);
+    )
+);
+$kbe_tax_post_qry = new WP_Query($kbe_tax_post_args);
 ?>
-        <div class="kbe_leftcol">
 
-            <!--<articles>-->
-            <div class="kbe_articles">
-                <h2><strong><?php echo $kbe_cat_name; ?></strong></h2>
+<h2><strong><?php echo $kbe_cat_name; ?></strong></h2>
 
-                <ul>
-            <?php
-                if($kbe_tax_post_qry->have_posts()) :
-                    while($kbe_tax_post_qry->have_posts()) :
-                        $kbe_tax_post_qry->the_post();
-            ?>
-                        <li>
-                            <a href="<?php the_permalink(); ?>">
-                                <?php the_title(); ?>
-                            </a>
-                        </li>
-            <?php
-                    endwhile;
-                endif;
-            ?>
-                </ul>
-
-            </div>
-        </div>
-        <!--/leftcol-->
-
+<ul>
+<?php
+if($kbe_tax_post_qry->have_posts()) :
+while($kbe_tax_post_qry->have_posts()) :
+$kbe_tax_post_qry->the_post();
+?>
+<li>
+    <a href="<?php the_permalink(); ?>">
+        <?php the_title(); ?>
+    </a>
+</li>
+<?php
+endwhile;
+endif;
+?>
+</ul>
