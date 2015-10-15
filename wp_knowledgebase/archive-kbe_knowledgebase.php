b2d66be7ca96c17f28a5064103c22b1a38006284
Sean Girard <seangirard@yahoo.com>
2015-10-15 12:03:46 -0700

    kb

--- wp_knowledgebase/archive-kbe_knowledgebase.php
+++ wp_knowledgebase/archive-kbe_knowledgebase.php
@@ -132,7 +132,7 @@ function spl_kbe_get_kb_cat_by_parent_id($id=0) {
       $html .= '</h3>';
       //$html .= '<pre>'.print_r($term, true).'</pre>';
       $html .= spl_kbe_get_kb_list_by_term_id($term->term_id);
-      $html .= spl_kbe_get_kb_cat_by_parent_id($term->term_id)
+      $html .= spl_kbe_get_kb_cat_by_parent_id($term->term_id);
       $html .= '</div>'; 
     }
     $html .= '</div>';
