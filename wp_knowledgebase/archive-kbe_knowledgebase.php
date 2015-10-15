042eb8a993e226ca900a47c12dda369fcb639baa
Sean Girard <seangirard@yahoo.com>
2015-10-15 11:13:47 -0700

    kb

--- wp_knowledgebase/archive-kbe_knowledgebase.php
+++ wp_knowledgebase/archive-kbe_knowledgebase.php
@@ -13,8 +13,9 @@ $kbe_cat_args = array(
                     'parent'        => 0
                 );
 $kbe_terms = get_terms(KBE_POST_TAXONOMY, $kbe_cat_args);
+?>
 
-foreach($kbe_terms as $kbe_taxonomy) : ?>
+<?php foreach($kbe_terms as $kbe_taxonomy) : ?>
   <div class="col-md-6">
     <h3>
       <a href="<?php echo get_term_link($kbe_taxonomy->slug, 'kbe_taxonomy') ?>">
@@ -35,7 +36,7 @@ foreach($kbe_terms as $kbe_taxonomy) : ?>
                           'hide_empty'    => true, 
                       );
   $kbe_child_terms = get_terms(KBE_POST_TAXONOMY, $kbe_child_cat_args);
-
+  ?>
 
   if($kbe_child_terms) :
   ?>
