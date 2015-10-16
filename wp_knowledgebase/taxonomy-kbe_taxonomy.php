1872d2c34e62a518ca0196c892c240bffe9b74c0
Sean Girard <seangirard@yahoo.com>
2015-10-16 07:49:04 -0700

    kb

--- wp_knowledgebase/taxonomy-kbe_taxonomy.php
+++ wp_knowledgebase/taxonomy-kbe_taxonomy.php
@@ -39,13 +39,14 @@ function spl_kbe_get_kb_cat_by_term_id($id) {
     foreach ( $terms as $term ) {
       $html .= '<li>';
       $html .= '<h5 class="uppercase">';
-      $html .= '<a href="'.get_term_link($term->slug, 'kbe_taxonomy').'">';
-      $html .= $term->name;
-      $html .= '</a>';
       $html .= '<span class="label label-warning pull-right">';
       $html .= $term->count;
       $html .= '</span>';
+      $html .= '<a href="'.get_term_link($term->slug, 'kbe_taxonomy').'">';
+      $html .= $term->name;
+      $html .= '</a>';
       $html .= '</h5>';
+      $html .= '<div class="clearfix"></div>';
       //$html .= spl_kbe_get_kb_list_by_term_id($term->term_id);
       $html .= spl_kbe_get_kb_cat_by_term_id($term->term_id);
       $html .= '</li>';
