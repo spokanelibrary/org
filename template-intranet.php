b311cf44c71b7a99a781269b109b71629a1aa7fd
Sean Girard <seangirard@yahoo.com>
2015-10-16 13:33:41 -0700

    sm

--- template-intranet.php
+++ template-intranet.php
@@ -89,7 +89,7 @@ Template Name: Intranet Home Page
 
       <div class="clearfix"></div>
 
-      <div class="col-md-6">
+      <div class="col-sm-6">
         <div class="panel spl-hero-intranet spl-hero-brand-blue-b">
           <div class="panel-heading">
             <h4>
@@ -106,7 +106,7 @@ Template Name: Intranet Home Page
         </div><!-- /.panel -->
       </div><!-- /.col -->
 
-      <div class="col-md-6">
+      <div class="col-sm-6">
         <div class="panel spl-hero-intranet spl-hero-brand-green-a">
           <div class="panel-heading">
             <h4>
@@ -124,7 +124,7 @@ Template Name: Intranet Home Page
 
       <div class="clearfix"></div>
 
-      <div class="col-md-6">
+      <div class="col-sm-6">
         <div class="panel spl-hero-intranet spl-hero-brand-orange-a">
           <div class="panel-heading">
             <h4>
@@ -140,7 +140,7 @@ Template Name: Intranet Home Page
         </div><!-- /.panel -->
       </div><!-- /.col -->
 
-      <div class="col-md-6">
+      <div class="col-sm-6">
         <div class="panel spl-hero-intranet spl-hero-brand-purple-a">
           <div class="panel-heading">
             <h4>
