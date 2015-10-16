32d2627bf9df6e226f77f7af6cb9d964b0dc0b5c
Sean Girard <seangirard@yahoo.com>
2015-10-16 12:00:06 -0700

    spl

--- templates/content-staff-single.php
+++ templates/content-staff-single.php
@@ -33,9 +33,9 @@
       </h4>
       <hr>
       <h3 class="text-success serif"><?php echo get_the_date();?></h3>
-      <h4 class="byline author vcard"><?php echo __('~', 'roots'); ?> <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><?php echo get_the_author(); ?></a></h4>
-      
       <?php the_content(); ?>
+      
+      <h5 class="byline author vcard"><?php echo __('~', 'roots'); ?> <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><?php echo get_the_author(); ?></a></h5>
 
       <?php //get_template_part('templates/entry-meta'); ?>
     </div>
