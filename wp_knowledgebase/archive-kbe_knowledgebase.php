47febf0bdd9f3260691670708b5d14a64f4b9192
Sean Girard <seangirard@yahoo.com>
2015-10-16 09:05:57 -0700

    kb

--- wp_knowledgebase/archive-kbe_knowledgebase.php
+++ wp_knowledgebase/archive-kbe_knowledgebase.php
@@ -25,6 +25,9 @@ function spl_kbe_get_kb_category($id=0) {
       $html .= '<div class="col-md-6">';
       $html .= '<div class="panel spl-hero-intranet spl-hero-brand-blue-c">';
       $html .= '<div class="panel-heading">';
+      $html .= '<h4 class="uppercase">';
+      $html .= '<i class="glyphicon glyphicon-list-alt"></i>';
+      $html .= '</h4>';
       //$html .= '<h4 class="uppercase">';
       //$html .= '<a href="'.get_term_link($term->slug, 'kbe_taxonomy').'">';
       //$html .= $term->name;
