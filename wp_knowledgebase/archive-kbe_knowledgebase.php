7f400eeb9de7ad1354f7da7cb3b6be407c1be84a
Sean Girard <seangirard@yahoo.com>
2015-10-23 16:02:58 -0700

    kb

--- wp_knowledgebase/archive-kbe_knowledgebase.php
+++ wp_knowledgebase/archive-kbe_knowledgebase.php
@@ -43,6 +43,7 @@ function spl_kbe_get_kb_category($id=0) {
       $html .= '</h4>';
       $html .= '</div>';  
       $html .= '<div class="panel-body">'; 
+      $html .= '<pre>'.print_r($category, true).'</pre>';
       //$html .= '<h4 class="uppercase">';
       //$html .= '<a class="" href="'.get_term_link($term->slug, 'kbe_taxonomy').'">';
       //$html .= 'View all articles <small>&rarr;</small>';
