07c6283a1e3198eaa6ced4b2f08e0e7baab8b918
Sean Girard <seangirard@yahoo.com>
2015-10-23 15:42:16 -0700

    cell

--- single-staff.php
+++ single-staff.php
@@ -57,8 +57,8 @@
   <div class="panel spl-hero-intranet staff-directory" style="box-shadow:none;">
     <div class="panel-heading" style="padding-left:0; padding-right:0;">
       <h2 class="text-primary" style="margin-top:0;"><?php echo $name; ?> 
-        <small>x<b class="text-success"><?php echo $phone; ?></b>
-        <?php if ( isset($cell) ) : ?>
+        <small>x <b class="text-success"><?php echo $phone; ?></b>
+        <?php if ( !empty($cell) ) : ?>
         &nbsp;&nbsp;<span class="">cell: <b class="text-success"><?php echo $cell; ?></b></span>
         <?php endif; ?>
         </small>
