5797c925cc4e38d5be5d3ba3e692b3e20118e26b
Sean Girard <seangirard@yahoo.com>
2015-10-27 14:41:47 -0700

    digest

---
+++ templates/org/pages/news-digest.php
@@ -0,0 +1,44 @@
+<div class="row">
+	<div class="col-lg-10 col-lg-offset-1">
+		<div class="spl-news spl-news-digest" style="margin-top: 0px;">
+
+
+				<div class="panel spl-hero-intranet spl-hero-brand-blue-e" style="">
+		
+			    <div class="panel-heading">
+			      <h3 style="margin-top:8px;">			        
+			        Library News 
+			        <small class="uppercase">
+			        Mid-October, 2015 
+			        </small>
+			      </h3>
+			    </div>
+					
+			    <div class="panel-body">
+			    	<h1>Introducing Statista: The Statistics Portal</h1>
+
+			    	<img class="aligncenter" style="max-width: 600px;" src="https://d28wbuch0jlv7v.cloudfront.net/images/infografik/normal/chartoftheday_3512_internet_adoption_in_2015_n.jpg" alt="Infographic: The Digital Divide Is Still a Thing | Statista" />
+
+						<hr />
+
+						<h3>Friends of the Library Fall Booksale is coming up</h3>
+						<h5>Why you should attend, the by the numbers:</h5>
+						<ul>
+							<li>You might take home <em>Barns of California</em>, <em>Dinosaurs Like Underpants</em>, and <em>Angle of Repose</em> all for less than $1.</li>
+							<li>33% of high school graduates will never read another book after high school - don’t let this happen to you!</li>
+							<li>There will be more than 10,000 books available. Be early and choose wisely; you can’t carry them all home.</li>
+							<li>100% of the proceeds are reinvested in the library to <a href="#">support important initiatives</a>.</li>
+						</ul>
+						The Friends of the Library Fall Booksale will be held Downtown, October 21 - 24th.
+
+						<a class="btn btn-warning" href="#"><b>Learn more:</b> Get dates, times, and more information &rarr;</a>
+
+						<hr>
+
+			    </div>
+
+			  </div>
+
+		</div>
+	</div>
+</div>
