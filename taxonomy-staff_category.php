8f38d2665275202d099ba0a4f4189ee156ff6532
Sean Girard <seangirard@yahoo.com>
2015-11-04 14:16:19 -0800

    staff

--- taxonomy-staff_category.php
+++ taxonomy-staff_category.php
@@ -55,7 +55,7 @@
 
   ?>
 
-  <div class="panel spl-hero-intranet staff-directory" style="box-shadow:none;">
+  <div class="panel spl-hero-intranet " style="">
     <div class="panel-heading" style="padding-left:0; padding-right:0;">
       <h3><a href="<?php echo $permalink; ?>"><?php echo $name; ?></a> 
         <small>x<b class="text-success"><?php echo $phone; ?></b>
