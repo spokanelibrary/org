526bb1d28dea782b821ed27f3905638f257a2517
Sean Girard <seangirard@yahoo.com>
2015-11-10 09:00:07 -0800

    staff

--- template-intranet.php
+++ template-intranet.php
@@ -162,21 +162,102 @@ Template Name: Intranet Home Page
 <div class="row">
 
   <div class="col-sm-6 col-md-3 col-md-push-9 col-lg-3 col-lg-push-9">
-    <div style="border:1px solid black; height: 300px;">
-     bulletin
-   </div>
+    <div class="panel spl-hero-intranet spl-hero-brand-blue-b hidden-xs">
+      <div class="panel-heading">
+        <h4>
+        <i class="glyphicon glyphicon-list"></i>
+        Help Desk</h4>
+      </div><!-- /.panel-heading -->
+      <div class="panel-body">
+          <ul class="list-unstyled">
+          <?php 
+          wp_nav_menu( 
+          array(
+            'menu' => 'Help Desk'
+          , 'container' => false
+          , 'items_wrap' => '%3$s'
+          )); 
+          ?>
+          </ul>            
+      </div><!-- /.panel-body -->
+    </div><!-- /.panel -->
+
+    <div class="panel spl-hero-intranet spl-hero-brand-gray-a">
+      <div class="panel-heading">
+        <h4>
+        <i class="glyphicon glyphicon-cog"></i>
+        Tech Bulletins</h4>
+      </div><!-- /.panel-heading -->
+      <div class="panel-body">
+        <?php echo do_shortcode('[spl_widget category-posts slug=tech count=3 format=excerpt size=sm]'); ?>
+      </div><!-- /.panel-body -->
+      <div class="panel-footer text-right">
+        <a class="" href="/news/tech/"><b>More</b> Tech Bulletins</a>&nbsp;<small class="text-muted">&rarr;</small> 
+      </div><!-- /.panel-footer -->
+    </div><!-- /.panel -->
+
+    <div class="panel spl-hero-intranet spl-hero-brand-purple-a">
+      <div class="panel-heading">
+        <h4>
+        <i class="glyphicon glyphicon-edit"></i>
+        Customer Service Tips</h4>
+      </div><!-- /.panel-heading -->
+      <div class="panel-body">
+        <?php echo do_shortcode('[spl_widget category-posts slug=cs count=3 format=excerpt size=sm]'); ?>
+      </div><!-- /.panel-body -->
+      <div class="panel-footer text-right">
+        <a class="" href="/news/cs/"><b>More</b> Customer Service Tips</a>&nbsp;<small class="text-muted">&rarr;</small> 
+      </div><!-- /.panel-footer -->
+    </div><!-- /.panel -->
   </div>
 
   <div class="hidden-xs col-sm-6 col-md-3 col-md-pull-3 col-lg-2 col-lg-pull-3">
-    <div style="border:1px solid black; height: 300px;">
-      events
-    </div>
+    <div class="panel spl-hero-intranet spl-hero-brand-blue-e">
+      <div class="panel-heading">
+        <h4>
+        <i class="glyphicon glyphicon-calendar"></i>
+        SPL Today</h4>
+      </div><!-- /.panel-heading -->
+      <div class="panel-body">            
+        <h4 style="margin-bottom:6px;">
+          <i class="glyphicon glyphicon-star text-success"></i>
+          <a href="http://www.spokanelibrary.org/">Facebook</a>
+        </h4>
+        <?php echo do_shortcode('[spl_widget home-page facebook refresh]'); ?>
+        <h4 style="margin-bottom:6px;">
+          <i class="glyphicon glyphicon-star text-success"></i>
+          <a href="http://www.spokanelibrary.org/">Activities</a>
+        </h4>
+        <script src="https://www.trumba.com/scripts/spuds.js"></script>
+        <script type="text/javascript">
+        $Trumba.addSpud({
+          webName: "spls-calendar",
+          spudType : "upcoming",
+          teaserBase : "http://www.spokanelibrary.org/calendar/",
+          url: { }
+        });
+        </script>
+      </div><!-- /.panel-body -->
+      <div class="panel-footer text-right">
+        <a class="" href="http://www.spokanelibrary.org/calendar"><b>More</b> Events</a>&nbsp;<small class="text-muted">&rarr;</small> 
+      </div><!-- /.panel-footer -->
+    </div><!-- /.panel -->
   </div>
   
   <div class="col-sm-12 col-md-6 col-md-pull-3 col-lg-7 col-lg-pull-3">
-    <div style="border:1px solid black; height: 600px;">
-      posts
-    </div>
+    <div class="panel spl-hero-intranet spl-hero-brand-blue-h">
+      <div class="panel-heading">
+        <h4>
+        <i class="glyphicon glyphicon-search"></i>
+        The Lens <small class="uppercase pull-right hidden-xs" style="color:#fff;">Library Employee News Service</small></h4>
+      </div><!-- /.panel-heading -->
+      <div class="panel-body">            
+        <?php echo do_shortcode('[spl_widget category-posts slug=spl count=6 format=full author]'); ?>
+      </div><!-- /.panel-body -->
+      <div class="panel-footer text-right">
+        <a class="" href="/news/spl/"><b>More</b> Library News &amp; Updates</a>&nbsp;<small class="text-muted">&rarr;</small> 
+      </div><!-- /.panel-footer -->
+    </div><!-- /.panel -->
   </div>
   
 </div>
