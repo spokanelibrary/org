f2668680d433613431462d8315a2b8d62910e31e
Sean Girard <seangirard@yahoo.com>
2015-11-10 08:27:22 -0800

    staff

--- template-intranet.php
+++ template-intranet.php
@@ -167,7 +167,7 @@ Template Name: Intranet Home Page
    </div>
   </div>
 
-  <div class="col-lg-2 col-lg-pull-10">
+  <div class="col-lg-2 col-lg-pull-6">
     <div style="border:1px solid black; height: 300px;">
       events
     </div>
