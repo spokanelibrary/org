28c30906a9cf66257a6b3fe4d44c940c53c60e99
Sean Girard <seangirard@yahoo.com>
2015-11-11 13:34:21 -0800

    comments

--- template-comments.php
+++ template-comments.php
@@ -8,14 +8,13 @@ Template Name: Comments
 <div class="row">
 
 	<div class="col-sm-12">
-
+		<?php get_template_part('templates/content', 'page'); ?>
+		
 		<div class="panel spl-hero-intranet spl-hero-brand-blue-a">
 			<div class="panel-heading">
 				<h4><?php the_title(); ?></h4>
 			</div>
 			<div class="panel-body">
-				<?php get_template_part('templates/content', 'page'); ?>
-
 				<div class="spl-staff-comments">
 					<?php comments_template('/templates/comments.php'); ?>
 				</div>
