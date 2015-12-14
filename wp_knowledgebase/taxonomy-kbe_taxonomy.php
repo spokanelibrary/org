91ec963645e09b807cbe3b2dfe257b3432a84271
Sean Girard <seangirard@yahoo.com>
2015-12-14 08:43:48 -0800

    kb

--- wp_knowledgebase/taxonomy-kbe_taxonomy.php
+++ wp_knowledgebase/taxonomy-kbe_taxonomy.php
@@ -32,8 +32,8 @@
 function spl_kbe_get_kb_cat_by_term_id($id) {
   $html = null;
   $args = array(
-                //'orderby'       => 'terms_order', 
-                'orderby'       => 'title', 
+                'orderby'       => 'terms_order', 
+                //'orderby'       => 'title', 
                 'order'         => 'ASC',
                 'hide_empty'    => true,
                 'parent'        => $id
@@ -47,7 +47,7 @@ function spl_kbe_get_kb_cat_by_term_id($id) {
       $html .= '<li>';
       $html .= '<b class="uppercase">';
       $html .= '<span class="label label-warning pull-right">';
-      $html .= $term->count;
+      $html .= $term->count . ' test';
       $html .= '</span>';
       $html .= '<small>';
       $html .= '<a href="'.get_term_link($term->slug, 'kbe_taxonomy').'">';
