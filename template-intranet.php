8b588290bfbadc52252e482e2bce011255370a7f
Sean Girard <seangirard@yahoo.com>
2016-02-02 14:37:29 -0800

    minutes

--- template-intranet.php
+++ template-intranet.php
@@ -81,7 +81,9 @@ Template Name: Intranet Home Page
       <div class="panel-body">
         <a class="btn btn-inverse btn-block" 
           href="http://files.spokanelibrary.org/Meeting-Minutes/">
-          Find Meeting Minutes &rarr;</a>
+          Browse Meeting Minutes &rarr;</a>
+          <hr>
+          
         <?php echo do_shortcode('[spl_widget category-posts slug=meeting-minutes count=3 format=excerpt size=sm]'); ?>
       </div><!-- /.panel-body -->
       <div class="panel-footer text-right">
