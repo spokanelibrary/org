eb590679ed3261bce174749f2da306ee58a58526
Sean Girard <seangirard@yahoo.com>
2016-02-19 09:06:34 -0800

    fs

--- template-beta.php
+++ template-beta.php
@@ -107,9 +107,27 @@ Template Name: Home Page
 
     <div class="row">
       <div class="col-md-6">
-        <div class="panel spl-hero-panel spl-hero-muted" style="background-image: url('http://www.spokanelibrary.org/assets/img/promos/photos/future-study-planning.jpg') !important;">
-          <div class="">
-            Test
+        <div class="panel spl-hero-panel spl-hero-muted" style="background-image: url('http://www.spokanelibrary.org/assets/img/promos/photos/future-study-planning.jpg');">
+          <div style="padding:16px;">
+            <div class="row">
+              <div class="col-md-6">
+                <a href="http://www.spokanelibraryfoundation.org/citizen-hall-of-fame/">
+                  <img class="img-responsive" style="margin:auto;" src="http://www.spokanelibrary.org/assets/img/promos/chof-logotype-crop.png">
+                </a>
+                <p class="text-center">
+                  <a href="http://www.spokanelibraryfoundation.org/citizen-hall-of-fame/"><b>Learn more</b></a> <small class="text-muted">&rarr;</small>
+                </p>
+              </div>
+              <div class="col-md-6">
+                <a href="https://www.eventbrite.com/e/spokane-citizen-hall-of-fame-breakfast-tickets-20594031302">
+                  <img class="img-responsive" style="margin:auto;" src="http://www.spokanelibrary.org/assets/img/promos/chof-clocktower.png">
+                </a>
+                <p class="text-center">
+                  <a href="https://www.eventbrite.com/e/spokane-citizen-hall-of-fame-breakfast-tickets-20594031302"><b>Attend the awards</b></a> <small class="text-muted">&rarr;</small>
+                </p>
+              </div>
+            </div>
+            <a class="btn btn-alt btn-block" href="/chof/">See the 2015 Inductees &rarr;</a>
           </div>
         </div>
       </div><!-- /.col -->
