289d8e7e4b134837fcb1b0bd5a2550c4c010f5ba
Sean Girard <seangirard@yahoo.com>
2016-02-19 11:13:25 -0800

    dimension test

--- template-beta.php
+++ template-beta.php
@@ -107,8 +107,10 @@ Template Name: Home Page
 
     <div class="row">
       <div class="col-md-6">
-        <div class="panel spl-hero-panel spl-hero-brand-blue-e" style="border-width:4px 1px; background: url('http://www.spokanelibrary.org/assets/img/promos/photos/future-study-planning.jpg') no-repeat;">
+        <div class="panel spl-hero-panel spl-hero-brand-blue-e" style="border-width:4px 1px; /*background: url('http://www.spokanelibrary.org/assets/img/promos/photos/future-study-planning.jpg') no-repeat;*/">
           <a href="/future-study/">
+            <img class="img-responsive" src="http://www.spokanelibrary.org/assets/img/promos/photos/future-study-planning.jpg">
+            <!--
             <div style="background: #fff; opacity: .85;">
               <h1 style="text-transform:uppercase; color:#237ca8; text-decoration:none !important;">You're invited to</h1>
               </div>
@@ -137,6 +139,7 @@ Template Name: Home Page
             <div style="background: #fff; opacity: .85;">
               <h2 class="text-center" style="color:#237ca8;">your 21<sup>st</sup> century library</h2>
             </div>
+            -->
           </a>
         </div>
       </div><!-- /.col -->
