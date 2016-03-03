f303fa0fa2496935d6fe962512f69bfd3e99591c
Sean Girard <seangirard@yahoo.com>
2016-03-03 08:47:32 -0800

    parent/guardian required

--- templates/org/forms/field/signup/shared/card-guardian.php
+++ templates/org/forms/field/signup/shared/card-guardian.php
@@ -3,7 +3,7 @@
   	<label for="" class="col-sm-3 control-label">My parent or guardian</label>
     <div class="col-sm-3">
       <span class="help-block">First</span>
-      <input type="text" class="form-control" id="spl-form-guardian-first" name="spl-form[signup][guardian][first]" value="" placeholder="">
+      <input type="text" class="form-control required" id="spl-form-guardian-first" name="spl-form[signup][guardian][first]" value="" placeholder="">
     </div>
     <div class="col-sm-3">
       <span class="help-block">Middle</span>
@@ -11,7 +11,7 @@
     </div>
     <div class="col-sm-3">
       <span class="help-block">Last</span>
-      <input type="text" class="form-control" id="spl-form-guardian-last" name="spl-form[signup][guardian][last]" value="" placeholder="">
+      <input type="text" class="form-control required" id="spl-form-guardian-last" name="spl-form[signup][guardian][last]" value="" placeholder="">
     </div>
     <div class="col-sm-9 col-sm-offset-3">
       <span class="help-block">
