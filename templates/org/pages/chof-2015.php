a1d5683ead67461b262b7cd51ba9112c74faf1e0
Sean Girard <seangirard@yahoo.com>
2016-03-09 11:09:05 -0800

    chof

--- templates/org/pages/chof-2015.php
+++ templates/org/pages/chof-2015.php
@@ -873,6 +873,8 @@
   	      	<div class="col-sm-4 col-md-4">
   			      <p>
   		    	  	<img class="img-responsive" src="/assets/img/promos/chof/Hutton_May-lg.jpg">
+	  	  	    	<br>
+	  	  	    	<small><b>Photo:</b> Courtesy of the Northwest Museum of Arts and Culture</small>
 	  	  	    </p>
 	  	  	  </div><!-- /.col -->
 	  	  	  <div class="col-sm-8 col-md-8">
