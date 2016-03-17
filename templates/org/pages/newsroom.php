f016b9419a626677433d9a620453d10531c373ea
Sean Girard <seangirard@yahoo.com>
2016-03-17 12:58:19 -0700

    news

--- templates/org/pages/newsroom.php
+++ templates/org/pages/newsroom.php
@@ -1,9 +1,9 @@
 <div class="row">
-	<div class="col-md-6">
+	<div class="col-md-8">
+		<h3>Press Releases</h3>
+		<?php echo do_shortcode('[spl_widget list-files prefix="" icon="file"]'); ?>
 	</div><!-- /.col -->
-	<div class="col-md-6">
+	<div class="col-md-4">
 	</div><!-- /.col -->
 </div><!-- /.row -->
 
-<h3>Press Releases</h3>
-<?php echo do_shortcode('[spl_widget list-files prefix="" icon="file"]'); ?>
