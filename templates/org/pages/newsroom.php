e11fe6c2d355c553e027d33c87d4286c3051cbe3
Sean Girard <seangirard@yahoo.com>
2016-03-17 12:59:46 -0700

    news

--- templates/org/pages/newsroom.php
+++ templates/org/pages/newsroom.php
@@ -4,6 +4,14 @@
 		<?php echo do_shortcode('[spl_widget list-files prefix="" icon="file"]'); ?>
 	</div><!-- /.col -->
 	<div class="col-md-4">
+		<div class="panel spl-hero-panel spl-hero-news">
+			<h4><i class="glyphicon glyphicon-bullhorn"></i> 
+				[spl_mailgun_current format=subtitle default_subtitle="In the current issue"]
+			</h4>
+			<div class="panel-body">
+				[spl_mailgun_current format=toc link_button=true excerpt]
+			</div>
+		</div>
 	</div><!-- /.col -->
 </div><!-- /.row -->
 
