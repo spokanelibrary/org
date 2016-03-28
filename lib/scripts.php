1b460a2222742e90a42a58722f35447a7a0f7749
Sean Girard <seangirard@yahoo.com>
2016-03-28 12:54:09 -0700

    chof

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'cc20c80a4aec48889cf2fd5298286686');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e049dbfa203ee1cd0117f81613b83149');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- templates/org/pages/chof-2016.php
+++ templates/org/pages/chof-2016.php
@@ -71,11 +71,13 @@
 			<div class="panel-body">
 				<div class="chof-control-panel">
 					<div class="chof-control-widget">
+						<div style="position:relative;">
 						<select class="form-control" id="chof-control-category">
 							<option>All categories</option>
 							<option>Historic Inductees</option>
 							<option>Arts &amp; Letters</option>
 						</select>
+						</div>
 					</div>
 					<img class="img-responsive" src="/assets/img/promos/chof/chof-shim.gif">
 					<div class="chof-text-wrapper">			
