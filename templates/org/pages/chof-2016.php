691ac1144d6a2eb84fa684976da30df736c6d36f
Sean Girard <seangirard@yahoo.com>
2016-04-08 11:20:44 -0700

    chof

--- templates/org/pages/chof-2016.php
+++ templates/org/pages/chof-2016.php
@@ -362,7 +362,7 @@
 		<a href="#" data-toggle="modal" data-target="#chof-modal-">
 			<div class="chof-panel chof-panel-sized chof-yellow chof-text-dark">
 				<div class="panel-body">
-					<img class="img-responsive" src="/assets/img/promos/chof/chof-shim.gif">
+					<img class="img-responsive" src="/assets/img/promos/chof/Elson_Floyd-sm.jpg">
 					<div class="chof-text-wrapper">			
 						<div class="chof-text-mask">
 							<h6 class="chof-title">Elson S. Floyd</h6>
