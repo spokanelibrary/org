18b4c4eb986ef6ece926b1e462fe497d6b9e2f17
Sean Girard <seangirard@yahoo.com>
2016-04-08 09:05:58 -0700

    chof

--- templates/org/pages/chof-2016.php
+++ templates/org/pages/chof-2016.php
@@ -143,7 +143,7 @@
 		<a href="#" data-toggle="modal" data-target="#chof-modal-">
 			<div class="chof-panel chof-panel-sized chof-brown">
 				<div class="panel-body">
-					<img class="img-responsive" src="/assets/img/promos/chof/chof-shim.gif">
+					<img class="img-responsive" src="/assets/img/promos/chof/Father_Josheph_Cataldo-sm.jpg">
 					<div class="chof-text-wrapper">			
 						<div class="chof-text-mask">
 							<h6 class="chof-title">Father Joseph Cataldo</h6>
