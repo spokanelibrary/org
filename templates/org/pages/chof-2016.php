d2636d92a78e34e47474aa24e769fdde204be8d8
Sean Girard <seangirard@yahoo.com>
2016-04-08 07:20:23 -0700

    chof

--- templates/org/pages/chof-2016.php
+++ templates/org/pages/chof-2016.php
@@ -19,9 +19,9 @@
 <p style="font-size: 18px;">
 	The Spokane Citizen Hall of Fame, created in 2015, honors great citizens of Spokane while providing funding toward the business and entrepreneurial services at Spokane Public Library. 
 </p>
-	<h4 class="text-center">We welcome your gifts and support.</h4>
+<h4 class="text-center">We welcome your gifts and support.</h4>
 <p class="text-center">
-	<a class="btn btn-warning btn-small">Donate now &rarr;</a>
+	<a href="http://www.spokanelibraryfoundation.org/donate/" class="btn btn-warning">Donate now &rarr;</a>
 </p>
 <hr>
 
