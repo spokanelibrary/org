75a3c242bef217171631624b3fab0c7696c22032
Sean Girard <seangirard@yahoo.com>
2016-04-08 10:27:39 -0700

    chof

--- templates/org/pages/chof-2016.php
+++ templates/org/pages/chof-2016.php
@@ -286,7 +286,7 @@
 		<a href="#" data-toggle="modal" data-target="#chof-modal-">
 			<div class="chof-panel chof-panel-sized chof-red">
 				<div class="panel-body">
-					<img class="img-responsive" src="/assets/img/promos/chof/chof-shim.gif">
+					<img class="img-responsive" src="/assets/img/promos/chof/Jeanne_Ager-sm.jpg">
 					<div class="chof-text-wrapper">			
 						<div class="chof-text-mask">
 							<h6 class="chof-title">Jeanne Ager</h6>
