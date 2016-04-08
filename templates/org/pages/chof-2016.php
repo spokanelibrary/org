8a25a6c9cef9e150e1b58415117f68aed9e8fb04
Sean Girard <seangirard@yahoo.com>
2016-04-08 10:06:23 -0700

    chof

--- templates/org/pages/chof-2016.php
+++ templates/org/pages/chof-2016.php
@@ -267,7 +267,7 @@
 		<a href="#" data-toggle="modal" data-target="#chof-modal-">
 			<div class="chof-panel chof-panel-sized chof-orange">
 				<div class="panel-body">
-					<img class="img-responsive" src="/assets/img/promos/chof/Darin_Nevin-sm.jpg">
+					<img class="img-responsive" src="/assets/img/promos/chof/Darin_Neven-sm.jpg">
 					<div class="chof-text-wrapper">			
 						<div class="chof-text-mask">
 							<h6 class="chof-title">Dr. Darin Neven</h6>
