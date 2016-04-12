ab068dc1763a190ab74c3facb6164c3fac5b0d15
Sean Girard <seangirard@yahoo.com>
2016-04-12 11:00:01 -0700

    chof

--- templates/org/pages/chof-2016.php
+++ templates/org/pages/chof-2016.php
@@ -1034,7 +1034,7 @@
 </div>
 
 <!-- MODAL -->
-<div class="modal fade chof-modal" id="chof-modal-darin-nevin" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
+<div class="modal fade chof-modal" id="chof-modal-darin-neven" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
   <div class="modal-dialog modal-lg">
     <div class="modal-content chof-orange">
       <div class="modal-body">
@@ -1048,6 +1048,35 @@
 	  	  	  </div><!-- /.col -->
 	  	  	  <div class="col-sm-8 col-md-8">
 	  	  	  	<h3 class="modal-title uppercase">Dr. Darin Nevin</h3>
+	  	  	  	<blockquote>
+	  	  	  		&ldquo;  &rdquo;
+	  	  	  	</blockquote>
+	  	  	  	<p>
+	  	  	  	</p>
+  	      	</div><!-- /.col -->
+	  	    </div><!-- /.row -->
+	      </div><!-- /.panel-body -->
+
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- MODAL -->
+<div class="modal fade chof-modal" id="chof-modal-jeanne-ager" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content chof-red">
+      <div class="modal-body">
+        
+        <div class="panel-body">  
+  	    	<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+      	  <h3 class="normal chof-text-red">Public Service &amp; Philanthropy <small>2016</small></h3>
+      	  <div class="row">
+  	      	<div class="col-sm-4 col-md-4">
+  			      <img class="img-responsive" src="/assets/img/promos/chof/Jeanne_Ager-lg.jpg">
+	  	  	  </div><!-- /.col -->
+	  	  	  <div class="col-sm-8 col-md-8">
+	  	  	  	<h3 class="modal-title uppercase">Jeanne Ager</h3>
 	  	  	  	<blockquote>
 	  	  	  		&ldquo;  &rdquo;
 	  	  	  	</blockquote>
