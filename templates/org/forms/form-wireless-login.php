e0500600b1d69a837f5a57fed6926e04492030e0
Sean Girard <seangirard@yahoo.com>
2016-04-26 09:34:56 -0700

    wifi login

--- templates/org/forms/form-wireless-login.php
+++ templates/org/forms/form-wireless-login.php
@@ -90,12 +90,7 @@ $username = ( false == $mobile ) ? $branch : $branch.'_'.'mobile';
 		Use of the library computer network constitutes an agreement that you accept the following terms and conditions:
 	</div><!-- /.panel-heading -->
 	<div class="panel-body">
-		<p>
-			All Library customers are expected to access the Internet in compliance with the <a href="/policies/inet/">Internet Use Policy</a> and <a href="/uploads/rules_of_conduct.pdf">Rules of Conduct</a>, and within the boundaries of the law.
-  		Viewing or downloading illegal or copyright-protected materials, obscenity, child pornography, or materials harmful to minors is prohibited.
-	  	<em>Please be considerate of other Library patrons.</em>
-		</p>
-
+		
 		<form 
         action="http://10.14.50.2:9997/login" 
         method="post" 
@@ -123,6 +118,13 @@ $username = ( false == $mobile ) ? $branch : $branch.'_'.'mobile';
         	<small class="glyphicon glyphicon-ok"></small>
         	I agree. Proceed &rarr;
         </button>
+
+        <p>
+        All Library customers are expected to access the Internet in compliance with the <a href="/policies/inet/">Internet Use Policy</a> and <a href="/uploads/rules_of_conduct.pdf">Rules of Conduct</a>, and within the boundaries of the law.
+        Viewing or downloading illegal or copyright-protected materials, obscenity, child pornography, or materials harmful to minors is prohibited.
+        <em>Please be considerate of other Library patrons.</em>
+        </p>
+        
     </form>
 
 	</div><!-- /.panel-body -->
