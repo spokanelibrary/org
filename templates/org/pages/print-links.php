442486e0101f0405a8f239fade039688d002d9a2
Sean Girard <seangirard@yahoo.com>
2016-06-28 11:46:13 -0700

    print

--- templates/org/pages/print-links.php
+++ templates/org/pages/print-links.php
@@ -2,11 +2,11 @@
   <div class="container-fluid">
       <ul class="nav navbar-nav">
         <li><a class="btn btn-primary" href="/printing/downtown/">Downtown</a></li>
-        <li><a href="/printing/east-side/">East Side</a></li>
-        <li><a href="/printing/hillyard/">Hillyard</a></li>
-        <li><a href="/printing/indian-trail/">Indian Trail</a></li>
-        <li><a href="/printing/shadle/">Shadle</a></li>
-        <li><a href="/printing/south-hill/">South Hill</a></li>
+        <li><a class="btn btn-primary" href="/printing/east-side/">East Side</a></li>
+        <li><a class="btn btn-primary" href="/printing/hillyard/">Hillyard</a></li>
+        <li><a class="btn btn-primary" href="/printing/indian-trail/">Indian Trail</a></li>
+        <li><a class="btn btn-primary" href="/printing/shadle/">Shadle</a></li>
+        <li><a class="btn btn-primary" href="/printing/south-hill/">South Hill</a></li>
       </ul>
   </div>
 </nav>
