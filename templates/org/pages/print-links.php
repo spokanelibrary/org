87471d77a27c5f2db53c6edec7df715d5c431e16
Sean Girard <seangirard@yahoo.com>
2016-06-28 11:48:03 -0700

    print

--- templates/org/pages/print-links.php
+++ templates/org/pages/print-links.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-inverse">
+<!--<nav class="navbar navbar-inverse">-->
   <div class="container-fluid">
       <ul class="nav navbar-nav">
         <li><a class="btn btn-primary" href="/printing/downtown/">Downtown</a></li>
@@ -9,4 +9,4 @@
         <li><a class="btn btn-primary" href="/printing/south-hill/">South Hill</a></li>
       </ul>
   </div>
-</nav>
+<!--</nav>-->
