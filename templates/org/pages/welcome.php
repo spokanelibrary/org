e8d69107cfdd6a13a79521a3314cd6a0aa0a8407
Sean Girard <seangirard@yahoo.com>
2016-07-06 11:53:36 -0700

    welcome

--- templates/org/pages/welcome.php
+++ templates/org/pages/welcome.php
@@ -12,10 +12,16 @@
 		<a class="btn btn-success" href="http://www.spokanelibrary.org"><i class="glyphicon glyphicon-home"></i> Go to the Library website &rarr;</a>
 	</div>
 </div>
-
-<a rel="external" href="http://read.spokanelibrary.org"><h2 class="text-center">Have you signed up for Summer Reading?</h2></a>
-<a rel="external" href="http://read.spokanelibrary.org"><img src="http://www.spokanelibrary.org/wordpress/wp-content/uploads/Summer_Reading_pac-promo.jpg" alt="Summer_Reading_pac-promo" width="800" height="533" class="aligncenter size-full wp-image-16925" /></a>
-
+<hr>
+<div class="row">
+	<div class="col-sm-12">
+		<a rel="external" href="http://read.spokanelibrary.org"><h2 class="text-center">Have you signed up for Summer Reading?</h2></a>
+	</div>
+	<div class="col-sm-6">
+		<a rel="external" href="http://read.spokanelibrary.org"><img src="http://www.spokanelibrary.org/wordpress/wp-content/uploads/Summer_Reading_pac-promo.jpg" alt="Summer_Reading_pac-promo" width="800" height="533" class="aligncenter size-full wp-image-16925" /></a>
+	</div>
+</div>
+</hr>
 <div class="row">
 	<div class="col-sm-6">
 		<h3 class="text-muted normal">Popular Websites <small>Search & Social</small></h3>
