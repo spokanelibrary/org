8c32615b853441f85de70a199fd021c00b22e38c
Sean Girard <seangirard@yahoo.com>
2016-07-28 10:31:10 -0700

    instagram

--- templates/org/header-social-connect.php
+++ templates/org/header-social-connect.php
@@ -1,10 +1,13 @@
 <div class="btn-group btn-group-sm" role="group" aria-label="Social Library Links">
   <a class="btn btn-default"
-      href="http://facebook.com/spokanelibrary" 
+      href="https://facebook.com/spokanelibrary" 
       title=""><img style="width:16px;" class="" src="/assets/img/icons/32px/facebook.png"></a>
   <a class="btn btn-default"
-      href="http://twitter.com/spokanelibrary" 
+      href="https://twitter.com/spokanelibrary" 
       title=""><img style="width:16px;" class="" src="/assets/img/icons/32px/twitter.png"></a>
+  <a class="btn btn-default"
+      href="https://instagram.com/spokanepubliclibrary" 
+      title=""><img style="width:16px;" class="" src="/assets/img/icons/32px/instagram.png"></a>
   <a class="btn btn-default" 
       href="/news/"
       title=""><b>News</b></a>
