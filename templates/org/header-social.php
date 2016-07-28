d9dd81e38c1553ea90bd77b34ff7dc59b15646cb
Sean Girard <seangirard@yahoo.com>
2016-07-28 10:28:33 -0700

    org

--- templates/org/header-social.php
+++ templates/org/header-social.php
@@ -1,9 +1,13 @@
   <a class=""
-    href="http://facebook.com/spokanelibrary" 
+    href="https://facebook.com/spokanelibrary" 
     title=""><img class="" src="/assets/img/icons/32px/facebook.png"></a>
   &nbsp;
   <a class=""
-    href="http://twitter.com/spokanelibrary" 
+    href="https://twitter.com/spokanelibrary" 
     title=""><img class="" src="/assets/img/icons/32px/twitter.png"></a>
+  &nbsp;
+  <a class=""
+    href="http://instagram.com/spokanepubliclibrary" 
+    title=""><img class="" src="/assets/img/icons/32px/instagram.png"></a>
 
 
