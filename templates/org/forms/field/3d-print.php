982661724cc758d4ad2c15721087bd1dc2490303
Sean Girard <seangirard@yahoo.com>
2016-08-18 08:59:21 -0700

    3d

--- templates/org/forms/field/3d-print.php
+++ templates/org/forms/field/3d-print.php
@@ -1,27 +1,11 @@
-<div class="row">
-
-  <div class="col-md-8">
-    
-    <div class="panel panel-primary">
-      <div class="panel-body">
-        <p class="lead text-center" style="margin-bottom:0;">
-        <a href="/ask">
-          <i class="glyphicon glyphicon-exclamation-sign"></i>
-          For questions by email, please <strong>ask a librarian</strong> &rarr;
-        </a>
-        </p>
-      </div><!-- /.panel-body -->
-    </div><!-- /.panel -->
-    
-    <?php //echo do_shortcode('[spl_widget login-form label]'); ?>
-    
+      
     <div class="panel panel-default">
       <div class="panel-body">
-        <form class="form-horizontal spl-form" id="spl-form-book" method="post" role="form">
+        <form class="form-horizontal spl-form" id="spl-form-3d" method="post" role="form">
           
           <?php include 'common/crass-response.php'; ?>
       
-          <input type="hidden" id="spl-form-id" name="spl-form[id]" value="book" />
+          <input type="hidden" id="spl-form-id" name="spl-form[id]" value="3d" />
           
           <input type="hidden" 
                 id="spl-form-ip" 
@@ -154,6 +138,8 @@
             <?php include 'common/field-login.php'; ?>
             <?php include 'common/field-contact.php'; ?>
 
+            <?php include 'common/field-location.php'; ?>
+
             <hr>
 
             <div class="form-group">
@@ -177,20 +163,3 @@
         </form>
       </div><!-- /.panel-body -->
     </div><!-- /.panel -->
-
-  </div><!-- /.row -->
-
-  <div class="col-md-4">
-    <?php echo do_shortcode('[spl_widget custom-page page=contact-info]'); ?>
-    <h4 class="text-muted">
-      Asked &amp; answered
-    </h4>
-    <p>
-      <a class="btn btn-block btn-primary" title="How do I?" href="/faq/">
-        <i class="glyphicon glyphicon-question-sign"></i>
-        Frequently Asked Questions &rarr;
-      </a>
-    </p>
-  </div><!-- /.col -->
-
-</div><!-- /.row -->
