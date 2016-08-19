012c4f5d1acf0994eb1debee8f800eca7688455a
Sean Girard <seangirard@yahoo.com>
2016-08-19 09:15:28 -0700

    stl

---
+++ templates/org/forms/field/common/shared/field-contact-barcode-required.php
@@ -0,0 +1,9 @@
+<div class="form-group">
+  <label for="spl-form-barcode" class="col-sm-4 control-label">My library card #</label>
+  <div class="col-sm-8">
+    <input type="text" class="form-control required" id="spl-form-barcode" name="spl-form[barcode]" placeholder="">
+  	<span class="help-block">
+  		14 digits, beginning with 27413.
+  	</span>
+  </div>
+</div>
