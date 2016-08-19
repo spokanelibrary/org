c8e650de9f333d5315c9f5fd4d1a7879bb790326
Sean Girard <seangirard@yahoo.com>
2016-08-19 09:11:26 -0700

    stl

--- templates/org/forms/field/common/shared/field-contact-info-no-mail.php
+++ templates/org/forms/field/common/shared/field-contact-info-no-mail.php
@@ -2,8 +2,8 @@
   <label class="col-sm-4 control-label">Contact information</label>
   <div class="col-sm-8">
   	<label for="spl-form-email" class="control-label">My email address</label>
-  	<input type="text" class="form-control" id="spl-form-email" name="spl-form[email]" placeholder="">
+  	<input type="text" class="form-control required" id="spl-form-email" name="spl-form[email]" placeholder="">
   	<label for="spl-form-phone" class="control-label">My phone number</label>
-  	<input type="text" class="form-control" id="spl-form-phone" name="spl-form[phone]" placeholder="">
+  	<input type="text" class="form-control required" id="spl-form-phone" name="spl-form[phone]" placeholder="">
   </div>
 </div>
