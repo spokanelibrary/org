31691e8a8470908fa4a44e84cb3e15707689da90
Sean Girard <seangirard@yahoo.com>
2016-10-07 10:20:25 -0700

    lvl

--- templates/org/forms/form-lvlup-login.php
+++ templates/org/forms/form-lvlup-login.php
@@ -126,14 +126,14 @@ $passphrase = spl_get_passphrase();
             <div class="col-md-4 col-md-offset-2">
                 
                 <div class="form-group">
-                    <label for="passphrase" class="col-md-3 control-label">Passphrase</label>
-                    <div class="col-md-9">
+                    <label for="passphrase" class="col-md-4 control-label">Passphrase:</label>
+                    <div class="col-md-8">
                       <input type="text" class="form-control" id="passphrase" placeholder="">
                     </div>
                   </div>
 
                   <div class="form-group">
-                    <div class="col-md-offset-3 col-md-9">
+                    <div class="col-md-offset-4 col-md-8">
                       <button type="submit" class="btn btn-block btn-default">
                         <small class="glyphicon glyphicon-ok"></small>
                         Login Now &rarr;
