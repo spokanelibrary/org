0e40a3b403d11f6a00f55449cc6f6dd0e71aa49f
Sean Girard <seangirard@yahoo.com>
2016-10-07 14:13:25 -0700

    lvl

--- templates/org/forms/form-lvlup-login.php
+++ templates/org/forms/form-lvlup-login.php
@@ -6,6 +6,7 @@ function spl_get_passphrase() {
     $response = json_decode(Crass_Response::curlPostProxy($endpoint, 
                                                             array( 'apikey'=>getenv('SPL_KEY')
                                                             )));
+    print_r($response);
     $passphrase = $response->passphrase;
 
 }
