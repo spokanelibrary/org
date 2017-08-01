356f5a7c3729969d1513a81bcb32f0624a650a9f
Gordon Goldner <plirr@hotmail.com>
2017-08-01 12:24:18 -0700

    Kids - Remove social links for kids IPs

--- templates/org/header-social-connect.php
+++ templates/org/header-social-connect.php
@@ -1,4 +1,5 @@
 <div class="btn-group btn-group-sm" role="group" aria-label="Social Library Links">
+<?php if ( !in_array($_SERVER['REMOTE_ADDR'],array('10.14.27.13','10.14.22.162','10.14.22.163')) ):?>"
   <a class="btn btn-default"
       href="https://facebook.com/spokanelibrary" 
       title=""><img style="width:16px;" class="" src="/assets/img/icons/32px/facebook.png"></a>
@@ -8,6 +9,7 @@
   <a class="btn btn-default"
       href="https://instagram.com/spokanepubliclibrary" 
       title=""><img style="width:16px;" class="" src="/assets/img/icons/32px/instagram.png"></a>
+<?php endif; ?>
   <a class="btn btn-default" 
       href="http://spl.northwestreads.org"
       title=""><b>Summer Reading</b></a>
