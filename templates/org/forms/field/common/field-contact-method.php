354fbf1e8c735d9640bd3bfded7519af54281855
Gordon Goldner <plirr@hotmail.com>
2019-03-19 11:47:29 -0700

    Contact - Remove postal option

--- templates/org/forms/field/common/field-contact-method.php
+++ templates/org/forms/field/common/field-contact-method.php
@@ -6,7 +6,6 @@
     <select class="form-control" id="spl-form-contact-method" name="spl-form[contact-method]">
       <option value="email" selected>Email</option>
       <option value="phone">Telephone</option>
-      <option value="postal">Postal Mail</option>
     </select>
     <span class="help-block">
       We prefer to use email. Let us know what works for you.
