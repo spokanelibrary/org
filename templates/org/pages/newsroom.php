06008944df273e0ec44810d55ac236ff166cb5a6
Gordon Goldner <plirr@hotmail.com>
2019-06-28 12:20:12 -0700

    Newsletter - Fix dates

--- templates/org/pages/newsroom.php
+++ templates/org/pages/newsroom.php
@@ -45,66 +45,66 @@
 <div class="row">
 	<div class="col-md-7 col-lg-8">
 	    <h2>Newsletters</h2>
-	    
-<h5>06/10/2019</h5>
-<p class="edit_me"><a href="http://eepurl.com/grK6m1" title="News from the Library: Win a Kobo Clara eReader!" target="_blank">News from the Library: Win a Kobo Clara eReader!</a></p>
 
-<h5>05/02/2019</h5>
-<p class="edit_me"><a href="http://eepurl.com/gpIrar" title="This Month at the Library: Understanding Omotenashi to K-Pop! the Global Music Phenomenon" target="_blank">This Month at the Library: Understanding Omotenashi to K-Pop! the Global Music Phenomenon</a></p>
+<h5>June 10, 2019</h5>
+<p class="edit_me"><b><a href="http://eepurl.com/grK6m1" title="News from the Library: Win a Kobo Clara eReader!" target="_blank">News from the Library: Win a Kobo Clara eReader!</a></b></p>
 
-<h5>04/10/2019</h5>
-<p class="edit_me"><a href="http://eepurl.com/glunIY" title="Library Giving Day 2019" target="_blank">Library Giving Day 2019</a></p>
+<h5>May 2, 2019</h5>
+<p class="edit_me"><b><a href="http://eepurl.com/gpIrar" title="This Month at the Library: Understanding Omotenashi to K-Pop! the Global Music Phenomenon" target="_blank">This Month at the Library: Understanding Omotenashi to K-Pop! the Global Music Phenomenon</a></b></p>
 
-<h5>04/04/2019</h5>
-<p class="edit_me"><a href="http://eepurl.com/gmUshT" title="News from the Library: We'll waive your fines" target="_blank">News from the Library: We'll waive your fines</a></p>
+<h5>April 10, 2019</h5>
+<p class="edit_me"><b><a href="http://eepurl.com/glunIY" title="Library Giving Day 2019" target="_blank">Library Giving Day 2019</a></b></p>
 
-<h5>03/11/2019</h5>
-<p class="edit_me"><a href="http://eepurl.com/gjHL0H" title="Special Announcement from Spokane Public Library" target="_blank">Special Announcement from Spokane Public Library</a></p>
+<h5>April 4, 2019</h5>
+<p class="edit_me"><b><a href="http://eepurl.com/gmUshT" title="News from the Library: We'll waive your fines" target="_blank">News from the Library: We'll waive your fines</a></b></p>
 
-<h5>03/07/2019</h5>
-<p class="edit_me"><a href="http://eepurl.com/gjFIaP" title="News from the Library: Hold the library in the palm of your hand" target="_blank">News from the Library: Hold the library in the palm of your hand</a></p>
+<h5>March 11, 2019</h5>
+<p class="edit_me"><b><a href="http://eepurl.com/gjHL0H" title="Special Announcement from Spokane Public Library" target="_blank">Special Announcement from Spokane Public Library</a></b></p>
 
-<h5>02/08/2019</h5>
-<p class="edit_me"><a href="http://eepurl.com/ggyUMH" title="News from the Library: Celebrate Black History Month with SPL" target="_blank">News from the Library: Celebrate Black History Month with SPL</a></p>
+<h5>March 07, 2019</h5>
+<p class="edit_me"><b><a href="http://eepurl.com/gjFIaP" title="News from the Library: Hold the library in the palm of your hand" target="_blank">News from the Library: Hold the library in the palm of your hand</a></b></p>
 
-<h5>01/17/2019</h5>
-<p class="edit_me"><a href="http://eepurl.com/geb2CL" title="News from the Library : We're Going Paperless!" target="_blank">News from the Library : We're Going Paperless!</a></p>
+<h5>February 8, 2019</h5>
+<p class="edit_me"><b><a href="http://eepurl.com/ggyUMH" title="News from the Library: Celebrate Black History Month with SPL" target="_blank">News from the Library: Celebrate Black History Month with SPL</a></b></p>
 
-<h5>12/20/2018</h5>
-<p class="edit_me"><a href="http://eepurl.com/gbSL8j" title="⭐&nbsp;Our favorite books of 2018&nbsp;⭐" target="_blank"><img draggable="false" class="emoji" alt="⭐" src="https://twemoji.maxcdn.com/2/72x72/2b50.png" style="width: 16px; height: 16px;">&nbsp;Our favorite books of 2018&nbsp;<img draggable="false" class="emoji" alt="⭐" src="https://twemoji.maxcdn.com/2/72x72/2b50.png" style="width: 16px; height: 16px;"></a></p>
+<h5>January 17, 2019</h5>
+<p class="edit_me"><b><a href="http://eepurl.com/geb2CL" title="News from the Library : We're Going Paperless!" target="_blank">News from the Library : We're Going Paperless!</a></b></p>
 
-<h5>12/06/2018</h5>
-<p class="edit_me"><a href="http://eepurl.com/dP56H9" title="News from the Library: Free Holiday Fun&nbsp;⛄🎄📚" target="_blank">News from the Library: Free Holiday Fun&nbsp;<img draggable="false" class="emoji" alt="⛄" src="https://twemoji.maxcdn.com/2/72x72/26c4.png" style="width: 16px; height: 16px;"><img draggable="false" class="emoji" alt="🎄" src="https://twemoji.maxcdn.com/2/72x72/1f384.png" style="width: 16px; height: 16px;"><img draggable="false" class="emoji" alt="📚" src="https://twemoji.maxcdn.com/2/72x72/1f4da.png" style="width: 16px; height: 16px;"></a></p>
+<h5>December 20, 2018</h5>
+<p class="edit_me"><b><a href="http://eepurl.com/gbSL8j" title="⭐&nbsp;Our favorite books of 2018&nbsp;⭐" target="_blank"><img draggable="false" class="emoji" alt="⭐" src="https://twemoji.maxcdn.com/2/72x72/2b50.png" style="width: 16px; height: 16px;">&nbsp;Our favorite books of 2018&nbsp;<img draggable="false" class="emoji" alt="⭐" src="https://twemoji.maxcdn.com/2/72x72/2b50.png" style="width: 16px; height: 16px;"></a></b></p>
 
-<h5>11/08/2018</h5>
-<p class="edit_me"><a href="http://eepurl.com/dMRLkc" title="News from the Library: Native American Heritage Month and more!" target="_blank">News from the Library: Native American Heritage Month and more!</a></p>
+<h5>December 6, 2018</h5>
+<p class="edit_me"><b><a href="http://eepurl.com/dP56H9" title="News from the Library: Free Holiday Fun&nbsp;⛄🎄📚" target="_blank">News from the Library: Free Holiday Fun&nbsp;<img draggable="false" class="emoji" alt="⛄" src="https://twemoji.maxcdn.com/2/72x72/26c4.png" style="width: 16px; height: 16px;"><img draggable="false" class="emoji" alt="🎄" src="https://twemoji.maxcdn.com/2/72x72/1f384.png" style="width: 16px; height: 16px;"><img draggable="false" class="emoji" alt="📚" src="https://twemoji.maxcdn.com/2/72x72/1f4da.png" style="width: 16px; height: 16px;"></a></b></p>
 
-<h5>10/25/2018</h5>
-<p class="edit_me"><a href="http://eepurl.com/dLeik-/" title="News from the Library: Meet the host of Sit and Be Fit at the Library tonight!" target="_blank">News from the Library: Meet the host of Sit and Be Fit at the Library tonight!</a></p>
+<h5>November 8, 2018</h5>
+<p class="edit_me"><b><a href="http://eepurl.com/dMRLkc" title="News from the Library: Native American Heritage Month and more!" target="_blank">News from the Library: Native American Heritage Month and more!</a></b></p>
 
-<h5>10/11/2018</h5>
-<p class="edit_me"><a href="http://eepurl.com/dJTqkg" title="News from the Library: Stock up at the Friends of the Library Book Sale&nbsp;📚" target="_blank">News from the Library: Stock up at the Friends of the Library Book Sale&nbsp;<img draggable="false" class="emoji" alt="📚" src="https://twemoji.maxcdn.com/2/72x72/1f4da.png" style="width: 16px; height: 16px;"></a></p>
+<h5>October 25, 2018</h5>
+<p class="edit_me"><b><a href="http://eepurl.com/dLeik-/" title="News from the Library: Meet the host of Sit and Be Fit at the Library tonight!" target="_blank">News from the Library: Meet the host of Sit and Be Fit at the Library tonight!</a></b></p>
 
-<h5>09/27/2018</h5>
-<p class="edit_me"><a href="http://eepurl.com/dIqken" title="Library News: Hear author Amy Stewart speak tonight!" target="_blank">Library News: Hear author Amy Stewart speak tonight!</a></p>
+<h5>October 11, 2018</h5>
+<p class="edit_me"><b><a href="http://eepurl.com/dJTqkg" title="News from the Library: Stock up at the Friends of the Library Book Sale&nbsp;📚" target="_blank">News from the Library: Stock up at the Friends of the Library Book Sale&nbsp;<img draggable="false" class="emoji" alt="📚" src="https://twemoji.maxcdn.com/2/72x72/1f4da.png" style="width: 16px; height: 16px;"></a></b></p>
 
-<h5>09/13/2018</h5>
-<p class="edit_me"><a href="http://eepurl.com/dGLuwz" title="News from the Library: Celebrating Hispanic Heritage Month" target="_blank">News from the Library: Celebrating Hispanic Heritage Month</a></p>
+<h5>September 27, 2018</h5>
+<p class="edit_me"><b><a href="http://eepurl.com/dIqken" title="Library News: Hear author Amy Stewart speak tonight!" target="_blank">Library News: Hear author Amy Stewart speak tonight!</a></b></p>
 
-<h5>08/30/2018</h5>
-<p class="edit_me"><a href="http://eepurl.com/dFoiR1" title="News from the Library: Learn about the Library of the Future" target="_blank">News from the Library: Learn about the Library of the Future</a></p>
+<h5>September 13, 2018</h5>
+<p class="edit_me"><b><a href="http://eepurl.com/dGLuwz" title="News from the Library: Celebrating Hispanic Heritage Month" target="_blank">News from the Library: Celebrating Hispanic Heritage Month</a></b></p>
 
-<h5>08/16/2018</h5>
-<p class="edit_me"><a href="http://eepurl.com/dD0Rrf" title="We need your feedback! Help us make a better website for you." target="_blank">We need your feedback! Help us make a better website for you.</a></p>
+<h5>August 30, 2018</h5>
+<p class="edit_me"><b><a href="http://eepurl.com/dFoiR1" title="News from the Library: Learn about the Library of the Future" target="_blank">News from the Library: Learn about the Library of the Future</a></b></p>
 
-<h5>08/02/2018</h5>
-<p class="edit_me"><a href="http://eepurl.com/dCKd8n" title="News from the Library: Summer Reading" target="_blank">News from the Library: Summer Reading</a></p>
+<h5>August 16, 2018</h5>
+<p class="edit_me"><b><a href="http://eepurl.com/dD0Rrf" title="We need your feedback! Help us make a better website for you." target="_blank">We need your feedback! Help us make a better website for you.</a></b></p>
 
-<h5>07/19/2018</h5>
-<p class="edit_me"><a href="http://eepurl.com/dBxQYv" title="News from the Library: Imagine the Library of the Future" target="_blank">News from the Library: Imagine the Library of the Future</a></p>
+<h5>August 2, 2018</h5>
+<p class="edit_me"><b><a href="http://eepurl.com/dCKd8n" title="News from the Library: Summer Reading" target="_blank">News from the Library: Summer Reading</a></b></p>
 
-<h5>07/05/2018</h5>
-<p class="edit_me"><a href="http://eepurl.com/dzzOLP" title="News from the Library: Watch the Latest Episode of Lilac City Live featuring Mayor Condor...err, Condon" target="_blank">News from the Library: Watch the Latest Episode of Lilac City Live featuring Mayor Condor...err, Condon</a></p>
+<h5>July 19, 2018</h5>
+<p class="edit_me"><b><a href="http://eepurl.com/dBxQYv" title="News from the Library: Imagine the Library of the Future" target="_blank">News from the Library: Imagine the Library of the Future</a></b></p>
+
+<h5>July 5, 2018</h5>
+<p class="edit_me"><b><a href="http://eepurl.com/dzzOLP" title="News from the Library: Watch the Latest Episode of Lilac City Live featuring Mayor Condor...err, Condon" target="_blank">News from the Library: Watch the Latest Episode of Lilac City Live featuring Mayor Condor...err, Condon</a></b></p>
 
 		<h2>Press releases</h2>
 		<?php echo do_shortcode('[spl_widget list-files prefix="" icon="file"]'); ?>
