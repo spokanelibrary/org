705f0f1e18da561f866c2a5b264d1c9a1324c491
Gordon Goldner <gordon.goldner@digitallizard.com>
2020-03-06 13:05:38 -0800

    Change locations in signup

--- templates/org/forms/field/signup/shared/card-location.php
+++ templates/org/forms/field/signup/shared/card-location.php
@@ -2,12 +2,14 @@
   <label for="spl-form-location" class="col-sm-3 control-label">My library branch</label>
   <div class="col-sm-3">
     <select class="form-control required" id="spl-form-location" name="spl-form[signup][location]">
-      <option value="dt" selected>Downtown</option>
+      <!-- <option value="dt" selected>Downtown</option> -->
       <option value="es">East Side</option>
       <option value="hy">Hillyard</option>
       <option value="it">Indian Trail</option>
-      <option value="sh">Shadle</option>
+      <option value="nt">Northtown Mall</option>
+      <!-- <option value="sh">Shadle</option> -->
       <option value="so">South Hill</option>
+      <option value="pl">STA Plaza</option>
     </select>
   </div>
   <div class="col-sm-6">
