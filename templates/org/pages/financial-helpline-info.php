e42c7f05dee821701758c7ad818c809c217d4907
Gordon Goldner <plirr@hotmail.com>
2020-05-28 10:33:02 -0700

    Add financial-helpline-info.php

---
+++ templates/org/pages/financial-helpline-info.php
@@ -0,0 +1,30 @@
+<div class="panel panel-primary">
+  <div class="panel-heading">
+    <h4>Spokane Public Library</h4>
+  </div><!-- /.panel-heading -->
+  <div class="panel-body">
+    <!-- <p>
+      <blockquote>
+      906 West Main Street
+      <br />
+      Spokane, WA 99201
+      </blockquote>
+    </p> -->
+    <h4>
+      Financial Helpline: 509.625.6650
+    </h4>
+    <p>
+      Open Monday-Friday 8-5
+    </p>
+  </div><!-- /.panel-body -->
+  <!--
+  <div class="panel-footer panel-transparent text-center">
+      <a class=""
+        href="http://facebook.com/spokanelibrary"
+        title=""><img src="/assets/img/icons/16px/facebook.png"> <strong>Facebook</strong></a>
+      &nbsp;&nbsp;
+      <a class="text-info"
+        href="http://twitter.com/spokanelibrary"
+        title=""><img src="/assets/img/icons/16px/twitter.png"> <strong>Twitter</strong></a>
+  </div> /.panel-footer -->
+</div><!-- /.panel -->
