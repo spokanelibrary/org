11b7a8e1027cb334b6a95704af83ccb94a25b0e1
Colin <colinstuartdev@gmail.com>
2020-06-02 08:48:16 -0700

    Move Kindle

--- templates/org/pages/apps.php
+++ templates/org/pages/apps.php
@@ -90,34 +90,6 @@
       </div>
     </div><!-- /.media -->
 
-    <p id="kindle">&nbsp;</p>
-    <div class="media">
-      <span class="media-object pull-left">
-        <img class="img-rounded img-responsive" style="width:57px; height:57px;" src="/assets/img/icons/apps/kindle.png">
-      </span>
-      <div class="media-body">
-        <h4 class="media-heading">
-          <em>Kindle:</em> eBook reader
-        </h4>
-        <p>
-          If you choose to borrow OverDrive eBooks in Kindle format you'll want to grab this Kindle reader app.
-          This is useful, for example, if you want to switch between reading on a Kindle device and a smartphone.
-          <a href="/ebooks/">More about eBooks at the library &rarr;</a>
-        </p>
-        <p>
-          <a rel="external" class="btn btn-info" href="https://itunes.apple.com/us/app/id302584613">iOS &rarr;</a>
-          <a rel="external" class="btn btn-success" href="https://play.google.com/store/apps/details?hl=en&id=com.amazon.kindle">Android &rarr;</a>
-        </p>
-        <a rel="external" href="https://www.windowsphone.com/en-us/store/app/amazon-kindle/48195fb4-ee0e-e011-9264-00237de2db9e">Windows Phone</a>
-        |
-        <a rel="external" href="https://itunes.apple.com/us/app/kindle/id405399194">Mac <small class="text-muted">(app store)</small></a>
-        |
-        <a rel="external" href="http://www.amazon.com/gp/kindle/pc/download">Windows <small class="text-muted">(.msi)</small></a>
-        |
-        <a rel="external" href="http://apps.microsoft.com/windows/en-us/app/kindle/1d7e4396-0143-4aed-8892-84eb75e799f3">Windows 8</a>
-      </div>
-    </div><!-- /.media -->
-
     <p id="freegal">&nbsp;</p>
     <div class="media">
       <span class="media-object pull-left">
@@ -171,6 +143,34 @@
         <a rel="external" href="http://www.barnesandnoble.com/w/oneclickdigital-recorded-books/1115073231?ean=2940147119860">Nook</a> -->
       </div>
     </div> <!-- /.media -->
+
+    <p id="kindle">&nbsp;</p>
+    <div class="media">
+      <span class="media-object pull-left">
+        <img class="img-rounded img-responsive" style="width:57px; height:57px;" src="/assets/img/icons/apps/kindle.png">
+      </span>
+      <div class="media-body">
+        <h4 class="media-heading">
+          <em>Kindle:</em> eBook reader
+        </h4>
+        <p>
+          If you choose to borrow OverDrive eBooks in Kindle format you'll want to grab this Kindle reader app.
+          This is useful, for example, if you want to switch between reading on a Kindle device and a smartphone.
+          <a href="/ebooks/">More about eBooks at the library &rarr;</a>
+        </p>
+        <p>
+          <a rel="external" class="btn btn-info" href="https://itunes.apple.com/us/app/id302584613">iOS &rarr;</a>
+          <a rel="external" class="btn btn-success" href="https://play.google.com/store/apps/details?hl=en&id=com.amazon.kindle">Android &rarr;</a>
+        </p>
+        <a rel="external" href="https://www.windowsphone.com/en-us/store/app/amazon-kindle/48195fb4-ee0e-e011-9264-00237de2db9e">Windows Phone</a>
+        |
+        <a rel="external" href="https://itunes.apple.com/us/app/kindle/id405399194">Mac <small class="text-muted">(app store)</small></a>
+        |
+        <a rel="external" href="http://www.amazon.com/gp/kindle/pc/download">Windows <small class="text-muted">(.msi)</small></a>
+        |
+        <a rel="external" href="http://apps.microsoft.com/windows/en-us/app/kindle/1d7e4396-0143-4aed-8892-84eb75e799f3">Windows 8</a>
+      </div>
+    </div><!-- /.media -->
 
 <!--     <p id="oneclick">&nbsp;</p>
     <div class="media">
