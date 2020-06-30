020f549d1469eb8e051fd9ca70774d270d2340e1
Gordon Goldner <plirr@hotmail.com>
2020-06-30 14:05:52 -0700

    Add curbside faq to connect

--- templates/org/pages/contact-info.php
+++ templates/org/pages/contact-info.php
@@ -3,9 +3,43 @@
     <h4>Spokane Public Library</h4>
   </div><!-- /.panel-heading -->
   <div class="panel-body">
-    <!-- <p>     
+    <!-- <p>
       <blockquote>
-      906 West Main Street 
+      906 West Main Street
+      <br />
+      Spokane, WA 99201
+      </blockquote>
+    </p> -->
+    <h3>
+    FAQ
+    </h3>
+    <h4>
+    How do I schedule a curbside pickup?
+    </h4>
+    <p>
+    Request materials using our online catalog. When notified that your items are ready, schedule a curbside pickup appointment. Read our Curbside Pickup FAQ here.
+    </p>
+    <h4>
+    Why are materials still showing as checked out on my account? I returned them several days ago.
+    </h4>
+    <p>
+    All materials will be quarantined for at least 72 hours after return. Rest assured they will be checked in soon and we never charge overdue fines.
+    </p>
+    <h4>
+    When will you reopen your library branches?
+    </h4>
+    <p>
+    Libraries are listed in Phase 3 of Washington State&quot;s reopening plans. The date for reopening has not been determined.
+    </p>
+  </div><!-- /.panel-body -->
+  <div class="panel panel-primary">
+  <div class="panel-heading">
+    <h4>Spokane Public Library</h4>
+  </div><!-- /.panel-heading -->
+  <div class="panel-body">
+    <!-- <p>
+      <blockquote>
+      906 West Main Street
       <br />
       Spokane, WA 99201
       </blockquote>
@@ -15,16 +49,16 @@
     </h4>
     <p>
       <a class="" href="/branches/">Branch Locations &amp; Hours</a> &rarr;
-    </p>    
+    </p>
   </div><!-- /.panel-body -->
   <!--
   <div class="panel-footer panel-transparent text-center">
       <a class=""
-        href="http://facebook.com/spokanelibrary" 
+        href="http://facebook.com/spokanelibrary"
         title=""><img src="/assets/img/icons/16px/facebook.png"> <strong>Facebook</strong></a>
       &nbsp;&nbsp;
-      <a class="text-info" 
-        href="http://twitter.com/spokanelibrary" 
+      <a class="text-info"
+        href="http://twitter.com/spokanelibrary"
         title=""><img src="/assets/img/icons/16px/twitter.png"> <strong>Twitter</strong></a>
   </div> /.panel-footer -->
 </div><!-- /.panel -->
