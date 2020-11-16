a0020ea47644c899c18c20cd0de66db73bc04bdc
Gordon Goldner <plirr@hotmail.com>
2020-11-16 10:04:16 -0800

    Remove Grab-N-Go from home page

--- template-home.php
+++ template-home.php
@@ -51,7 +51,7 @@ Template Name: Home Page
       <div class="clearfix visible-sm"></div>
 
       <div class="col-sm-4 col-sm-push-8 col-md-3 col-md-push-9 hidden-xs spl-hero-calendar">
-        <a href="https://www.spokanelibrary.org/grab-n-go/"><img src="/assets/img/promos/ReserveGrabGo.png" class="img-responsive"alt="Schedule an appointment to browse the collection"></a>
+        <!-- <a href="https://www.spokanelibrary.org/grab-n-go/"><img src="/assets/img/promos/ReserveGrabGo.png" class="img-responsive"alt="Schedule an appointment to browse the collection"></a> -->
         <a href="https://www.spokanelibrary.org/computer-reservation/"><img src="/assets/img/promos/ReserveaComputer.png" class="img-responsive"alt="Reserve a computer"></a>
         <!-- <a href="http://bookings.spokanelibrary.org/reserve"><img src="/assets/img/promos/MeetingRoomButton2.png" class="img-responsive"alt="Book a meeting room"></a> -->
         <a href="https://www.spokanelibrary.org/calendar/"><img src="/assets/img/promos/CalendarButton2.png" class="img-responsive" alt="Upcoming events and programs"></a>
