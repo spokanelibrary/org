f16d1e1bbedf769784453f37b061a7834d6fd274
Colin <colinstuartdev@gmail.com>
2021-06-04 11:37:08 -0700

    remove schedule curbside button

--- template-home.php
+++ template-home.php
@@ -55,7 +55,7 @@ Template Name: Home Page
         <a href="https://spokanelibrary.libnet.info/v/assets/309"><img src="/assets/img/promos/ReserveaComputer.png" class="img-responsive"alt="Reserve a computer"></a>
 		<a href="http://bookings.spokanelibrary.org/reserve"><img src="/assets/img/promos/MeetingRoomButton2.png" class="img-responsive"alt="Book a meeting room"></a> -->
         <a href="https://www.spokanelibrary.org/calendar/"><img src="/assets/img/promos/CalendarButton2.png" class="img-responsive" alt="Upcoming events and programs"></a>
-        <a href="https://www.spokanelibrary.org/curbside/"><img src="/assets/img/promos/CurbsideButton.png" class="img-responsive" alt="Schedule curbside pickup"></a>
+        <!-- <a href="https://www.spokanelibrary.org/curbside/"><img src="/assets/img/promos/CurbsideButton.png" class="img-responsive" alt="Schedule curbside pickup"></a> -->
       </div>
 
       <div class="col-sm-8 col-sm-pull-4 col-md-9 col-md-pull-3">
