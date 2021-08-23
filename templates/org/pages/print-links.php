03d71da4b5cf19097508855a9c4c06d7b61b4fc7
Colin <colinstuartdev@gmail.com>
2021-08-23 07:34:47 -0700

    change print link for HY

--- templates/org/pages/print-links.php
+++ templates/org/pages/print-links.php
@@ -3,7 +3,7 @@
       <ul class="nav navbar-nav">
         <li><a class="btn btn-primary" href="https://www.printeron.net/system/printspot/interface/select_file.jsp?url=spl/downtown&protocolSwitched=true">Downtown</a></li>
         <li><a class="btn btn-primary" href="https://www.printeron.net/system/printspot/interface/select_file.jsp?url=spl/eastside&protocolSwitched=true">East Side</a></li>
-        <li><a class="btn btn-primary" href="https://www.printeron.net/system/printspot/interface/select_file.jsp?url=spl/hillyard&protocolSwitched=true">Hillyard</a></li>
+        <li><a class="btn btn-primary" href="https://smartalec.smartalecprint.com/SmartAlec/?ID=SpokaneWA">Hillyard</a></li>
         <li><a class="btn btn-primary" href="https://www.printeron.net/system/printspot/interface/select_file.jsp?url=spl/indiantrail&protocolSwitched=true">Indian Trail</a></li>
         <li><a class="btn btn-primary" href="https://www.printeron.net/system/printspot/interface/select_file.jsp?url=spl/shadle&protocolSwitched=true">Shadle</a></li>
         <li><a class="btn btn-primary" href="https://www.printeron.net/system/printspot/interface/select_file.jsp?url=spl/southhill&protocolSwitched=true">South Hill</a></li>
